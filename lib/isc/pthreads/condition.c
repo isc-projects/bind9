21eb2ca7bcdcf122eca60f2e0e7a00361e1e177c
Bob Halley <source@isc.org>
1998-10-16 22:15:23 +0000

    more complete fix for typo

--- lib/isc/pthreads/condition.c
+++ lib/isc/pthreads/condition.c
@@ -6,13 +6,11 @@ boolean_t
 os_condition_waituntil(os_condition_t *c, os_mutex_t *m, os_time_t *t,
 		       boolean_t *timeout)
 {
-	int result;
 	struct timespec ts;
 
 	ts.tv_sec = t->seconds;
 	ts.tv_nsec = t->nanoseconds;
-	result = pthread_cond_timedwait(c, m, &ts);
-	if (result == 0) {
+	if (pthread_cond_timedwait(c, m, &ts) == 0) {
 		*timeout = FALSE;
 		return (TRUE);
 	} else if (errno == ETIMEDOUT) {
