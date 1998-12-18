06c29e22e02d26408e057a7c63e9d552f4599b91
Bob Halley <source@isc.org>
1998-12-18 21:16:57 +0000

    rwlock macros

--- lib/isc/util.h
+++ lib/isc/util.h
@@ -31,15 +31,15 @@
  */
 
 #define LOCK(lp) \
-	INSIST(isc_mutex_lock((lp)) == ISC_R_SUCCESS);
+	INSIST(isc_mutex_lock((lp)) == ISC_R_SUCCESS)
 #define UNLOCK(lp) \
-	INSIST(isc_mutex_unlock((lp)) == ISC_R_SUCCESS);
+	INSIST(isc_mutex_unlock((lp)) == ISC_R_SUCCESS)
 #define BROADCAST(cvp) \
-	INSIST(isc_condition_broadcast((cvp)) == ISC_R_SUCCESS);
+	INSIST(isc_condition_broadcast((cvp)) == ISC_R_SUCCESS)
 #define SIGNAL(cvp) \
-	INSIST(isc_condition_signal((cvp)) == ISC_R_SUCCESS);
+	INSIST(isc_condition_signal((cvp)) == ISC_R_SUCCESS)
 #define WAIT(cvp, lp) \
-	INSIST(isc_condition_wait((cvp), (lp)) == ISC_R_SUCCESS);
+	INSIST(isc_condition_wait((cvp), (lp)) == ISC_R_SUCCESS)
 
 /*
  * isc_condition_waituntil can return ISC_R_TIMEDOUT, so we
@@ -48,5 +48,10 @@
 
 #define WAITUNTIL(cvp, lp, tp) \
 	isc_condition_waituntil((cvp), (lp), (tp))
+
+#define RWLOCK(lp, t) \
+	INSIST(isc_rwlock_lock((lp), (t)) == ISC_R_SUCCESS)
+#define RWUNLOCK(lp, t) \
+	INSIST(isc_rwlock_unlock((lp), (t)) == ISC_R_SUCCESS)
 
 #endif /* ISC_UTIL_H */
