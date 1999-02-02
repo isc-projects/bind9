28ed3013196c373745dbde87b75a490148dab840
Bob Halley <source@isc.org>
1999-02-02 00:38:23 +0000

    Solaris port: inet_pton, AF_INET6

--- acconfig.h
+++ acconfig.h
@@ -1,2 +1,13 @@
 /* define if you need inet_ntop() */
 #undef NEED_INET_NTOP
+
+/* define if you need inet_pton() */
+#undef NEED_INET_PTON
+
+/* define if you need AF_INET6 */
+#undef NEED_AF_INET6
+
+/* Probably not the right place... */
+#ifdef NEED_AF_INET6
+#define AF_INET6 99
+#endif
