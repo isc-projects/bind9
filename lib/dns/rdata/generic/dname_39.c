bdfffc68dbadd78147bce6f26f25f48e9283da84
Mark Andrews <marka@isc.org>
1999-02-02 05:03:10 +0000

    Initial Implementation

---
+++ lib/dns/rdata/generic/dname_39.c
@@ -0,0 +1,141 @@
+/*
+ * Copyright (C) 1999 Internet Software Consortium.
+ * 
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM DISCLAIMS
+ * ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL INTERNET SOFTWARE
+ * CONSORTIUM BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ */
+
+ /* $Id: dname_39.c,v 1.1 1999/02/02 05:03:10 marka Exp $ */
+
+ /* draft-ietf-dnsind-dname-02.txt */
+
+#ifndef RDATA_GENERIC_DNAME_39_H
+#define RDATA_GENERIC_DNAME_39_H
+
+static dns_result_t
+fromtext_dname(dns_rdataclass_t class, dns_rdatatype_t type,
+	       isc_lex_t *lexer, dns_name_t *origin,
+	       isc_boolean_t downcase, isc_buffer_t *target)
+{
+	isc_token_t token;
+	dns_name_t name;
+	isc_buffer_t buffer;
+
+	REQUIRE(type == 39);
+	class = class;	/*unused*/
+	
+	RETERR(gettoken(lexer, &token, isc_tokentype_string, ISC_FALSE));
+	
+	dns_name_init(&name, NULL);
+	buffer_fromregion(&buffer, &token.value.as_region,
+			  ISC_BUFFERTYPE_TEXT);
+	origin = (origin != NULL) ? origin : dns_rootname;
+	return (dns_name_fromtext(&name, &buffer, origin, downcase, target));
+}
+
+static dns_result_t
+totext_dname(dns_rdata_t *rdata, dns_name_t *origin, isc_buffer_t *target) {
+	isc_region_t region;
+	dns_name_t name;
+	dns_name_t prefix;
+	isc_boolean_t sub;
+
+	REQUIRE(rdata->type == 39);
+
+	dns_name_init(&name, NULL);
+	dns_name_init(&prefix, NULL);
+
+	dns_rdata_toregion(rdata, &region);
+	dns_name_fromregion(&name, &region);
+
+	sub = name_prefix(&name, origin, &prefix);
+
+	return (dns_name_totext(&prefix, sub, target));
+}
+
+static dns_result_t
+fromwire_dname(dns_rdataclass_t class, dns_rdatatype_t type,
+	       isc_buffer_t *source, dns_decompress_t *dctx,
+	       isc_boolean_t downcase, isc_buffer_t *target)
+{
+	dns_name_t name;
+
+	REQUIRE(type == 39);
+	class = class;	/*unused*/
+
+	dns_name_init(&name, NULL);
+	return(dns_name_fromwire(&name, source, dctx, downcase, target));
+}
+
+static dns_result_t
+towire_dname(dns_rdata_t *rdata, dns_compress_t *cctx, isc_buffer_t *target) {
+	dns_name_t name;
+	isc_region_t region;
+
+	REQUIRE(rdata->type == 39);
+
+	dns_name_init(&name, NULL);
+	dns_rdata_toregion(rdata, &region);
+	dns_name_fromregion(&name, &region);
+
+	return (dns_name_towire(&name, cctx, target));
+}
+
+static int
+compare_dname(dns_rdata_t *rdata1, dns_rdata_t *rdata2) {
+	dns_name_t name1;
+	dns_name_t name2;
+	isc_region_t region1;
+	isc_region_t region2;
+
+	REQUIRE(rdata1->type == rdata2->type);
+	REQUIRE(rdata1->class == rdata2->class);
+	REQUIRE(rdata1->type == 39);
+
+	dns_name_init(&name1, NULL);
+	dns_name_init(&name2, NULL);
+
+	dns_rdata_toregion(rdata1, &region1);
+	dns_rdata_toregion(rdata2, &region2);
+
+	dns_name_fromregion(&name1, &region1);
+	dns_name_fromregion(&name2, &region2);
+
+	return (dns_name_compare(&name1, &name2));
+}
+
+static dns_result_t
+fromstruct_dname(dns_rdataclass_t class, dns_rdatatype_t type, void *source,
+		 isc_buffer_t *target)
+{
+
+	REQUIRE(type == 39);
+
+	class = class;	/*unused*/
+
+	source = source;
+	target = target;
+
+	return (DNS_R_NOTIMPLEMENTED);
+}
+
+static dns_result_t
+tostruct_dname(dns_rdata_t *rdata, void *target) {
+	
+	REQUIRE(rdata->type == 39);
+
+	target = target;
+
+	return (DNS_R_NOTIMPLEMENTED);
+}
+#endif	/* RDATA_GENERIC_DNAME_39_H */
