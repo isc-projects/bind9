0237695c5e0156a818b8579b3e01be2d6eadffa4
Andreas Gustafsson <source@isc.org>
1999-05-26 06:21:08 +0000

    initial version of generic network address structure

---
+++ lib/isc/unix/include/isc/netaddr.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 1998, 1999  Internet Software Consortium.
+ * 
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM DISCLAIMS
+ * ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL INTERNET SOFTWARE
+ * CONSORTIUM BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ */
+
+#ifndef ISC_NETADDR_H
+#define ISC_NETADDR_H 1
+
+#include <netinet/in.h>
+
+#include <isc/lang.h>
+
+ISC_LANG_BEGINDECLS
+
+typedef struct isc_netaddr {
+	union {
+    		struct in_addr in;
+#ifdef notyet
+		struct in6_addr in6;
+#endif
+	} type;
+} isc_netaddr_t;
+
+ISC_LANG_ENDDECLS
+
+#endif /* ISC_NETADDR_H */
