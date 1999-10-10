95360399dc7c09557cb8231d4104559c7dc1cdb5
James Brister <source@isc.org>
1999-10-10 17:09:37 +0000

    Placeholders for future config file structures.

---
+++ lib/dns/config/confresolv.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 1999  Internet Software Consortium.
+ * 
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM DISCLAIMS
+ * ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL INTERNET SOFTWARE
+ * CONSORTIUM BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ */
+
+#include <config.h>
+
+#include <dns/confresolv.h>
+
+#include "confpvt.h"
+
+isc_result_t
+dns_c_resolv_new(isc_log_t *lctx, isc_mem_t *mem, dns_c_resolv_t **cfgres)
+{
+	(void)lctx ; (void) mem; (void) cfgres;
+	
+	/* XXX nothing yet */
+
+	return (ISC_R_SUCCESS);
+}
+
+
+isc_result_t
+dns_c_resolv_delete(isc_log_t *lctx, dns_c_resolv_t **cfgres)
+{
+	(void) lctx ; (void) cfgres;
+	/* XXX nothin yet */
+
+	return (ISC_R_SUCCESS);
+}
+
+		
+
---
+++ lib/dns/include/dns/confcache.h
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 1999  Internet Software Consortium.
+ * 
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM DISCLAIMS
+ * ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL INTERNET SOFTWARE
+ * CONSORTIUM BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ */
+
+#ifndef DNS_CONFIG_CONFCACHE_H
+#define DNS_CONFIG_CONFCACHE_H 1
+
+/*****
+ ***** Module Info
+ *****/
+
+/*
+ * A container for the cache configuration data pulled from a
+ * named.conf-style config file.
+ */
+
+/*
+ *
+ * MP:
+ *	Client must do necessary locking.
+ *      
+ * Reliability:
+ *
+ *	No problems.
+ *
+ * Resources:
+ *
+ *	Use memory managers supplied by client.
+ *
+ * Security:
+ *
+ *	N/A
+ *      
+ */
+
+/***
+ *** Imports
+ ***/
+
+#include <config.h>
+
+#include <isc/mem.h>
+#include <isc/log.h>
+
+
+/***
+ *** Types
+ ***/
+
+typedef struct dns_c_cache		dns_c_cache_t;
+
+struct dns_c_cache
+{
+	isc_mem_t	       *mem;
+
+	/* XXX need this fleshed out */
+};
+
+
+
+
+/***
+ *** Functions
+ ***/
+
+isc_result_t dns_c_cache_new(isc_log_t *lctx,
+			     isc_mem_t *mem, dns_c_cache_t **cfgres);
+/*
+ * Creates a new cache-config object.
+ *
+ * Requires:
+ *	mem be a pointer to a valid memory manager.
+ *	newres be a valid non-NULL pointer.
+ * 
+ * Returns:
+ *	ISC_R_SUCCESS		-- all is well
+ *	ISC_R_NOMEMORY		-- out of memory
+ * 
+ */
+
+isc_result_t dns_c_cache_delete(isc_log_t *lctx, dns_c_cache_t **cfgres);
+/*
+ * Deletes the config-cache object and its contents.
+ *
+ * Requires:
+ *	cfgres be a valid non-NULL pointer. The pointer it points to
+ *	can be NULL or must be a valid dns_c_cache_t object.
+ * 
+ * Returns:
+ *	ISC_R_SUCCESS		-- all is well
+ * 
+ */
+
+
+#endif /* DNS_CONFIG_CONFCACHE_H */
