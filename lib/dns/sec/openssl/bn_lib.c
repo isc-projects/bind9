f044f20f15de9368426b87536a568c0778adc252
Brian Wellington <source@isc.org>
1999-10-11 22:00:27 +0000

    removed initializer warning

--- lib/dns/sec/openssl/bn_lib.c
+++ lib/dns/sec/openssl/bn_lib.c
@@ -124,7 +124,7 @@ int BN_get_params(int which)
 BIGNUM *BN_value_one(void)
 	{
 	static BN_ULONG data_one=1L;
-	static BIGNUM const_one={&data_one,1,1,0};
+	static BIGNUM const_one={&data_one,1,1,0,0};
 
 	return(&const_one);
 	}
