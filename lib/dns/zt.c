4a80a005507e0f69135a6e6fe45718e84cdf8c56
Bob Halley <source@isc.org>
1999-10-13 18:19:56 +0000

    Latest from ~marka/bind9.

--- lib/dns/zt.c
+++ lib/dns/zt.c
@@ -17,6 +17,7 @@
 
 #include <config.h>
 #include <isc/assertions.h>
+#include <isc/magic.h>
 #include "../isc/util.h"
 #include <dns/zt.h>
 
@@ -30,7 +31,7 @@ struct dns_zt {
 };
 
 #define ZTMAGIC 0x5a54626cU	/* ZTbl */
-#define VALID_ZT(zt) ((zt) != NULL && (zt)->magic == ZTMAGIC)
+#define VALID_ZT(zt) ISC_MAGIC_VALID(zt, ZTMAGIC)
 
 static void auto_detach(void *, void *);
 
@@ -45,6 +46,7 @@ dns_zt_create(isc_mem_t *mctx, dns_rdataclass_t rdclass, dns_zt_t **zt) {
 	if (new == NULL)
 		return (DNS_R_NOMEMORY);
 
+	new->table = NULL;
 	result = dns_rbt_create(mctx, auto_detach, NULL, &new->table);
 	if (result != DNS_R_SUCCESS)
 		goto cleanup0;
@@ -77,13 +79,20 @@ dns_result_t
 dns_zt_mount(dns_zt_t *zt, dns_zone_t *zone) {
 	dns_result_t result;
 	dns_zone_t *dummy = NULL;
+	dns_name_t name;
 
 	REQUIRE(VALID_ZT(zt));
 
-	dns_zone_attach(zone, &dummy);
+	dns_name_init(&name, NULL);
+	result = dns_zone_getorigin(zone, zt->mctx, &name);
+	if (result != DNS_R_SUCCESS)
+		return (result);
 	LOCK(&zt->lock);
-	result = dns_rbt_addname(zt->table, dns_zone_getorigin(zone), zone);
+	result = dns_rbt_addname(zt->table, &name, zone);
 	UNLOCK(&zt->lock);
+	dns_name_free(&name, zt->mctx);
+	if (result == DNS_R_SUCCESS)
+		dns_zone_attach(zone, &dummy);
 
 	return (result);
 }
@@ -91,13 +100,18 @@ dns_zt_mount(dns_zt_t *zt, dns_zone_t *zone) {
 dns_result_t
 dns_zt_unmount(dns_zt_t *zt, dns_zone_t *zone) {
 	dns_result_t result;
+	dns_name_t name;
 
 	REQUIRE(VALID_ZT(zt));
 
+	dns_name_init(&name, NULL);
+	result = dns_zone_getorigin(zone, zt->mctx, &name);
+	if (result != DNS_R_SUCCESS)
+		return (result);
 	LOCK(&zt->lock);
-	result = dns_rbt_deletename(zt->table, dns_zone_getorigin(zone),
-				    ISC_FALSE);
+	result = dns_rbt_deletename(zt->table, &name, ISC_FALSE);
 	UNLOCK(&zt->lock);
+	dns_name_free(&name, zt->mctx);
 	return (result);
 }
 
@@ -106,12 +120,16 @@ dns_zt_find(dns_zt_t *zt, dns_name_t *name, dns_name_t *foundname,
 		   dns_zone_t **zone)
 {
 	dns_result_t result;
+	dns_zone_t *dummy = NULL;
 
 	REQUIRE(VALID_ZT(zt));
 
 	LOCK(&zt->lock);
-	result = dns_rbt_findname(zt->table, name, foundname, (void **)zone);
+	result = dns_rbt_findname(zt->table, name, foundname, (void **)&dummy);
 	UNLOCK(&zt->lock);
+
+	if (result == DNS_R_SUCCESS || result == DNS_R_PARTIALMATCH)
+		dns_zone_attach(dummy, zone);
 
 	return (result);
 }
@@ -160,11 +178,64 @@ dns_zt_attach(dns_zt_t *zt, dns_zt_t **ztp) {
 	*ztp = zt;
 }
 
+void
+dns_zt_print(dns_zt_t *zt) {
+	dns_rbtnode_t *node;
+	dns_rbtnodechain_t chain;
+	dns_result_t result;
+	dns_zone_t *zone;
+
+	REQUIRE(VALID_ZT(zt));
+
+	dns_rbtnodechain_init(&chain, zt->mctx);
+	result = dns_rbtnodechain_first(&chain, zt->table, NULL, NULL);
+	while (result == DNS_R_NEWORIGIN || result == DNS_R_SUCCESS) {
+		result = dns_rbtnodechain_current(&chain, NULL, NULL,
+						  &node);
+		if (result == DNS_R_SUCCESS) {
+			zone = node->data;
+			if (zone != NULL)
+				(void)dns_zone_print(zone);
+		}
+		result = dns_rbtnodechain_next(&chain, NULL, NULL);
+	}
+	dns_rbtnodechain_invalidate(&chain);
+}
+
+void
+dns_zt_load(dns_zt_t *zt) {
+	dns_rbtnode_t *node;
+	dns_rbtnodechain_t chain;
+	dns_result_t result;
+	dns_zone_t *zone;
+
+	REQUIRE(VALID_ZT(zt));
+
+	dns_rbtnodechain_init(&chain, zt->mctx);
+	result = dns_rbtnodechain_first(&chain, zt->table, NULL, NULL);
+	while (result == DNS_R_NEWORIGIN || result == DNS_R_SUCCESS) {
+		result = dns_rbtnodechain_current(&chain, NULL, NULL,
+						  &node);
+		if (result == DNS_R_SUCCESS) {
+			zone = node->data;
+			if (zone != NULL)
+				(void)dns_zone_load(zone);
+		}
+		result = dns_rbtnodechain_next(&chain, NULL, NULL);
+	}
+	dns_rbtnodechain_invalidate(&chain);
+}
+
+/***
+ *** Private
+ ***/
+
 static void
 auto_detach(void *zone, void *xxx) {
 	dns_zone_t *dummy = zone;
 
 	xxx = xxx;	/*unused*/
 
+	printf("auto_detach\n");
 	dns_zone_detach(&dummy);
 }
