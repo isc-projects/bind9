202d738d87033b93205e4d5f64d15485aa529682
Bob Halley <source@isc.org>
1999-10-22 19:34:03 +0000

    add

---
+++ bin/named/include/named/log.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 1999  Internet Software Consortium.
+ * 
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM DISCLAIMS
+ * ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL INTERNET SOFTWARE
+ * CONSORTIUM BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ */
+
+#ifndef NS_LOG_H
+#define NS_LOG_H 1
+
+#include <isc/types.h>
+#include <isc/log.h>
+
+#include <dns/log.h>
+
+#include <named/globals.h>
+
+ISC_LANG_BEGINDECLS
+
+#define NS_LOGCATEGORY_GENERAL		(&ns_g_categories[0])
+#define NS_LOGCATEGORY_CLIENT		(&ns_g_categories[1])
+#define NS_LOGCATEGORY_NETWORK		(&ns_g_categories[2])
+
+#define NS_LOGMODULE_MAIN		(&ns_g_modules[0])
+#define NS_LOGMODULE_CLIENT		(&ns_g_modules[1])
+#define NS_LOGMODULE_SERVER		(&ns_g_modules[2])
+#define NS_LOGMODULE_QUERY		(&ns_g_modules[3])
+#define NS_LOGMODULE_INTERFACEMGR	(&ns_g_modules[4])
+
+isc_result_t
+ns_log_init(void);
+
+void
+ns_log_shutdown(void);
+
+ISC_LANG_ENDDECLS
+
+#endif /* NS_LOG_H */
