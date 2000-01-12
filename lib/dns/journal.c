c398950a4213de27d70d5d8d1ed5df463e0b3902
Andreas Gustafsson <source@isc.org>
2000-01-12 00:59:08 +0000

    fixed memory leak introduced by previous change

--- lib/dns/journal.c
+++ lib/dns/journal.c
@@ -194,6 +194,7 @@ dns_db_createsoatuple(dns_db_t *db, dns_dbversion_t *ver, isc_mem_t *mctx,
 				      &rdata, tp);
 
 	dns_rdataset_disassociate(&rdataset);
+	dns_db_detachnode(db, &node);
 	return (ISC_R_SUCCESS);
 
  freenode:
