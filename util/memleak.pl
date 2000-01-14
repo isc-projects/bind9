e6cf8bb62c0c5bc1e8ca97a7314660a6f46a368e
David Lawrence <source@isc.org>
2000-01-14 00:33:14 +0000

    also include isc_mem_stats output for allocations with non-0 remaining

--- util/memleak.pl
+++ util/memleak.pl
@@ -3,8 +3,14 @@
 # Massage the output from ISC_MEM_DEBUG to extract mem_get() calls
 # with no corresponding mem_put().
 
+$mem_stats = '';
+
 while (<>) {
     $gets{$1.$2} = $_ if /mem(pool)?_get.*-> 0x([0-9a-f]+)/;
     delete $gets{$1.$2} if /mem(pool)?_put\(0x[0-9a-f]+, 0x([0-9a-f]+)/;
+    $mem_stats .= $_ if /\d+ gets, +(\d+) rem/ && $1 > 0;
 }
 print join('', values %gets);
+print $mem_stats;
+
+exit(0);
