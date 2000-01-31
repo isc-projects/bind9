36e3f1b8e58fc2345a30154657ecc53c176037fa
David Lawrence <source@isc.org>
2000-01-31 15:18:48 +0000

    definitions for OMAPI support

---
+++ bin/named/include/named/omapi.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2000  Internet Software Consortium.
+ * 
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM DISCLAIMS
+ * ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL INTERNET SOFTWARE
+ * CONSORTIUM BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ */
+
+#include <omapi/omapi.h>
+
+/*
+ * This string is the registration name of objects of type control_object_t.
+ */
+#define NS_OMAPI_CONTROL		"control"
+
+#define NS_OMAPI_COMMAND_RELOAD		"reload"
+#define NS_OMAPI_COMMAND_RELOADCONFIG	"reload-config"
+#define NS_OMAPI_COMMAND_RELOADZONES	"reload-zones"
+
+isc_result_t
+ns_omapi_listen(isc_mem_t *mctx, omapi_object_t **managerp);
+
