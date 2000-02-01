c4f192b4d8e9af188cc6b5b642c76bcb16eb8a6b
Bob Halley <source@isc.org>
2000-02-01 23:12:16 +0000

    add copyright

--- bin/tests/update_test.pl
+++ bin/tests/update_test.pl
@@ -1,5 +1,20 @@
 #!/usr/bin/perl
 #
+# Copyright (C) 1999, 2000  Internet Software Consortium.
+# 
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM DISCLAIMS
+# ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL INTERNET SOFTWARE
+# CONSORTIUM BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+# DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+# PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+# SOFTWARE.
+#
 # Dynamic update test suite.
 #
 # Usage:
@@ -21,7 +36,7 @@
 #
 #    perl -MCPAN -e "install Net::DNS"
 #
-# $Id: update_test.pl,v 1.2 1999/11/19 19:06:18 gson Exp $
+# $Id: update_test.pl,v 1.3 2000/02/01 23:12:16 halley Exp $
 #
 
 use Getopt::Std;
