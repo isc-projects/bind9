dda4bfe6499d09ea8123447579e56069d8176fcb
Bob Halley <source@isc.org>
2000-03-23 19:52:19 +0000

    update copyrights

--- lib/dns/include/dns/confndc.h
+++ lib/dns/include/dns/confndc.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1999, 2000  Internet Software Consortium.
+ * Copyright (C) 2000  Internet Software Consortium.
  * 
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
