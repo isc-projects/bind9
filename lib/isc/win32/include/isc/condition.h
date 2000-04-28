a2605214c27439a8af2ad4bd9a8630dcfcc8152e
David Lawrence <source@isc.org>
2000-04-28 21:39:34 +0000

    135.   [cleanup]       Win32's <isc/condition.h> did not need                         <isc/boolean.h>, now uses <isc/types.h> in place                         of <isc/time.h>, and needed ISC_LANG_BEGINDECLS                         and ISC_LANG_ENDDECLS.

--- lib/isc/win32/include/isc/condition.h
+++ lib/isc/win32/include/isc/condition.h
@@ -20,22 +20,36 @@
 
 #include <windows.h>
 
-#include <isc/boolean.h>
+#include <isc/lang.h>
 #include <isc/result.h>
 #include <isc/mutex.h>
-#include <isc/time.h>
+#include <isc/types.h>
 
 typedef struct isc_condition {
-	HANDLE			events[2];	
+	HANDLE 		events[2];	
 	unsigned int	waiters;
 } isc_condition_t;
 
-isc_result_t isc_condition_init(isc_condition_t *);
-isc_result_t isc_condition_wait(isc_condition_t *, isc_mutex_t *);
-isc_result_t isc_condition_signal(isc_condition_t *);
-isc_result_t isc_condition_broadcast(isc_condition_t *);
-isc_result_t isc_condition_destroy(isc_condition_t *);
-isc_result_t isc_condition_waituntil(isc_condition_t *, isc_mutex_t *,
-				     isc_time_t *);
+ISC_LANG_BEGINDECLS
+
+isc_result_t
+isc_condition_init(isc_condition_t *);
+
+isc_result_t
+isc_condition_wait(isc_condition_t *, isc_mutex_t *);
+
+isc_result_t
+isc_condition_signal(isc_condition_t *);
+
+isc_result_t
+isc_condition_broadcast(isc_condition_t *);
+
+isc_result_t
+isc_condition_destroy(isc_condition_t *);
+
+isc_result_t
+isc_condition_waituntil(isc_condition_t *, isc_mutex_t *, isc_time_t *);
+
+ISC_LANG_ENDDECLS
 
 #endif /* ISC_CONDITION_H */
