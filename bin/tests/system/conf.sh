5d545650bf114207aeefa79b76983f2c4dda8a12
Andreas Gustafsson <source@isc.org>
2000-05-17 02:22:23 +0000

    KEYSIGNER variable was not set

--- bin/tests/system/conf.sh
+++ bin/tests/system/conf.sh
@@ -8,8 +8,9 @@ TOP="`cd ../../..; pwd`"
 NAMED=$TOP/bin/named/named
 KEYGEN=$TOP/bin/tests/keygen
 SIGNER=$TOP/bin/tests/signer
+KEYSIGNER=$TOP/bin/tests/keysigner
 KEYSETTOOL=$TOP/bin/tests/keysettool
 
 SUBDIRS="xfer dnssec xferquota"
 
-export NAMED KEYGEN SIGNER KEYSETTOOL
+export NAMED KEYGEN SIGNER KEYSIGNER KEYSETTOOL
