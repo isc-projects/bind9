bffe4c151c2a61976009146e7441b581b4816604
Andreas Gustafsson <source@isc.org>
2000-06-05 19:07:46 +0000

    no need to verify that servers are up as start.sh now takes care of that; no need to remove dig output before test

--- bin/tests/system/dnssec/tests.sh
+++ bin/tests/system/dnssec/tests.sh
@@ -23,32 +23,7 @@ echo "A:A test to determine online functionality of dnssec tools"
 # Perform tests
 #
 
-if [ -f dig.out.ns2 ]; then
-	rm -f dig.out.ns2
-fi
-if [ -f dig.out.ns3 ]; then
-	rm -f dig.out.ns3
-fi
-if [ -f dig.out.ns4 ]; then
-	rm -f dig.out.ns4
-fi
-
-# Make sure all of the servers are up
-status=0;
-../../../dig/dig +tcp +noadd +nosea +nostat +noquest +nocomm +nocmd . \
-	@10.53.0.2 soa > dig.out.ns2
-status=`expr $status + $?`
-grep ";" dig.out.ns2
-
-../../../dig/dig +tcp +noadd +nosea +nostat +noquest +nocomm +nocmd . \
-	@10.53.0.3 soa > dig.out.ns3
-status=`expr $status + $?`
-grep ";" dig.out.ns3
-
-../../../dig/dig +tcp +noadd +nosea +nostat +noquest +nocomm +nocmd . \
-	@10.53.0.4 soa > dig.out.ns4
-status=`expr $status + $?`
-grep ";" dig.out.ns4
+status=0
 
 rm -f dig.out.*
 
