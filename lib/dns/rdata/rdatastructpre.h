ba6f0d85b45a08112bafe9a92c7174a46bc18bfd
Mark Andrews <marka@isc.org>
2000-06-06 02:03:23 +0000

    Add macro to initalise the common part of a rdata struct.

--- lib/dns/rdata/rdatastructpre.h
+++ lib/dns/rdata/rdatastructpre.h
@@ -31,3 +31,9 @@ typedef struct dns_rdatacommon {
 	ISC_LINK(struct dns_rdatacommon)	link;
 } dns_rdatacommon_t;
 
+#define DNS_RDATACOMMON_INIT(DATA, RDTYPE, RDCLASS) \
+	do { \
+		(DATA)->common.rdtype = (RDTYPE); \
+		(DATA)->common.rdclass = (RDCLASS); \
+		ISC_LINK_INIT(&(DATA)->common, link); \
+	} while (0)
