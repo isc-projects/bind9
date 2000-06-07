0cb394091d7e56f1f728cc6aa2b2948563e7ce8a
Brian Wellington <source@isc.org>
2000-06-07 01:43:33 +0000

    The root keyset file (.keyset) wasn't being deleted, since it wasn't matched by the wildcard.

--- bin/tests/system/dnssec/clean.sh
+++ bin/tests/system/dnssec/clean.sh
@@ -15,7 +15,7 @@
 # ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 # SOFTWARE.
 
-rm -f */K* */*.keyset */*.signedkey */*.signed */trusted.conf
+rm -f */K* */.keyset */*.keyset */*.signedkey */*.signed */trusted.conf
 rm -f ns1/root.db ns2/example.db ns3/secure.example.db
 rm -f ns3/unsecure.example.db ns3/bogus.example.db
 rm -f dig.out.* 
