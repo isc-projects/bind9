1cad45e9cf1f75bd6cf5353a933c4b9a0be07319
Andreas Gustafsson <source@isc.org>
2000-06-09 20:59:32 +0000

    whitespace changes in copyright message

--- lib/dns/include/dns/types.h
+++ lib/dns/include/dns/types.h
@@ -4,7 +4,7 @@
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM DISCLAIMS
  * ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL INTERNET SOFTWARE
--- lib/lwres/gai_strerror.c
+++ lib/lwres/gai_strerror.c
@@ -1,10 +1,10 @@
 /*
  * Copyright (C) 2000  Internet Software Consortium.
- *
+ * 
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM DISCLAIMS
  * ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL INTERNET SOFTWARE
@@ -15,7 +15,7 @@
  * SOFTWARE.
  */
 
-/* $Id: gai_strerror.c,v 1.8 2000/06/08 18:37:29 gson Exp $ */
+/* $Id: gai_strerror.c,v 1.9 2000/06/09 20:56:25 gson Exp $ */
 
 #include <lwres/netdb.h>
 
