33f236e3a196ce9d9e4fa60e3386b30ff8de4905
David Lawrence <source@isc.org>
2000-06-23 03:10:02 +0000

    protect sys/types.h and unistd.h from check-includes.pl removal recommendation

--- lib/dns/gen-unix.h
+++ lib/dns/gen-unix.h
@@ -15,7 +15,7 @@
  * SOFTWARE.
  */
 
-/* $Id: gen-unix.h,v 1.8 2000/06/22 21:54:25 tale Exp $ */
+/* $Id: gen-unix.h,v 1.9 2000/06/23 03:10:02 tale Exp $ */
 
 /*
  * This file is responsible for defining two operations that are not
@@ -32,10 +32,10 @@
 #ifndef DNS_GEN_UNIX_H
 #define DNS_GEN_UNIX_H 1
 
-#include <sys/types.h>
+#include <sys/types.h>          /* Required on some systems for dirent.h. */
 
 #include <dirent.h>
-#include <unistd.h>
+#include <unistd.h>		/* XXXDCL Required for ?. */
 
 #include <isc/boolean.h>
 #include <isc/lang.h>
