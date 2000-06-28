749944739d7eb53a179754ae7a0502ab4913b995
Andreas Gustafsson <source@isc.org>
2000-06-28 17:11:52 +0000

    pullup: Memory leak in ixfr processing and grammatical error in debug message

--- bin/dig/dighost.c
+++ bin/dig/dighost.c
@@ -15,7 +15,7 @@
  * SOFTWARE.
  */
 
-/* $Id: dighost.c,v 1.58 2000/06/26 00:57:18 gson Exp $ */
+/* $Id: dighost.c,v 1.58.2.1 2000/06/28 17:11:52 gson Exp $ */
 
 /*
  * Notice to programmers:  Do not use this code as an example of how to
@@ -1585,7 +1585,7 @@ check_for_more_data(dig_query_t *query, dns_message_t *msg,
 				if (rdata.type != dns_rdatatype_soa)
 					goto NEXT_RDATA;
 				/* Now we have an SOA.  Work with it. */
-				debug ("Got a SOA");
+				debug ("Got an SOA");
 				result = dns_rdata_tostruct(&rdata,
 							    &soa,
 							    mctx);
@@ -1612,6 +1612,7 @@ check_for_more_data(dig_query_t *query, dns_message_t *msg,
 					query->second_rr_rcvd = ISC_TRUE;
 					query->second_rr_serial =
 						soa.serial;
+					dns_rdata_freestruct(&soa);
 					goto NEXT_RDATA;
 				}
 				if (query->second_rr_serial == 0) {
