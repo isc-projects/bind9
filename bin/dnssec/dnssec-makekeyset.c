c490e7db3de3b8836a6ba16d5190b462865caa9b
Andreas Gustafsson <source@isc.org>
2000-08-02 21:59:30 +0000

    pullup: -h wasn't handled properly.

--- bin/dnssec/dnssec-makekeyset.c
+++ bin/dnssec/dnssec-makekeyset.c
@@ -17,7 +17,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: dnssec-makekeyset.c,v 1.28 2000/06/22 21:49:02 tale Exp $ */
+/* $Id: dnssec-makekeyset.c,v 1.28.2.1 2000/08/02 21:59:30 gson Exp $ */
 
 #include <config.h>
 
@@ -146,7 +146,7 @@ main(int argc, char *argv[]) {
 
 	dns_result_register();
 
-	while ((ch = isc_commandline_parse(argc, argv, "s:e:t:r:v:")) != -1)
+	while ((ch = isc_commandline_parse(argc, argv, "s:e:t:r:v:h")) != -1)
 	{
 		switch (ch) {
 		case 's':
