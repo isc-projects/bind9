a3e7b6450ecb2cffe9032246c87e08e64fe24cc1
Andreas Gustafsson <source@isc.org>
2000-08-10 23:42:17 +0000

    style

--- lib/isc/unix/socket.c
+++ lib/isc/unix/socket.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: socket.c,v 1.156 2000/08/10 23:11:11 bwelling Exp $ */
+/* $Id: socket.c,v 1.157 2000/08/10 23:42:17 gson Exp $ */
 
 #include <config.h>
 
@@ -2579,7 +2579,7 @@ isc_socket_bind(isc_socket_t *sock, isc_sockaddr_t *sockaddr) {
 
 	if (sock->pf != sockaddr->type.sa.sa_family) {
 		UNLOCK(&sock->lock);
-		return(ISC_R_FAMILY);
+		return (ISC_R_FAMILY);
 	}
 	if (setsockopt(sock->fd, SOL_SOCKET, SO_REUSEADDR, (void *)&on,
 		       sizeof on) < 0) {
