efd2baaa2bd247f41aae8901d254c2d79424b11d
Brian Wellington <source@isc.org>
2000-08-11 18:45:30 +0000

    Remove the random.data file created by genrandom.

--- bin/tests/system/dnssec/clean.sh
+++ bin/tests/system/dnssec/clean.sh
@@ -15,10 +15,11 @@
 # NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
 # WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: clean.sh,v 1.8 2000/08/01 01:14:31 tale Exp $
+# $Id: clean.sh,v 1.9 2000/08/11 18:45:30 bwelling Exp $
 
 rm -f */K* */keyset-* */signedkey-* */*.signed */trusted.conf
 rm -f ns1/root.db ns2/example.db ns3/secure.example.db
 rm -f ns3/unsecure.example.db ns3/bogus.example.db
 rm -f dig.out.*
+rm -f random.data
 
