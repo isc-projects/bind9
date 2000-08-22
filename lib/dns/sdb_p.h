697d35a286539b807cfd60ea72454969376553d4
Andreas Gustafsson <source@isc.org>
2000-08-22 01:37:50 +0000

    oops, new multiple inclusion protection macro did not follow naming convention either

--- lib/dns/sdb_p.h
+++ lib/dns/sdb_p.h
@@ -15,10 +15,10 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: sdb_p.h,v 1.2 2000/08/22 01:35:34 gson Exp $ */
+/* $Id: sdb_p.h,v 1.3 2000/08/22 01:37:50 gson Exp $ */
 
-#ifndef DNS_SDB_H
-#define DNS_SDB_H 1
+#ifndef DNS_SDB_P_H
+#define DNS_SDB_P_H 1
 
 /*****
  ***** Module Info
@@ -53,4 +53,4 @@ dns_sdb_create(isc_mem_t *mctx, dns_name_t *origin, dns_dbtype_t type,
 
 ISC_LANG_ENDDECLS
 
-#endif /* DNS_SDB_H */
+#endif /* DNS_SDB_P_H */
