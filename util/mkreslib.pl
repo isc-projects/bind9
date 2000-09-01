0a2eab4e35357a269a727ff97840d6f23666157f
Andreas Gustafsson <source@isc.org>
2000-09-01 01:06:23 +0000

    coding style

--- util/mkreslib.pl
+++ util/mkreslib.pl
@@ -15,7 +15,7 @@
 # NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
 # WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: mkreslib.pl,v 1.7 2000/08/01 01:33:32 tale Exp $
+# $Id: mkreslib.pl,v 1.8 2000/09/01 01:06:23 gson Exp $
 
 # Build a res_* library distribution from the BIND 8 source
 
@@ -36,18 +36,18 @@
 #
 
 # Don't bother keeping contrib or doc around in the new tarball
-system ("rm -rf Makefile contrib doc");
-system ("mv src/* src/.??* .");
-system ("rmdir src");
+system("rm -rf Makefile contrib doc");
+system("mv src/* src/.??* .");
+system("rmdir src");
 
 # We don't want bin/, conf/, tests/, or OLD/
-system ("rm -rf bin conf tests OLD");
+system("rm -rf bin conf tests OLD");
 
 # Move the old README away
-system ("mv README README.bind8");
+system("mv README README.bind8");
 
 # Make a new README
-open (README, ">README") || warn "README: $!";
+open(README, ">README") || warn "README: $!";
 print README <<EOF;
 This is the resolver library from BIND 8, provided for legacy software
 needing access to these functions.  Programmers of new software are encouraged
@@ -55,34 +55,35 @@ to use the new lightweight resolver library instead.
 
 See the README.bind8 file for the original README shipped with BIND 8.
 EOF
-close (README);
+close(README);
 
-system ("mv CHANGES CHANGES.bind8");
-system ("mv INSTALL INSTALL.bind8");
+system("mv CHANGES CHANGES.bind8");
+system("mv INSTALL INSTALL.bind8");
 
 # The following files aren't useful here
-system ("rm -rf DNSSEC SUPPORT TODO");
+system("rm -rf DNSSEC SUPPORT TODO");
 
 # Massage the Makefile
-system ("mv Makefile Makefile.bind8");
+system("mv Makefile Makefile.bind8");
 
-open (MAKEIN, "Makefile.bind8") || warn "Makefile.bind8: $!";
-open (MAKEOUT, ">Makefile") || warn "Makefile: $!";
+open(MAKEIN, "Makefile.bind8") || warn "Makefile.bind8: $!";
+open(MAKEOUT, ">Makefile") || warn "Makefile: $!";
 
 while (<MAKEIN>) {
-  if (/^SUBDIRS= (.*)$/) {
-    $line = $1;
-    $line =~ s/bin//;
-    print MAKEOUT "SUBDIRS= $line";
-    next;
-  }
-  if (/^links:/) {
-    goto DONE;
-  }
-  print MAKEOUT;
+	if (/^SUBDIRS= (.*)$/) {
+		$line = $1;
+		$line =~ s/bin//;
+		print MAKEOUT "SUBDIRS= $line";
+		next;
+	}
+	if (/^links:/) {
+		goto DONE;
+	}
+	print MAKEOUT;
 }
-DONE:
-print MAKEOUT "FRC:\n";
-close (MAKEIN);
-close (MAKEOUT);
 
+DONE:
+    
+print MAKEOUT "FRC:\n";
+close(MAKEIN);
+close(MAKEOUT);
