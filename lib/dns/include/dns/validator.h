c663d46e47e389952b28edc18f93582f26b9505e
Andreas Gustafsson <source@isc.org>
2000-12-21 16:27:22 +0000

    need #include <dst/key.h>

--- lib/dns/include/dns/validator.h
+++ lib/dns/include/dns/validator.h
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: validator.h,v 1.14 2000/12/01 01:03:12 gson Exp $ */
+/* $Id: validator.h,v 1.15 2000/12/21 16:27:22 gson Exp $ */
 
 #ifndef DNS_VALIDATOR_H
 #define DNS_VALIDATOR_H 1
@@ -52,6 +52,8 @@
 #include <isc/mutex.h>
 
 #include <dns/types.h>
+
+#include <dst/key.h>
 
 /*
  * A dns_validatorevent_t is sent when a 'validation' completes.
