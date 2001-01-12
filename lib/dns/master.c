eff5a7ccfd5547cdf0f3ce4e752c2b2434e2db55
Andreas Gustafsson <source@isc.org>
2001-01-12 20:19:00 +0000

    pullup:  679.   [bug]           $INCLUDE could leak memory and file descriptors on                         reload. [RT #639]

--- lib/dns/master.c
+++ lib/dns/master.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: master.c,v 1.88.2.2 2001/01/09 22:43:46 bwelling Exp $ */
+/* $Id: master.c,v 1.88.2.3 2001/01/12 20:19:00 gson Exp $ */
 
 #include <config.h>
 
@@ -1965,12 +1965,7 @@ load_quantum(isc_task_t *task, isc_event_t *event) {
 	else
 		result = load(&ctx);
 	if (result == DNS_R_CONTINUE) {
-		isc_task_send(task, &event);
-	} else if (result == ISC_R_SUCCESS && ctx->parent) {
-		/* Pop ctx and continue. */
-		event->ev_arg = ctx->parent;
-		ctx->parent = NULL;
-		dns_loadctx_detach(&ctx);
+		event->ev_arg = ctx;
 		isc_task_send(task, &event);
 	} else {
 		if (ctx->rate_limited)
