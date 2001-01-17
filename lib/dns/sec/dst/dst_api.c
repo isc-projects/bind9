d7483d67f7aa8f80d5f880aa4650f2bdb90d6623
Brian Wellington <source@isc.org>
2001-01-17 00:33:37 +0000

    Move dst_region_computeid into a new file dst/keyid.c, so that all of the dnssec code doesn't need to be linked in by using rdata routines.

--- lib/dns/sec/dst/dst_api.c
+++ lib/dns/sec/dst/dst_api.c
@@ -19,7 +19,7 @@
 
 /*
  * Principal Author: Brian Wellington
- * $Id: dst_api.c,v 1.69 2001/01/09 21:55:27 bwelling Exp $
+ * $Id: dst_api.c,v 1.70 2001/01/17 00:33:36 bwelling Exp $
  */
 
 #include <config.h>
@@ -760,33 +760,6 @@ dst_key_secretsize(const dst_key_t *key, unsigned int *n) {
 			return (DST_R_UNSUPPORTEDALG);
 	}
 	return (ISC_R_SUCCESS);
-}
-
-isc_uint16_t
-dst_region_computeid(const isc_region_t *source, const unsigned int alg) {
-	isc_uint32_t ac;
-	const unsigned char *p;
-	int size;
-
-	REQUIRE(source != NULL);
-
-	if (source->length < 4)
-		return (0);
-
-	p = source->base;
-	size = source->length;
-
-	if (alg == DST_ALG_RSAMD5)
-		return ((p[size - 3] << 8) + p[size - 2]);
-
-	for (ac = 0; size > 1; size -= 2, p += 2)
-		ac += ((*p) << 8) + *(p + 1);
-
-	if (size > 0)
-		ac += ((*p) << 8);
-	ac += (ac >> 16) & 0xffff;
-
-	return ((isc_uint16_t)(ac & 0xffff));
 }
 
 /***
