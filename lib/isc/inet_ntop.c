1ac2c28488fb5de80b3ce9aac3500d215cb61728
Brian Wellington <source@isc.org>
2001-01-25 19:39:48 +0000

    711.   [bug]           The libisc and liblwres implementations of                         inet_ntop contained an off by one error.

--- lib/isc/inet_ntop.c
+++ lib/isc/inet_ntop.c
@@ -17,7 +17,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 static char rcsid[] =
-	"$Id: inet_ntop.c,v 1.10 2001/01/09 21:56:07 bwelling Exp $";
+	"$Id: inet_ntop.c,v 1.11 2001/01/25 19:39:48 bwelling Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <config.h>
@@ -86,7 +86,8 @@ inet_ntop4(const unsigned char *src, char *dst, size_t size)
 	static const char *fmt = "%u.%u.%u.%u";
 	char tmp[sizeof "255.255.255.255"];
 
-	if ((size_t)sprintf(tmp, fmt, src[0], src[1], src[2], src[3]) > size) {
+	if ((size_t)sprintf(tmp, fmt, src[0], src[1], src[2], src[3]) >= size)
+	{
 		errno = ENOSPC;
 		return (NULL);
 	}
@@ -183,7 +184,7 @@ inet_ntop6(const unsigned char *src, char *dst, size_t size)
 	/*
 	 * Check for overflow, copy, and we're done.
 	 */
-	if ((size_t)(tp - tmp) > size) {
+	if ((size_t)(tp - tmp) >= size) {
 		errno = ENOSPC;
 		return (NULL);
 	}
