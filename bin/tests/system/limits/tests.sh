927fe088822a31009daf56e82572f156ee55ab5f
Andreas Gustafsson <source@isc.org>
2001-02-14 02:42:10 +0000

    leave more evidence when failing

--- bin/tests/system/limits/tests.sh
+++ bin/tests/system/limits/tests.sh
@@ -15,7 +15,7 @@
 # NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
 # WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: tests.sh,v 1.13 2001/01/09 21:43:27 bwelling Exp $
+# $Id: tests.sh,v 1.14 2001/02/14 02:42:10 gson Exp $
 
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
@@ -23,35 +23,35 @@ SYSTEMTESTTOP=..
 status=0
 
 echo "I:1000 A records"
-$DIG +tcp +norec 1000.example. @10.53.0.1 a -p 5300 > dig.out.ns1 || status=1
+$DIG +tcp +norec 1000.example. @10.53.0.1 a -p 5300 > dig.out.1000 || status=1
 #dig 1000.example. @10.53.0.1 a -p 5300 > knowngood.dig.out.1000
-$PERL ../digcomp.pl knowngood.dig.out.1000 dig.out.ns1 || status=1
+$PERL ../digcomp.pl knowngood.dig.out.1000 dig.out.1000 || status=1
 
 echo "I:2000 A records"
-$DIG +tcp +norec 2000.example. @10.53.0.1 a -p 5300 > dig.out.ns1 || status=1
+$DIG +tcp +norec 2000.example. @10.53.0.1 a -p 5300 > dig.out.2000 || status=1
 #dig 2000.example. @10.53.0.1 a -p 5300 > knowngood.dig.out.2000
-$PERL ../digcomp.pl knowngood.dig.out.2000 dig.out.ns1 || status=1
+$PERL ../digcomp.pl knowngood.dig.out.2000 dig.out.2000 || status=1
 
 echo "I:3000 A records"
-$DIG +tcp +norec 3000.example. @10.53.0.1 a -p 5300 > dig.out.ns1 || status=1
+$DIG +tcp +norec 3000.example. @10.53.0.1 a -p 5300 > dig.out.3000 || status=1
 #dig 3000.example. @10.53.0.1 a -p 5300 > knowngood.dig.out.3000
-$PERL ../digcomp.pl knowngood.dig.out.3000 dig.out.ns1 || status=1
+$PERL ../digcomp.pl knowngood.dig.out.3000 dig.out.3000 || status=1
 
 echo "I:4000 A records"
-$DIG +tcp +norec 4000.example. @10.53.0.1 a -p 5300 > dig.out.ns1 || status=1
+$DIG +tcp +norec 4000.example. @10.53.0.1 a -p 5300 > dig.out.4000 || status=1
 #dig 4000.example. @10.53.0.1 a -p 5300 > knowngood.dig.out.4000
-$PERL ../digcomp.pl knowngood.dig.out.4000 dig.out.ns1 || status=1
+$PERL ../digcomp.pl knowngood.dig.out.4000 dig.out.4000 || status=1
 
 echo "I:exactly maximum rrset"
-$DIG +tcp +norec a-maximum-rrset.example. @10.53.0.1 a -p 5300 > dig.out.ns1 \
+$DIG +tcp +norec a-maximum-rrset.example. @10.53.0.1 a -p 5300 > dig.out.a-maximum-rrset \
 	|| status=1
 #dig a-maximum-rrset.example. @10.53.0.1 a -p 5300 > knowngood.dig.out.a-maximum-rrset
-$PERL ../digcomp.pl knowngood.dig.out.a-maximum-rrset dig.out.ns1 || status=1
+$PERL ../digcomp.pl knowngood.dig.out.a-maximum-rrset dig.out.a-maximum-rrset || status=1
 
 echo "I:exceed maximum rrset (5000 A records)"
-$DIG +tcp +norec 5000.example. @10.53.0.1 a -p 5300 > dig.out.ns1 || status=1
+$DIG +tcp +norec 5000.example. @10.53.0.1 a -p 5300 > dig.out.exceed || status=1
 # Look for truncation bit (tc).
-grep 'flags: .*tc.*;' dig.out.ns1 > /dev/null || {
+grep 'flags: .*tc.*;' dig.out.exceed > /dev/null || {
     echo "I:TC bit was not set"
     status=1
 }
