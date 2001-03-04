494e26b3332ffa6edfdc1c14c654137f47e09fff
Brian Wellington <source@isc.org>
2001-03-04 21:17:42 +0000

    clean up properly

--- bin/tests/system/limits/clean.sh
+++ bin/tests/system/limits/clean.sh
@@ -15,10 +15,10 @@
 # NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
 # WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: clean.sh,v 1.6 2001/01/09 21:43:26 bwelling Exp $
+# $Id: clean.sh,v 1.7 2001/03/04 21:17:42 bwelling Exp $
 
 #
 # Clean up after limits tests.
 #
-rm -f dig.out.ns1
+rm -f dig.out.*
 
