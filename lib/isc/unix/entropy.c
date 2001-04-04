f8b3c627949bd4bc2f6aafb3dab2f56e3aa9ba06
Brian Wellington <source@isc.org>
2001-04-04 20:57:35 +0000

    804.	[bug]		Attempting to obtain entropy could fail in some 			situations.  This would be most common on systems 			with user-space threads. [RT #1131]

--- lib/isc/unix/entropy.c
+++ lib/isc/unix/entropy.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: entropy.c,v 1.57 2001/02/05 23:00:18 bwelling Exp $ */
+/* $Id: entropy.c,v 1.58 2001/04/04 20:57:35 bwelling Exp $ */
 
 #include <config.h>
 
@@ -395,7 +395,7 @@ get_from_filesource(isc_entropysource_t *source, isc_uint32_t desired) {
 		ndesired = ISC_MIN(desired, sizeof(buf));
 		n = read(fd, buf, ndesired);
 		if (n < 0) {
-			if (errno == EAGAIN)
+			if (errno == EAGAIN || errno == EINTR)
 				goto out;
 			close(fd);
 			source->bad = ISC_TRUE;
