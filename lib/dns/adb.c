8358be5851b039fff1aa5a0ccd4068dbca820046
Brian Wellington <source@isc.org>
2001-05-29 22:44:12 +0000

    uninitialized variable

--- lib/dns/adb.c
+++ lib/dns/adb.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: adb.c,v 1.177 2001/05/02 17:29:17 halley Exp $ */
+/* $Id: adb.c,v 1.178 2001/05/29 22:44:12 bwelling Exp $ */
 
 /*
  * Implementation notes
@@ -3809,7 +3809,7 @@ fetch_name_aaaa(dns_adbname_t *adbname) {
 static isc_result_t
 fetch_name_a6(dns_adbname_t *adbname, isc_boolean_t start_at_root) {
 	isc_result_t result;
-	dns_adbfetch6_t *fetch;
+	dns_adbfetch6_t *fetch = NULL;
 	dns_adb_t *adb;
 	dns_name_t *name;
 	dns_rdataset_t rdataset;
