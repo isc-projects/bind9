689705af293682d49863e647cdab453ba7784c68
cvs2git <source@isc.org>
2001-05-31 18:44:03 +0000

    This commit was manufactured by cvs2git to create branch 'unlabeled-1.926.2'.

--- lib/dns/sec/openssl/include/openssl/opensslconf.h
+++
@@ -1,7 +0,0 @@
-#ifndef OPENSSL_OPENSSLCONF_H
-#define OPENSSL_OPENSSLCONF_H 1
-
-/* This would be filled in by the openssl configure script if it was run. */
-
-#endif /* OPENSSL_OPENSSLCONF_H */
-
