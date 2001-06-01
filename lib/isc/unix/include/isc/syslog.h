d0b6ece407a33c3b728875f299219dba5281f969
Brian Wellington <source@isc.org>
2001-06-01 01:11:28 +0000

    ISC_LANG_BEGINDECLS -> ISC_LANG_ENDDECLS

--- lib/isc/unix/include/isc/syslog.h
+++ lib/isc/unix/include/isc/syslog.h
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: syslog.h,v 1.1 2001/03/02 19:25:19 bwelling Exp $ */
+/* $Id: syslog.h,v 1.2 2001/06/01 01:11:28 bwelling Exp $ */
 
 #ifndef ISC_SYSLOG_H
 #define ISC_SYSLOG_H 1
@@ -40,6 +40,6 @@ isc_syslog_facilityfromstring(const char *str, int *facilityp);
  * 	ISC_R_NOTFOUND
  */
 
-ISC_LANG_BEGINDECLS
+ISC_LANG_ENDDECLS
 
 #endif /* ISC_SYSLOG_H */
