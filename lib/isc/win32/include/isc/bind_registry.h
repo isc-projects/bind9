f950ce9932c6609f02c08e30bb196045ab19163b
Danny Mayer <source@isc.org>
2001-07-06 05:45:21 +0000

    BIND registry header information

---
+++ lib/isc/win32/include/isc/bind_registry.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2001  Internet Software Consortium.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM
+ * DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+ * INTERNET SOFTWARE CONSORTIUM BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
+ * FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+ * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/* $Id: bind_registry.h,v 1.1 2001/07/06 05:45:21 mayer Exp $ */
+
+#ifndef _BIND_REGISTRY_H
+#define _BIND_REGISTRY_H
+
+#define BIND_SUBKEY				"Software\\ISC\\BIND"
+#define BIND_SESSION			"CurrentSession"
+#define BIND_SESSION_SUBKEY		"Software\\ISC\\BIND\\CurrentSession"
+#define BIND_UNINSTALL_SUBKEY	"Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\ISC BIND"
+
+#define EVENTLOG_APP_SUBKEY		"SYSTEM\\CurrentControlSet\\Services\\EventLog\\Application"
+#define BIND_MESSAGE_SUBKEY		"SYSTEM\\CurrentControlSet\\Services\\EventLog\\Application\\named"
+#define XFER_MESSAGE_SUBKEY		"SYSTEM\\CurrentControlSet\\Services\\EventLog\\Application\\named-xfer"
+#define BIND_MESSAGE_NAME		"named"
+#define XFER_MESSAGE_NAME		"named-xfer"
+
+#define BIND_CONFIGFILE	0
+#define BIND_DEBUGLEVEL	1
+#define BIND_QUERYLOG	2
+#define BIND_FOREGROUND	3
+#define BIND_PORT		4
+
+#endif
