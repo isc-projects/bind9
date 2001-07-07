0a2d5c990559ce2b9f95df752db6e93024d9a250
Brian Wellington <source@isc.org>
2001-07-07 00:03:26 +0000

    924.   [port]          Apply #910 to lib/lwres.  [RT #987]

--- lib/lwres/include/lwres/net.h
+++ lib/lwres/include/lwres/net.h
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: net.h,v 1.12 2001/06/08 23:53:41 tale Exp $ */
+/* $Id: net.h,v 1.13 2001/07/07 00:03:26 bwelling Exp $ */
 
 #ifndef LWRES_NET_H
 #define LWRES_NET_H 1
@@ -68,6 +68,29 @@
 
 #ifdef LWRES_PLATFORM_HAVEINADDR6
 #define in6_addr in_addr6	/* Required for pre RFC2133 implementations. */
+#endif
+
+/*
+ * Required for some pre RFC2133 implementations.
+ * IN6ADDR_ANY_INIT and IN6ADDR_LOOPBACK_INIT were added in
+ * draft-ietf-ipngwg-bsd-api-04.txt or draft-ietf-ipngwg-bsd-api-05.txt.  
+ * If 's6_addr' is defined then assume that there is a union and three
+ * levels otherwise assume two levels required.
+ */
+#ifndef IN6ADDR_ANY_INIT
+#ifdef s6_addr
+#define IN6ADDR_ANY_INIT { { { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 } } }
+#else
+#define IN6ADDR_ANY_INIT { { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 } }
+#endif
+#endif
+
+#ifndef IN6ADDR_LOOPBACK_INIT
+#ifdef s6_addr
+#define IN6ADDR_LOOPBACK_INIT { { { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 } } }
+#else
+#define IN6ADDR_LOOPBACK_INIT { { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 } }
+#endif
 #endif
 
 #ifndef AF_INET6
