68a8db1efb0cd13e0e3d59362ab9db1a4a8bc496
Andreas Gustafsson <source@isc.org>
2001-07-18 23:33:04 +0000

    ISC-style copyright

--- contrib/queryperf/queryperf.c
+++ contrib/queryperf/queryperf.c
@@ -1,9 +1,24 @@
+/*
+ * Copyright (C) 2000, 2001  Nominum, Inc.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM
+ * DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+ * INTERNET SOFTWARE CONSORTIUM BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
+ * FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+ * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 /***
  ***	DNS Query Performance Testing Tool  (queryperf.c)
  ***
- ***	Version $Id: queryperf.c,v 1.1 2001/07/12 02:02:09 gson Exp $
- ***
- ***	Copyright (C) 2000, 2001  Nominum, Inc.  All Rights Reserved.
+ ***	Version $Id: queryperf.c,v 1.1.1.2 2001/07/18 23:33:04 gson Exp $
  ***
  ***	Stephen Jacob <sj@nominum.com>
  ***/
@@ -149,7 +164,7 @@ void
 show_startup_info(void) {
 	printf("\n"
 "DNS Query Performance Testing Tool\n"
-"Version: $Id: queryperf.c,v 1.1 2001/07/12 02:02:09 gson Exp $\n"
+"Version: $Id: queryperf.c,v 1.1.1.2 2001/07/18 23:33:04 gson Exp $\n"
 "\n");
 }
 
