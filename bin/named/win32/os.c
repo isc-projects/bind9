ed1f8b162a6560894827d6fb1d2c29e895f9cffd
Danny Mayer <source@isc.org>
2001-07-27 05:02:11 +0000

    added named.key file path to global variable

--- bin/named/win32/os.c
+++ bin/named/win32/os.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: os.c,v 1.2 2001/07/18 18:42:13 gson Exp $ */
+/* $Id: os.c,v 1.3 2001/07/27 05:02:11 mayer Exp $ */
 
 #include <config.h>
 #include <stdarg.h>
@@ -57,6 +57,7 @@ ns_paths_init() {
 	ns_g_conffile = isc_ntpaths_get(NAMED_CONF_PATH);
 	ns_g_defaultpidfile = isc_ntpaths_get(NAMED_PID_PATH);
 	lwresd_g_defaultpidfile = isc_ntpaths_get(LWRESD_PID_PATH);
+	ns_g_autorndckeyfile = isc_ntpaths_get(NAMED_KEY_PATH);
 
 	Initialized = TRUE;
 }
