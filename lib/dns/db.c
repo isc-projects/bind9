e11fb6da2abf7f12bbaee1490142a2253179cfcf
Andreas Gustafsson <source@isc.org>
2001-09-04 23:10:36 +0000

    pullup:  978.   [bug]           dns_db_attachversion() had an invalid REQUIRE()                         condition.

--- lib/dns/db.c
+++ lib/dns/db.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: db.c,v 1.65.4.2 2001/05/14 23:50:47 gson Exp $ */
+/* $Id: db.c,v 1.65.4.3 2001/09/04 23:10:36 gson Exp $ */
 
 /***
  *** Imports
@@ -389,7 +389,7 @@ dns_db_attachversion(dns_db_t *db, dns_dbversion_t *source,
 	REQUIRE(DNS_DB_VALID(db));
 	REQUIRE((db->attributes & DNS_DBATTR_CACHE) == 0);
 	REQUIRE(source != NULL);
-	REQUIRE(targetp != NULL && *targetp != NULL);
+	REQUIRE(targetp != NULL && *targetp == NULL);
 
 	(db->methods->attachversion)(db, source, targetp);
 
