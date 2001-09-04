e5094285ce3f375a49bb2aaa290e43b8c56a879b
Andreas Gustafsson <source@isc.org>
2001-09-04 19:36:33 +0000

    pullup: Update to change path to resolv.conf and define RESOLV_CONF

--- lib/isc/win32/ntpaths.c
+++ lib/isc/win32/ntpaths.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: ntpaths.c,v 1.6.2.1 2001/08/30 20:29:53 gson Exp $ */
+/* $Id: ntpaths.c,v 1.6.2.2 2001/09/04 19:36:31 gson Exp $ */
 
 /*
  * This module fetches the required path information that is specific
@@ -79,8 +79,8 @@ isc_ntpaths_init() {
 	strcpy(lwresd_confFile, namedBase);
 	strcat(lwresd_confFile, "\\etc\\lwresd.conf");
 
-	strcpy(lwresd_resolvconfFile, namedBase);
-	strcat(lwresd_resolvconfFile, "\\etc\\resolv.conf");
+	strcpy(lwresd_resolvconfFile, systemDir);
+	strcat(lwresd_resolvconfFile, "\\Drivers\\etc\\resolv.conf");
 
 	strcpy(rndc_keyFile, namedBase);
 	strcat(rndc_keyFile, "\\etc\\rndc.key");
