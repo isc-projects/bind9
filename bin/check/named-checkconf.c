08bed4d70afbae446384fef4c92f005fb2103cee
Andreas Gustafsson <source@isc.org>
2001-10-02 17:20:46 +0000

    Reverted accidental bulk commit with log message of "%"

--- bin/check/named-checkconf.c
+++ bin/check/named-checkconf.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: named-checkconf.c,v 1.17 2001/10/02 01:09:03 gson Exp $ */
+/* $Id: named-checkconf.c,v 1.18 2001/10/02 17:20:41 gson Exp $ */
 
 #include <config.h>
 
@@ -51,8 +51,7 @@ isc_log_t *logc = NULL;
 
 static void
 usage(void) {
-        fprintf(stderr, "usage: named-checkconf [-v] [-z] [-t directory] "
-		"[named.conf]\n");
+        fprintf(stderr, "usage: named-checkconf [-v] [-z] [-t directory] [named.conf]\n");
         exit(1);
 }
 
