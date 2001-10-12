23b81eeddd4d5ef3320d395883d07206c3861596
Andreas Gustafsson <source@isc.org>
2001-10-12 20:11:43 +0000

    pullup: the tkey test would randomly fail due to key IDs with a leading zero being interpreted as octal numbers

--- bin/tests/system/tkey/ns1/setup.sh
+++ bin/tests/system/tkey/ns1/setup.sh
@@ -15,11 +15,11 @@
 # NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
 # WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: setup.sh,v 1.2 2001/01/11 20:51:19 bwelling Exp $
+# $Id: setup.sh,v 1.2.2.1 2001/10/12 20:11:43 gson Exp $
 
 RANDFILE=../random.data
 
 keyname=`$KEYGEN -a DH -b 768 -n host -r $RANDFILE server`
-keyid=`echo $keyname | perl -p -e 's/^.*\+//;'`
+keyid=`echo $keyname | perl -p -e 's/^.*\+0*//;'`
 rm -f named.conf
 perl -p -e "s/KEYID/$keyid/;" < named.conf.in > named.conf
