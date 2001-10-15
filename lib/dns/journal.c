d7a33c75ee88d506bd8d84799321838a233d8910
Mark Andrews <marka@isc.org>
2001-10-15 05:01:03 +0000

    pullup: 1052.  [bug]           Journals were not being created in binary mode                        resulting in "journal format not recognized" error                        under Windows NT/2000.  [RT #1889]

--- lib/dns/journal.c
+++ lib/dns/journal.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: journal.c,v 1.71.2.1 2001/01/09 22:43:40 bwelling Exp $ */
+/* $Id: journal.c,v 1.71.2.2 2001/10/15 05:01:03 marka Exp $ */
 
 #include <config.h>
 
@@ -515,7 +515,7 @@ journal_file_create(isc_mem_t *mctx, const char *filename) {
 
 	INSIST(sizeof(journal_rawheader_t) == JOURNAL_HEADER_SIZE);
 
-	result = isc_stdio_open(filename, "w", &fp);
+	result = isc_stdio_open(filename, "wb", &fp);
 	if (result != ISC_R_SUCCESS) {
 		isc_log_write(JOURNAL_COMMON_LOGARGS, ISC_LOG_ERROR,
 			      "%s: create: %s",
