b30e2a1c2117cca243567fbffec730e3664bdfb9
Mark Andrews <marka@isc.org>
2001-10-19 00:31:44 +0000

    Don't include EXCLUDED when kitting.

--- util/kit.sh
+++ util/kit.sh
@@ -15,7 +15,7 @@
 # NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
 # WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: kit.sh,v 1.14.2.1 2001/01/09 22:53:34 bwelling Exp $
+# $Id: kit.sh,v 1.14.2.2 2001/10/19 00:31:44 marka Exp $
 
 # Make a release kit
 #
@@ -91,7 +91,7 @@ sh util/sanitize_all.sh
 
 # Omit some files and directories from the kit.
 
-rm -rf TODO conftools util doc/design doc/dev doc/expired \
+rm -rf EXCLUDED TODO conftools util doc/design doc/dev doc/expired \
     doc/html doc/todo doc/private bin/lwresd doc/man/ctoman \
     doc/man/isc doc/man/bin/resolver.5 \
     bin/tests/system/relay lib/cfg
