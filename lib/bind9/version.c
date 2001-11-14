f109354dcea7d907175969b220fbb21dc2c8707c
Brian Wellington <source@isc.org>
2001-11-14 22:01:40 +0000

    copyright

--- lib/bind9/version.c
+++ lib/bind9/version.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1998-2001  Internet Software Consortium.
+ * Copyright (C) 2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: version.c,v 1.2 2001/10/16 23:20:48 gson Exp $ */
+/* $Id: version.c,v 1.3 2001/11/14 22:01:40 bwelling Exp $ */
 
 #include <bind9/version.h>
 
