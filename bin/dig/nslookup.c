d1a95dea1febb0dd4a62d6b772284aabbee0e8ca
Mark Andrews <marka@isc.org>
2002-03-20 22:45:11 +0000

    pullup: 1237.   [bug]           nslookup: "set q=type" failed.

--- bin/dig/nslookup.c
+++ bin/dig/nslookup.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: nslookup.c,v 1.90.2.2 2002/02/08 03:57:05 marka Exp $ */
+/* $Id: nslookup.c,v 1.90.2.3 2002/03/20 22:45:11 marka Exp $ */
 
 #include <config.h>
 
@@ -548,6 +548,9 @@ setoption(char *opt) {
 	} else if (strncasecmp(opt, "qu=", 3) == 0) {
 		if (testtype(&opt[3]))
 			safecpy(deftype, &opt[3], sizeof(deftype));
+	} else if (strncasecmp(opt, "q=", 2) == 0) {
+		if (testtype(&opt[2]))
+			safecpy(deftype, &opt[2], sizeof(deftype));
 	} else if (strncasecmp(opt, "domain=", 7) == 0) {
 		safecpy(domainopt, &opt[7], sizeof(domainopt));
 		set_search_domain(domainopt);
