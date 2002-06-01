53a96ddd5398369e88273cc83a37f05f6a416756
Mark Andrews <marka@isc.org>
2002-06-01 02:05:24 +0000

    pullup: 1243.   [bug]           It was possible to trigger a REQUIRE() in                         dns_message_findtype(). [RT #2659]

--- lib/dns/resolver.c
+++ lib/dns/resolver.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: resolver.c,v 1.218.2.7 2001/11/16 21:47:27 bwelling Exp $ */
+/* $Id: resolver.c,v 1.218.2.7.2.1 2002/06/01 02:05:24 marka Exp $ */
 
 #include <config.h>
 
@@ -3297,6 +3297,7 @@ check_related(void *arg, dns_name_t *addname, dns_rdatatype_t type) {
 				/*
 				 * Do we have its SIG too?
 				 */
+				rdataset = NULL;
 				result = dns_message_findtype(name,
 						      dns_rdatatype_sig,
 						      type, &rdataset);
