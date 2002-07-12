0f2442d92676559eb16727ed98cd37e0c2ac5adf
Mark Andrews <marka@isc.org>
2002-07-12 03:13:24 +0000

    sync with mainline

--- util/check-pullups.pl
+++ util/check-pullups.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 #
-# Copyright (C) 2001  Internet Software Consortium.
+# Copyright (C) 2001, 2002  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -15,7 +15,7 @@
 # NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
 # WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: check-pullups.pl,v 1.4 2001/05/31 22:51:43 bwelling Exp $
+# $Id: check-pullups.pl,v 1.4.2.1 2002/07/12 03:13:24 marka Exp $
 
 # Given two CHANGES files, list [bug] entries present in the
 # first one but not in the second one.
@@ -23,7 +23,7 @@
 
 use FileHandle;
 
-$/ = "";
+# $/ = "";
 
 # Read the CHANGES file $fn and return a hash of change
 # texts and categories indexed by change number.
@@ -37,15 +37,25 @@ sub readfile {
 
 	my ($changeid, $category);
 
+	$changeid = "none";
+	$category = "none";
+
 	while (<$fh>) {
-		if (m/---.* released ---/) {
-			next;
-		} elsif (m/^# /) {
-			next;
-		} elsif (m/^\s*(\d+)\.\s+\[(\w+)\]/) {
+		if (m/^\s*(\d+)\.\s+\[(\w+)\]/) {
 			$changeid = $1;
 			$category = $2;
 			# print "*** $1 $2\n";
+		} elsif (m/---.* released ---/) {
+			$changeid = "none";
+			$category = "none";
+			next;
+		} elsif (m/^# /) {
+			$changeid = "none";
+			$category = "none";
+			next;
+		}
+		if ($changeid eq "none") {
+			next;
 		}
 		$changes->{$changeid}->{text} .= $_;
 		$changes->{$changeid}->{category} = $category;
@@ -54,15 +64,20 @@ sub readfile {
 	return $changes;
 }
 
-@ARGV == 2 or die "usage: $0 changes-file-1 changes-file-2\n";
+@ARGV == 2 || @ARGV == 3 or die "usage: $0 changes-file-1 changes-file-2\n";
 
 my $c1 = readfile($ARGV[0]);
 my $c2 = readfile($ARGV[1]);
+if (@ARGV == 3) {
+	$c3 = readfile($ARGV[2]);
+} else {
+	my $c3 = { };
+}
 
 foreach my $c (sort {$a <=> $b} keys %$c1) {
 	my $category = $c1->{$c}->{category};
 	if (($category eq "bug" || $category eq "port") &&
-	    !exists($c2->{$c})) {
+	    !exists($c2->{$c}) && !exists($c3->{$c})) {
 		print $c1->{$c}->{text};
 	}
 }
