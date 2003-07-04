5798e5b3d452ece4494aa64cecfc553efa27ba11
Mark Andrews <marka@isc.org>
2003-07-04 05:21:47 +0000

    update WIDE copyright notice.

--- lib/lwres/getnameinfo.c
+++ lib/lwres/getnameinfo.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: getnameinfo.c,v 1.31 2003/04/11 07:25:29 marka Exp $ */
+/* $Id: getnameinfo.c,v 1.32 2003/07/04 05:21:47 marka Exp $ */
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -29,11 +29,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *    This product includes software developed by WIDE Project and
- *    its contributors.
- * 4. Neither the name of the project nor the names of its contributors
+ * 3. Neither the name of the project nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
