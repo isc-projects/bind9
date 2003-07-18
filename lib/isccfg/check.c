8667ca6c74b918db62d76f7ee77a5fb4785dd215
Mark Andrews <marka@isc.org>
2003-07-18 06:07:25 +0000

    1489.   [compat]        Treat 'allow-update' on slave zones as a warning.                         [RT #3469]

--- lib/isccfg/check.c
+++ lib/isccfg/check.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: check.c,v 1.14.2.16 2002/04/23 02:00:03 marka Exp $ */
+/* $Id: check.c,v 1.14.2.17 2003/07/18 06:07:25 marka Exp $ */
 
 #include <config.h>
 
@@ -243,11 +243,18 @@ check_zoneconf(cfg_obj_t *zconfig, isc_symtab_t *symtab, isc_log_t *logctx,
 		    cfg_map_get(zoptions, options[i].name, &obj) ==
 		    ISC_R_SUCCESS)
 		{
-			cfg_obj_log(obj, logctx, ISC_LOG_ERROR,
-				    "option '%s' is not allowed in '%s' "
-				    "zone '%s'",
-				    options[i].name, typestr, zname);
-			result = ISC_R_FAILURE;
+			if (strcmp(options[i].name, "allow-update") != 0 ||
+			    ztype != SLAVEZONE) {
+				cfg_obj_log(obj, logctx, ISC_LOG_ERROR,
+					    "option '%s' is not allowed "
+					    "in '%s' zone '%s'",
+					    options[i].name, typestr, zname);
+					result = ISC_R_FAILURE;
+			} else
+				cfg_obj_log(obj, logctx, ISC_LOG_WARNING,
+					    "option '%s' is not allowed "
+					    "in '%s' zone '%s'",
+					    options[i].name, typestr, zname);
 		}
 	}
 
