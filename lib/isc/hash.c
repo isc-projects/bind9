eb126c1c7723fcaa39b7c833b24ad662e7feb712
Mark Andrews <marka@isc.org>
2003-07-30 01:15:18 +0000

    #include <isc/string.h> [RT #8537]

--- lib/isc/hash.c
+++ lib/isc/hash.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: hash.c,v 1.2.2.3 2003/07/29 04:30:07 marka Exp $ */
+/* $Id: hash.c,v 1.2.2.4 2003/07/30 01:15:18 marka Exp $ */
 
 /*
  * Some portion of this code was derived from universal hash function
@@ -69,6 +69,7 @@ if advised of the possibility of such damage.
 #include <isc/random.h>
 #include <isc/refcount.h>
 #include <isc/rwlock.h>
+#include <isc/string.h>
 #include <isc/util.h>
 
 #define HASH_MAGIC		ISC_MAGIC('H', 'a', 's', 'h')
