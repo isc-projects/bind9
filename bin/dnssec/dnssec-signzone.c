39c8267888f34e54515757666d4b7068ede546b3
Mark Andrews <marka@isc.org>
2003-08-22 06:40:04 +0000

    1396.   [func]          dnssec-signzone: adjust the default signing time by                         1 hour to allow for clock skew.

--- bin/dnssec/dnssec-signzone.c
+++ bin/dnssec/dnssec-signzone.c
@@ -17,7 +17,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: dnssec-signzone.c,v 1.139.2.2.4.4 2003/08/18 07:35:48 marka Exp $ */
+/* $Id: dnssec-signzone.c,v 1.139.2.2.4.5 2003/08/22 06:40:04 marka Exp $ */
 
 #include <config.h>
 
@@ -1455,7 +1455,7 @@ usage(void) {
 	fprintf(stderr, "\t-d directory\n");
 	fprintf(stderr, "\t\tdirectory to find signedkey files (.)\n");
 	fprintf(stderr, "\t-s YYYYMMDDHHMMSS|+offset:\n");
-	fprintf(stderr, "\t\tSIG start time - absolute|offset (now)\n");
+	fprintf(stderr, "\t\tSIG start time - absolute|offset (now - 1 hour)\n");
 	fprintf(stderr, "\t-e YYYYMMDDHHMMSS|+offset|\"now\"+offset]:\n");
 	fprintf(stderr, "\t\tSIG end time  - absolute|from start|from now "
 				"(now + 30 days)\n");
@@ -1627,7 +1627,7 @@ main(int argc, char *argv[]) {
 	if (startstr != NULL)
 		starttime = strtotime(startstr, now, now);
 	else
-		starttime = now;
+		starttime = now - 3600;  /* Allow for some clock skew. */
 
 	if (endstr != NULL)
 		endtime = strtotime(endstr, now, starttime);
