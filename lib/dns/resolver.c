baee883e378402698eeb7326538a7d432c8350d1
Mark Andrews <marka@isc.org>
2003-09-21 13:05:16 +0000

    1511.   [bug]           delegation-only was generating false positives                         on negative answers from subzones.

--- lib/dns/resolver.c
+++ lib/dns/resolver.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: resolver.c,v 1.267 2003/09/21 02:52:35 marka Exp $ */
+/* $Id: resolver.c,v 1.268 2003/09/21 13:05:16 marka Exp $ */
 
 #include <config.h>
 
@@ -380,7 +380,8 @@ fix_mustbedelegationornxdomain(dns_message_t *message, fetchctx_t *fctx) {
 			if (type == dns_rdatatype_soa &&
 			    dns_name_equal(name, domain))
 				keep_auth = ISC_TRUE;
-			if (type != dns_rdatatype_ns)
+			if (type != dns_rdatatype_ns &&
+			    type != dns_rdatatype_soa)
 				continue;
 			if (dns_name_equal(name, domain))
 				goto munge;
