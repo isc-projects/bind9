b64048e1a696fc8c39536b5126e3293cbd4502f3
Mark Andrews <marka@isc.org>
2003-09-22 00:35:12 +0000

    include <dns/rdataclass.h>

--- lib/dns/resolver.c
+++ lib/dns/resolver.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: resolver.c,v 1.187.2.10.6.5 2003/09/22 00:13:04 marka Exp $ */
+/* $Id: resolver.c,v 1.187.2.10.6.6 2003/09/22 00:35:12 marka Exp $ */
 
 #include <config.h>
 
@@ -35,6 +35,7 @@
 #include <dns/ncache.h>
 #include <dns/peer.h>
 #include <dns/rdata.h>
+#include <dns/rdataclass.h>
 #include <dns/rdatalist.h>
 #include <dns/rdataset.h>
 #include <dns/rdatastruct.h>
