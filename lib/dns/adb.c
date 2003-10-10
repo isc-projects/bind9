71247c33a4745a6c03ab761a3e5999205a7ba064
Mark Andrews <marka@isc.org>
2003-10-10 00:19:01 +0000

    INSIST -> RUNTIME_CHECK

--- lib/dns/adb.c
+++ lib/dns/adb.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: adb.c,v 1.206 2003/10/10 00:13:50 marka Exp $ */
+/* $Id: adb.c,v 1.207 2003/10/10 00:19:01 marka Exp $ */
 
 /*
  * Implementation notes
@@ -3487,8 +3487,8 @@ dns_adb_flush(dns_adb_t *adb) {
 		/*
 		 * Call our cleanup routines.
 		 */
-		INSIST(cleanup_names(adb, i, INT_MAX) == ISC_FALSE);
-		INSIST(cleanup_entries(adb, i, INT_MAX) == ISC_FALSE);
+		RUNTIME_CHECK(cleanup_names(adb, i, INT_MAX) == ISC_FALSE);
+		RUNTIME_CHECK(cleanup_entries(adb, i, INT_MAX) == ISC_FALSE);
 	}
 
 #ifdef DUMP_ADB_AFTER_CLEANING
