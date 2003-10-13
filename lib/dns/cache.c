d88e1c30a4d4232a1a0520ad0d498b40a40cafd2
Mark Andrews <marka@isc.org>
2003-10-13 00:40:29 +0000

    three -> two

--- lib/dns/cache.c
+++ lib/dns/cache.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: cache.c,v 1.54 2003/09/30 05:56:10 marka Exp $ */
+/* $Id: cache.c,v 1.55 2003/10/13 00:40:29 marka Exp $ */
 
 #include <config.h>
 
@@ -40,7 +40,7 @@
 #define VALID_CACHE(cache)	ISC_MAGIC_VALID(cache, CACHE_MAGIC)
 
 /*
- * The following three variables control incremental cleaning.
+ * The following two variables control incremental cleaning.
  * MINSIZE is how many bytes is the floor for dns_cache_setcachesize().
  * CLEANERINCREMENT is how many nodes are examined in one pass.
  */
