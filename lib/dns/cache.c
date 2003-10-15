e724512f2be8d3de5f2982ebba0b2cdaf8325702
Mark Andrews <marka@isc.org>
2003-10-15 07:01:45 +0000

    1525.   [bug]           dns_cache_create() could dereference a NULL pointer                         during error cleanup.

--- lib/dns/cache.c
+++ lib/dns/cache.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: cache.c,v 1.55 2003/10/13 00:40:29 marka Exp $ */
+/* $Id: cache.c,v 1.56 2003/10/15 07:01:45 marka Exp $ */
 
 #include <config.h>
 
@@ -248,7 +248,9 @@ dns_cache_create(isc_mem_t *mctx, isc_taskmgr_t *taskmgr,
 	for (i = 0; i < cache->db_argc; i++)
 		if (cache->db_argv[i] != NULL)
 			isc_mem_free(mctx, cache->db_argv[i]);
-	isc_mem_put(mctx, cache->db_argv, cache->db_argc * sizeof(char *));
+	if (cache->db_argv != NULL)
+		isc_mem_put(mctx, cache->db_argv,
+			    cache->db_argc * sizeof(char *));
  cleanup_dbtype:
 	isc_mem_free(mctx, cache->db_type);
  cleanup_filelock:
@@ -565,6 +567,8 @@ cache_cleaner_init(dns_cache_t *cache, isc_taskmgr_t *taskmgr,
 	return (ISC_R_SUCCESS);
 
  cleanup:
+	if (cleaner->overmem_event != NULL)
+		isc_event_free(&cleaner->overmem_event);
 	if (cleaner->resched_event != NULL)
 		isc_event_free(&cleaner->resched_event);
 	if (cleaner->cleaning_timer != NULL)
