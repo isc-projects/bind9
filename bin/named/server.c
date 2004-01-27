b3d3e578410209d849efb59c9ea4925ca5c9ca68
Mark Andrews <marka@isc.org>
2004-01-27 01:19:41 +0000

    update wording

--- bin/named/server.c
+++ bin/named/server.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: server.c,v 1.409 2004/01/26 23:44:12 marka Exp $ */
+/* $Id: server.c,v 1.410 2004/01/27 01:19:41 marka Exp $ */
 
 #include <config.h>
 
@@ -801,8 +801,8 @@ configure_view(dns_view_t *view, cfg_obj_t *config, cfg_obj_t *vconfig,
 	CHECK(get_view_querysource_dispatch(maps, AF_INET6, &dispatch6));
 	if (dispatch4 == NULL && dispatch6 == NULL) {
 		UNEXPECTED_ERROR(__FILE__, __LINE__,
-				 "unable to obtain neither a IPv4 or"
-				 " a IPv6 dispatch");
+				 "unable to obtain neither an IPv4 nor"
+				 " an IPv6 dispatch");
 		result = ISC_R_UNEXPECTED;
 		goto cleanup;
 	}
