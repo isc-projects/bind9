4230c2e85e44b1cf6e7ec17a695a8581b9a09940
Mark Andrews <marka@isc.org>
2004-02-17 03:34:55 +0000

    lookup RRSIG (46) not SIG (24) in getrrsetbyname test.

--- bin/tests/system/lwresd/lwtest.c
+++ bin/tests/system/lwresd/lwtest.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: lwtest.c,v 1.24 2002/08/27 04:53:39 marka Exp $ */
+/* $Id: lwtest.c,v 1.25 2004/02/17 03:34:55 marka Exp $ */
 
 #include <config.h>
 
@@ -761,7 +761,7 @@ main(void) {
 	test_getrrsetbyname("a.example1.", 1, 1, 1, 0, 1);
 	test_getrrsetbyname("e.example1.", 1, 1, 1, 1, 1);
 	test_getrrsetbyname("e.example1.", 1, 255, 1, 1, 0);
-	test_getrrsetbyname("e.example1.", 1, 24, 1, 0, 1);
+	test_getrrsetbyname("e.example1.", 1, 46, 1, 0, 1);
 	test_getrrsetbyname("", 1, 1, 0, 0, 0);
 
 	if (fails == 0)
