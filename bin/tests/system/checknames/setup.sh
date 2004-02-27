362a27e81e6de298b6ff31457ad22380a6d825c2
Mark Andrews <marka@isc.org>
2004-02-27 20:44:09 +0000

    1586.   [func]          "check-names" is now implemented.

---
+++ bin/tests/system/checknames/setup.sh
@@ -0,0 +1,7 @@
+cp ns1/ignore.example.db.in ns1/ignore.example.db
+cp ns1/warn.example.db.in ns1/warn.example.db
+cp ns1/fail.example.db.in ns1/fail.example.db
+
+cp ns1/ignore.update.db.in ns1/ignore.update.db
+cp ns1/warn.update.db.in ns1/warn.update.db
+cp ns1/fail.update.db.in ns1/fail.update.db
