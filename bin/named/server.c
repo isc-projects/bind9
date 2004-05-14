4e46ccaeb869d10f15cbfdd6307b5e0764260c10
Mark Andrews <marka@isc.org>
2004-05-14 01:04:46 +0000

    move cleanup of in_roothints earlier to make it easier to detect reference count mismatches.

--- bin/named/server.c
+++ bin/named/server.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: server.c,v 1.339.2.28 2004/04/20 07:10:48 marka Exp $ */
+/* $Id: server.c,v 1.339.2.29 2004/05/14 01:04:46 marka Exp $ */
 
 #include <config.h>
 
@@ -2387,6 +2387,8 @@ shutdown_server(isc_task_t *task, isc_event_t *event) {
 	if (server->blackholeacl != NULL)
 		dns_acl_detach(&server->blackholeacl);
 
+	dns_db_detach(&server->in_roothints);
+
 	isc_task_endexclusive(server->task);
 
 	isc_task_detach(&server->task);
@@ -2516,8 +2518,6 @@ ns_server_destroy(ns_server_t **serverp) {
 	isc_event_free(&server->reload_event);
 
 	INSIST(ISC_LIST_EMPTY(server->viewlist));
-
-	dns_db_detach(&server->in_roothints);
 
 	dns_aclenv_destroy(&server->aclenv);
 
