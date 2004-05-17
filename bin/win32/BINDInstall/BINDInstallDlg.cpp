c928cebdc511aca424bd4f9fbd61246038967f30
Mark Andrews <marka@isc.org>
2004-05-17 10:14:37 +0000

    do not install dnssec-makekeyset and dnssec-signkey.

--- bin/win32/BINDInstall/BINDInstallDlg.cpp
+++ bin/win32/BINDInstall/BINDInstallDlg.cpp
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: BINDInstallDlg.cpp,v 1.17 2004/05/17 10:14:06 marka Exp $ */
+/* $Id: BINDInstallDlg.cpp,v 1.18 2004/05/17 10:14:37 marka Exp $ */
 
 /*
  * Copyright (c) 1999-2000 by Nortel Networks Corporation
@@ -138,8 +138,6 @@ const FileData installFiles[] =
 	{"nslookup.exe", FileData::BinDir, FileData::Normal, FALSE},
 	{"rndc-confgen.exe", FileData::BinDir, FileData::Normal, FALSE},
 	{"dnssec-keygen.exe", FileData::BinDir, FileData::Normal, FALSE},
-	{"dnssec-makekeyset.exe", FileData::BinDir, FileData::Normal, FALSE},
-	{"dnssec-signkey.exe", FileData::BinDir, FileData::Normal, FALSE},
 	{"dnssec-signzone.exe", FileData::BinDir, FileData::Normal, FALSE},
 	{"named-checkconf.exe", FileData::BinDir, FileData::Normal, FALSE},
 	{"named-checkzone.exe", FileData::BinDir, FileData::Normal, FALSE},
