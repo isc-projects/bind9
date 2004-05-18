3802d6611cdad88e3519a0f4b9430e2d0d49cebe
Mark Andrews <marka@isc.org>
2004-05-18 01:38:39 +0000

    include <sys/param.h> for NetBSD

--- lib/isc/unix/os.c
+++ lib/isc/unix/os.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: os.c,v 1.11.2.1 2004/03/09 06:12:11 marka Exp $ */
+/* $Id: os.c,v 1.11.2.2 2004/05/18 01:38:39 marka Exp $ */
 
 #include <config.h>
 
@@ -55,7 +55,8 @@ hpux_ncpus(void) {
 #endif /* __hpux */
 
 #if defined(HAVE_SYS_SYSCTL_H) && defined(HAVE_SYSCTLBYNAME)
-#include <sys/types.h>
+#include <sys/types.h>  /* for FreeBSD */
+#include <sys/param.h>  /* for NetBSD */
 #include <sys/sysctl.h>
 
 static int
