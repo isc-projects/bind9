ae507663cfa05e0e218ecd88a867e79fa447feac
Mark Andrews <marka@isc.org>
2004-07-01 04:53:14 +0000

    indenting

--- lib/isc/unix/socket.c
+++ lib/isc/unix/socket.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: socket.c,v 1.240 2004/06/30 23:35:16 marka Exp $ */
+/* $Id: socket.c,v 1.241 2004/07/01 04:53:14 marka Exp $ */
 
 #include <config.h>
 
@@ -2864,7 +2864,7 @@ isc_socket_bind(isc_socket_t *sock, isc_sockaddr_t *sockaddr) {
 	 */
 	if (isc_sockaddr_getport(sockaddr) != (in_port_t)0 &&
 	    setsockopt(sock->fd, SOL_SOCKET, SO_REUSEADDR, (void *)&on,
-			       sizeof(on)) < 0) {
+		       sizeof(on)) < 0) {
 		UNEXPECTED_ERROR(__FILE__, __LINE__,
 				 "setsockopt(%d) %s", sock->fd,
 				 isc_msgcat_get(isc_msgcat, ISC_MSGSET_GENERAL,
