2597c68ffee3d98cfaf4eceb37054ade6810fdfb
Mark Andrews <marka@isc.org>
2004-08-10 00:35:01 +0000

    improve dns_name_getlabelsequence() description.

--- lib/dns/include/dns/name.h
+++ lib/dns/include/dns/name.h
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: name.h,v 1.108 2004/07/22 00:09:27 marka Exp $ */
+/* $Id: name.h,v 1.109 2004/08/10 00:35:01 marka Exp $ */
 
 #ifndef DNS_NAME_H
 #define DNS_NAME_H 1
@@ -572,6 +572,9 @@ dns_name_getlabelsequence(const dns_name_t *source, unsigned int first,
  *
  * Notes:
  *	Numbering starts at 0.
+ *
+ *	Given "rc.vix.com.", the label 0 is "rc", and label 3 is the
+ *	root label.
  *
  *	'target' refers to the same memory as 'source', so 'source'
  *	must not be changed while 'target' is still in use.
