ed18f744963c431ff76489aa01ec6fc9fac137fb
Mark Andrews <marka@isc.org>
2004-08-28 05:53:12 +0000

    1703.   [bug]           named would loop sending NOTIFY messages when it                         failed to receive a response. [RT #12322]

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: zone.c,v 1.419 2004/08/27 12:35:03 marka Exp $ */
+/* $Id: zone.c,v 1.420 2004/08/28 05:53:12 marka Exp $ */
 
 #include <config.h>
 
@@ -5180,9 +5180,8 @@ notify_done(isc_task_t *task, isc_event_t *event) {
 	 * the soa if we see a formerr and had sent a SOA.
 	 */
 	isc_event_free(&event);
-	if ((result == ISC_R_TIMEDOUT ||
-	     (message != NULL && message->rcode == dns_rcode_formerr &&
-	      (notify->flags & DNS_NOTIFY_NOSOA) == 0))) {
+	if (message != NULL && message->rcode == dns_rcode_formerr &&
+	    (notify->flags & DNS_NOTIFY_NOSOA) == 0) {
 		notify->flags |= DNS_NOTIFY_NOSOA;
 		dns_request_destroy(&notify->request);
 		result = notify_send_queue(notify);
