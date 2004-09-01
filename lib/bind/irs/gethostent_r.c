a62a385b4082c76f7b269a8b451048f2620b6c6e
Mark Andrews <marka@isc.org>
2004-09-01 02:03:07 +0000

    pullup: 1682.   [bug]           gethostby*_r() implementations that copied the return                         pointer got NULL returned on success.

--- lib/bind/irs/gethostent_r.c
+++ lib/bind/irs/gethostent_r.c
@@ -16,7 +16,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static const char rcsid[] = "$Id: gethostent_r.c,v 1.4.206.2 2004/04/22 03:32:06 marka Exp $";
+static const char rcsid[] = "$Id: gethostent_r.c,v 1.4.206.3 2004/09/01 02:03:07 marka Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <port_before.h>
@@ -47,7 +47,7 @@ gethostbyname_r(const char *name,  struct hostent *hptr, HOST_R_ARGS) {
 	HOST_R_ERRNO;
 
 #ifdef HOST_R_SETANSWER
-	if (he == NULL || (n = copy_hostent(he, hptr, HOST_R_COPY)) == 0)
+	if (he == NULL || (n = copy_hostent(he, hptr, HOST_R_COPY)) != 0)
 		*answerp = NULL;
 	else
 		*answerp = hptr;
@@ -72,7 +72,7 @@ gethostbyaddr_r(const char *addr, int len, int type,
 	HOST_R_ERRNO;
 
 #ifdef HOST_R_SETANSWER
-	if (he == NULL || (n = copy_hostent(he, hptr, HOST_R_COPY)) == 0)
+	if (he == NULL || (n = copy_hostent(he, hptr, HOST_R_COPY)) != 0)
 		*answerp = NULL;
 	else
 		*answerp = hptr;
@@ -102,7 +102,7 @@ gethostent_r(struct hostent *hptr, HOST_R_ARGS) {
 	HOST_R_ERRNO;
 
 #ifdef HOST_R_SETANSWER
-	if (he == NULL || (n = copy_hostent(he, hptr, HOST_R_COPY)) == 0)
+	if (he == NULL || (n = copy_hostent(he, hptr, HOST_R_COPY)) != 0)
 		*answerp = NULL;
 	else
 		*answerp = hptr;
