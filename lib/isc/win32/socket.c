41b1e059611bbaa3fb4c80a47f058686d431b715
Mark Andrews <marka@isc.org>
2004-09-01 04:32:18 +0000

    fix typo in unexecuted code

--- lib/isc/win32/socket.c
+++ lib/isc/win32/socket.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: socket.c,v 1.5.2.13.2.12 2004/06/30 23:34:55 marka Exp $ */
+/* $Id: socket.c,v 1.5.2.13.2.13 2004/09/01 04:32:18 marka Exp $ */
 
 /* This code has been rewritten to take advantage of Windows Sockets
  * I/O Completion Ports and Events. I/O Completion Ports is ONLY
@@ -1918,7 +1918,7 @@ isc_socket_create(isc_socketmgr_t *manager, int pf, isc_sockettype_t type,
 		if ((pf == AF_INET6)
 		    && (setsockopt(sock->fd, IPPROTO_IPV6, IPV6_PKTINFO,
 				   (void *)&on, sizeof(on)) < 0)) {
-			isc__strerror(WSAGetLaastError(), strbuf, sizeof(strbuf));
+			isc__strerror(WSAGetLastError(), strbuf, sizeof(strbuf));
 			UNEXPECTED_ERROR(__FILE__, __LINE__,
 					 "setsockopt(%d, IPV6_PKTINFO) %s: %s",
 					 sock->fd,
