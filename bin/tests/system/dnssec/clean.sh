756f078904cae6c7932090eadcc95722f14aff07
Mark Andrews <marka@isc.org>
2004-09-07 04:14:44 +0000

    clean up */dsset-*, */dlvset-* and ns2/dlv.db

--- bin/tests/system/dnssec/clean.sh
+++ bin/tests/system/dnssec/clean.sh
@@ -15,13 +15,13 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: clean.sh,v 1.15 2004/03/10 01:06:04 marka Exp $
+# $Id: clean.sh,v 1.16 2004/09/07 04:14:44 marka Exp $
 
-rm -f */K* */keyset-* */signedkey-* */*.signed */trusted.conf */tmp*
+rm -f */K* */keyset-* */dsset-* */dlvset-* */signedkey-* */*.signed */trusted.conf */tmp*
 rm -f ns1/root.db ns2/example.db ns3/secure.example.db
 rm -f ns3/unsecure.example.db ns3/bogus.example.db ns3/keyless.example.db
 rm -f ns3/dynamic.example.db ns3/dynamic.example.db.signed.jnl
 rm -f */example.bk
 rm -f dig.out.*
 rm -f random.data
-
+rm -f ns2/dlv.db
