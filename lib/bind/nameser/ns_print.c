3ca9bffc1d5db55ccb41d03bf2cf3e94b5d714fd
Mark Andrews <marka@isc.org>
2004-09-16 07:02:53 +0000

    silence compiler

--- lib/bind/nameser/ns_print.c
+++ lib/bind/nameser/ns_print.c
@@ -16,7 +16,7 @@
  */
 
 #ifndef lint
-static const char rcsid[] = "$Id: ns_print.c,v 1.8 2004/09/16 00:56:12 marka Exp $";
+static const char rcsid[] = "$Id: ns_print.c,v 1.9 2004/09/16 07:02:52 marka Exp $";
 #endif
 
 /* Import. */
@@ -706,7 +706,7 @@ ns_sprintrrf(const u_char *msg, size_t msglen,
 	char *p;
 
 	len = SPRINTF((tmp, "\\# %u%s\t; %s", (unsigned)(edata - rdata),
-		       rdlen != 0 ? " (" : "", comment));
+		       rdlen != 0U ? " (" : "", comment));
 	T(addstr(tmp, len, &buf, &buflen));
 	while (rdata < edata) {
 		p = tmp;
