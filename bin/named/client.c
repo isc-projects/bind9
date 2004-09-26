22e6067c162084375647929a6e693d37bd76722a
Mark Andrews <marka@isc.org>
2004-09-26 22:36:34 +0000

    Update error message. s/isc_socket_recv()/isc_socket_recv2()/

--- bin/named/client.c
+++ bin/named/client.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: client.c,v 1.219.18.2 2004/07/23 02:57:23 marka Exp $ */
+/* $Id: client.c,v 1.219.18.3 2004/09/26 22:36:34 marka Exp $ */
 
 #include <config.h>
 
@@ -1938,7 +1938,7 @@ client_udprecv(ns_client_t *client) {
 				  client->task, client->recvevent, 0);
 	if (result != ISC_R_SUCCESS) {
 		UNEXPECTED_ERROR(__FILE__, __LINE__,
-				 "isc_socket_recv() failed: %s",
+				 "isc_socket_recv2() failed: %s",
 				 isc_result_totext(result));
 		/*
 		 * This cannot happen in the current implementation, since
