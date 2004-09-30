314cac23654229f5df93af0c41231fd3c2187621
Mark Andrews <marka@isc.org>
2004-09-30 23:56:22 +0000

    1735.   [bug]           'dig +sigtrace' could die with a REQUIRE failure.                         [RE #12688]

--- bin/dig/dighost.c
+++ bin/dig/dighost.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: dighost.c,v 1.267 2004/09/16 02:10:41 marka Exp $ */
+/* $Id: dighost.c,v 1.268 2004/09/30 23:56:22 marka Exp $ */
 
 /*
  * Notice to programmers:  Do not use this code as an example of how to
@@ -4937,7 +4937,7 @@ prove_nx_domain(dns_message_t *msg,
 	dns_rdataset_t  * nsecset = NULL;
 	dns_rdataset_t  * signsecset = NULL ;
 	dns_rdata_t       nsec = DNS_RDATA_INIT;
-	dns_name_t      * nsecname = NULL;
+	dns_name_t      * nsecname;
 	dns_rdata_nsec_t  nsecstruct;
   
 	if ((result = dns_message_firstname(msg, DNS_SECTION_AUTHORITY))
@@ -4948,6 +4948,7 @@ prove_nx_domain(dns_message_t *msg,
 	}
  
 	do {
+		nsecname = NULL;
 		dns_message_currentname(msg, DNS_SECTION_AUTHORITY, &nsecname);
 		nsecset = search_type(nsecname, dns_rdatatype_nsec,
 				      dns_rdatatype_any);
@@ -4993,7 +4994,6 @@ prove_nx_domain(dns_message_t *msg,
 
 			dns_rdata_freestruct(&nsecstruct);
 		}
-		nsecname = NULL;
 	} while (dns_message_nextname(msg, DNS_SECTION_AUTHORITY)
 		 == ISC_R_SUCCESS);
 
