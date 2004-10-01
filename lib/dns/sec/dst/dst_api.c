1439cfe000e98393cfc4742ca86ece14144ed03d
Mark Andrews <marka@isc.org>
2004-10-01 00:20:49 +0000

    1736.   [bug]           dst_key_fromnamedfile() could fail to read a                         public key. [RT #12687]
    Not strictly necessary for 9.2.x but will prevent problems if we
    ever need to add a extra flag bit to 9.2.

--- lib/dns/sec/dst/dst_api.c
+++ lib/dns/sec/dst/dst_api.c
@@ -18,7 +18,7 @@
 
 /*
  * Principal Author: Brian Wellington
- * $Id: dst_api.c,v 1.88.2.6 2004/03/09 06:11:39 marka Exp $
+ * $Id: dst_api.c,v 1.88.2.7 2004/10/01 00:20:49 marka Exp $
  */
 
 #include <config.h>
@@ -380,7 +380,7 @@ dst_key_fromnamedfile(const char *filename, int type, isc_mem_t *mctx,
 	if (result != ISC_R_SUCCESS)
 		return (result);
 
-	if (type == DST_TYPE_PUBLIC ||
+	if ((type & (DST_TYPE_PRIVATE | DST_TYPE_PUBLIC)) == DST_TYPE_PUBLIC ||
 	    (pubkey->key_flags & DNS_KEYFLAG_TYPEMASK) == DNS_KEYTYPE_NOKEY)
 	{
 		result = computeid(pubkey);
