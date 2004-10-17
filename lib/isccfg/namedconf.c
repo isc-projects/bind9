5ad808cbc9cb4f1a12bf89c89156878461b79fa3
Mark Andrews <marka@isc.org>
2004-10-17 23:19:15 +0000

    1747.   [bug]           BIND 8 compatability: named/named-checkconf failed                         to parse "host-statistics-max" in named.conf.

--- lib/isccfg/namedconf.c
+++ lib/isccfg/namedconf.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: namedconf.c,v 1.30.18.8 2004/10/14 00:51:33 marka Exp $ */
+/* $Id: namedconf.c,v 1.30.18.9 2004/10/17 23:19:15 marka Exp $ */
 
 #include <config.h>
 
@@ -588,6 +588,7 @@ options_clauses[] = {
 	{ "has-old-clients", &cfg_type_boolean, CFG_CLAUSEFLAG_OBSOLETE },
 	{ "heartbeat-interval", &cfg_type_uint32, 0 },
 	{ "host-statistics", &cfg_type_boolean, CFG_CLAUSEFLAG_NOTIMP },
+	{ "host-statistics-max", &cfg_type_uint32, CFG_CLAUSEFLAG_NOTIMP },
 	{ "hostname", &cfg_type_qstringornone, 0 },
 	{ "interface-interval", &cfg_type_uint32, 0 },
 	{ "listen-on", &cfg_type_listenon, CFG_CLAUSEFLAG_MULTI },
