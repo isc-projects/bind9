21094b43a2dc4e1f569a9ed894f5369470cd1a7f
Mark Andrews <marka@isc.org>
2004-10-21 01:53:44 +0000

    1749.   [bug]           'check-names response ignore;' failed to ignore.                         [RT #12866]

--- lib/dns/resolver.c
+++ lib/dns/resolver.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: resolver.c,v 1.295 2004/09/16 02:10:42 marka Exp $ */
+/* $Id: resolver.c,v 1.296 2004/10/21 01:53:44 marka Exp $ */
 
 #include <config.h>
 
@@ -5251,7 +5251,7 @@ resquery_response(isc_task_t *task, isc_event_t *event) {
 			     domainbuf, namebuf, typebuf, classbuf, addrbuf);
 	}
 
-	if ((fctx->res->options | DNS_RESOLVER_CHECKNAMES) != 0)
+	if ((fctx->res->options & DNS_RESOLVER_CHECKNAMES) != 0)
 		checknames(message);
 
 	/*
