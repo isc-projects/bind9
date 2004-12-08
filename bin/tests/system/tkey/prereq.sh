ef556c237615a3c46506a3be9f86c032c2dd7fdd
Mark Andrews <marka@isc.org>
2004-12-08 06:12:04 +0000

    1789.   [bug]           Prerequisite test for tkey and dnssec could fail                         with "configure --with-libtool".

--- bin/tests/system/tkey/prereq.sh
+++ bin/tests/system/tkey/prereq.sh
@@ -15,9 +15,11 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: prereq.sh,v 1.4.2.1 2004/03/09 06:10:18 marka Exp $
+# $Id: prereq.sh,v 1.4.2.2 2004/12/08 06:12:03 marka Exp $
 
-if $KEYGEN -a RSA -b 512 -n zone -r $KEYGEN foo > /dev/null 2>&1
+../../genrandom 400 random.data
+
+if $KEYGEN -a RSA -b 512 -n zone -r random.data foo > /dev/null 2>&1
 then
     rm -f foo*
 else
