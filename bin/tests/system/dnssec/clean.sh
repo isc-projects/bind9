3002b5952de09eee4282ad72981e46dfecc5df61
Mark Andrews <marka@isc.org>
2004-12-14 01:02:50 +0000

    cleanup a couple more files

--- bin/tests/system/dnssec/clean.sh
+++ bin/tests/system/dnssec/clean.sh
@@ -15,12 +15,13 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: clean.sh,v 1.16 2004/09/07 04:14:44 marka Exp $
+# $Id: clean.sh,v 1.17 2004/12/14 01:02:49 marka Exp $
 
 rm -f */K* */keyset-* */dsset-* */dlvset-* */signedkey-* */*.signed */trusted.conf */tmp*
 rm -f ns1/root.db ns2/example.db ns3/secure.example.db
 rm -f ns3/unsecure.example.db ns3/bogus.example.db ns3/keyless.example.db
 rm -f ns3/dynamic.example.db ns3/dynamic.example.db.signed.jnl
+rm -f ns2/private.secure.example.db
 rm -f */example.bk
 rm -f dig.out.*
 rm -f random.data
--- bin/tests/system/xferquota/clean.sh
+++ bin/tests/system/xferquota/clean.sh
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: clean.sh,v 1.10 2004/03/10 01:06:06 marka Exp $
+# $Id: clean.sh,v 1.11 2004/12/14 01:02:50 marka Exp $
 
 #
 # Clean up after zone transfer quota tests.
@@ -24,3 +24,4 @@
 rm -f ns1/zone*.example.db ns1/zones.conf
 rm -f ns2/zone*.example.bk ns2/zones.conf
 rm -f dig.out.* ns2/changing.bk
+rm -f ns1/changing.db
