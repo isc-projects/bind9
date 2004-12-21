1ba466b68ece96bb9ff3b81fff6770539c2cd507
Tatuya JINMEI 神明達哉 <jinmei@isc.org>
2004-12-21 10:54:12 +0000

    new copyright for new files

--- lib/dns/acache.c
+++ lib/dns/acache.c
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +16,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: acache.c,v 1.2 2004/12/21 10:45:16 jinmei Exp $ */
+/* $Id: acache.c,v 1.3 2004/12/21 10:54:11 jinmei Exp $ */
 
 #include <config.h>
 
--- lib/dns/include/dns/acache.h
+++ lib/dns/include/dns/acache.h
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +16,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: acache.h,v 1.2 2004/12/21 10:45:18 jinmei Exp $ */
+/* $Id: acache.h,v 1.3 2004/12/21 10:54:12 jinmei Exp $ */
 
 #ifndef DNS_ACACHE_H
 #define DNS_ACACHE_H 1
