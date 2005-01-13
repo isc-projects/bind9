e216a945296f8447b97042f54102f624d0572421
Mark Andrews <marka@isc.org>
2005-01-13 23:57:30 +0000

    update copyrights

--- lib/dns/diff.c
+++ lib/dns/diff.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: diff.c,v 1.9.18.1 2005/01/11 23:56:59 marka Exp $ */
+/* $Id: diff.c,v 1.9.18.2 2005/01/13 23:57:30 marka Exp $ */
 
 #include <config.h>
 
