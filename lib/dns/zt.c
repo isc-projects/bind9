38636245d13677d2391128abd536ecf919fa50f3
Mark Andrews <marka@isc.org>
2005-01-17 04:11:03 +0000

    update copyrights

--- lib/dns/zt.c
+++ lib/dns/zt.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: zt.c,v 1.38.18.1 2005/01/14 03:28:02 marka Exp $ */
+/* $Id: zt.c,v 1.38.18.2 2005/01/17 04:11:03 marka Exp $ */
 
 #include <config.h>
 
