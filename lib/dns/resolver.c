7fed21a109b9a7eb74b67f3350264a774b4daf0c
Mark Andrews <marka@isc.org>
2005-01-20 00:01:46 +0000

    update copyright notice

--- lib/dns/resolver.c
+++ lib/dns/resolver.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: resolver.c,v 1.300 2005/01/19 23:25:41 marka Exp $ */
+/* $Id: resolver.c,v 1.301 2005/01/20 00:01:46 marka Exp $ */
 
 #include <config.h>
 
