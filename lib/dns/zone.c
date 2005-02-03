7b0b3cbaa84e157312b09ddae16de09d6fa594de
Mark Andrews <marka@isc.org>
2005-02-03 23:48:30 +0000

    update copyright notice

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: zone.c,v 1.333.2.37 2005/02/03 05:13:28 marka Exp $ */
+/* $Id: zone.c,v 1.333.2.38 2005/02/03 23:48:30 marka Exp $ */
 
 #include <config.h>
 
