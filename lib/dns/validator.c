2d7fc01cb302bfb7ff10aa2fbee529389e0211f2
Mark Andrews <marka@isc.org>
2005-02-09 05:19:30 +0000

    update copyright notice

--- lib/dns/validator.c
+++ lib/dns/validator.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: validator.c,v 1.125 2005/02/08 23:51:32 marka Exp $ */
+/* $Id: validator.c,v 1.126 2005/02/09 05:19:30 marka Exp $ */
 
 #include <config.h>
 
--- lib/isc/win32/include/isc/ipv6.h
+++ lib/isc/win32/include/isc/ipv6.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: ipv6.h,v 1.14 2005/02/09 03:38:25 marka Exp $ */
+/* $Id: ipv6.h,v 1.15 2005/02/09 05:19:30 marka Exp $ */
 
 #ifndef ISC_IPV6_H
 #define ISC_IPV6_H 1
