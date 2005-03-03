822b6e16428a847566d20c2c6bc4284e3e82119a
Mark Andrews <marka@isc.org>
2005-03-03 06:33:28 +0000

    update copyright notice

--- bin/check/named-checkconf.c
+++ bin/check/named-checkconf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: named-checkconf.c,v 1.28.18.1 2005/03/03 04:53:40 marka Exp $ */
+/* $Id: named-checkconf.c,v 1.28.18.2 2005/03/03 06:33:28 marka Exp $ */
 
 #include <config.h>
 
