69a50c6d756a52a126fc077d88fe75d439389d89
Mark Andrews <marka@isc.org>
2005-03-16 01:03:32 +0000

    1815.   [bug]           nsupdate triggered a REQUIRE if the server was set                         without also setting the zone and it encountered                         a CNAME and was using TSIG.  [RT #13086]

--- bin/nsupdate/nsupdate.c
+++ bin/nsupdate/nsupdate.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: nsupdate.c,v 1.130.18.5 2004/09/16 02:12:10 marka Exp $ */
+/* $Id: nsupdate.c,v 1.130.18.6 2005/03/16 01:03:32 marka Exp $ */
 
 #include <config.h>
 
@@ -1634,6 +1634,7 @@ recvsoa(isc_task_t *task, isc_event_t *event) {
 		ddebug("Destroying request [%p]", request);
 		dns_request_destroy(&request);
 		dns_message_renderreset(soaquery);
+		dns_message_settsigkey(soaquery, NULL);
 		sendrequest(localaddr, &servers[ns_inuse], soaquery, &request);
 		isc_mem_put(mctx, reqinfo, sizeof(nsu_requestinfo_t));
 		isc_event_free(&event);
@@ -1813,6 +1814,7 @@ recvsoa(isc_task_t *task, isc_event_t *event) {
 	dns_name_clone(&tname, name);
 	dns_request_destroy(&request);
 	dns_message_renderreset(soaquery);
+	dns_message_settsigkey(soaquery, NULL);
 	if (userserver != NULL)
 		sendrequest(localaddr, userserver, soaquery, &request);
 	else
