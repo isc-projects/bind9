657b6cd6a4cca03e0cef9a079c8d0bf4d73ffdd7
Mark Andrews <marka@isc.org>
2005-05-09 00:41:48 +0000

    update copyright notice

--- lib/lwres/print.c
+++ lib/lwres/print.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2001, 2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: print.c,v 1.5 2005/05/06 02:09:50 marka Exp $ */
+/* $Id: print.c,v 1.6 2005/05/09 00:41:48 marka Exp $ */
 
 #include <config.h>
 
