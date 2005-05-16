3783523d04ad2e9eb708b323962fee4372e25e7c
Mark Andrews <marka@isc.org>
2005-05-16 05:31:22 +0000

    typo in comment

--- bin/named/query.c
+++ bin/named/query.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: query.c,v 1.257.18.8 2005/04/27 05:00:33 sra Exp $ */
+/* $Id: query.c,v 1.257.18.9 2005/05/16 05:31:22 marka Exp $ */
 
 /*! \file */
 
@@ -3482,7 +3482,7 @@ query_find(ns_client_t *client, dns_fetchevent_t *event, dns_rdatatype_t qtype)
 		/*
 		 * Add SOA.  If the query was for a SOA record force the
 		 * ttl to zero so that it is possible for clients to find
-		 * the containing zone of a arbitary name with a stub
+		 * the containing zone of an arbitrary name with a stub
 		 * resolver and not have it cached.
 		 */
 		if (qtype == dns_rdatatype_soa)
