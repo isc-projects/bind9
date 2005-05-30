7676f07711141e8f351ca3a0d8b248b53b686b2e
Mark Andrews <marka@isc.org>
2005-05-30 01:59:25 +0000

    update copyright notice

--- lib/bind9/check.c
+++ lib/bind9/check.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2001-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: check.c,v 1.37.6.30 2005/05/27 00:51:45 marka Exp $ */
+/* $Id: check.c,v 1.37.6.31 2005/05/30 01:59:25 marka Exp $ */
 
 #include <config.h>
 
