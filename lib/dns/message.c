2fa0485e9e969dd42dd10339354d7949db46111a
Mark Andrews <marka@isc.org>
2005-06-07 01:42:29 +0000

    1861.   [bug]           dig could trigger a INSIST on certain malformed                         responses. [RT #14801]

--- lib/dns/message.c
+++ lib/dns/message.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: message.c,v 1.226 2005/04/27 04:56:48 sra Exp $ */
+/* $Id: message.c,v 1.227 2005/06/07 01:42:29 marka Exp $ */
 
 /*! \file */
 
@@ -1478,6 +1478,13 @@ getsection(isc_buffer_t *source, dns_message_t *msg, dns_decompress_t *dctx,
 			free_name = ISC_FALSE;
 		}
 
+		if (seen_problem) {
+			if (free_name)
+				isc_mempool_put(msg->namepool, name);
+			if (free_rdataset)
+				isc_mempool_put(msg->rdspool, rdataset);
+			free_name = free_rdataset = ISC_FALSE;
+		}
 		INSIST(free_name == ISC_FALSE);
 		INSIST(free_rdataset == ISC_FALSE);
 	}
