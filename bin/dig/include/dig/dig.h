5ebe1a379a7e89b6be9bb64a504d5a695070c884
Mark Andrews <marka@isc.org>
2005-06-19 22:47:27 +0000

    remove #define ROOTNS 1

--- bin/dig/include/dig/dig.h
+++ bin/dig/include/dig/dig.h
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: dig.h,v 1.71.2.6.2.9 2005/03/31 23:56:14 marka Exp $ */
+/* $Id: dig.h,v 1.71.2.6.2.10 2005/06/19 22:47:27 marka Exp $ */
 
 #ifndef DIG_H
 #define DIG_H
@@ -64,14 +64,6 @@
  * Lookup_limit is just a limiter, keeping too many lookups from being
  * created.  It's job is mainly to prevent the program from running away
  * in a tight loop of constant lookups.  It's value is arbitrary.
- */
-
-#define ROOTNS 1
-/*
- * Set the number of root servers to ask for information when running in
- * trace mode.
- * XXXMWS -- trace mode is currently semi-broken, and this number *MUST*
- * be 1.
  */
 
 /*
