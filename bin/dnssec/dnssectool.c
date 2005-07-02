022f6372bcd0c6b12c281cf05cfb5f09f5981bb6
Mark Andrews <marka@isc.org>
2005-07-02 02:42:29 +0000

    update copyright notice

--- bin/dnssec/dnssectool.c
+++ bin/dnssec/dnssectool.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000, 2001, 2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: dnssectool.c,v 1.31.2.6 2005/07/01 03:55:03 marka Exp $ */
+/* $Id: dnssectool.c,v 1.31.2.7 2005/07/02 02:42:29 marka Exp $ */
 
 #include <config.h>
 
