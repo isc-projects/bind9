d2debaa4944a4ecb86813738f695c331f6df949c
Mark Andrews <marka@isc.org>
2005-07-04 23:14:21 +0000

    1895.   [bug]           fctx_create() could return success even though it                         failed. [RT #14993]

--- lib/dns/resolver.c
+++ lib/dns/resolver.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: resolver.c,v 1.284.18.30 2005/06/27 00:20:00 marka Exp $ */
+/* $Id: resolver.c,v 1.284.18.31 2005/07/04 23:14:21 marka Exp $ */
 
 /*! \file */
 
@@ -2698,7 +2698,7 @@ fctx_create(dns_resolver_t *res, dns_name_t *name, dns_rdatatype_t type,
 	    unsigned int options, unsigned int bucketnum, fetchctx_t **fctxp)
 {
 	fetchctx_t *fctx;
-	isc_result_t result = ISC_R_SUCCESS;
+	isc_result_t result;
 	isc_result_t iresult;
 	isc_interval_t interval;
 	dns_fixedname_t fixed;
@@ -2720,8 +2720,10 @@ fctx_create(dns_resolver_t *res, dns_name_t *name, dns_rdatatype_t type,
 	strcat(buf, "/");	/* checked */
 	strcat(buf, typebuf);	/* checked */
 	fctx->info = isc_mem_strdup(res->buckets[bucketnum].mctx, buf);
-	if (fctx->info == NULL)
+	if (fctx->info == NULL) {
+		result = ISC_R_NOMEMORY;
 		goto cleanup_fetch;
+	}
 	FCTXTRACE("create");
 	dns_name_init(&fctx->name, NULL);
 	result = dns_name_dup(name, res->buckets[bucketnum].mctx, &fctx->name);
