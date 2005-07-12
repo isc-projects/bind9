da76da34cd338727672d07919ae530cb0e46083d
Mark Andrews <marka@isc.org>
2005-07-12 05:47:28 +0000

    update copyright notice

--- bin/dig/nslookup.c
+++ bin/dig/nslookup.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: nslookup.c,v 1.101.18.6 2005/07/04 03:10:59 marka Exp $ */
+/* $Id: nslookup.c,v 1.101.18.7 2005/07/12 05:47:28 marka Exp $ */
 
 #include <config.h>
 
