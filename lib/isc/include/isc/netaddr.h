c5de31cbc92a6159730cc67cdb0519865e78bea5
Mark Andrews <marka@isc.org>
2005-07-28 04:58:27 +0000

    1907.   [bug]           Extend ISC_SOCKADDR_FORMATSIZE and                         ISC_NETADDR_FORMATSIZE to allow for scope details.

--- lib/isc/include/isc/netaddr.h
+++ lib/isc/include/isc/netaddr.h
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: netaddr.h,v 1.18.12.7 2004/03/08 09:04:52 marka Exp $ */
+/* $Id: netaddr.h,v 1.18.12.8 2005/07/28 04:58:27 marka Exp $ */
 
 #ifndef ISC_NETADDR_H
 #define ISC_NETADDR_H 1
@@ -81,7 +81,7 @@ isc_netaddr_format(const isc_netaddr_t *na, char *array, unsigned int size);
  */
 
 #define ISC_NETADDR_FORMATSIZE \
-	sizeof("xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:XXX.XXX.XXX.XXX")
+	sizeof("xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:XXX.XXX.XXX.XXX%SSSSSSSSSS")
 /*
  * Minimum size of array to pass to isc_netaddr_format().
  */
