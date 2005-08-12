5009e2f53d42295d22c0ddf0d289000a6ff38e58
Mark Andrews <marka@isc.org>
2005-08-12 01:37:02 +0000

    silence compiler warning

--- lib/bind/nameser/ns_parse.c
+++ lib/bind/nameser/ns_parse.c
@@ -16,7 +16,7 @@
  */
 
 #ifndef lint
-static const char rcsid[] = "$Id: ns_parse.c,v 1.3.2.2 2004/03/09 09:17:37 marka Exp $";
+static const char rcsid[] = "$Id: ns_parse.c,v 1.3.2.3 2005/08/12 01:37:02 marka Exp $";
 #endif
 
 /* Import. */
@@ -40,7 +40,12 @@ static void	setsection(ns_msg *msg, ns_sect sect);
 
 /* Macros. */
 
+#ifndef SOLARIS2
 #define RETERR(err) do { errno = (err); return (-1); } while (0)
+#else
+#define RETERR(err) \
+	do { errno = (err); if (errno == errno) return (-1); } while (0)
+#endif
 
 /* Public. */
 
