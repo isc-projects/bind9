e6b2687c1233bb434850255fe38cf1adb277e0fd
Mark Andrews <marka@isc.org>
2005-08-25 00:21:01 +0000

    1915.   [func]          dig now has a '-q queryname' and '+showsearch' options.                         [RT #15034]

--- bin/dig/dighost.c
+++ bin/dig/dighost.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: dighost.c,v 1.259.18.20 2005/07/04 03:10:58 marka Exp $ */
+/* $Id: dighost.c,v 1.259.18.21 2005/08/25 00:20:59 marka Exp $ */
 
 /*! \file
  *  \note
@@ -102,6 +102,7 @@ isc_boolean_t
 	free_now = ISC_FALSE,
 	cancel_now = ISC_FALSE,
 	usesearch = ISC_FALSE,
+	showsearch = ISC_FALSE,
 	qr = ISC_FALSE,
 	is_dst_up = ISC_FALSE;
 in_port_t port = 53;
@@ -2886,7 +2887,7 @@ recv_done(isc_task_t *task, isc_event_t *event) {
 
 	if (!l->doing_xfr || l->xfr_q == query) {
 		if (msg->rcode != dns_rcode_noerror && l->origin != NULL) {
-			if (!next_origin(msg, query)) {
+			if (!next_origin(msg, query) || showsearch) {
 				printmessage(query, msg, ISC_TRUE);
 				received(b->used, &sevent->address, query);
 			}
