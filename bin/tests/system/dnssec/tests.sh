675d69697741f03ed95e3dbfd7bf4b20efb299df
Mark Andrews <marka@isc.org>
2005-09-06 03:51:37 +0000

    update copyright notice

--- bin/tests/system/dnssec/tests.sh
+++ bin/tests/system/dnssec/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2000-2002  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: tests.sh,v 1.46 2005/08/25 00:56:07 marka Exp $
+# $Id: tests.sh,v 1.47 2005/09/06 03:51:34 marka Exp $
 
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
--- contrib/sdb/pgsql/zonetodb.c
+++ contrib/sdb/pgsql/zonetodb.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000, 2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: zonetodb.c,v 1.14 2005/08/16 04:22:37 marka Exp $ */
+/* $Id: zonetodb.c,v 1.15 2005/09/06 03:51:35 marka Exp $ */
 
 #include <stdlib.h>
 #include <string.h>
