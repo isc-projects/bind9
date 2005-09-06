885ccd6dfb6a16b0298c86386c04e36456642900
Mark Andrews <marka@isc.org>
2005-09-06 07:29:33 +0000

    update copyright notice

--- lib/dns/rdata/generic/ds_43.c
+++ lib/dns/rdata/generic/ds_43.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: ds_43.c,v 1.7.18.1 2005/09/06 06:48:39 marka Exp $ */
+/* $Id: ds_43.c,v 1.7.18.2 2005/09/06 07:29:32 marka Exp $ */
 
 /* draft-ietf-dnsext-delegation-signer-05.txt */
 
