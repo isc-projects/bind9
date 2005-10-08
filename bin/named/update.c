5f5a0806d178f1056aa9866b551311ef1f4f702d
Mark Andrews <marka@isc.org>
2005-10-08 00:20:53 +0000

    update copyright notice

--- bin/named/update.c
+++ bin/named/update.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: update.c,v 1.88.2.16 2005/10/07 04:21:38 marka Exp $ */
+/* $Id: update.c,v 1.88.2.17 2005/10/08 00:20:53 marka Exp $ */
 
 #include <config.h>
 
