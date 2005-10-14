454d71306d13eac24b4528d9eb3df7fe6b1df403
Mark Andrews <marka@isc.org>
2005-10-14 05:44:12 +0000

    silence compiler

--- lib/bind/resolv/res_mkupdate.c
+++ lib/bind/resolv/res_mkupdate.c
@@ -22,7 +22,7 @@
  */
 
 #if !defined(lint) && !defined(SABER)
-static const char rcsid[] = "$Id: res_mkupdate.c,v 1.4.18.3 2005/10/11 00:25:12 marka Exp $";
+static const char rcsid[] = "$Id: res_mkupdate.c,v 1.4.18.4 2005/10/14 05:44:12 marka Exp $";
 #endif /* not lint */
 
 #include "port_before.h"
@@ -925,10 +925,10 @@ res_mkupdrec(int section, const char *dname,
 	}
 	INIT_LINK(rrecp, r_link);
 	INIT_LINK(rrecp, r_glink);
- 	rrecp->r_class = class;
-	rrecp->r_type = type;
+ 	rrecp->r_class = (ns_class)class;
+	rrecp->r_type = (ns_type)type;
 	rrecp->r_ttl = ttl;
-	rrecp->r_section = section;
+	rrecp->r_section = (ns_sect)section;
 	return (rrecp);
 }
 
