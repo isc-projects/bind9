e6d667397a6a8a0e34b981a5df91d011b1fa2a38
Mark Andrews <marka@isc.org>
2005-11-03 00:02:56 +0000

    update copyright notice

--- bin/tests/system/xfer/tests.sh
+++ bin/tests/system/xfer/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2000, 2001  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: tests.sh,v 1.28 2005/11/02 04:56:49 marka Exp $
+# $Id: tests.sh,v 1.29 2005/11/03 00:02:56 marka Exp $
 
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
