edb9d49ef8f2d31eb8a805b1e8671adddfdb2f57
Mark Andrews <marka@isc.org>
2006-01-04 03:08:42 +0000

    1948.   [bug]           If was possible to trigger a REQUIRE failure in                         xfrin.c:maybe_free() if named ran out of memory.                         [RT #15568]

--- lib/dns/xfrin.c
+++ lib/dns/xfrin.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: xfrin.c,v 1.124.2.10 2005/11/03 23:41:22 marka Exp $ */
+/* $Id: xfrin.c,v 1.124.2.11 2006/01/04 03:08:42 marka Exp $ */
 
 #include <config.h>
 
@@ -789,7 +789,18 @@ xfrin_create(isc_mem_t *mctx,
 	return (ISC_R_SUCCESS);
 
  failure:
-	xfrin_fail(xfr, result, "failed creating transfer context");
+	if (xfr->timer != NULL)
+		isc_timer_detach(&xfr->timer);
+	if (dns_name_dynamic(&xfr->name))
+		dns_name_free(&xfr->name, xfr->mctx);
+	if (xfr->tsigkey != NULL)
+		dns_tsigkey_detach(&xfr->tsigkey);
+	if (xfr->db != NULL)
+		dns_db_detach(&xfr->db);
+	isc_task_detach(&xfr->task);
+	dns_zone_idetach(&xfr->zone);
+	isc_mem_put(mctx, xfr, sizeof(*xfr));
+
 	return (result);
 }
 
