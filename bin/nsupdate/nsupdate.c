54dd0df9c91c5aaaa129ed9232ff63efa30d5d5a
Mark Andrews <marka@isc.org>
2006-01-06 04:50:14 +0000

    1970.   [bug]           nsupdate: adjust UDP timeout when falling back to                         unsigned SOA query. [RT #15775]

--- bin/nsupdate/nsupdate.c
+++ bin/nsupdate/nsupdate.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: nsupdate.c,v 1.103.2.15.2.20 2005/03/17 03:58:26 marka Exp $ */
+/* $Id: nsupdate.c,v 1.103.2.15.2.21 2006/01/06 04:50:14 marka Exp $ */
 
 #include <config.h>
 
@@ -1665,7 +1665,7 @@ recvsoa(isc_task_t *task, isc_event_t *event) {
 		result = dns_request_createvia3(requestmgr, soaquery,
 						localaddr, addr, 0, NULL,
 						FIND_TIMEOUT * 20,
-						FIND_TIMEOUT * 20, 3,
+						FIND_TIMEOUT, 3,
 						global_task, recvsoa, reqinfo,
 						&request);
 		check_result(result, "dns_request_createvia");
