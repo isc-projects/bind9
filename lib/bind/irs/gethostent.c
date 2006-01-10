279c537985bbf4eb7ed1ca55167692c6b6926a47
Mark Andrews <marka@isc.org>
2006-01-10 05:10:24 +0000

    1971.   [port]          linux: make detection of missing IF_NAMESIZE more                         robust. [RT #15443]

--- lib/bind/irs/gethostent.c
+++ lib/bind/irs/gethostent.c
@@ -16,7 +16,7 @@
  */
 
 #if !defined(LINT) && !defined(CODECENTER)
-static const char rcsid[] = "$Id: gethostent.c,v 1.1.2.4 2004/03/17 01:54:20 marka Exp $";
+static const char rcsid[] = "$Id: gethostent.c,v 1.1.2.5 2006/01/10 05:10:24 marka Exp $";
 #endif
 
 /* Imports */
@@ -608,7 +608,7 @@ scan_interfaces6(int *have_v4, int *have_v6) {
 }
 #endif
 
-#ifdef __linux
+#if ( defined(__linux__) || defined(__linux) || defined(LINUX) )
 #ifndef IF_NAMESIZE
 # ifdef IFNAMSIZ
 #  define IF_NAMESIZE  IFNAMSIZ
