70b15d3957a9db82a53321140f15b7b9692d3bfb
Mark Andrews <marka@isc.org>
2006-01-10 23:50:41 +0000

    update copyright notice

--- lib/dns/name.c
+++ lib/dns/name.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1998-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: name.c,v 1.144.18.13 2006/01/10 03:03:26 marka Exp $ */
+/* $Id: name.c,v 1.144.18.14 2006/01/10 23:50:41 marka Exp $ */
 
 /*! \file */
 
