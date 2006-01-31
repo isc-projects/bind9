fc7685a7f1f0ab4764c0e51e86edd7c35424ae5d
Mark Andrews <marka@isc.org>
2006-01-31 00:35:21 +0000

    silence compiler warnings

--- lib/isc/include/isc/md5.h
+++ lib/isc/include/isc/md5.h
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: md5.h,v 1.11 2005/04/29 00:23:40 marka Exp $ */
+/* $Id: md5.h,v 1.12 2006/01/31 00:35:21 marka Exp $ */
 
 /*! \file 
  * \brief This is the header file for the MD5 message-digest algorithm.
@@ -46,7 +46,7 @@
 #include <isc/lang.h>
 #include <isc/types.h>
 
-#define ISC_MD5_DIGESTLENGTH 16
+#define ISC_MD5_DIGESTLENGTH 16U
 
 typedef struct {
 	isc_uint32_t buf[4];
