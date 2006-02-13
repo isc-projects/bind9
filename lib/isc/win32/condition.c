317cc1a488123aa101d514f54078f7238aec2cf5
Mark Andrews <marka@isc.org>
2006-02-13 23:50:51 +0000

    update copyright notice

--- lib/isc/win32/condition.c
+++ lib/isc/win32/condition.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2006  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1998-2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: condition.c,v 1.17.2.2 2006/02/13 03:41:50 marka Exp $ */
+/* $Id: condition.c,v 1.17.2.3 2006/02/13 23:50:51 marka Exp $ */
 
 #include <config.h>
 
