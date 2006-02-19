0cfc2b930ce8b1fd2d7bb25e00bbfcc45a92d9a8
Mark Andrews <marka@isc.org>
2006-02-19 06:50:48 +0000

    update copyright notice

--- lib/isccfg/include/isccfg/grammar.h
+++ lib/isccfg/include/isccfg/grammar.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2002, 2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: grammar.h,v 1.10 2006/02/17 00:24:21 marka Exp $ */
+/* $Id: grammar.h,v 1.11 2006/02/19 06:50:48 marka Exp $ */
 
 #ifndef ISCCFG_GRAMMAR_H
 #define ISCCFG_GRAMMAR_H 1
--- lib/isccfg/parser.c
+++ lib/isccfg/parser.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: parser.c,v 1.121 2006/02/17 00:24:21 marka Exp $ */
+/* $Id: parser.c,v 1.122 2006/02/19 06:50:48 marka Exp $ */
 
 /*! \file */
 
