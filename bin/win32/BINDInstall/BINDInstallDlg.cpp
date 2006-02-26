6c4435e3eec4421c0e6b3250cf97db65f788b7d9
Mark Andrews <marka@isc.org>
2006-02-26 22:28:38 +0000

    update copyright notice

--- bin/win32/BINDInstall/BINDInstallDlg.cpp
+++ bin/win32/BINDInstall/BINDInstallDlg.cpp
@@ -1,5 +1,5 @@
 /*
- * Portions Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
+ * Portions Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
  * Portions Copyright (C) 2001, 2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: BINDInstallDlg.cpp,v 1.22 2006/02/24 03:47:22 marka Exp $ */
+/* $Id: BINDInstallDlg.cpp,v 1.23 2006/02/26 22:28:38 marka Exp $ */
 
 /*
  * Copyright (c) 1999-2000 by Nortel Networks Corporation
