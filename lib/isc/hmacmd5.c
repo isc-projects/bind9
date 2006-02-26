e86581466652132f069eae8d1c5427d59e50602d
Mark Andrews <marka@isc.org>
2006-02-26 22:34:05 +0000

    remove redundant memset

--- lib/isc/hmacmd5.c
+++ lib/isc/hmacmd5.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: hmacmd5.c,v 1.5.2.1 2004/03/09 06:11:46 marka Exp $ */
+/* $Id: hmacmd5.c,v 1.5.2.2 2006/02/26 22:34:05 marka Exp $ */
 
 /*
  * This code implements the HMAC-MD5 keyed hash algorithm
@@ -65,7 +65,6 @@ void
 isc_hmacmd5_invalidate(isc_hmacmd5_t *ctx) {
 	isc_md5_invalidate(&ctx->md5ctx);
 	memset(ctx->key, 0, sizeof (ctx->key));
-	memset(ctx, 0, sizeof (ctx));
 }
 
 /*
