619448102d0cad325e76ea446d73c3a430dcdaa9
Mark Andrews <marka@isc.org>
2006-03-01 02:49:40 +0000

    1994.   [port]          OpenSSL 0.9.8 support. [RT #15694]

--- lib/dns/opensslrsa_link.c
+++ lib/dns/opensslrsa_link.c
@@ -17,7 +17,7 @@
 
 /*
  * Principal Author: Brian Wellington
- * $Id: opensslrsa_link.c,v 1.1.2.3 2006/01/04 23:50:17 marka Exp $
+ * $Id: opensslrsa_link.c,v 1.1.2.4 2006/03/01 02:48:37 marka Exp $
  */
 #ifdef OPENSSL
 
@@ -204,6 +204,94 @@ opensslrsa_compare(const dst_key_t *key1, const dst_key_t *key2) {
 	}
 	return (ISC_TRUE);
 }
+
+#ifndef HAVE_RSA_GENERATE_KEY
+/* ====================================================================
+ * Copyright (c) 1998-2002 The OpenSSL Project.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this
+ *    software must display the following acknowledgment:
+ *    "This product includes software developed by the OpenSSL Project
+ *    for use in the OpenSSL Toolkit. (http://www.openssl.org/)"
+ *
+ * 4. The names "OpenSSL Toolkit" and "OpenSSL Project" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission. For written permission, please contact
+ *    openssl-core@openssl.org.
+ *
+ * 5. Products derived from this software may not be called "OpenSSL"
+ *    nor may "OpenSSL" appear in their names without prior written
+ *    permission of the OpenSSL Project.
+ *
+ * 6. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the OpenSSL Project
+ *    for use in the OpenSSL Toolkit (http://www.openssl.org/)"
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE OpenSSL PROJECT ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OpenSSL PROJECT OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This product includes cryptographic software written by Eric Young
+ * (eay@cryptsoft.com).  This product includes software written by Tim
+ * Hudson (tjh@cryptsoft.com).
+ *
+ */
+static RSA *
+RSA_generate_key(int bits, unsigned long e_value,
+		 void (*callback)(int,int,void *), void *cb_arg)
+{
+        BN_GENCB cb;
+        size_t i;
+        RSA *rsa = RSA_new();
+        BIGNUM *e = BN_new();
+
+        if (rsa == NULL || e == NULL)
+		goto err;
+
+        /* The problem is when building with 8, 16, or 32 BN_ULONG,
+         * unsigned long can be larger */
+        for (i = 0; i < sizeof(unsigned long) * 8; i++) {
+                if ((e_value & (1UL<<i)) != 0)
+                        BN_set_bit(e, i);
+	}
+
+        BN_GENCB_set_old(&cb, callback, cb_arg);
+
+        if (RSA_generate_key_ex(rsa, bits, e, &cb)) {
+                BN_free(e);
+                return (rsa);
+        }
+err:
+        if (e != NULL)
+		BN_free(e);
+        if (rsa != NULL)
+		RSA_free(rsa);
+        return (NULL);
+}
+#endif
 
 static isc_result_t
 opensslrsa_generate(dst_key_t *key, int exp) {
