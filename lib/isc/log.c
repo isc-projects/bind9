bcb8cea06c83a9652e4482c9981b1ab0c3375409
Mark Andrews <marka@isc.org>
2006-03-01 02:25:35 +0000

    1993.   [bug]           Log messsage, via syslog, were missing the space                         after the timestamp if "print-time yes" was specified.                         [RT #15844]

--- lib/isc/log.c
+++ lib/isc/log.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: log.c,v 1.70.2.8.2.12 2004/06/11 00:35:38 marka Exp $ */
+/* $Id: log.c,v 1.70.2.8.2.13 2006/03/01 02:25:35 marka Exp $ */
 
 /* Principal Authors: DCL */
 
@@ -1728,8 +1728,9 @@ isc_log_doit(isc_log_t *lctx, isc_logcategory_t *category,
 				syslog_level = syslog_map[-level];
 
 			(void)syslog(FACILITY(channel) | syslog_level,
-			       "%s%s%s%s%s%s%s%s%s",
+			       "%s%s%s%s%s%s%s%s%s%s",
 			       printtime     ? time_string	: "",
+			       printtime     ? " "		: "",
 			       printtag      ? lcfg->tag	: "",
 			       printtag      ? ": "		: "",
 			       printcategory ? category->name	: "",
