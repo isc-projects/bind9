fb9395c8dd2267c2dfded51dd29c80135bb11f8d
Mark Andrews <marka@isc.org>
2006-03-02 23:48:50 +0000

    update copyright notice

--- bin/dig/host.c
+++ bin/dig/host.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: host.c,v 1.107 2006/03/02 01:45:46 marka Exp $ */
+/* $Id: host.c,v 1.108 2006/03/02 23:48:50 marka Exp $ */
 
 /*! \file */
 
