069a2ad007cdd9c898ec827dd26eade4c4b4cdea
Mark Andrews <marka@isc.org>
2006-04-03 00:00:42 +0000

    update copyright notice

--- lib/isc/print.c
+++ lib/isc/print.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2001, 2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: print.c,v 1.30 2006/03/21 15:34:02 explorer Exp $ */
+/* $Id: print.c,v 1.31 2006/04/03 00:00:42 marka Exp $ */
 
 /*! \file */
 
