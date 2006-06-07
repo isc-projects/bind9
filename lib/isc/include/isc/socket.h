03f65be27b368dc846e99f604c4368fd0c20b83e
Mark Andrews <marka@isc.org>
2006-06-07 00:29:46 +0000

    update copyright notice

--- lib/isc/include/isc/socket.h
+++ lib/isc/include/isc/socket.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1998-2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: socket.h,v 1.64 2006/06/06 00:53:36 marka Exp $ */
+/* $Id: socket.h,v 1.65 2006/06/07 00:29:46 marka Exp $ */
 
 #ifndef ISC_SOCKET_H
 #define ISC_SOCKET_H 1
