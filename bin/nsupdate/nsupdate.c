bdecdef309cb5ce97c826c97514cec76754f2c1b
Mark Andrews <marka@isc.org>
2006-06-09 23:50:52 +0000

    update copyright notice

--- bin/nsupdate/nsupdate.c
+++ bin/nsupdate/nsupdate.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: nsupdate.c,v 1.103.2.27 2006/06/09 07:28:36 marka Exp $ */
+/* $Id: nsupdate.c,v 1.103.2.28 2006/06/09 23:50:52 marka Exp $ */
 
 #include <config.h>
 
