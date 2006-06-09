c7efca8420205a70134dd65823886c41165a59d5
Mark Andrews <marka@isc.org>
2006-06-09 00:54:09 +0000

    update copyright notice

--- bin/named/log.c
+++ bin/named/log.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: log.c,v 1.42 2006/06/07 02:28:28 marka Exp $ */
+/* $Id: log.c,v 1.43 2006/06/09 00:54:09 marka Exp $ */
 
 /*! \file */
 
