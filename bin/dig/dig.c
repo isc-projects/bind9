2b67af24fa02867fbc703e89277400b10d0c8b92
Mark Andrews <marka@isc.org>
2006-07-21 23:50:15 +0000

    2056.   [bug]           dig: ixfr= was not being treated case insensitively                         at all times. [RT #15955]

--- bin/dig/dig.c
+++ bin/dig/dig.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: dig.c,v 1.212 2006/05/15 06:10:58 marka Exp $ */
+/* $Id: dig.c,v 1.213 2006/07/21 23:50:15 marka Exp $ */
 
 /*! \file */
 
@@ -1571,7 +1571,7 @@ parse_args(isc_boolean_t is_batchfile, isc_boolean_t config_only,
 			 * Anything which isn't an option
 			 */
 			if (open_type_class) {
-				if (strncmp(rv[0], "ixfr=", 5) == 0) {
+				if (strncasecmp(rv[0], "ixfr=", 5) == 0) {
 					rdtype = dns_rdatatype_ixfr;
 					result = ISC_R_SUCCESS;
 				} else {
