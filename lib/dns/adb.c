1195c1452c56200da8dc737e2464c9a5a421c3a4
Mark Andrews <marka@isc.org>
2006-08-30 23:49:58 +0000

    update copyright notice

--- lib/dns/adb.c
+++ lib/dns/adb.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: adb.c,v 1.227 2006/08/30 23:09:18 marka Exp $ */
+/* $Id: adb.c,v 1.228 2006/08/30 23:49:58 marka Exp $ */
 
 /*! \file 
  *
