1cd62e9b855dd1d3b692e8ada4f2fcbdd1670bdb
Mark Andrews <marka@isc.org>
2006-09-27 00:29:46 +0000

    2083.   [port]          win32: Visual C++ 2005 support.

--- bin/win32/BINDInstall/BINDInstallDlg.cpp
+++ bin/win32/BINDInstall/BINDInstallDlg.cpp
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: BINDInstallDlg.cpp,v 1.15.18.8 2006/02/26 22:28:37 marka Exp $ */
+/* $Id: BINDInstallDlg.cpp,v 1.15.18.9 2006/09/27 00:29:46 marka Exp $ */
 
 /*
  * Copyright (c) 1999-2000 by Nortel Networks Corporation
@@ -113,7 +113,10 @@ const FileData installFiles[] =
 	{"msvcrt.dll", FileData::WinSystem, FileData::Critical, TRUE},
 #  endif
 #endif
-#if _MSC_VER >= 1310
+#if _MSC_VER >= 1400
+	{"mfc80.dll", FileData::WinSystem, FileData::Critical, TRUE},
+	{"msvcr80.dll", FileData::WinSystem, FileData::Critical, TRUE},
+#elif _MSC_VER >= 1310
 	{"mfc71.dll", FileData::WinSystem, FileData::Critical, TRUE},
 	{"msvcr71.dll", FileData::WinSystem, FileData::Critical, TRUE},
 #elif _MSC_VER > 1200
