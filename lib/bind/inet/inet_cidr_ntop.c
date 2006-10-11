38a648041df3a874a9f072e97deb94e3c6ba9ea9
Mark Andrews <marka@isc.org>
2006-10-11 02:32:34 +0000

    2095.   [port]          libbind: alway prototype inet_cidr_ntop_ipv6() and                         net_cidr_ntop_ipv6(). [RT #16388]

--- lib/bind/inet/inet_cidr_ntop.c
+++ lib/bind/inet/inet_cidr_ntop.c
@@ -16,7 +16,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static const char rcsid[] = "$Id: inet_cidr_ntop.c,v 1.1.2.4 2005/11/03 23:41:22 marka Exp $";
+static const char rcsid[] = "$Id: inet_cidr_ntop.c,v 1.1.2.5 2006/10/11 02:32:34 marka Exp $";
 #endif
 
 #include "port_before.h"
@@ -40,10 +40,10 @@ static const char rcsid[] = "$Id: inet_cidr_ntop.c,v 1.1.2.4 2005/11/03 23:41:22
 # define SPRINTF(x) ((size_t)sprintf x)
 #endif
 
-static char *	inet_cidr_ntop_ipv4 __P((const u_char *src, int bits,
-					 char *dst, size_t size));
-static char *	inet_cidr_ntop_ipv6 __P((const u_char *src, int bits,
-					 char *dst, size_t size));
+static char *
+inet_cidr_ntop_ipv4(const u_char *src, int bits, char *dst, size_t size);
+static char *
+inet_cidr_ntop_ipv6(const u_char *src, int bits, char *dst, size_t size);
 
 /*
  * char *
