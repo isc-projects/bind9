adc7f34bdc662085d5ca89c801b8832351847d6a
Mark Andrews <marka@isc.org>
2006-10-11 03:59:42 +0000

    WIN32 not WIN

--- lib/dns/opensslrsa_link.c
+++ lib/dns/opensslrsa_link.c
@@ -17,7 +17,7 @@
 
 /*
  * Principal Author: Brian Wellington
- * $Id: opensslrsa_link.c,v 1.1.4.7 2006/10/11 02:21:15 marka Exp $
+ * $Id: opensslrsa_link.c,v 1.1.4.8 2006/10/11 03:59:42 marka Exp $
  */
 #ifdef OPENSSL
 
@@ -47,7 +47,7 @@
  * We don't use configure for windows so enforce the OpenSSL version
  * here.  Unlike with configure we don't support overriding this test.
  */
-#ifdef WIN
+#ifdef WIN32
 #if !((OPENSSL_VERSION_NUMBER >= 0x009070cfL && \
        OPENSSL_VERSION_NUMBER < 0x009080000L) || \
       OPENSSL_VERSION_NUMBER >= 0x0090804fL) 
