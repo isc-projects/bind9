2a5600ad8b7630d3affaf36036abfb459b6ef6bd
Mark Andrews <marka@isc.org>
2007-01-08 02:35:58 +0000

    prime_len -> key->key_size

--- lib/dns/openssldh_link.c
+++ lib/dns/openssldh_link.c
@@ -18,7 +18,7 @@
 
 /*
  * Principal Author: Brian Wellington
- * $Id: openssldh_link.c,v 1.8 2007/01/08 01:52:27 marka Exp $
+ * $Id: openssldh_link.c,v 1.9 2007/01/08 02:35:58 marka Exp $
  */
 
 #ifdef OPENSSL
@@ -158,8 +158,7 @@ openssldh_generate(dst_key_t *key, int generator) {
 			else
 				dh->p = &bn1536;
 			dh->g = &bn2;
-		}
-		else
+		} else
 			generator = 2;
 	}
 
@@ -171,7 +170,8 @@ openssldh_generate(dst_key_t *key, int generator) {
 
 		BN_GENCB_set_old(&cb, NULL, NULL);
 
-		if (!DH_generate_parameters_ex(dh, prime_len, generator, &cb)) {
+		if (!DH_generate_parameters_ex(dh, key->key_size, generator,
+					       &cb)) {
 			DH_free(dh);
 			return (dst__openssl_toresult(DST_R_OPENSSLFAILURE));
 		}
