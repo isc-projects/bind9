c66cf786842d886e8127fb758aef36ef21f43ccf
Mark Andrews <marka@isc.org>
2007-02-01 23:55:19 +0000

    update copyright notice

--- lib/isc/win32/socket.c
+++ lib/isc/win32/socket.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: socket.c,v 1.5.2.13.2.20 2007/02/01 04:42:29 marka Exp $ */
+/* $Id: socket.c,v 1.5.2.13.2.21 2007/02/01 23:55:19 marka Exp $ */
 
 /* This code has been rewritten to take advantage of Windows Sockets
  * I/O Completion Ports and Events. I/O Completion Ports is ONLY
