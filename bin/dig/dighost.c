1e70c9a36639bb1e8d537dcefa3cd12a7bb39db0
Mark Andrews <marka@isc.org>
2007-02-14 01:19:52 +0000

    2136.   [bug]           nslookup/host looped if there was no search list                         and the host didn't exist. [RT #16657]

--- bin/dig/dighost.c
+++ bin/dig/dighost.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: dighost.c,v 1.297 2006/12/07 05:52:15 marka Exp $ */
+/* $Id: dighost.c,v 1.298 2007/02/14 01:19:52 marka Exp $ */
 
 /*! \file
  *  \note
@@ -1635,7 +1635,7 @@ next_origin(dns_message_t *msg, dig_query_t *query) {
 	if (query->lookup->origin == NULL && query->lookup->need_search) {
 		lookup = requeue_lookup(query->lookup, ISC_TRUE);
 		lookup->origin = ISC_LIST_HEAD(search_list);
-		query->lookup->need_search = ISC_FALSE;
+		lookup->need_search = ISC_FALSE;
 	} else {
 		search = ISC_LIST_NEXT(query->lookup->origin, link);
 		if (search == NULL && query->lookup->done_as_is)
