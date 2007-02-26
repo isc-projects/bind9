46217fcd14b7984e0e1f09814a5a33fb0ddc7ee7
Mark Andrews <marka@isc.org>
2007-02-26 00:05:34 +0000

    2140.   [bug]           libbind: missing unlock on pthread_key_create()                         failures. [RT #16654]

--- lib/bind/irs/irs_data.c
+++ lib/bind/irs/irs_data.c
@@ -16,7 +16,7 @@
  */
 
 #if !defined(LINT) && !defined(CODECENTER)
-static const char rcsid[] = "$Id: irs_data.c,v 1.7.18.3 2006/03/10 00:20:08 marka Exp $";
+static const char rcsid[] = "$Id: irs_data.c,v 1.7.18.4 2007/02/26 00:05:34 marka Exp $";
 #endif
 
 #include "port_before.h"
@@ -132,8 +132,10 @@ net_data_init(const char *conf_file) {
 		if (pthread_mutex_lock(&keylock) != 0)
 			return (NULL);
 		if (!once) {
-			if (pthread_key_create(&key, net_data_destroy) != 0)
+			if (pthread_key_create(&key, net_data_destroy) != 0) {
+				pthread_mutex_unlock(&keylock);
 				return (NULL);
+			}
 			once = 1;
 		}
 		if (pthread_mutex_unlock(&keylock) != 0)
