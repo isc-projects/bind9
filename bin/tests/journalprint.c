3ee5ba2a22b47b7840ae4bf8b92837a13e05eff4
Automatic Updater <source@isc.org>
2007-02-27 23:46:48 +0000

    update copyright notice

--- bin/tests/journalprint.c
+++ bin/tests/journalprint.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000, 2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: journalprint.c,v 1.10 2007/02/27 00:39:37 marka Exp $ */
+/* $Id: journalprint.c,v 1.11 2007/02/27 23:46:48 tbox Exp $ */
 
 /*! \file */
 #include <config.h>
