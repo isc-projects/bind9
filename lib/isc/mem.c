acb6a99ccb1aa588140853e515fa61542fda77d9
Mark Andrews <marka@isc.org>
2007-02-27 03:31:04 +0000

    temorarially comment out INSIST

--- lib/isc/mem.c
+++ lib/isc/mem.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: mem.c,v 1.132 2007/02/26 22:57:03 marka Exp $ */
+/* $Id: mem.c,v 1.133 2007/02/27 03:31:04 marka Exp $ */
 
 /*! \file */
 
@@ -1948,7 +1948,7 @@ isc_mem_checkdestroyed(FILE *file) {
 		}
 		fflush(file);
 #endif
-		INSIST(0);
+		/* INSIST(0); XXXMPA temorarially comment out. */
 	}
 	UNLOCK(&lock);
 }
