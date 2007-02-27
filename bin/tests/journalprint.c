02750677aff7922b978c3c9095bf0f5c17d2e131
Mark Andrews <marka@isc.org>
2007-02-27 00:54:22 +0000

    2151.   [bug]           Missing newline in usage message for journalprint.                         [RT #16679]

--- bin/tests/journalprint.c
+++ bin/tests/journalprint.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: journalprint.c,v 1.3.2.5 2006/08/25 05:25:49 marka Exp $ */
+/* $Id: journalprint.c,v 1.3.2.6 2007/02/27 00:54:22 marka Exp $ */
 
 #include <config.h>
 
@@ -33,7 +33,7 @@ main(int argc, char **argv) {
 	isc_mem_t *mctx = NULL;
 
 	if (argc != 2) {
-		printf("usage: %s journal", argv[0]);
+		printf("usage: %s journal\n", argv[0]);
 		return(1);
 	}
 
