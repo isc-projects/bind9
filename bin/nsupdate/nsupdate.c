45fd3ed42b9ac99a2dd1df35945244b8a6c5b966
Automatic Updater <source@isc.org>
2007-03-01 23:46:42 +0000

    update copyright notice

--- bin/nsupdate/nsupdate.c
+++ bin/nsupdate/nsupdate.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: nsupdate.c,v 1.147 2007/02/28 23:57:39 marka Exp $ */
+/* $Id: nsupdate.c,v 1.148 2007/03/01 23:46:42 tbox Exp $ */
 
 /*! \file */
 
