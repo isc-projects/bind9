c0a659b3532d69fbbceebf71d4554779f27c2b33
Automatic Updater <source@isc.org>
2007-03-06 23:45:54 +0000

    update copyright notice

--- bin/tests/system/start.pl
+++ bin/tests/system/start.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 #
-# Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2001  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: start.pl,v 1.3.206.2 2007/03/06 07:05:35 marka Exp $
+# $Id: start.pl,v 1.3.206.3 2007/03/06 23:45:54 tbox Exp $
 
 # Framework for starting test servers.
 # Based on the type of server specified, check for port availability, remove
