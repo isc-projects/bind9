184d77489af7d0483c3fc6af4705a2a17453c5f6
Automatic Updater <source@isc.org>
2007-03-30 23:46:45 +0000

    update copyright notice

--- bin/dig/dig.c
+++ bin/dig/dig.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: dig.c,v 1.214 2007/03/30 03:46:13 each Exp $ */
+/* $Id: dig.c,v 1.215 2007/03/30 23:46:45 tbox Exp $ */
 
 /*! \file */
 
