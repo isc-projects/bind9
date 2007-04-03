c31d980b95df096dfe5073565fbd42dadcaa6402
Automatic Updater <source@isc.org>
2007-04-03 23:42:54 +0000

    update copyright notice

--- bin/named/server.c
+++ bin/named/server.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: server.c,v 1.339.2.39 2007/04/03 00:09:00 marka Exp $ */
+/* $Id: server.c,v 1.339.2.40 2007/04/03 23:42:54 tbox Exp $ */
 
 #include <config.h>
 
