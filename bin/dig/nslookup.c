c6a98f2a4bc3ec0c723adab7f493d24b505ff570
Automatic Updater <source@isc.org>
2007-04-24 23:46:56 +0000

    update copyright notice

--- bin/dig/nslookup.c
+++ bin/dig/nslookup.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: nslookup.c,v 1.115 2007/04/24 07:20:45 marka Exp $ */
+/* $Id: nslookup.c,v 1.116 2007/04/24 23:46:56 tbox Exp $ */
 
 #include <config.h>
 
