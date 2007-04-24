5de9c4469089d1e9481e5e7942ffb34c7a5492c4
Mark Andrews <marka@isc.org>
2007-04-24 07:13:28 +0000

    2168.   [bug]           nsupdate: in non-interactive mode treat syntax errors                         as fatal errors. [RT #16785]

--- bin/nsupdate/nsupdate.c
+++ bin/nsupdate/nsupdate.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: nsupdate.c,v 1.103.2.15.2.24 2006/12/07 05:39:26 marka Exp $ */
+/* $Id: nsupdate.c,v 1.103.2.15.2.25 2007/04/24 07:13:28 marka Exp $ */
 
 #include <config.h>
 
@@ -1409,8 +1409,11 @@ user_interaction(void) {
 	isc_uint16_t result = STATUS_MORE;
 
 	ddebug("user_interaction()");
-	while ((result == STATUS_MORE) || (result == STATUS_SYNTAX))
+	while ((result == STATUS_MORE) || (result == STATUS_SYNTAX)) {
 		result = get_next_command();
+		if (!interactive && result == STATUS_SYNTAX)
+			fatal("syntax error");
+	}
 	if (result == STATUS_SEND)
 		return (ISC_TRUE);
 	return (ISC_FALSE);
