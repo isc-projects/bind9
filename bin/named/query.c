9fd12ce000abacebbde71cbe661d72f5ff73a0a8
Automatic Updater <source@isc.org>
2007-04-30 23:45:58 +0000

    update copyright notice

--- bin/named/query.c
+++ bin/named/query.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: query.c,v 1.198.2.13.4.46 2007/04/30 03:05:50 marka Exp $ */
+/* $Id: query.c,v 1.198.2.13.4.47 2007/04/30 23:45:58 tbox Exp $ */
 
 #include <config.h>
 
