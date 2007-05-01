33e50736247d2dcab04c478eac72e063e186131b
Automatic Updater <source@isc.org>
2007-05-01 23:45:25 +0000

    update copyright notice

--- bin/win32/BINDInstall/BINDInstallDlg.cpp
+++ bin/win32/BINDInstall/BINDInstallDlg.cpp
@@ -1,5 +1,5 @@
 /*
- * Portions Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
+ * Portions Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
  * Portions Copyright (C) 2001-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: BINDInstallDlg.cpp,v 1.6.2.18 2007/05/01 03:26:08 marka Exp $ */
+/* $Id: BINDInstallDlg.cpp,v 1.6.2.19 2007/05/01 23:45:25 tbox Exp $ */
 
 /*
  * Copyright (c) 1999-2000 by Nortel Networks Corporation
