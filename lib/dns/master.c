bb4e20398f44ff53b4d61377021ab4d89821aca4
Automatic Updater <source@isc.org>
2007-05-02 23:46:54 +0000

    update copyright notice

--- lib/dns/master.c
+++ lib/dns/master.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: master.c,v 1.162 2007/05/02 04:06:24 marka Exp $ */
+/* $Id: master.c,v 1.163 2007/05/02 23:46:54 tbox Exp $ */
 
 /*! \file */
 
