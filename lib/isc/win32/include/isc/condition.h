c3ac3a7ce13a0d611bf27dcee153aaf7f87a72b3
Mark Andrews <marka@isc.org>
2007-05-09 23:51:44 +0000

    2175.   [bug]           win32: windows broadcast condition variable support                         was broken. [RT #16592]

--- lib/isc/win32/include/isc/condition.h
+++ lib/isc/win32/include/isc/condition.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1998-2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: condition.h,v 1.14 2004/03/05 05:12:05 marka Exp $ */
+/* $Id: condition.h,v 1.14.18.1 2007/05/09 23:51:44 marka Exp $ */
 
 #ifndef ISC_CONDITION_H
 #define ISC_CONDITION_H 1
@@ -24,11 +24,22 @@
 
 #include <isc/lang.h>
 #include <isc/mutex.h>
+#include <isc/thread.h>
 #include <isc/types.h>
+
+typedef struct isc_condition_thread isc_condition_thread_t;
+
+struct isc_condition_thread {
+	unsigned long				th;
+	HANDLE					handle[2];
+	ISC_LINK(isc_condition_thread_t)	link;
+
+};
 
 typedef struct isc_condition {
 	HANDLE 		events[2];
 	unsigned int	waiters;
+	ISC_LIST(isc_condition_thread_t) threadlist;
 } isc_condition_t;
 
 ISC_LANG_BEGINDECLS
