e170d165058e9ed1c785def0110d7ffaa4fef1ae
Mark Andrews <marka@isc.org>
2007-05-15 02:19:25 +0000

    2177.   [bug]           Array bounds overrun on read (rcodetext). [RT #16798]

--- lib/dns/message.c
+++ lib/dns/message.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: message.c,v 1.194.2.20 2006/03/01 01:34:05 marka Exp $ */
+/* $Id: message.c,v 1.194.2.21 2007/05/15 02:19:25 marka Exp $ */
 
 /***
  *** Imports
@@ -2939,8 +2939,7 @@ dns_message_sectiontotext(dns_message_t *msg, dns_section_t section,
 		ADD_STRING(target, ";; ");
 		if (msg->opcode != dns_opcode_update) {
 			ADD_STRING(target, sectiontext[section]);
-		}
-		else {
+		} else {
 			ADD_STRING(target, updsectiontext[section]);
 		}
 		ADD_STRING(target, " SECTION:\n");
@@ -3062,7 +3061,12 @@ dns_message_totext(dns_message_t *msg, const dns_master_style_t *style,
 		ADD_STRING(target, ";; ->>HEADER<<- opcode: ");
 		ADD_STRING(target, opcodetext[msg->opcode]);
 		ADD_STRING(target, ", status: ");
-		ADD_STRING(target, rcodetext[msg->rcode]);
+		if (msg->rcode < (sizeof(rcodetext)/sizeof(rcodetext[0]))) {
+			ADD_STRING(target, rcodetext[msg->rcode]);
+		} else {
+			snprintf(buf, sizeof(buf), "%4u", msg->rcode);
+			ADD_STRING(target, buf);
+		}
 		ADD_STRING(target, ", id: ");
 		sprintf(buf, "%6u", msg->id);
 		ADD_STRING(target, buf);
