404bc0354a7eb5ae24c50b581c4911d9a2e7bede
Mark Andrews <marka@isc.org>
2007-05-18 05:34:02 +0000

    2182.   [bug]           dns_dispatch_createtcp() and dispatch_createudp()                         could return ISC_R_SUCCESS when they ran out of                         memory. [RT #16365]

--- lib/dns/dispatch.c
+++ lib/dns/dispatch.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: dispatch.c,v 1.101.2.6.2.15 2007/02/07 23:57:57 marka Exp $ */
+/* $Id: dispatch.c,v 1.101.2.6.2.16 2007/05/18 05:34:02 marka Exp $ */
 
 #include <config.h>
 
@@ -1618,8 +1618,10 @@ dns_dispatch_createtcp(dns_dispatchmgr_t *mgr, isc_socket_t *sock,
 					    DNS_EVENT_DISPATCHCONTROL,
 					    destroy_disp, disp,
 					    sizeof(isc_event_t));
-	if (disp->ctlevent == NULL)
+	if (disp->ctlevent == NULL) {
+		result = ISC_R_NOMEMORY;
 		goto kill_task;
+	}
 
 	isc_task_setname(disp->task, "tcpdispatch", disp);
 
@@ -1800,8 +1802,10 @@ dispatch_createudp(dns_dispatchmgr_t *mgr, isc_socketmgr_t *sockmgr,
 					    DNS_EVENT_DISPATCHCONTROL,
 					    destroy_disp, disp,
 					    sizeof(isc_event_t));
-	if (disp->ctlevent == NULL)
+	if (disp->ctlevent == NULL) {
+		result = ISC_R_NOMEMORY;
 		goto kill_task;
+	}
 
 	isc_task_setname(disp->task, "udpdispatch", disp);
 
