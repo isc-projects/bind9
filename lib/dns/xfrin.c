02a7eada71e12331f7adfc2fabc310456fb1dfe3
Mark Andrews <marka@isc.org>
2007-05-24 02:57:06 +0000

    2194.   [bug]           Close journal before calling 'done' in xfrin.c.

--- lib/dns/xfrin.c
+++ lib/dns/xfrin.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: xfrin.c,v 1.124.2.4.2.18 2007/05/24 01:56:31 tbox Exp $ */
+/* $Id: xfrin.c,v 1.124.2.4.2.19 2007/05/24 02:57:06 marka Exp $ */
 
 #include <config.h>
 
@@ -717,6 +717,11 @@ xfrin_fail(dns_xfrin_ctx_t *xfr, isc_result_t result, const char *msg) {
 			result = DNS_R_BADIXFR;
 	}
 	xfrin_cancelio(xfr);
+	/*
+	 * Close the journal.
+	 */
+	if (xfr->ixfr.journal != NULL)
+		dns_journal_destroy(&xfr->ixfr.journal);
 	if (xfr->done != NULL) {
 		(xfr->done)(xfr->zone, result);
 		xfr->done = NULL;
