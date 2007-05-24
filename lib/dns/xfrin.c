9f4a452a3d0ee0091df1d07973deb59a5bf938ee
Automatic Updater <source@isc.org>
2007-05-24 01:56:31 +0000

    update copyright notice

--- lib/dns/xfrin.c
+++ lib/dns/xfrin.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: xfrin.c,v 1.124.2.4.2.17 2007/05/24 01:50:48 marka Exp $ */
+/* $Id: xfrin.c,v 1.124.2.4.2.18 2007/05/24 01:56:31 tbox Exp $ */
 
 #include <config.h>
 
