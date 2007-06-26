c123a39e88a43d71988787ddbefcc9ea2846f32a
Mark Andrews <marka@isc.org>
2007-06-26 06:09:08 +0000

    replace gettimeofday with isc_time_now

--- lib/dns/dispatch.c
+++ lib/dns/dispatch.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: dispatch.c,v 1.101.2.6.2.13.6.2 2007/06/26 04:56:17 marka Exp $ */
+/* $Id: dispatch.c,v 1.101.2.6.2.13.6.3 2007/06/26 06:09:08 marka Exp $ */
 
 #include <config.h>
 
@@ -29,6 +29,7 @@
 #include <isc/print.h>
 #include <isc/string.h>
 #include <isc/task.h>
+#include <isc/time.h>
 #include <isc/util.h>
 
 #include <dns/acl.h>
@@ -2562,12 +2563,12 @@ nsid_next(dns_nsid_t *nsid) {
 
 static isc_result_t
 nsid_init(isc_mem_t *mctx, dns_nsid_t *nsid, isc_boolean_t usepool) {
-        struct timeval now;
+        isc_time_t now;
         pid_t mypid;
         isc_uint16_t a1ndx, a2ndx, a3ndx, c1ndx, c2ndx, c3ndx;
         int i;
 
-        gettimeofday(&now, NULL);
+	isc_time_now(&now);
         mypid = getpid();
 
         /* Initialize the state */
