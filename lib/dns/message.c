1fcfbb18ef1864b60a58fde4ce18831aebc27a74
Mark Andrews <marka@isc.org>
2007-08-14 00:32:13 +0000

    2210.   [bug]           Deleting class specific records via UPDATE could                         fail.  [RT #17074]

--- lib/dns/message.c
+++ lib/dns/message.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: message.c,v 1.194.2.22 2007/05/15 23:45:26 tbox Exp $ */
+/* $Id: message.c,v 1.194.2.23 2007/08/14 00:32:13 marka Exp $ */
 
 /***
  *** Imports
@@ -1305,6 +1305,11 @@ getsection(isc_buffer_t *source, dns_message_t *msg, dns_decompress_t *dctx,
 			rdata->type = rdtype;
 			rdata->flags = DNS_RDATA_UPDATE;
 			result = ISC_R_SUCCESS;
+		} else if (rdclass == dns_rdataclass_none &&
+			   msg->opcode == dns_opcode_update &&
+			   sectionid == DNS_SECTION_UPDATE) {
+			result = getrdata(source, msg, dctx, msg->rdclass,
+					  rdtype, rdatalen, rdata);
 		} else
 			result = getrdata(source, msg, dctx, rdclass,
 					  rdtype, rdatalen, rdata);
