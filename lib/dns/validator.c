b5ded8a160cbf86f6e1ed6af37960e86267aa98f
Mark Andrews <marka@isc.org>
2007-08-27 04:47:14 +0000

    2218.   [bug]           Remove unnecessary REQUIRE from dns_validator_create().                         [RT #16976]

--- lib/dns/validator.c
+++ lib/dns/validator.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: validator.c,v 1.119.18.31 2007/04/27 06:37:38 marka Exp $ */
+/* $Id: validator.c,v 1.119.18.32 2007/08/27 04:47:14 marka Exp $ */
 
 /*! \file */
 
@@ -2819,7 +2819,6 @@ dns_validator_create(dns_view_t *view, dns_name_t *name, dns_rdatatype_t type,
 	dns_validatorevent_t *event;
 
 	REQUIRE(name != NULL);
-	REQUIRE(type != 0);
 	REQUIRE(rdataset != NULL ||
 		(rdataset == NULL && sigrdataset == NULL && message != NULL));
 	REQUIRE(validatorp != NULL && *validatorp == NULL);
