4a14be8faf3849abe2425c9b63a244bea2adad2b
Automatic Updater <source@isc.org>
2007-08-31 23:46:25 +0000

    update copyright notice

--- lib/isc/unix/ifiter_ioctl.c
+++ lib/isc/unix/ifiter_ioctl.c
@@ -1,8 +1,8 @@
 /*
- * Copyright (C) 2004-2006  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
- * Permission to use, copy, modify, and distribute this software for any
+ * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
  *
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: ifiter_ioctl.c,v 1.44.18.12 2007/08/31 03:44:52 marka Exp $ */
+/* $Id: ifiter_ioctl.c,v 1.44.18.13 2007/08/31 23:46:25 tbox Exp $ */
 
 /*! \file
  * \brief
