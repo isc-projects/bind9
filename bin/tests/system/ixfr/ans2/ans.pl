433604d76aca844cc63a59c964184d5a5ef92b37
Automatic Updater <source@isc.org>
2007-09-24 23:45:58 +0000

    update copyright notice

--- bin/tests/system/ixfr/ans2/ans.pl
+++ bin/tests/system/ixfr/ans2/ans.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 #
-# Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2001  Internet Software Consortium.
 #
-# Permission to use, copy, modify, and distribute this software for any
+# Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
 # copyright notice and this permission notice appear in all copies.
 #
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: ans.pl,v 1.2.206.2 2007/09/24 04:16:48 marka Exp $
+# $Id: ans.pl,v 1.2.206.3 2007/09/24 23:45:57 tbox Exp $
 
 #
 # This is the name server from hell.  It provides canned
--- doc/misc/format-options.pl
+++ doc/misc/format-options.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 #
-# Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2001  Internet Software Consortium.
 #
-# Permission to use, copy, modify, and distribute this software for any
+# Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
 # copyright notice and this permission notice appear in all copies.
 #
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: format-options.pl,v 1.1.206.2 2007/09/24 04:24:54 marka Exp $
+# $Id: format-options.pl,v 1.1.206.3 2007/09/24 23:45:58 tbox Exp $
 
 print <<END;
 
