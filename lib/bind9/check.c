520ccb71a62fe219f8fd558fe672f66449e8d60a
Mark Andrews <marka@isc.org>
2007-11-26 01:59:40 +0000

    2262.   [bug]           Error status from all but the last view could be                         lost. [RT #17292]

--- lib/bind9/check.c
+++ lib/bind9/check.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: check.c,v 1.44.18.35 2007/09/13 05:04:01 each Exp $ */
+/* $Id: check.c,v 1.44.18.36 2007/11/26 01:59:40 marka Exp $ */
 
 /*! \file */
 
@@ -1919,7 +1919,7 @@ bind9_check_namedconf(const cfg_obj_t *config, isc_log_t *logctx,
 					    "previous definition: %s:%u",
 					    key, file, line);
 				result = tresult;
-			} else if (result != ISC_R_SUCCESS) {
+			} else if (tresult != ISC_R_SUCCESS) {
 				result = tresult;
 			} else if ((strcasecmp(key, "_bind") == 0 &&
 				    vclass == dns_rdataclass_ch) ||
