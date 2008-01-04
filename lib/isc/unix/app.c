8a805888e9c39b305e11219d1d305eec2b7f3570
Automatic Updater <source@isc.org>
2008-01-04 23:46:04 +0000

    update copyright notice

--- lib/isc/unix/app.c
+++ lib/isc/unix/app.c
@@ -1,8 +1,8 @@
 /*
- * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2008  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
- * Permission to use, copy, modify, and distribute this software for any
+ * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
  *
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: app.c,v 1.50.18.3 2008/01/04 03:35:50 marka Exp $ */
+/* $Id: app.c,v 1.50.18.4 2008/01/04 23:46:04 tbox Exp $ */
 
 /*! \file */
 
