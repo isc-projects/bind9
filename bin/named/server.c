0a61938a98d0c7ced85ff9f7559cbbbb391fc5bf
Mark Andrews <marka@isc.org>
2008-01-09 04:52:56 +0000

    2292.   [bug]           Log if the working directory is not writable.                         [RT #17312]
    2291.   [bug]           PR_SET_DUMPABLE may be set too late.  Also report
                            failure to set PR_SET_DUMPABLE. [RT #17312]

--- bin/named/server.c
+++ bin/named/server.c
@@ -15,13 +15,14 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: server.c,v 1.419.18.59 2007/12/02 18:58:20 marka Exp $ */
+/* $Id: server.c,v 1.419.18.60 2008/01/09 04:52:56 marka Exp $ */
 
 /*! \file */
 
 #include <config.h>
 
 #include <stdlib.h>
+#include <unistd.h>
 
 #include <isc/app.h>
 #include <isc/base64.h>
@@ -2755,6 +2756,15 @@ load_configuration(const char *filename, ns_server_t *server,
 						    &config);
 	}
 	CHECK(result);
+
+	/*
+	 * Check that the working directory is writable.
+	 */
+	if (access(".", W_OK) != 0) {
+		isc_log_write(ns_g_lctx, NS_LOGCATEGORY_GENERAL,
+			      NS_LOGMODULE_SERVER, ISC_LOG_ERROR,
+			      "the working directory is not writable");
+	}
 
 	/*
 	 * Check the validity of the configuration.
