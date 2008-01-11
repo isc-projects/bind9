395f6d6ee7375908caf1aa9ea854da9ec3629a60
Automatic Updater <source@isc.org>
2008-01-11 23:46:27 +0000

    update copyright notice

--- bin/named/lwaddr.c
+++ bin/named/lwaddr.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005, 2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2007, 2008  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000, 2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: lwaddr.c,v 1.8.130.1 2008/01/11 03:04:03 marka Exp $ */
+/* $Id: lwaddr.c,v 1.8.130.2 2008/01/11 23:46:27 tbox Exp $ */
 
 /*! \file */
 
