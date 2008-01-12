494143860bbe118050f46ecac3d196c779d4b7b0
Mark Andrews <marka@isc.org>
2008-01-12 21:58:25 +0000

    2298.   [bug]           isc_mutex_lock() failure not caught in                         bin/tests/timers/t_timers.c. [RT #17468]

--- bin/tests/timers/t_timers.c
+++ bin/tests/timers/t_timers.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: t_timers.c,v 1.26 2007/06/19 23:47:07 tbox Exp $ */
+/* $Id: t_timers.c,v 1.27 2008/01/12 21:58:25 marka Exp $ */
 
 #include <config.h>
 
@@ -256,7 +256,7 @@ t_timers_x(isc_timertype_t timertype, isc_time_t *expires,
 		return;
 	}
 
-	isc_mutex_lock(&Tx_mx);
+	isc_result = isc_mutex_lock(&Tx_mx);
 	if (isc_result != ISC_R_SUCCESS) {
 		t_info("isc_mutex_lock failed %s\n",
 		       isc_result_totext(isc_result));
