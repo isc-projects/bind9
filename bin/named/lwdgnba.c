33288720bd1550af811d62701a0849ddfda12dbb
Mark Andrews <marka@isc.org>
2008-01-14 13:06:01 +0000

    2303.   [bug]           Remove unnecessary code from bin/named/lwdgnba.c.                         [RT #17471]

--- bin/named/lwdgnba.c
+++ bin/named/lwdgnba.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: lwdgnba.c,v 1.16.18.2 2005/04/29 00:15:24 marka Exp $ */
+/* $Id: lwdgnba.c,v 1.16.18.3 2008/01/14 13:06:01 marka Exp $ */
 
 /*! \file */
 
@@ -219,8 +219,6 @@ ns_lwdclient_processgnba(ns_lwdclient_t *client, lwres_buffer_t *b) {
 	result = lwres_gnbarequest_parse(cm->lwctx,
 					 b, &client->pkt, &req);
 	if (result != LWRES_R_SUCCESS)
-		goto out;
-	if (req->addr.address == NULL)
 		goto out;
 
 	client->options = 0;
