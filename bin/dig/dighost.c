fce2de524a90e419d18d0357030fc478012ecf4e
Automatic Updater <source@isc.org>
2008-01-15 23:45:32 +0000

    update copyright notice

--- bin/dig/dighost.c
+++ bin/dig/dighost.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2008  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: dighost.c,v 1.221.2.19.2.43 2008/01/15 01:13:03 marka Exp $ */
+/* $Id: dighost.c,v 1.221.2.19.2.44 2008/01/15 23:45:32 tbox Exp $ */
 
 /*
  * Notice to programmers:  Do not use this code as an example of how to
--- lib/dns/master.c
+++ lib/dns/master.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2006, 2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2006-2008  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: master.c,v 1.122.2.8.2.22 2008/01/15 01:13:03 marka Exp $ */
+/* $Id: master.c,v 1.122.2.8.2.23 2008/01/15 23:45:32 tbox Exp $ */
 
 #include <config.h>
 
