4602a309289f1033b27446cd9c8242a36ab37b77
Automatic Updater <source@isc.org>
2008-01-15 23:46:32 +0000

    update copyright notice

--- lib/dns/master.c
+++ lib/dns/master.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2008  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: master.c,v 1.166.94.1 2008/01/15 01:13:17 marka Exp $ */
+/* $Id: master.c,v 1.166.94.2 2008/01/15 23:46:32 tbox Exp $ */
 
 /*! \file */
 
--- lib/dns/validator.c
+++ lib/dns/validator.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2008  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: validator.c,v 1.155.52.1 2008/01/15 01:13:17 marka Exp $ */
+/* $Id: validator.c,v 1.155.52.2 2008/01/15 23:46:32 tbox Exp $ */
 
 #include <config.h>
 
