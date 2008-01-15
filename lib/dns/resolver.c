2b0addd3cad11aae34830883f11ffb616ce0f7f8
Automatic Updater <source@isc.org>
2008-01-15 23:46:59 +0000

    update copyright notice

--- lib/dns/resolver.c
+++ lib/dns/resolver.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2008  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: resolver.c,v 1.357 2008/01/15 12:12:19 marka Exp $ */
+/* $Id: resolver.c,v 1.358 2008/01/15 23:46:59 tbox Exp $ */
 
 /*! \file */
 
