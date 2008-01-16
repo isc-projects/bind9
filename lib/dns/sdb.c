5788284cb88eb2f49dccab735420140a0773eeac
Automatic Updater <source@isc.org>
2008-01-16 23:45:55 +0000

    update copyright notice

--- lib/dns/sdb.c
+++ lib/dns/sdb.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2008  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000, 2001, 2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: sdb.c,v 1.45.18.14 2008/01/16 21:58:11 marka Exp $ */
+/* $Id: sdb.c,v 1.45.18.15 2008/01/16 23:45:55 tbox Exp $ */
 
 /*! \file */
 
