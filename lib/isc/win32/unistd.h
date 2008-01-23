438763e4ca6626b2d3779d4bc8dbdb8646631768
Evan Hunt <each@isc.org>
2008-01-23 02:32:17 +0000

    Fixes needed to compile on windows

--- lib/isc/win32/unistd.h
+++ lib/isc/win32/unistd.h
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: unistd.h,v 1.6 2007/06/19 23:47:19 tbox Exp $ */
+/* $Id: unistd.h,v 1.6.128.1 2008/01/23 02:32:17 each Exp $ */
 
 /* None of these are defined in NT, so define them for our use */
 #define O_NONBLOCK 1
@@ -34,5 +34,20 @@
 #undef F_DUPFD 
 
 int fcntl(int, int, ...);
+
+/*
+ * access() related definitions for winXP
+ */
+#include <io.h>
+
+#ifndef W_OK
+#define W_OK 2
+#endif
+
+#ifndef R_OK
+#define R_OK 4
+#endif
+
+#define access _access
 
 #include <process.h>
