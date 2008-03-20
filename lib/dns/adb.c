1a76f45e1c9ca59aba7d80e7bdb717176a67ed0e
Automatic Updater <source@isc.org>
2008-03-20 23:45:32 +0000

    update copyright notice

--- lib/dns/adb.c
+++ lib/dns/adb.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2008  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: adb.c,v 1.181.2.11.2.31 2008/03/20 22:45:17 jinmei Exp $ */
+/* $Id: adb.c,v 1.181.2.11.2.32 2008/03/20 23:45:32 tbox Exp $ */
 
 /*
  * Implementation notes
