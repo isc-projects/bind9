7405bdffd4d0e17b43775a29b07d1a819ea22188
Automatic Updater <source@isc.org>
2008-03-29 23:47:08 +0000

    update copyright notice

--- lib/bind9/check.c
+++ lib/bind9/check.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: check.c,v 1.88 2008/03/28 23:47:02 tbox Exp $ */
+/* $Id: check.c,v 1.89 2008/03/29 23:47:08 tbox Exp $ */
 
 /*! \file */
 
@@ -1872,7 +1872,7 @@ bind9_check_logging(const cfg_obj_t *config, isc_log_t *logctx,
 			channel = cfg_listelt_value(delement);
 			channelname = cfg_obj_asstring(channel);
 			tresult = isc_symtab_lookup(symtab, channelname, 1,
-					  	    &symvalue);
+						    &symvalue);
 			if (tresult != ISC_R_SUCCESS) {
 				cfg_obj_log(channel, logctx, ISC_LOG_ERROR,
 					    "undefined channel: '%s'",
