07555e64d9102eae058efd58f872b4a3b9ddff61
Mark Andrews <marka@isc.org>
2008-04-02 02:56:23 +0000

    2350.   [port]          win32: IPv6 support. [RT #17797]

--- lib/dns/resolver.c
+++ lib/dns/resolver.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: resolver.c,v 1.362 2008/03/31 05:00:30 marka Exp $ */
+/* $Id: resolver.c,v 1.363 2008/04/02 02:56:23 marka Exp $ */
 
 /*! \file */
 
@@ -2271,6 +2271,13 @@ fctx_getaddresses(fetchctx_t *fctx) {
 	}
 
 	while (sa != NULL) {
+		if ((isc_sockaddr_pf(sa) == AF_INET &&
+			 fctx->res->dispatchv4 == NULL) ||
+		    (isc_sockaddr_pf(sa) == AF_INET6 &&
+			fctx->res->dispatchv6 == NULL)) {
+				sa = ISC_LIST_NEXT(sa, link);
+				continue;
+		}
 		ai = NULL;
 		result = dns_adb_findaddrinfo(fctx->adb,
 					      sa, &ai, 0);  /* XXXMLG */
