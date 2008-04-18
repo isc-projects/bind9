4fcd03af9ffd55c7ecb508048fb789f123465b25
Evan Hunt <each@isc.org>
2008-04-18 19:47:48 +0000

    Missing type in internal_next6() function declaration

--- lib/isc/win32/interfaceiter.c
+++ lib/isc/win32/interfaceiter.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: interfaceiter.c,v 1.12 2008/04/02 23:46:57 tbox Exp $ */
+/* $Id: interfaceiter.c,v 1.13 2008/04/18 19:47:48 each Exp $ */
 
 /*
  * Note that this code will need to be revisited to support IPv6 Interfaces.
@@ -429,6 +429,7 @@ internal_next(isc_interfaceiter_t *iter) {
 	return (ISC_R_SUCCESS);
 }
 
+static isc_result_t
 internal_next6(isc_interfaceiter_t *iter) {
 	if (iter->pos6 == 0)
 		return (ISC_R_NOMORE);
