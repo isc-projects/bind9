efe34b8ddbecf45d1671efbcba30bdb75410c98a
Mark Andrews <marka@isc.org>
2008-07-18 01:26:20 +0000

    2389    [bug]           Move the working directory writable to after the                         ns_os_changeuser() call. [RT #18326]

--- bin/named/server.c
+++ bin/named/server.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: server.c,v 1.512 2008/07/12 05:58:08 jinmei Exp $ */
+/* $Id: server.c,v 1.513 2008/07/18 01:26:20 marka Exp $ */
 
 /*! \file */
 
@@ -2966,15 +2966,6 @@ load_configuration(const char *filename, ns_server_t *server,
 	CHECK(result);
 
 	/*
-	 * Check that the working directory is writable.
-	 */
-	if (access(".", W_OK) != 0) {
-		isc_log_write(ns_g_lctx, NS_LOGCATEGORY_GENERAL,
-			      NS_LOGMODULE_SERVER, ISC_LOG_ERROR,
-			      "the working directory is not writable");
-	}
-
-	/*
 	 * Check the validity of the configuration.
 	 */
 	CHECK(bind9_check_namedconf(config, ns_g_lctx, ns_g_mctx));
@@ -3410,6 +3401,15 @@ load_configuration(const char *filename, ns_server_t *server,
 	 */
 	if (first_time)
 		ns_os_changeuser();
+
+	/*
+	 * Check that the working directory is writable.
+	 */
+	if (access(".", W_OK) != 0) {
+		isc_log_write(ns_g_lctx, NS_LOGCATEGORY_GENERAL,
+			      NS_LOGMODULE_SERVER, ISC_LOG_ERROR,
+			      "the working directory is not writable");
+	}
 
 	/*
 	 * Configure the logging system.
