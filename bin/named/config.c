db07eedf7670bc2ad2673111a932fe26136e4d26
Francis Dupont <fdupont@isc.org>
2008-07-20 09:41:06 +0000

    "files unlimited" in code [rt18331]

--- bin/named/config.c
+++ bin/named/config.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: config.c,v 1.82.38.5 2008/05/28 21:00:24 each Exp $ */
+/* $Id: config.c,v 1.82.38.6 2008/07/20 09:41:06 fdupont Exp $ */
 
 /*! \file */
 
@@ -52,7 +52,7 @@ options {\n\
 #ifndef WIN32
 "	coresize default;\n\
 	datasize default;\n\
-	files default;\n\
+	files unlimited;\n\
 	stacksize default;\n"
 #endif
 "	deallocate-on-exit true;\n\
