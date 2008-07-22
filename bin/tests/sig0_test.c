1d16cf8bb8596c3e4dc1123a5bdf360bf24a272b
Automatic Updater <source@isc.org>
2008-07-22 23:47:04 +0000

    update copyright notice

--- bin/tests/sig0_test.c
+++ bin/tests/sig0_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005, 2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2007, 2008  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000, 2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: sig0_test.c,v 1.16 2008/07/22 03:43:04 marka Exp $ */
+/* $Id: sig0_test.c,v 1.17 2008/07/22 23:47:04 tbox Exp $ */
 
 #include <config.h>
 
--- bin/tests/sock_test.c
+++ bin/tests/sock_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2007, 2008  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1998-2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: sock_test.c,v 1.53 2008/07/22 03:43:04 marka Exp $ */
+/* $Id: sock_test.c,v 1.54 2008/07/22 23:47:04 tbox Exp $ */
 
 #include <config.h>
 
