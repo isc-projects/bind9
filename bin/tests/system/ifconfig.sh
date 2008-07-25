40cf3a243eccbbb4a7ac3e1a97a20520beb68154
Automatic Updater <source@isc.org>
2008-07-25 23:47:45 +0000

    update copyright notice

--- bin/tests/system/ifconfig.sh
+++ bin/tests/system/ifconfig.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-# Copyright (C) 2004  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2008  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2000-2003  Internet Software Consortium.
 #
-# Permission to use, copy, modify, and distribute this software for any
+# Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
 # copyright notice and this permission notice appear in all copies.
 #
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: ifconfig.sh,v 1.35.2.8.2.8.24.1 2008/07/25 21:03:12 fdupont Exp $
+# $Id: ifconfig.sh,v 1.35.2.8.2.8.24.2 2008/07/25 23:47:45 tbox Exp $
 
 #
 # Set up interface aliases for bind9 system tests.
