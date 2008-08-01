ca65030581b87ec1e9134b97b46209c541013d9e
Automatic Updater <source@isc.org>
2008-08-01 23:45:29 +0000

    update copyright notice

--- lib/isc/win32/resource.c
+++ lib/isc/win32/resource.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2007, 2008  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000, 2001, 2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: resource.c,v 1.2.12.7 2008/08/01 02:12:46 jinmei Exp $ */
+/* $Id: resource.c,v 1.2.12.8 2008/08/01 23:45:29 tbox Exp $ */
 
 #include <config.h>
 
