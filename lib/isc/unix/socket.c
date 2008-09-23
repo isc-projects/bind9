6a858014fbbd461c2f286120f0e42a03521f248c
Mark Andrews <marka@isc.org>
2008-09-23 02:30:57 +0000

    2444.   [port]          Linux, FreeBSD, AIX: Turn off path mtu discovery                         (clear DF) for UDP responses and requests.

--- lib/isc/unix/socket.c
+++ lib/isc/unix/socket.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: socket.c,v 1.275.10.26 2008/09/04 07:58:07 marka Exp $ */
+/* $Id: socket.c,v 1.275.10.27 2008/09/23 02:30:57 marka Exp $ */
 
 /*! \file */
 
@@ -2030,6 +2030,27 @@ opensocket(isc_socketmgr_t *manager, isc_socket_t *sock) {
 #endif
 #endif /* ISC_PLATFORM_HAVEIPV6 */
 #endif /* defined(USE_CMSG) */
+
+#if defined(IP_MTU_DISCOVER) && defined(IP_PMTUDISC_DONT)
+		/*
+		 * Turn off Path MTU discovery on IPv4/UDP sockets.
+		 */
+		if (sock->pf == AF_INET) {
+			int action = IP_PMTUDISC_DONT;
+			(void)setsockopt(sock->fd, IPPROTO_IP, IP_MTU_DISCOVER,
+					 &action, sizeof(action));
+		}
+#endif
+#if defined(IP_DONTFRAG)
+		/*
+		 * Turn off Path MTU discovery on IPv4/UDP sockets.
+		 */
+		if (sock->pf == AF_INET) {
+			int off = 0;
+			(void)setsockopt(sock->fd, IPPROTO_IP, IP_DONTFRAG,
+					 &off, sizeof(off));
+		}
+#endif
 
 #if defined(SO_RCVBUF)
 		optlen = sizeof(size);
