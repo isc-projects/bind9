a418129ad0288a6fc58d7842a5b21d0653038e19
Mark Andrews <marka@isc.org>
2008-09-25 04:15:41 +0000

    0 -> dns_rdatatype_none

--- lib/dns/xfrin.c
+++ lib/dns/xfrin.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: xfrin.c,v 1.157.12.6 2008/09/25 03:54:04 marka Exp $ */
+/* $Id: xfrin.c,v 1.157.12.7 2008/09/25 04:15:41 marka Exp $ */
 
 /*! \file */
 
@@ -433,7 +433,8 @@ xfr_rr(dns_xfrin_ctx_t *xfr, dns_name_t *name, isc_uint32_t ttl,
 
 	xfr->nrecs++;
 
-	if (rdata->type == 0 || dns_rdatatype_ismeta(rdata->type))
+	if (rdata->type == dns_rdatatype_none ||
+	    dns_rdatatype_ismeta(rdata->type))
 		FAIL(DNS_R_FORMERR);
 
  redo:
