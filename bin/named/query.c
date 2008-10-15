24a56e15f7fd09c7ec3b22c380fb04891e23dec4
Mark Andrews <marka@isc.org>
2008-10-15 02:37:11 +0000

    silence compiler warning

--- bin/named/query.c
+++ bin/named/query.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: query.c,v 1.311 2008/10/02 20:35:39 marka Exp $ */
+/* $Id: query.c,v 1.312 2008/10/15 02:37:11 marka Exp $ */
 
 /*! \file */
 
@@ -3693,6 +3693,7 @@ query_find(ns_client_t *client, dns_fetchevent_t *event, dns_rdatatype_t qtype)
 	empty_wild = ISC_FALSE;
 	options = 0;
 	resuming = ISC_FALSE;
+	is_zone = ISC_FALSE;
 
 	if (event != NULL) {
 		/*
@@ -3702,7 +3703,6 @@ query_find(ns_client_t *client, dns_fetchevent_t *event, dns_rdatatype_t qtype)
 
 		want_restart = ISC_FALSE;
 		authoritative = ISC_FALSE;
-		is_zone = ISC_FALSE;
 
 		qtype = event->qtype;
 		if (qtype == dns_rdatatype_rrsig || qtype == dns_rdatatype_sig)
