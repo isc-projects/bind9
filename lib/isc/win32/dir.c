d9edfd56422af0e709d4548ce838f2167d1ea115
Automatic Updater <source@isc.org>
2008-11-02 23:47:01 +0000

    update copyright notice

--- lib/isc/win32/dir.c
+++ lib/isc/win32/dir.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2007, 2008  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: dir.c,v 1.15 2008/11/02 12:48:39 fdupont Exp $ */
+/* $Id: dir.c,v 1.16 2008/11/02 23:47:01 tbox Exp $ */
 
 /* Principal Authors: DCL */
 
