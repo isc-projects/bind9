eb8265942b3019d34e365432314b63decc84728f
Tatuya JINMEI 神明達哉 <jinmei@isc.org>
2008-11-20 00:04:17 +0000

    2498.	[bug]		Removed a bogus function argument used with 			ISC_SOCKET_USE_POLLWATCH: it could cause compiler 			warning or crash named with the debug 1 level 			of logging. [RT #18917]
    (Note that this entry is placed below the 9.6.0rc1 mark)

--- lib/isc/unix/socket.c
+++ lib/isc/unix/socket.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: socket.c,v 1.307 2008/11/12 23:47:17 tbox Exp $ */
+/* $Id: socket.c,v 1.308 2008/11/20 00:04:17 jinmei Exp $ */
 
 /*! \file */
 
@@ -3365,7 +3365,6 @@ watcher(void *uap) {
 						    ISC_LOGCATEGORY_GENERAL,
 						    ISC_LOGMODULE_SOCKET,
 						    ISC_LOG_DEBUG(1),
-						    ISC_LOG_INFO,
 						    "unexpected POLL timeout");
 				}
 				pollstate = poll_active;
