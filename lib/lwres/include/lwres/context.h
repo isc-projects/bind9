07b2d897cea0e1f83b9064d45f29b2e36ae617ae
Automatic Updater <source@isc.org>
2008-12-17 23:46:34 +0000

    update copyright notice

--- lib/lwres/include/lwres/context.h
+++ lib/lwres/include/lwres/context.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2008  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000, 2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: context.h,v 1.21.128.1 2008/12/17 19:21:25 jinmei Exp $ */
+/* $Id: context.h,v 1.21.128.2 2008/12/17 23:46:34 tbox Exp $ */
 
 #ifndef LWRES_CONTEXT_H
 #define LWRES_CONTEXT_H 1
