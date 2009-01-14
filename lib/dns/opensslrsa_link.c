03faa7804e82fc99402affbd742c4476581fe005
Automatic Updater <source@isc.org>
2009-01-14 23:48:00 +0000

    update copyright notice

--- lib/dns/opensslrsa_link.c
+++ lib/dns/opensslrsa_link.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2008  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2009  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -17,7 +17,7 @@
 
 /*
  * Principal Author: Brian Wellington
- * $Id: opensslrsa_link.c,v 1.21 2009/01/14 17:28:47 fdupont Exp $
+ * $Id: opensslrsa_link.c,v 1.22 2009/01/14 23:48:00 tbox Exp $
  */
 #ifdef OPENSSL
 #ifndef USE_EVP
