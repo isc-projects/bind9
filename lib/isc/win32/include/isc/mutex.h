3b8b44559a404bc956e63909ba46d9a00ceec0fb
Francis Dupont <fdupont@isc.org>
2009-01-18 00:11:07 +0000

    spelling

--- lib/isc/win32/include/isc/mutex.h
+++ lib/isc/win32/include/isc/mutex.h
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: mutex.h,v 1.20 2008/09/12 04:46:25 marka Exp $ */
+/* $Id: mutex.h,v 1.21 2009/01/18 00:11:07 fdupont Exp $ */
 
 #ifndef ISC_MUTEX_H
 #define ISC_MUTEX_H 1
@@ -28,7 +28,7 @@
 typedef CRITICAL_SECTION isc_mutex_t;
 
 /*
- * This definition is here since somve versions of WINBASE.H
+ * This definition is here since some versions of WINBASE.H
  * omits it for some reason.
  */
 #if (_WIN32_WINNT < 0x0400)
