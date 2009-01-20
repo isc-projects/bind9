df4a53b4f8374d7e9a217a749b1524e55f4dd066
Automatic Updater <source@isc.org>
2009-01-20 23:46:02 +0000

    update copyright notice

--- bin/check/check-tool.c
+++ bin/check/check-tool.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2008  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2009  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: check-tool.c,v 1.10.18.21 2009/01/20 03:12:10 marka Exp $ */
+/* $Id: check-tool.c,v 1.10.18.22 2009/01/20 23:46:02 tbox Exp $ */
 
 /*! \file */
 
