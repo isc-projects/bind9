b7e19163d048106fb9307b5581e877012d11a5af
Mark Andrews <marka@isc.org>
2009-03-02 02:00:34 +0000

    2566.   [bug]           dst__privstruct_writefile() could miss write errors.                         [RT #19360]

--- lib/dns/dst_api.c
+++ lib/dns/dst_api.c
@@ -31,7 +31,7 @@
 
 /*
  * Principal Author: Brian Wellington
- * $Id: dst_api.c,v 1.16.12.2 2009/02/16 23:47:15 tbox Exp $
+ * $Id: dst_api.c,v 1.16.12.3 2009/03/02 02:00:34 marka Exp $
  */
 
 /*! \file */
@@ -1163,9 +1163,12 @@ write_public_key(const dst_key_t *key, int type, const char *directory) {
 	fwrite(r.base, 1, r.length, fp);
 
 	fputc('\n', fp);
+	fflush(fp);
+	if (ferror(fp))
+		ret = DST_R_WRITEERROR;
 	fclose(fp);
 
-	return (ISC_R_SUCCESS);
+	return (ret);
 }
 
 static isc_result_t
