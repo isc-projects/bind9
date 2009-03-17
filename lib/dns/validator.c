cf75764f4012e1114bd93614ff5ca9befd81ee33
Automatic Updater <source@isc.org>
2009-03-17 23:49:17 +0000

    update copyright notice

--- lib/dns/validator.c
+++ lib/dns/validator.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2008  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2009  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: validator.c,v 1.155.52.8.2.1 2009/03/17 02:11:19 marka Exp $ */
+/* $Id: validator.c,v 1.155.52.8.2.2 2009/03/17 23:49:17 tbox Exp $ */
 
 #include <config.h>
 
