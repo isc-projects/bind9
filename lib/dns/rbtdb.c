1ffa559d2b179e5c5d05059b7647bfa32a034471
Tatuya JINMEI 神明達哉 <jinmei@isc.org>
2009-04-07 02:50:37 +0000

    wording fix for cpp error message (rt #19569)

--- lib/dns/rbtdb.c
+++ lib/dns/rbtdb.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: rbtdb.c,v 1.248.12.16 2009/03/05 05:00:13 marka Exp $ */
+/* $Id: rbtdb.c,v 1.248.12.17 2009/04/07 02:50:37 jinmei Exp $ */
 
 /*! \file */
 
@@ -328,7 +328,7 @@ struct acachectl {
  */
 #ifdef DNS_RBTDB_CACHE_NODE_LOCK_COUNT
 #if DNS_RBTDB_CACHE_NODE_LOCK_COUNT <= 1
-#error "DNS_RBTDB_CACHE_NODE_LOCK_COUNT must be larger 1"
+#error "DNS_RBTDB_CACHE_NODE_LOCK_COUNT must be larger than 1"
 #else
 #define DEFAULT_CACHE_NODE_LOCK_COUNT DNS_RBTDB_CACHE_NODE_LOCK_COUNT
 #endif
