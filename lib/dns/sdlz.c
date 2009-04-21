ab381c1e22e0ed732170428937d20d13146d863a
Automatic Updater <source@isc.org>
2009-04-21 23:48:04 +0000

    update copyright notice

--- lib/dns/sdlz.c
+++ lib/dns/sdlz.c
@@ -1,5 +1,5 @@
 /*
- * Portions Copyright (C) 2005-2008  Internet Systems Consortium, Inc. ("ISC")
+ * Portions Copyright (C) 2005-2009  Internet Systems Consortium, Inc. ("ISC")
  * Portions Copyright (C) 1999-2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -50,7 +50,7 @@
  * USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: sdlz.c,v 1.19 2009/04/21 00:41:02 jinmei Exp $ */
+/* $Id: sdlz.c,v 1.20 2009/04/21 23:48:04 tbox Exp $ */
 
 /*! \file */
 
@@ -846,7 +846,7 @@ find(dns_db_t *db, dns_name_t *name, dns_dbversion_t *version,
 					dns_rdataset_disassociate(rdataset);
 					if (sigrdataset != NULL &&
 					    dns_rdataset_isassociated
-					    		(sigrdataset)) {
+							(sigrdataset)) {
 						dns_rdataset_disassociate
 							(sigrdataset);
 					}
