50e71de9018577e17fac880b9693d6f8fab92377
Automatic Updater <source@isc.org>
2009-04-29 23:46:55 +0000

    update copyright notice

--- lib/dns/db.c
+++ lib/dns/db.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005, 2007, 2008  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2007-2009  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2001, 2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: db.c,v 1.83.128.3 2009/04/29 22:22:32 marka Exp $ */
+/* $Id: db.c,v 1.83.128.4 2009/04/29 23:46:55 tbox Exp $ */
 
 /*! \file */
 
