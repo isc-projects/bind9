7a272c6b0de3b8c0ad018b9896e287da19c43bef
Automatic Updater <source@isc.org>
2009-05-06 23:47:50 +0000

    update copyright notice

--- lib/dns/include/dns/events.h
+++ lib/dns/include/dns/events.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007, 2009  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: events.h,v 1.50 2009/05/06 22:53:54 jinmei Exp $ */
+/* $Id: events.h,v 1.51 2009/05/06 23:47:50 tbox Exp $ */
 
 #ifndef DNS_EVENTS_H
 #define DNS_EVENTS_H 1
