75006929339c93676bbbc47c677023619061259e
Automatic Updater <source@isc.org>
2009-07-11 23:46:45 +0000

    update copyright notice

--- bin/named/control.c
+++ bin/named/control.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007, 2009  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2001-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: control.c,v 1.33.62.1 2009/07/11 04:28:14 marka Exp $ */
+/* $Id: control.c,v 1.33.62.2 2009/07/11 23:46:45 tbox Exp $ */
 
 /*! \file */
 
@@ -56,7 +56,7 @@ command_compare(const char *text, const char *command) {
 
 /*%
  * This function is called to process the incoming command
- * when a control channel message is received.  
+ * when a control channel message is received.
  */
 isc_result_t
 ns_control_docommand(isccc_sexpr_t *message, isc_buffer_t *text) {
