0282f038ebfb22b0b62f55533d064eaac1933e99
Automatic Updater <source@isc.org>
2009-07-29 23:47:43 +0000

    update copyright notice

--- bin/tests/system/nsupdate/clean.sh
+++ bin/tests/system/nsupdate/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007, 2009  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2000, 2001  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: clean.sh,v 1.13 2009/07/29 17:52:00 each Exp $
+# $Id: clean.sh,v 1.14 2009/07/29 23:47:43 tbox Exp $
 
 #
 # Clean up after zone transfer tests.
