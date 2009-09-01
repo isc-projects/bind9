dd0bd9bbc283b8ec3355bb2f54d223094127a681
Automatic Updater <source@isc.org>
2009-09-01 23:46:02 +0000

    update copyright notice

--- lib/lwres/context.c
+++ lib/lwres/context.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005, 2007, 2008  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2007-2009  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000, 2001, 2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: context.c,v 1.45.18.11 2009/09/01 07:18:54 each Exp $ */
+/* $Id: context.c,v 1.45.18.12 2009/09/01 23:46:02 tbox Exp $ */
 
 /*! \file context.c
    lwres_context_create() creates a #lwres_context_t structure for use in
