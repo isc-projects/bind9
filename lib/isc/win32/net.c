2226e9ec2561911ae9d75bbcdc7b835145732438
Evan Hunt <each@isc.org>
2009-09-23 00:46:03 +0000

    fix typo in net.c -- s/closeocket/closesocket/

--- lib/isc/win32/net.c
+++ lib/isc/win32/net.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: net.c,v 1.18 2008/08/08 05:06:49 marka Exp $ */
+/* $Id: net.c,v 1.18.64.1 2009/09/23 00:46:03 each Exp $ */
 
 #include <config.h>
 
@@ -197,7 +197,7 @@ try_ipv6only(void) {
 	ipv6only_result = ISC_R_SUCCESS;
 
 close:
-	closeocket(s);
+	closesocket(s);
 	return;
 #endif /* IPV6_V6ONLY */
 }
