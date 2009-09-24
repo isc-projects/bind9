b4086a1fde26bd3b71bfdde64d7d1b9132372f2a
Automatic Updater <source@isc.org>
2009-09-24 23:46:53 +0000

    update copyright notice

--- lib/isc/unix/ifiter_getifaddrs.c
+++ lib/isc/unix/ifiter_getifaddrs.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005, 2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2007, 2009  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: ifiter_getifaddrs.c,v 1.9.128.1 2009/09/24 06:42:00 marka Exp $ */
+/* $Id: ifiter_getifaddrs.c,v 1.9.128.2 2009/09/24 23:46:53 tbox Exp $ */
 
 /*! \file
  * \brief
