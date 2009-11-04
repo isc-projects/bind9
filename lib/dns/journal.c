2484c7db7a4e6fea66d4c6532316be4711dad3d4
Mark Andrews <marka@isc.org>
2009-11-04 04:22:16 +0000

    spelling

--- lib/dns/journal.c
+++ lib/dns/journal.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: journal.c,v 1.108 2009/11/04 03:46:44 marka Exp $ */
+/* $Id: journal.c,v 1.109 2009/11/04 04:22:16 marka Exp $ */
 
 #include <config.h>
 
@@ -1888,7 +1888,7 @@ diff_namespace(isc_mem_t *mctx,
 		return (result);
 	result = dns_db_createiterator(db[1], options, &dbit[1]);
 	if (result != ISC_R_SUCCESS)
-		goto cleanup_interator;
+		goto cleanup_iterator;
 
 	itresult[0] = dns_dbiterator_first(dbit[0]);
 	itresult[1] = dns_dbiterator_first(dbit[1]);
@@ -1956,7 +1956,7 @@ diff_namespace(isc_mem_t *mctx,
 
  failure:
 	dns_dbiterator_destroy(&dbit[1]);
- cleanup_interator:
+ cleanup_iterator:
 	dns_dbiterator_destroy(&dbit[0]);
 	return (result);
 }
