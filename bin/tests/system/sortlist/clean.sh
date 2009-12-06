8b82c01d74e6a96759f51007832d6a833ea437e7
Automatic Updater <source@isc.org>
2009-12-06 23:48:29 +0000

    update copyright notice

--- bin/tests/system/sortlist/clean.sh
+++ bin/tests/system/sortlist/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007, 2009  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2000, 2001  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: clean.sh,v 1.8 2009/12/06 03:04:39 each Exp $
+# $Id: clean.sh,v 1.9 2009/12/06 23:48:29 tbox Exp $
 
 rm -f *.dig *.good *.out
 rm -f */named.memstats
