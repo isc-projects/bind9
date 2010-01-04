842920c7db2db1de1a0f9571f77aee5ea92ae5bb
Automatic Updater <source@isc.org>
2010-01-04 23:48:51 +0000

    update copyright notice

--- lib/dns/rbtdb.c
+++ lib/dns/rbtdb.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2010  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: rbtdb.c,v 1.297 2010/01/04 22:30:14 each Exp $ */
+/* $Id: rbtdb.c,v 1.298 2010/01/04 23:48:51 tbox Exp $ */
 
 /*! \file */
 
