f9d193397ab38c4de11438945568fa8f069a41b0
Automatic Updater <source@isc.org>
2010-01-06 23:48:07 +0000

    update copyright notice

--- bin/dnssec/dnssec-settime.c
+++ bin/dnssec/dnssec-settime.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2009, 2010  Internet Systems Consortium, Inc. ("ISC")
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -14,7 +14,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: dnssec-settime.c,v 1.19.34.3 2010/01/06 00:53:56 each Exp $ */
+/* $Id: dnssec-settime.c,v 1.19.34.4 2010/01/06 23:48:07 tbox Exp $ */
 
 /*! \file */
 
