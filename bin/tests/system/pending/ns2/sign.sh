727708b9379e886e6303b20b1ec39900345f7817
Automatic Updater <source@isc.org>
2010-01-07 23:46:46 +0000

    update copyright notice

--- bin/tests/system/pending/ns2/sign.sh
+++ bin/tests/system/pending/ns2/sign.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 #
-# Copyright (C) 2009  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2009, 2010  Internet Systems Consortium, Inc. ("ISC")
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -14,7 +14,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: sign.sh,v 1.2.2.5 2010/01/07 17:49:22 each Exp $
+# $Id: sign.sh,v 1.2.2.6 2010/01/07 23:46:46 tbox Exp $
 
 SYSTEMTESTTOP=../..
 . $SYSTEMTESTTOP/conf.sh
--- lib/dns/resolver.c
+++ lib/dns/resolver.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2010  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: resolver.c,v 1.355.12.47 2010/01/07 17:49:22 each Exp $ */
+/* $Id: resolver.c,v 1.355.12.48 2010/01/07 23:46:46 tbox Exp $ */
 
 /*! \file */
 
