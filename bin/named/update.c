cc9ed75dd9e60cb89405a6bc653a2564a82e1185
Automatic Updater <source@isc.org>
2010-02-26 23:50:59 +0000

    update copyright notice

--- bin/named/update.c
+++ bin/named/update.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2010  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: update.c,v 1.180 2010/02/26 01:39:49 marka Exp $ */
+/* $Id: update.c,v 1.181 2010/02/26 23:50:59 tbox Exp $ */
 
 #include <config.h>
 
