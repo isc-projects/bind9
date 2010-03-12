230987e819c3315bf71dcae063feaf7fa372296b
Automatic Updater <source@isc.org>
2010-03-12 23:51:11 +0000

    update copyright notice

--- bin/named/query.c
+++ bin/named/query.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2010  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: query.c,v 1.337 2010/03/12 01:48:35 marka Exp $ */
+/* $Id: query.c,v 1.338 2010/03/12 23:51:07 tbox Exp $ */
 
 /*! \file */
 
--- lib/dns/gssapictx.c
+++ lib/dns/gssapictx.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2010  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000, 2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: gssapictx.c,v 1.15 2010/03/12 03:47:08 marka Exp $ */
+/* $Id: gssapictx.c,v 1.16 2010/03/12 23:51:11 tbox Exp $ */
 
 #include <config.h>
 
