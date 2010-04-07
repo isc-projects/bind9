a2732746146ca7dac4f71c8cdf7fdd39941f7b25
Automatic Updater <source@isc.org>
2010-04-07 23:48:30 +0000

    update copyright notice

--- lib/dns/rootns.c
+++ lib/dns/rootns.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005, 2007, 2008  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2007, 2008, 2010  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: rootns.c,v 1.36.50.1 2010/04/07 07:10:06 marka Exp $ */
+/* $Id: rootns.c,v 1.36.50.2 2010/04/07 23:48:30 tbox Exp $ */
 
 /*! \file */
 
