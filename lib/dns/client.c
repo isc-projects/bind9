4bb846d522b0ba36450ff1413c740cec8d5913b7
Automatic Updater <source@isc.org>
2010-04-13 23:49:44 +0000

    update copyright notice

--- lib/dns/client.c
+++ lib/dns/client.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2010  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2009, 2010  Internet Systems Consortium, Inc. ("ISC")
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -14,7 +14,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: client.c,v 1.6.32.1 2010/04/13 19:15:56 sar Exp $ */
+/* $Id: client.c,v 1.6.32.2 2010/04/13 23:49:44 tbox Exp $ */
 
 #include <config.h>
 
