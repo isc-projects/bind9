3c66ae112846c4185db01c501fe1d90aa8572af3
Automatic Updater <source@isc.org>
2010-04-21 23:48:05 +0000

    update copyright notice

--- lib/dns/time.c
+++ lib/dns/time.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005, 2007, 2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2007, 2009, 2010  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1998-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: time.c,v 1.31.332.3 2010/04/21 02:23:33 marka Exp $ */
+/* $Id: time.c,v 1.31.332.4 2010/04/21 23:48:05 tbox Exp $ */
 
 /*! \file */
 
