d3798f2bffe122be4a93b2dcba2990b404d1eebf
Automatic Updater <source@isc.org>
2010-05-06 23:50:56 +0000

    update copyright notice

--- bin/dnssec/dnssec-revoke.c
+++ bin/dnssec/dnssec-revoke.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2009, 2010  Internet Systems Consortium, Inc. ("ISC")
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -14,7 +14,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: dnssec-revoke.c,v 1.21 2010/05/06 05:31:19 marka Exp $ */
+/* $Id: dnssec-revoke.c,v 1.22 2010/05/06 23:50:56 tbox Exp $ */
 
 /*! \file */
 
