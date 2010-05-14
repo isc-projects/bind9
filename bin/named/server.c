01565f50ca6dbf2c44ebffdb76c8fe37b3b555e7
Automatic Updater <source@isc.org>
2010-05-14 23:47:50 +0000

    update copyright notice

--- bin/named/server.c
+++ bin/named/server.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: server.c,v 1.520.12.14 2010/05/14 03:31:06 marka Exp $ */
+/* $Id: server.c,v 1.520.12.15 2010/05/14 23:47:48 tbox Exp $ */
 
 /*! \file */
 
@@ -228,7 +228,7 @@ static const struct {
 	{ "8.B.D.0.1.0.0.2.IP6.ARPA", ISC_FALSE },
 
 	/* ORCHID Prefix, RFC 4843. */
-	{ "0.1.1.0.0.2.IP6.ARPA", ISC_FALSE }, 
+	{ "0.1.1.0.0.2.IP6.ARPA", ISC_FALSE },
 
 	{ NULL, ISC_FALSE }
 };
--- lib/dns/include/dns/types.h
+++ lib/dns/include/dns/types.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2010  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1998-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: types.h,v 1.130.50.6 2010/05/14 00:33:23 marka Exp $ */
+/* $Id: types.h,v 1.130.50.7 2010/05/14 23:47:50 tbox Exp $ */
 
 #ifndef DNS_TYPES_H
 #define DNS_TYPES_H 1
