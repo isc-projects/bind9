ee980f5002fd8a27b269fc6a0f1b766c6d25d34f
Automatic Updater <source@isc.org>
2010-05-17 23:49:51 +0000

    update copyright notice

--- bin/tools/genrandom.c
+++ bin/tools/genrandom.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005, 2007, 2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2007, 2009, 2010  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: genrandom.c,v 1.4.152.2 2010/05/17 05:31:08 marka Exp $ */
+/* $Id: genrandom.c,v 1.4.152.3 2010/05/17 23:49:51 tbox Exp $ */
 
 /*! \file */
 #include <config.h>
