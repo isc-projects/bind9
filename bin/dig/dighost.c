63932c9e6b06e327bbe1ca27a6bebbd6137da5ce
Automatic Updater <source@isc.org>
2010-05-18 02:32:04 +0000

    update copyright notice

--- bin/dig/dighost.c
+++ bin/dig/dighost.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2010  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: dighost.c,v 1.311.70.12 2010/05/18 01:50:32 marka Exp $ */
+/* $Id: dighost.c,v 1.311.70.13 2010/05/18 02:32:04 tbox Exp $ */
 
 /*! \file
  *  \note
