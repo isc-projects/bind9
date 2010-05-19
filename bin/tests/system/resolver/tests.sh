90de0c8201384ac7720dbf83e94744a425c23557
Automatic Updater <source@isc.org>
2010-05-19 09:32:03 +0000

    update copyright notice

--- bin/tests/system/resolver/tests.sh
+++ bin/tests/system/resolver/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007, 2010  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2000, 2001  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: tests.sh,v 1.9.426.1 2010/05/19 06:56:01 marka Exp $
+# $Id: tests.sh,v 1.9.426.2 2010/05/19 09:32:03 tbox Exp $
 
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
