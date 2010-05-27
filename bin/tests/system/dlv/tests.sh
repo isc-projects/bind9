03295042463f0e2b0db01ce97d1604312e87b5e0
Automatic Updater <source@isc.org>
2010-05-27 23:49:55 +0000

    update copyright notice

--- bin/tests/system/dlv/tests.sh
+++ bin/tests/system/dlv/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007, 2010  Internet Systems Consortium, Inc. ("ISC")
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -14,7 +14,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: tests.sh,v 1.4.558.1 2010/05/26 06:30:43 marka Exp $
+# $Id: tests.sh,v 1.4.558.2 2010/05/27 23:49:55 tbox Exp $
 
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
