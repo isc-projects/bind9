c7c7ba3977ec09eabc33c4f910894eb4f9ff81bb
Automatic Updater <source@isc.org>
2010-06-21 23:46:48 +0000

    update copyright notice

--- bin/tests/virtual-time/autosign-zsk/ns1/wrap.sh
+++ bin/tests/virtual-time/autosign-zsk/ns1/wrap.sh
@@ -1,4 +1,4 @@
-# Copyright (C) 2009  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2010  Internet Systems Consortium, Inc. ("ISC")
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -12,7 +12,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: wrap.sh,v 1.2 2010/06/21 02:31:46 marka Exp $
+# $Id: wrap.sh,v 1.3 2010/06/21 23:46:48 tbox Exp $
 
 #
 # Wrapper for named
