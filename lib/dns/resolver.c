b479070c833faef4ec66579b90399c39c4717ae9
Automatic Updater <source@isc.org>
2010-06-23 23:45:31 +0000

    update copyright notice

--- lib/dns/resolver.c
+++ lib/dns/resolver.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: resolver.c,v 1.355.12.55 2010/06/23 01:50:23 marka Exp $ */
+/* $Id: resolver.c,v 1.355.12.56 2010/06/23 23:45:31 tbox Exp $ */
 
 /*! \file */
 
@@ -5767,22 +5767,22 @@ fctx_decreference(fetchctx_t *fctx) {
 	INSIST(fctx->references > 0);
 	fctx->references--;
 	if (fctx->references == 0) {
-                /*
-                 * No one cares about the result of this fetch anymore.
-                 */
-                if (fctx->pending == 0 && fctx->nqueries == 0 &&
-                    ISC_LIST_EMPTY(fctx->validators) && SHUTTINGDOWN(fctx)) {
-                        /*
-                         * This fctx is already shutdown; we were just
-                         * waiting for the last reference to go away.
-                         */
-                        bucket_empty = fctx_destroy(fctx);
-                } else {
-                        /*
-                         * Initiate shutdown.
-                         */
-                        fctx_shutdown(fctx);
-                }
+		/*
+		 * No one cares about the result of this fetch anymore.
+		 */
+		if (fctx->pending == 0 && fctx->nqueries == 0 &&
+		    ISC_LIST_EMPTY(fctx->validators) && SHUTTINGDOWN(fctx)) {
+			/*
+			 * This fctx is already shutdown; we were just
+			 * waiting for the last reference to go away.
+			 */
+			bucket_empty = fctx_destroy(fctx);
+		} else {
+			/*
+			 * Initiate shutdown.
+			 */
+			fctx_shutdown(fctx);
+		}
 	}
 	return (bucket_empty);
 }
