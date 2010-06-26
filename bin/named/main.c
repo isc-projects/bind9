1b67d9b719766459344caededa1e3f670985d311
Automatic Updater <source@isc.org>
2010-06-26 23:46:49 +0000

    update copyright notice

--- bin/named/main.c
+++ bin/named/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2010  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: main.c,v 1.177 2010/06/26 00:20:33 marka Exp $ */
+/* $Id: main.c,v 1.178 2010/06/26 23:46:49 tbox Exp $ */
 
 /*! \file */
 
