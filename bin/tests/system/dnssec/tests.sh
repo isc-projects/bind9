a7d2b922ee12553c7dddca6df670e2096bbf934d
Mark Andrews <marka@isc.org>
2010-06-28 01:34:11 +0000

    match the dig.out.ns#.$n to the nameserver

--- bin/tests/system/dnssec/tests.sh
+++ bin/tests/system/dnssec/tests.sh
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: tests.sh,v 1.65 2010/06/28 01:31:49 marka Exp $
+# $Id: tests.sh,v 1.66 2010/06/28 01:34:11 marka Exp $
 
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
@@ -874,8 +874,8 @@ status=`expr $status + $ret`
 echo "I:checking a non-cachable NODATA works ($n)"
 ret=0
 $DIG $DIGOPTS +noauth a.nosoa.secure.example. txt @10.53.0.7 \
-	> dig.out.ns2.test$n || ret=1
-grep "AUTHORITY: 0" dig.out.ns2.test$n > /dev/null || ret=1
+	> dig.out.ns7.test$n || ret=1
+grep "AUTHORITY: 0" dig.out.ns7.test$n > /dev/null || ret=1
 $DIG $DIGOPTS +noauth a.nosoa.secure.example. txt @10.53.0.4 \
 	> dig.out.ns4.test$n || ret=1
 grep "status: NOERROR" dig.out.ns4.test$n > /dev/null || ret=1
@@ -886,8 +886,8 @@ status=`expr $status + $ret`
 echo "I:checking a non-cachable NXDOMAIN works ($n)"
 ret=0
 $DIG $DIGOPTS +noauth b.nosoa.secure.example. txt @10.53.0.7 \
-	> dig.out.ns2.test$n || ret=1
-grep "AUTHORITY: 0" dig.out.ns2.test$n > /dev/null || ret=1
+	> dig.out.ns7.test$n || ret=1
+grep "AUTHORITY: 0" dig.out.ns7.test$n > /dev/null || ret=1
 $DIG $DIGOPTS +noauth b.nosoa.secure.example. txt @10.53.0.4 \
 	> dig.out.ns4.test$n || ret=1
 grep "status: NXDOMAIN" dig.out.ns4.test$n > /dev/null || ret=1
