9b7efe7aca3b017089da1530bc0e6fde762d01a5
Evan Hunt <each@isc.org>
2010-08-12 04:04:14 +0000

    Removed a leftover UNUSED statement referencing a parameter that doesn't exist anymore.

--- lib/dns/view.c
+++ lib/dns/view.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: view.c,v 1.167 2010/08/11 18:14:19 each Exp $ */
+/* $Id: view.c,v 1.168 2010/08/12 04:04:14 each Exp $ */
 
 /*! \file */
 
@@ -1656,7 +1656,6 @@ dns_view_setnewzones(dns_view_t *view, isc_boolean_t allow, void *cfgctx,
 	}
 #else
 	UNUSED(allow);
-	UNUSED(parser);
 	UNUSED(cfgctx);
 	UNUSED(cfg_destroy);
 #endif
