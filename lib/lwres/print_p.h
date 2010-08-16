05ceebcb1b25c5c967fa53ef5caf3b034f003f70
Mark Andrews <marka@isc.org>
2010-08-16 22:39:36 +0000

    silence redefinition warnings MacOS

--- lib/lwres/print_p.h
+++ lib/lwres/print_p.h
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: print_p.h,v 1.4 2007/06/19 23:47:22 tbox Exp $ */
+/* $Id: print_p.h,v 1.4.558.1 2010/08/16 22:39:36 marka Exp $ */
 
 #ifndef LWRES_PRINT_P_H
 #define LWRES_PRINT_P_H 1
@@ -67,17 +67,26 @@ LWRES_LANG_BEGINDECLS
 int
 lwres__print_vsnprintf(char *str, size_t size, const char *format, va_list ap)
      LWRES_FORMAT_PRINTF(3, 0);
+#ifdef vsnprintf
+#undef vsnprintf
+#endif
 #define vsnprintf lwres__print_vsnprintf
 
 int
 lwres__print_snprintf(char *str, size_t size, const char *format, ...)
      LWRES_FORMAT_PRINTF(3, 4);
+#ifdef snprintf
+#undef snprintf
+#endif
 #define snprintf lwres__print_snprintf
 #endif /* LWRES_PLATFORM_NEEDVSNPRINTF */
 
 #ifdef LWRES_PLATFORM_NEEDSPRINTF
 int
 lwres__print_sprintf(char *str, const char *format, ...) LWRES_FORMAT_PRINTF(2, 3);
+#ifdef sprintf
+#undef sprintf
+#endif
 #define sprintf lwres__print_sprintf
 #endif
 
