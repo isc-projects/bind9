03df994501f06074099ba463b13eb3af25f155a0
Mark Andrews <marka@isc.org>
2010-08-16 05:37:18 +0000

    2942.   [contrib]       zone2sqlite failed to setup the entropy sources.                         [RT #21610]

--- contrib/sdb/sqlite/zone2sqlite.c
+++ contrib/sdb/sqlite/zone2sqlite.c
@@ -15,7 +15,7 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: zone2sqlite.c,v 1.2 2008/09/24 02:46:21 marka Exp $ */
+/* $Id: zone2sqlite.c,v 1.2.50.1 2010/08/16 05:37:18 marka Exp $ */
 
 #include <stdlib.h>
 #include <string.h>
@@ -27,7 +27,9 @@
 
 #include <dns/db.h>
 #include <dns/dbiterator.h>
+#include <isc/entropy.h>
 #include <dns/fixedname.h>
+#include <isc/hash.h>
 #include <dns/name.h>
 #include <dns/rdata.h>
 #include <dns/rdataset.h>
@@ -157,6 +159,7 @@ main(int argc, char *argv[])
     dns_rdataset_t rdataset;
     dns_rdata_t rdata = DNS_RDATA_INIT;
     isc_mem_t *mctx = NULL;
+    isc_entropy_t *ectx = NULL;
     isc_buffer_t b;
     isc_result_t result;
 
@@ -173,9 +176,12 @@ main(int argc, char *argv[])
     
     dns_result_register();
     
-    mctx = NULL;
     result = isc_mem_create(0, 0, &mctx);
     check_result(result, "isc_mem_create");
+    result = isc_entropy_create(mctx, &ectx);
+    check_result(result, "isc_entropy_create");
+    result = isc_hash_create(mctx, ectx, DNS_NAME_MAXWIRE);
+    check_result(result, "isc_hash_create");
     
     isc_buffer_init(&b, porigin, strlen(porigin));
     isc_buffer_add(&b, strlen(porigin));
@@ -293,6 +299,8 @@ main(int argc, char *argv[])
     
     dns_dbiterator_destroy(&dbiter);
     dns_db_detach(&db);
+    isc_hash_destroy();
+    isc_entropy_detach(&ectx);
     isc_mem_destroy(&mctx);
 
     closeandexit(0);
