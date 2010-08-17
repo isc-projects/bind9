16512d13868f884ab485f3ddd036c4b907a16a63
Mark Andrews <marka@isc.org>
2010-08-17 04:10:56 +0000

    update default id range to match that used (1..7)

--- bin/tests/system/testsock.pl
+++ bin/tests/system/testsock.pl
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: testsock.pl,v 1.16 2007/06/19 23:47:00 tbox Exp $
+# $Id: testsock.pl,v 1.16.128.1 2010/08/17 04:10:56 marka Exp $
 
 # Test whether the interfaces on 10.53.0.* are up.
 
@@ -33,7 +33,7 @@ my @ids;
 if ($id != 0) {
 	@ids = ($id);
 } else {
-	@ids = (1..5);
+	@ids = (1..6);
 }
 
 foreach $id (@ids) {
