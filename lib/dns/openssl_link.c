ce26def0951823e4decd7cd01f2196f0c3c32f79
Automatic Updater <source@isc.org>
2010-09-15 12:37:35 +0000

    update copyright notice

--- lib/dns/openssl_link.c
+++ lib/dns/openssl_link.c
@@ -1,5 +1,5 @@
 /*
- * Portions Copyright (C) 2004-2009  Internet Systems Consortium, Inc. ("ISC")
+ * Portions Copyright (C) 2004-2010  Internet Systems Consortium, Inc. ("ISC")
  * Portions Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -31,7 +31,7 @@
 
 /*
  * Principal Author: Brian Wellington
- * $Id: openssl_link.c,v 1.22.112.4 2010/09/15 03:26:09 marka Exp $
+ * $Id: openssl_link.c,v 1.22.112.5 2010/09/15 12:37:35 tbox Exp $
  */
 #ifdef OPENSSL
 
