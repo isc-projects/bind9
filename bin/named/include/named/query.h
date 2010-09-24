7041e869864c9eb1036640eeb94f3cbb70c230ed
Automatic Updater <source@isc.org>
2010-09-24 08:31:23 +0000

    update copyright notice

--- bin/named/include/named/query.h
+++ bin/named/include/named/query.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005, 2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2007, 2010  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: query.h,v 1.41 2010/09/24 05:09:02 marka Exp $ */
+/* $Id: query.h,v 1.42 2010/09/24 08:31:23 tbox Exp $ */
 
 #ifndef NAMED_QUERY_H
 #define NAMED_QUERY_H 1
