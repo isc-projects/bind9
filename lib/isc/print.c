3691f0d7668d6bbaf2a678e8f1df5c0ca02bfc7b
Automatic Updater <source@isc.org>
2010-10-18 23:46:17 +0000

    update copyright notice

--- lib/isc/print.c
+++ lib/isc/print.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2008  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2008, 2010  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2001, 2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: print.c,v 1.35.130.1 2010/10/18 04:02:40 marka Exp $ */
+/* $Id: print.c,v 1.35.130.2 2010/10/18 23:46:17 tbox Exp $ */
 
 /*! \file */
 
