4071d667be5fb5914051d25abdcd3cdca8134eb2
Automatic Updater <source@isc.org>
2010-10-19 23:47:10 +0000

    update copyright notice

--- bin/dig/host.c
+++ bin/dig/host.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2007, 2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007, 2009, 2010  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: host.c,v 1.121 2010/10/19 02:48:17 marka Exp $ */
+/* $Id: host.c,v 1.122 2010/10/19 23:47:10 tbox Exp $ */
 
 /*! \file */
 
