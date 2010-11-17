fff1bf76bd00162eb2e9b15a8842bf154a577d24
Automatic Updater <source@isc.org>
2010-11-17 23:45:46 +0000

    update copyright notice

--- lib/dns/journal.c
+++ lib/dns/journal.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005, 2007-2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2007-2010  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: journal.c,v 1.103.48.7 2010/11/17 00:36:27 each Exp $ */
+/* $Id: journal.c,v 1.103.48.8 2010/11/17 23:45:45 tbox Exp $ */
 
 #include <config.h>
 
