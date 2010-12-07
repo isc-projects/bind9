39ba8c773721dcb16208144821c0179cbe7c3c46
Automatic Updater <source@isc.org>
2010-12-07 23:46:26 +0000

    update copyright notice

--- bin/tests/system/nsupdate/setup.sh
+++ bin/tests/system/nsupdate/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004, 2007, 2009  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007, 2009, 2010  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2000, 2001  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: setup.sh,v 1.14.4.1 2010/12/07 03:01:37 marka Exp $
+# $Id: setup.sh,v 1.14.4.2 2010/12/07 23:46:26 tbox Exp $
 
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
