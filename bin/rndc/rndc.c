6d6a3a820a80231c6f4b829aabb2cabc7d337e1f
johnd <johnd>
2010-12-10 20:08:07 +0000

    Include "loadkeys" in rndc help output. [RT #22493]

--- bin/rndc/rndc.c
+++ bin/rndc/rndc.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: rndc.c,v 1.126.66.4 2010/07/11 00:12:18 each Exp $ */
+/* $Id: rndc.c,v 1.126.66.5 2010/12/10 20:08:07 johnd Exp $ */
 
 /*! \file */
 
@@ -119,6 +119,8 @@ command is one of the following:\n\
   reconfig	Reload configuration file and new zones only.\n\
   sign zone [class [view]]\n\
 		Update zone keys, and sign as needed.\n\
+  loadkeys zone [class [view]]\n\
+		Update keys without signing immediately.\n\
   stats		Write server statistics to the statistics file.\n\
   querylog	Toggle query logging.\n\
   dumpdb [-all|-cache|-zones] [view ...]\n\
