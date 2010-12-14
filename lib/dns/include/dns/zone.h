27ee4b8ca4f27616c9028eed51c37fd17648867e
Automatic Updater <source@isc.org>
2010-12-14 23:46:09 +0000

    update copyright notice

--- lib/dns/include/dns/zone.h
+++ lib/dns/include/dns/zone.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2010  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: zone.h,v 1.160.50.7 2010/12/14 00:48:23 marka Exp $ */
+/* $Id: zone.h,v 1.160.50.8 2010/12/14 23:46:09 tbox Exp $ */
 
 #ifndef DNS_ZONE_H
 #define DNS_ZONE_H 1
