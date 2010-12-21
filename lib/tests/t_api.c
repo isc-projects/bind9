c2170a4bd022e20d7dd75e56f9ccff8e7cbbb356
Mark Andrews <marka@isc.org>
2010-12-21 04:20:23 +0000

    2994.   [port]          NetBSD: use pthreads by default on NetBSD >= 5.0, and                         do not use threads on earlier versions.  Also kill                         the unproven-pthreads, mit-pthreads, and ptl2 support.

--- lib/tests/t_api.c
+++ lib/tests/t_api.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: t_api.c,v 1.67 2010/06/08 23:50:24 tbox Exp $ */
+/* $Id: t_api.c,v 1.68 2010/12/21 04:20:23 marka Exp $ */
 
 /*! \file */
 
@@ -242,15 +242,6 @@ main(int argc, char **argv) {
 
 	sa.sa_flags = 0;
 	sigfillset(&sa.sa_mask);
-
-#ifdef SIGCHLD
-	/*
-	 * This is mostly here for NetBSD's pthread implementation, until
-	 * people catch up to the latest unproven-pthread package.
-	 */
-	sa.sa_handler = SIG_DFL;
-	(void)sigaction(SIGCHLD, &sa, NULL);
-#endif
 
 	sa.sa_handler = t_sighandler;
 	(void)sigaction(SIGINT,  &sa, NULL);
