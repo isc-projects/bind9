16ae8f12c2bdabeae9f65ca9f4023beff4569848
Mark Andrews <marka@isc.org>
2011-01-13 08:46:34 +0000

    add -V to foreground flags

--- bin/named/win32/ntservice.c
+++ bin/named/win32/ntservice.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: ntservice.c,v 1.14 2009/06/23 23:47:44 tbox Exp $ */
+/* $Id: ntservice.c,v 1.15 2011/01/13 08:46:34 marka Exp $ */
 
 #include <config.h>
 #include <stdio.h>
@@ -70,7 +70,8 @@ int bindmain()
 	while (argv[i]) {
 		if (!strcmp(argv[i], "-f") ||
 		    !strcmp(argv[i], "-g") ||
-		    !strcmp(argv[i], "-v")) {
+		    !strcmp(argv[i], "-v") ||
+		    !strcmp(argv[i], "-V")) {
 			foreground = TRUE;
 			break;
 		}
