2ec440d467468226cde6b31feae7aedd044610e0
Automatic Updater <source@isc.org>
2011-01-13 22:29:39 +0000

    update copyright notice

--- bin/named/win32/ntservice.c
+++ bin/named/win32/ntservice.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2006, 2007, 2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2006, 2007, 2009, 2011  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: ntservice.c,v 1.12.332.3 2011/01/13 22:04:18 marka Exp $ */
+/* $Id: ntservice.c,v 1.12.332.4 2011/01/13 22:29:39 tbox Exp $ */
 
 #include <config.h>
 #include <stdio.h>
