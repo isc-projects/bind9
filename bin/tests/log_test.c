a09fff70519da03cd15ae43eacb80dbe9a6de1d9
Automatic Updater <source@isc.org>
2011-01-14 00:51:43 +0000

    update copyright notice

--- bin/tests/log_test.c
+++ bin/tests/log_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2007, 2011  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: log_test.c,v 1.27 2011/01/14 00:44:53 marka Exp $ */
+/* $Id: log_test.c,v 1.28 2011/01/14 00:51:43 tbox Exp $ */
 
 /* Principal Authors: DCL */
 
