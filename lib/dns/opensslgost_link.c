c674aacfd655b89f171c61c0a9683cf82af4b3d4
Automatic Updater <source@isc.org>
2011-01-19 23:47:12 +0000

    update copyright notice

--- lib/dns/opensslgost_link.c
+++ lib/dns/opensslgost_link.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2010, 2011  Internet Systems Consortium, Inc. ("ISC")
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -14,7 +14,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: opensslgost_link.c,v 1.4 2011/01/19 03:09:14 marka Exp $ */
+/* $Id: opensslgost_link.c,v 1.5 2011/01/19 23:47:12 tbox Exp $ */
 
 #include <config.h>
 
