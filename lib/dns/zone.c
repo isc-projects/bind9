bf7327383b5a60553cb0b58e8c5ab912f1770a2c
Automatic Updater <source@isc.org>
2011-02-07 12:15:09 +0000

    update copyright notice

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2010  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2011  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: zone.c,v 1.483.36.24 2011/02/07 00:18:39 marka Exp $ */
+/* $Id: zone.c,v 1.483.36.25 2011/02/07 12:15:09 tbox Exp $ */
 
 /*! \file */
 
