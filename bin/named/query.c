ff6618557cd03bd37fc2be1afaa2fb7ac0999677
Scott Mann <smann@isc.org>
2011-02-18 15:27:58 +0000

    Fix prz SERVFAILs after failed zone transfer (RT23246)

--- bin/named/query.c
+++ bin/named/query.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: query.c,v 1.353.8.1 2011/02/03 07:39:02 marka Exp $ */
+/* $Id: query.c,v 1.353.8.2 2011/02/18 15:27:58 smann Exp $ */
 
 /*! \file */
 
@@ -4041,8 +4041,8 @@ rpz_find(ns_client_t *client, dns_rdatatype_t qtype, dns_name_t *qnamef,
 	version = NULL;
 	result = rpz_getdb(client, rpz_type, qnamef, zonep, dbp, &version);
 	if (result != ISC_R_SUCCESS) {
-		*policyp = DNS_RPZ_POLICY_ERROR;
-		return (DNS_R_SERVFAIL);
+		*policyp = DNS_RPZ_POLICY_MISS;
+		return (DNS_R_NXDOMAIN);
 	}
 
 	dns_fixedname_init(&fixed);
