c58cd7fb62234edb89947c7313bf9b4aa0fefb72
Automatic Updater <source@isc.org>
2011-02-28 23:45:54 +0000

    update copyright notice

--- lib/isc/unix/socket.c
+++ lib/isc/unix/socket.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2010  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2011  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1998-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: socket.c,v 1.308.12.18 2011/02/28 12:54:54 marka Exp $ */
+/* $Id: socket.c,v 1.308.12.19 2011/02/28 23:45:54 tbox Exp $ */
 
 /*! \file */
 
