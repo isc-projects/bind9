343af1a5389a659afddc3d09189d3d7f8a292ae5
Automatic Updater <source@isc.org>
2011-03-02 23:47:27 +0000

    update copyright notice

--- bin/tests/system/testsock.pl
+++ bin/tests/system/testsock.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 #
-# Copyright (C) 2004, 2007, 2010  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007, 2010, 2011  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2000, 2001  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: testsock.pl,v 1.18.70.1 2011/03/02 04:47:11 marka Exp $
+# $Id: testsock.pl,v 1.18.70.2 2011/03/02 23:47:27 tbox Exp $
 
 # Test whether the interfaces on 10.53.0.* are up.
 
