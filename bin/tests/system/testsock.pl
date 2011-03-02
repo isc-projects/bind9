d6e0f47d451af53388474cc412cf79ab772903da
Mark Andrews <marka@isc.org>
2011-03-02 04:43:22 +0000

    3051.   [bug]           NS records obsure DS records at the bottom of the                         zone if both are present. [RT #23035]

--- bin/tests/system/testsock.pl
+++ bin/tests/system/testsock.pl
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: testsock.pl,v 1.16.332.2 2010/08/17 23:45:47 tbox Exp $
+# $Id: testsock.pl,v 1.16.332.3 2011/03/02 04:43:22 marka Exp $
 
 # Test whether the interfaces on 10.53.0.* are up.
 
@@ -47,5 +47,4 @@ foreach $id (@ids) {
 	    	or die sprintf("$0: bind(%s, %d): $!\n",
 			       inet_ntoa($addr), $port);
 	close(SOCK);
-	sleep(1);
 }
