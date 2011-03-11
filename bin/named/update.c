4f587beb8eb70f198346333bce5b89df3ac5c88e
Mark Andrews <marka@isc.org>
2011-03-11 12:51:40 +0000

    3071.   [bug]           has_nsec could be used unintialised in                         update.c:next_active. [RT #20256]

--- bin/named/update.c
+++ bin/named/update.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: update.c,v 1.190 2011/03/11 06:11:21 marka Exp $ */
+/* $Id: update.c,v 1.191 2011/03/11 12:51:40 marka Exp $ */
 
 #include <config.h>
 
@@ -1692,7 +1692,7 @@ next_active(ns_client_t *client, dns_zone_t *zone, dns_db_t *db,
 {
 	isc_result_t result;
 	dns_dbiterator_t *dbit = NULL;
-	isc_boolean_t has_nsec;
+	isc_boolean_t has_nsec = ISC_FALSE;
 	unsigned int wraps = 0;
 	isc_boolean_t secure = dns_db_issecure(db);
 
