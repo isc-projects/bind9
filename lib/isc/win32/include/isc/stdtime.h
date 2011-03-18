08d01c3a4fe1c4631c482c332940688a0dac192f
Automatic Updater <source@isc.org>
2011-03-18 23:46:09 +0000

    update copyright notice

--- lib/isc/win32/include/isc/stdtime.h
+++ lib/isc/win32/include/isc/stdtime.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005, 2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2007, 2011  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: stdtime.h,v 1.12.332.1 2011/03/18 09:09:45 fdupont Exp $ */
+/* $Id: stdtime.h,v 1.12.332.2 2011/03/18 23:46:09 tbox Exp $ */
 
 #ifndef ISC_STDTIME_H
 #define ISC_STDTIME_H 1
