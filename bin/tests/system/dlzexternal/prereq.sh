7dc5273a3e48fee0c4be51e3ab7ac85ddf983d9f
Mark Andrews <marka@isc.org>
2011-03-20 09:03:47 +0000

    skip not untested

--- bin/tests/system/dlzexternal/prereq.sh
+++ bin/tests/system/dlzexternal/prereq.sh
@@ -14,12 +14,12 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: prereq.sh,v 1.6 2011/03/10 23:47:50 tbox Exp $
+# $Id: prereq.sh,v 1.7 2011/03/20 09:03:47 marka Exp $
 
 TOP=${SYSTEMTESTTOP:=.}/../../../..
 
 ./dlopen ||  {
         echo "I:dlopen() not supported - skipping dlzexternal test"
-        exit 1
+        exit 255
 }
 exit 0
