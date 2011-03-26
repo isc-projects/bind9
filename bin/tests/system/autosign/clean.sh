44ecaec099c8541e5a9634d1ee44ea609cf7f263
Automatic Updater <source@isc.org>
2011-03-26 23:47:13 +0000

    update copyright notice

--- bin/tests/system/autosign/clean.sh
+++ bin/tests/system/autosign/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2009, 2010  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2009-2011  Internet Systems Consortium, Inc. ("ISC")
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -14,7 +14,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: clean.sh,v 1.7.16.1 2011/03/25 23:53:52 each Exp $
+# $Id: clean.sh,v 1.7.16.2 2011/03/26 23:47:13 tbox Exp $
 
 rm -f */K* */dsset-* */*.signed */trusted.conf */tmp* */*.jnl */*.bk
 rm -f active.key inact.key del.key unpub.key standby.key rev.key
