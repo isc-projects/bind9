e2d4cd0ae441c639cd8821e28f1e093c7855871a
Automatic Updater <source@isc.org>
2011-03-30 23:47:12 +0000

    update copyright notice

--- bin/tests/system/tsiggss/prereq.sh
+++ bin/tests/system/tsiggss/prereq.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2010  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2010, 2011  Internet Systems Consortium, Inc. ("ISC")
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -14,7 +14,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: prereq.sh,v 1.4 2011/03/30 15:48:41 smann Exp $
+# $Id: prereq.sh,v 1.5 2011/03/30 23:47:12 tbox Exp $
 
 TOP=${SYSTEMTESTTOP:=.}/../../../..
 
