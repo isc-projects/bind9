c7e1812d02d183337db991c19b4214546cf2ebb3
Scott Mann <smann@isc.org>
2011-04-05 19:16:54 +0000

    a few more "--with-gssapi" as default fixes to correct problems on test systems

--- bin/tests/system/tsiggss/gssapi_krb.c
+++ bin/tests/system/tsiggss/gssapi_krb.c
@@ -14,13 +14,19 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: gssapi_krb.c,v 1.2 2011/03/30 15:48:41 smann Exp $ */
+/* $Id: gssapi_krb.c,v 1.3 2011/04/05 19:16:54 smann Exp $ */
 
 #include <config.h>
 
 int
 main() {
-#if defined(HAVE_GSSAPI_H) && defined(HAVE_KRB5_H)
+#if (defined(HAVE_GSSAPI_H) || \
+     defined(HAVE_GSSAPI_GSSAPI_H)) && \
+    (defined(HAVE_KRB5_H) || \
+     defined(HAVE_KRB5_KRB5_H) || \
+     defined(HAVE_GSSAPI_GSSAPI_KRB5_H) || \
+     defined(HAVE_GSSAPI_KRB5_H) || \
+     defined(HAVE_KERBEROSV5_KRB5_H))
 	return (0);
 #else
 	return (1);
