06584e923e71acc369e4c04a28377fa90374fd43
Automatic Updater <source@isc.org>
2011-04-19 23:46:18 +0000

    update copyright notice

--- bin/tests/system/dlz/tests.sh
+++ bin/tests/system/dlz/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2010  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2010, 2011  Internet Systems Consortium, Inc. ("ISC")
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -14,7 +14,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: tests.sh,v 1.2.4.3 2011/04/19 22:32:51 each Exp $
+# $Id: tests.sh,v 1.2.4.4 2011/04/19 23:46:18 tbox Exp $
 
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
