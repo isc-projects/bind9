f1d4986b832124b63905e2d5ca401d1d0498c9b8
Mark Andrews <marka@isc.org>
2011-05-02 23:56:59 +0000

    treat asb(x) < 500ms as 0

--- bin/tests/system/autosign/tests.sh
+++ bin/tests/system/autosign/tests.sh
@@ -14,7 +14,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: tests.sh,v 1.25 2011/05/02 05:05:05 marka Exp $
+# $Id: tests.sh,v 1.26 2011/05/02 23:56:59 marka Exp $
 
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
@@ -897,9 +897,15 @@ check_interval () {
             awk -F: '
                      {
                        x = ($6+ $5*60000 + $4*3600000) - ($3+ $2*60000 + $1*3600000);
+		       # abs(x) < 500 ms treat as 'now'
+		       if (x < 500 && x > -500)
+                         x = 0;
+		       # convert to seconds
 		       x = x/1000;
+		       # handle end of day roll over
 		       if (x < 0)
 			 x = x + 24*3600;
+		       # handle log timestamp being a few milliseconds later
                        if (x != int(x))
                          x = int(x + 1);
                        if (int(x) > int(interval))
