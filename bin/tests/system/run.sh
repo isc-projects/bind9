e1dcab768c55740781467f5c57da664edf700eb3
Automatic Updater <source@isc.org>
2011-05-02 23:46:06 +0000

    update copyright notice

--- bin/tests/system/run.sh
+++ bin/tests/system/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007, 2011  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2000, 2001  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: run.sh,v 1.42.332.1 2011/05/01 23:38:23 marka Exp $
+# $Id: run.sh,v 1.42.332.2 2011/05/02 23:46:06 tbox Exp $
 
 #
 # Run a system test.
