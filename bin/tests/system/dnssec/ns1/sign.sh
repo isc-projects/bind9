9f0648fcdb547e45d219e93de5805e0bb871eb95
Mark Andrews <marka@isc.org>
2011-05-03 16:09:48 +0000

    grep was not precise enough leading to test failure

--- bin/tests/system/dnssec/ns1/sign.sh
+++ bin/tests/system/dnssec/ns1/sign.sh
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: sign.sh,v 1.30.32.4 2010/11/16 01:21:49 marka Exp $
+# $Id: sign.sh,v 1.30.32.5 2011/05/03 16:09:48 marka Exp $
 
 SYSTEMTESTTOP=../..
 . $SYSTEMTESTTOP/conf.sh
@@ -30,7 +30,7 @@ zonefile=root.db
 
 cp ../ns2/dsset-example. .
 cp ../ns2/dsset-dlv. .
-grep "8 [12]" ../ns2/dsset-algroll. > dsset-algroll.
+grep "8 [12] " ../ns2/dsset-algroll. > dsset-algroll.
 
 keyname=`$KEYGEN -q -r $RANDFILE -a RSAMD5 -b 768 -n zone $zone`
 
