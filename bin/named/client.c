cdd50f38ecd12ce44ce912bc5b918d388db940be
Automatic Updater <source@isc.org>
2011-05-06 23:45:18 +0000

    update copyright notice

--- bin/named/client.c
+++ bin/named/client.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2009, 2011  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: client.c,v 1.219.18.34 2011/05/05 23:59:10 marka Exp $ */
+/* $Id: client.c,v 1.219.18.35 2011/05/06 23:45:18 tbox Exp $ */
 
 #include <config.h>
 
