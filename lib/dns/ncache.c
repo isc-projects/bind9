9b1f73380161febf85d1b870d16908ac5954e2b1
Evan Hunt <each@isc.org>
2011-05-26 23:10:44 +0000

    3121.   [security]      An authoritative name server sending a negative                         response containing a very large RRset could                         trigger an off-by-one error in the ncache code                         and crash named. [RT #24650]

--- lib/dns/ncache.c
+++ lib/dns/ncache.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: ncache.c,v 1.50.124.2 2011/02/28 01:20:02 tbox Exp $ */
+/* $Id: ncache.c,v 1.50.124.3 2011/05/26 23:10:44 each Exp $ */
 
 /*! \file */
 
@@ -186,7 +186,7 @@ dns_ncache_addoptout(dns_message_t *message, dns_db_t *cache,
 					 */
 					isc_buffer_availableregion(&buffer,
 								   &r);
-					if (r.length < 2)
+					if (r.length < 3)
 						return (ISC_R_NOSPACE);
 					isc_buffer_putuint16(&buffer,
 							     rdataset->type);
