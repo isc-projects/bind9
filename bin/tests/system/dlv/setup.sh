539e5b98ec22b9b107e158a25c57b4580b74b37f
Evan Hunt <each@isc.org>
2011-05-26 23:56:27 +0000

    rolling 9.4-ESV-R4-P1:
    3121.   [security]      An authoritative name server sending a negative
                            response containing a very large RRset could
                            trigger an off-by-one error in the ncache code
                            and crash named. [RT #24650]
    
    3120.	[bug]		Named could fail to validate zones listed in a DLV
    			that validated insecure without using DLV and had
    			DS records in the parent zone. [RT #24631]

--- bin/tests/system/dlv/setup.sh
+++ bin/tests/system/dlv/setup.sh
@@ -14,8 +14,8 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: setup.sh,v 1.2 2004/05/14 04:58:19 marka Exp $
+# $Id: setup.sh,v 1.2.2.1.82.1 2011/05/26 23:56:26 each Exp $
 
 ../../genrandom 400 random.data
 
-(cd ns3 && sh -e sign.sh)
+(cd ns1 && sh -e sign.sh)
