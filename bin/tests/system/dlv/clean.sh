9e914eadd0bfba686bd87c761ffff5a19d7cf641
Automatic Updater <source@isc.org>
2011-05-26 23:47:06 +0000

    update copyright notice

--- bin/tests/system/dlv/clean.sh
+++ bin/tests/system/dlv/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004, 2007, 2010  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007, 2010, 2011  Internet Systems Consortium, Inc. ("ISC")
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -14,7 +14,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: clean.sh,v 1.7.120.1 2011/05/26 04:47:14 marka Exp $
+# $Id: clean.sh,v 1.7.120.2 2011/05/26 23:47:05 tbox Exp $
 
 rm -f random.data
 rm -f ns*/named.run
