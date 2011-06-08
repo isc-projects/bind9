ec0f6a33650b3cba86bae87c1f4d315d22f9ca84
Automatic Updater <source@isc.org>
2011-06-08 23:46:25 +0000

    update copyright notice

--- lib/dns/message.c
+++ lib/dns/message.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2010  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2011  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: message.c,v 1.245.50.7.6.1 2011/06/08 22:03:10 each Exp $ */
+/* $Id: message.c,v 1.245.50.7.6.2 2011/06/08 23:46:25 tbox Exp $ */
 
 /*! \file */
 
