c33e22f65a4b3f8819971929343d45ce94f25e18
Automatic Updater <source@isc.org>
2011-07-21 23:47:53 +0000

    update copyright notice

--- lib/isc/unix/socket.c
+++ lib/isc/unix/socket.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: socket.c,v 1.339 2011/07/21 01:40:50 marka Exp $ */
+/* $Id: socket.c,v 1.340 2011/07/21 23:47:53 tbox Exp $ */
 
 /*! \file */
 
@@ -1388,7 +1388,7 @@ build_msghdr_send(isc__socket_t *sock, isc_socketevent_t *dev,
 #if defined(IPV6_USE_MIN_MTU)
 		/*
 		 * Set IPV6_USE_MIN_MTU as a per packet option as FreeBSD
-		 * ignores setsockopt(IPV6_USE_MIN_MTU) when IPV6_PKTINFO 
+		 * ignores setsockopt(IPV6_USE_MIN_MTU) when IPV6_PKTINFO
 		 * is used.
 		 */
 		cmsgp = (struct cmsghdr *)(sock->sendcmsgbuf +
