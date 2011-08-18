016b20872a52a5ac54fbf0651b7d0b6e5da3a55e
Mark Andrews <marka@isc.org>
2011-08-18 17:37:14 +0000

    cast to unsigned

--- lib/dns/dst_api.c
+++ lib/dns/dst_api.c
@@ -31,7 +31,7 @@
 
 /*
  * Principal Author: Brian Wellington
- * $Id: dst_api.c,v 1.47.22.7 2011/08/18 06:41:59 marka Exp $
+ * $Id: dst_api.c,v 1.47.22.8 2011/08/18 17:37:14 marka Exp $
  */
 
 /*! \file */
@@ -1507,7 +1507,7 @@ write_public_key(const dst_key_t *key, int type, const char *directory) {
 	fprintf(fp, " ");
 
 	isc_buffer_usedregion(&classb, &r);
-	if (isc_util_fwrite(r.base, 1, r.length, fp) != (int)r.length)
+	if ((unsigned)isc_util_fwrite(r.base, 1, r.length, fp) != r.length)
                ret = DST_R_WRITEERROR;
 
 	if ((type & DST_TYPE_KEY) != 0)
@@ -1516,7 +1516,7 @@ write_public_key(const dst_key_t *key, int type, const char *directory) {
 		fprintf(fp, " DNSKEY ");
 
 	isc_buffer_usedregion(&textb, &r);
-	if (isc_util_fwrite(r.base, 1, r.length, fp) != (int)r.length)
+	if ((unsigned)isc_util_fwrite(r.base, 1, r.length, fp) != r.length)
                ret = DST_R_WRITEERROR;
 
 	fputc('\n', fp);
