45a89e832db35e03c6345ea1fe4cd1c1570ba89a
Scott Mann <smann@isc.org>
2011-08-24 19:53:11 +0000

    Fix compilation failure in ATF due to gcc4.6.0 "improvements." RT #25598.

--- unit/atf-src/atf-c/check.c
+++ unit/atf-src/atf-c/check.c
@@ -301,13 +301,12 @@ atf_check_result_init(atf_check_result_t *r, const char *const *argv,
                       const atf_fs_path_t *dir)
 {
     atf_error_t err;
-    const char *workdir;
 
     r->pimpl = malloc(sizeof(struct atf_check_result_impl));
     if (r->pimpl == NULL)
         return atf_no_memory_error();
 
-    workdir = atf_config_get("atf_workdir");
+    (void) atf_config_get("atf_workdir");
 
     err = array_to_list(argv, &r->pimpl->m_argv);
     if (atf_is_error(err))
