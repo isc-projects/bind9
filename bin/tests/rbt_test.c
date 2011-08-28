82ed23194ee14f8f3c2929fdb38bdabe5ea32f46
Automatic Updater <source@isc.org>
2011-08-28 23:45:28 +0000

    update copyright notice

--- bin/tests/rbt_test.c
+++ bin/tests/rbt_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005, 2007, 2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2007, 2009, 2011  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: rbt_test.c,v 1.50.104.1 2011/08/28 09:28:32 marka Exp $ */
+/* $Id: rbt_test.c,v 1.50.104.2 2011/08/28 23:45:27 tbox Exp $ */
 
 #include <config.h>
 
