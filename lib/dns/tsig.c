72c727e63b4c90741049ed20bd04820c3688813b
Mark Andrews <marka@isc.org>
2011-08-29 04:05:12 +0000

    INSIST(response);

--- lib/dns/tsig.c
+++ lib/dns/tsig.c
@@ -16,7 +16,7 @@
  */
 
 /*
- * $Id: tsig.c,v 1.136.18.7 2011/08/25 23:45:14 tbox Exp $
+ * $Id: tsig.c,v 1.136.18.8 2011/08/29 04:05:12 marka Exp $
  */
 /*! \file */
 #include <config.h>
@@ -824,8 +824,10 @@ dns_tsig_sign(dns_message_t *msg) {
 		}
 		/* Digest the timesigned and fudge */
 		isc_buffer_clear(&databuf);
-		if (tsig.error == dns_tsigerror_badtime)
+		if (tsig.error == dns_tsigerror_badtime) {
+			INSIST(response);
 			tsig.timesigned = querytsig.timesigned;
+		}
 		isc_buffer_putuint48(&databuf, tsig.timesigned);
 		isc_buffer_putuint16(&databuf, tsig.fudge);
 		isc_buffer_usedregion(&databuf, &r);
