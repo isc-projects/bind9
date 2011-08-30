87d1c0b3e201c70acdbda76dc538bf0947d9e474
Mark Andrews <marka@isc.org>
2011-08-30 14:05:34 +0000

    silence clang warnings

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: zone.c,v 1.483.36.34 2011/08/09 03:18:38 marka Exp $ */
+/* $Id: zone.c,v 1.483.36.35 2011/08/30 14:05:34 marka Exp $ */
 
 /*! \file */
 
@@ -7476,7 +7476,7 @@ refresh_callback(isc_task_t *task, isc_event_t *event) {
 	dns_rdata_t rdata = DNS_RDATA_INIT;
 	dns_rdata_soa_t soa;
 	isc_result_t result;
-	isc_uint32_t serial, oldserial;
+	isc_uint32_t serial, oldserial = 0;
 	unsigned int j;
 
 	zone = revent->ev_arg;
