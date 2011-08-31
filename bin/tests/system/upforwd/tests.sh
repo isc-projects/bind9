a6ab2774bcead89018699763eeb106cf444c92da
Automatic Updater <source@isc.org>
2011-08-31 23:46:44 +0000

    update copyright notice

--- bin/tests/system/upforwd/tests.sh
+++ bin/tests/system/upforwd/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007, 2011  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2000, 2001  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: tests.sh,v 1.11 2011/08/31 06:49:10 marka Exp $ 
+# $Id: tests.sh,v 1.12 2011/08/31 23:46:43 tbox Exp $ 
 
 # ns1 = stealth master
 # ns2 = slave with update forwarding disabled; not currently used
