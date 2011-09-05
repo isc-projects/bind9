b3d6edf7cf7127582ec6e4846f2360db4e79d05e
Automatic Updater <source@isc.org>
2011-09-05 23:46:54 +0000

    update copyright notice

--- bin/tests/cfg_test.c
+++ bin/tests/cfg_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005, 2007, 2009, 2010  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2007, 2009-2011  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2001, 2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: cfg_test.c,v 1.24 2011/09/05 18:00:22 each Exp $ */
+/* $Id: cfg_test.c,v 1.25 2011/09/05 23:46:54 tbox Exp $ */
 
 /*! \file */
 
