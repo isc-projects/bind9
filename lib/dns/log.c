c43b3107b6ba40b0bcd67710a27219ed8bc64ff4
Automatic Updater <source@isc.org>
2011-10-15 23:46:14 +0000

    update copyright notice

--- lib/dns/log.c
+++ lib/dns/log.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2007, 2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007, 2009, 2011  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2001, 2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: log.c,v 1.47.198.1 2011/10/14 03:51:06 marka Exp $ */
+/* $Id: log.c,v 1.47.198.2 2011/10/15 23:46:14 tbox Exp $ */
 
 /*! \file */
 
