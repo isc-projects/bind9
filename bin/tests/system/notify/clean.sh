611c5eacdb55e13cc1c1f92e2dfb98c8aaaf646c
Automatic Updater <source@isc.org>
2011-10-17 23:45:22 +0000

    update copyright notice

--- bin/tests/system/notify/clean.sh
+++ bin/tests/system/notify/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007, 2011  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2000, 2001  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: clean.sh,v 1.12.250.1 2011/10/17 02:41:22 marka Exp $
+# $Id: clean.sh,v 1.12.250.2 2011/10/17 23:45:21 tbox Exp $
 
 #
 # Clean up after zone transfer tests.
