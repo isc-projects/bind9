12cc8e4b55df18a747734304fe0ed2201a0f6381
Automatic Updater <source@isc.org>
2011-10-17 23:45:49 +0000

    update copyright notice

--- bin/tests/system/lwresd/tests.sh
+++ bin/tests/system/lwresd/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007, 2011  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2000, 2001  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: tests.sh,v 1.18.558.1 2011/10/17 05:43:43 marka Exp $
+# $Id: tests.sh,v 1.18.558.2 2011/10/17 23:45:48 tbox Exp $
 
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
