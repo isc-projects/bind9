fccaeca218d042166fdd7640a490f447cd9ef2dd
Automatic Updater <source@isc.org>
2011-10-17 23:46:09 +0000

    update copyright notice

--- bin/tests/system/lwresd/tests.sh
+++ bin/tests/system/lwresd/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007, 2011  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2000, 2001  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: tests.sh,v 1.18.814.1 2011/10/17 05:43:18 marka Exp $
+# $Id: tests.sh,v 1.18.814.2 2011/10/17 23:46:06 tbox Exp $
 
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
--- bin/tests/system/notify/clean.sh
+++ bin/tests/system/notify/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007, 2011  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2000, 2001  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: clean.sh,v 1.12.732.1 2011/10/17 02:00:16 marka Exp $
+# $Id: clean.sh,v 1.12.732.2 2011/10/17 23:46:09 tbox Exp $
 
 #
 # Clean up after zone transfer tests.
