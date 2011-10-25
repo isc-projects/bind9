1e112f85f681b86a78fa34dd6ac0ab30bab9517b
Automatic Updater <source@isc.org>
2011-10-25 23:46:16 +0000

    update copyright notice

--- lib/isc/hmacsha.c
+++ lib/isc/hmacsha.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2007, 2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2005-2007, 2009, 2011  Internet Systems Consortium, Inc. ("ISC")
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -14,7 +14,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: hmacsha.c,v 1.10.168.1 2011/10/25 03:13:54 marka Exp $ */
+/* $Id: hmacsha.c,v 1.10.168.2 2011/10/25 23:46:16 tbox Exp $ */
 
 /*
  * This code implements the HMAC-SHA1, HMAC-SHA224, HMAC-SHA256, HMAC-SHA384
