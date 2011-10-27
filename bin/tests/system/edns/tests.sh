3f5b58b3fa2e450aa4f66036910b2c94dc32dac1
Automatic Updater <source@isc.org>
2011-10-27 23:45:57 +0000

    update copyright notice

--- bin/tests/system/edns/tests.sh
+++ bin/tests/system/edns/tests.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004, 2007, 2011  Internet Systems Consortium, Inc. ("ISC")
-# Copyright (C) 2000, 2001  Internet Software Consortium.
+# Copyright (C) 2011  Internet Systems Consortium, Inc. ("ISC")
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -15,7 +14,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: tests.sh,v 1.2.2.2 2011/10/27 20:26:11 smann Exp $ 
+# $Id: tests.sh,v 1.2.2.3 2011/10/27 23:45:56 tbox Exp $ 
 
 # ns1 = forward only server
 # ans2 = modified ans.pl master
