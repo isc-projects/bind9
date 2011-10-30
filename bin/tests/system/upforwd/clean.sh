f308b3c2e05e269e6e5f7067b60f7ef4627a4054
Automatic Updater <source@isc.org>
2011-10-30 23:46:15 +0000

    update copyright notice

--- bin/tests/system/upforwd/clean.sh
+++ bin/tests/system/upforwd/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007, 2011  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2000, 2001  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: clean.sh,v 1.10 2011/10/30 23:14:23 each Exp $
+# $Id: clean.sh,v 1.11 2011/10/30 23:46:15 tbox Exp $
 
 #
 # Clean up after zone transfer tests.
