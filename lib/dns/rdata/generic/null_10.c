1ab9944f6b41e6b52da60ff2e328aa49b30e3989
Automatic Updater <source@isc.org>
2011-11-02 23:46:24 +0000

    update copyright notice

--- lib/dns/rdata/generic/null_10.c
+++ lib/dns/rdata/generic/null_10.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2007, 2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2007, 2009, 2011  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1998-2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: null_10.c,v 1.45 2011/11/02 01:01:52 marka Exp $ */
+/* $Id: null_10.c,v 1.46 2011/11/02 23:46:24 tbox Exp $ */
 
 /* Reviewed: Thu Mar 16 13:57:50 PST 2000 by explorer */
 
