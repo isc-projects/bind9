8558d6300e16bce265979a0cc51d8122068e4e7f
Automatic Updater <source@isc.org>
2011-11-03 23:45:34 +0000

    update copyright notice

--- bin/tests/system/tkey/tests.sh
+++ bin/tests/system/tkey/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004, 2007, 2009  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007, 2009, 2011  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2001  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: tests.sh,v 1.9.126.1 2011/11/03 22:06:43 each Exp $
+# $Id: tests.sh,v 1.9.126.2 2011/11/03 23:45:34 tbox Exp $
 
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
