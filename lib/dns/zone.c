77b8f02d19efea79c34f0d4afcefc7ad65de8000
Evan Hunt <each@isc.org>
2011-11-03 03:26:19 +0000

    3195.	[cleanup]	Silence "file not found" warnings when loading 			managed-keys zone. [RT #26340]

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: zone.c,v 1.645 2011/11/03 02:54:47 each Exp $ */
+/* $Id: zone.c,v 1.646 2011/11/03 03:26:19 each Exp $ */
 
 /*! \file */
 
@@ -3514,7 +3514,11 @@ zone_postload(dns_zone_t *zone, dns_db_t *db, isc_time_t loadtime,
 				     "no master file, requesting db");
 			maybe_send_securedb(zone);
 		} else {
-			dns_zone_log(zone, ISC_LOG_ERROR,
+			int level = ISC_LOG_ERROR;
+			if (zone->type == dns_zone_key &&
+			    result == ISC_R_FILENOTFOUND)
+				level = ISC_LOG_DEBUG(1);
+			dns_zone_log(zone, level,
 				     "loading from master file %s failed: %s",
 				     zone->masterfile,
 				     dns_result_totext(result));
