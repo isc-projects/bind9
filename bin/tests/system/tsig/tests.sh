5e62f59109d5faa5491a25ae8994034730f3e66f
Automatic Updater <source@isc.org>
2011-11-06 23:45:51 +0000

    update copyright notice

--- bin/tests/system/tsig/tests.sh
+++ bin/tests/system/tsig/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2005-2007  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2005-2007, 2011  Internet Systems Consortium, Inc. ("ISC")
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -14,7 +14,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: tests.sh,v 1.5.558.1 2011/11/06 23:25:29 marka Exp $
+# $Id: tests.sh,v 1.5.558.2 2011/11/06 23:45:51 tbox Exp $
 
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
