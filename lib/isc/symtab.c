95304538774ed8dedcd65fe074899adfe1c7f4a6
Mark Andrews <marka@isc.org>
2011-11-30 06:07:52 +0000

    move declaration to start of block

--- lib/isc/symtab.c
+++ lib/isc/symtab.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: symtab.c,v 1.30.558.1 2011/11/30 04:26:22 each Exp $ */
+/* $Id: symtab.c,v 1.30.558.2 2011/11/30 06:07:52 marka Exp $ */
 
 /*! \file */
 
@@ -206,8 +206,9 @@ grow_table(isc_symtab_t *symtab) {
 		elt_t *elt, *nelt;
 
 		for (elt = HEAD(symtab->table[i]); elt != NULL; elt = nelt) {
-			nelt = NEXT(elt, link);
 			unsigned int hv;
+
+			nelt = NEXT(elt, link);
 
 			UNLINK(symtab->table[i], elt, link);
 			hv = hash(elt->key, symtab->case_sensitive);
