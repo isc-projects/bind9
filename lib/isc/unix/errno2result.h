a09e70c94769dcb03c47b89667b591950a29bcb7
Automatic Updater <source@isc.org>
2011-12-02 23:46:56 +0000

    update copyright notice

--- lib/isc/unix/errno2result.h
+++ lib/isc/unix/errno2result.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005, 2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2007, 2011  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000, 2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: errno2result.h,v 1.13 2011/12/02 07:15:17 marka Exp $ */
+/* $Id: errno2result.h,v 1.14 2011/12/02 23:46:56 tbox Exp $ */
 
 #ifndef UNIX_ERRNO2RESULT_H
 #define UNIX_ERRNO2RESULT_H 1
