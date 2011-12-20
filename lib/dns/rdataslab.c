90d80c57821d36a8811eaae3a3ab1de39be3d781
Automatic Updater <source@isc.org>
2011-12-20 23:45:15 +0000

    update copyright notice

--- lib/dns/rdataslab.c
+++ lib/dns/rdataslab.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2010  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2011  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: rdataslab.c,v 1.48.50.5 2011/12/20 00:56:58 marka Exp $ */
+/* $Id: rdataslab.c,v 1.48.50.6 2011/12/20 23:45:15 tbox Exp $ */
 
 /*! \file */
 
