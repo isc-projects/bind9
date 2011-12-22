ac255047aa7925107809da2b2bf724cf999dee2b
Automatic Updater <source@isc.org>
2011-12-22 23:45:33 +0000

    update copyright notice

--- bin/check/named-checkzone.c
+++ bin/check/named-checkzone.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2010  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2011  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: named-checkzone.c,v 1.59.4.3 2011/12/22 17:28:32 each Exp $ */
+/* $Id: named-checkzone.c,v 1.59.4.4 2011/12/22 23:45:33 tbox Exp $ */
 
 /*! \file */
 
