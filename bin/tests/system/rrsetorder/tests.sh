e16ecd6aba160f396fbf98113703d9bad3a5be2b
Automatic Updater <source@isc.org>
2011-12-23 23:45:49 +0000

    update copyright notice

--- bin/tests/system/rrsetorder/tests.sh
+++ bin/tests/system/rrsetorder/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2006-2008  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2006-2008, 2011  Internet Systems Consortium, Inc. ("ISC")
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -14,7 +14,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: tests.sh,v 1.8.44.2 2011/12/23 01:08:14 marka Exp $
+# $Id: tests.sh,v 1.8.44.3 2011/12/23 23:45:49 tbox Exp $
 
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
