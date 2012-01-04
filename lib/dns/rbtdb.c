eec7032275e62f88f48d043881e2daa39ffa54f1
Evan Hunt <each@isc.org>
2012-01-04 03:12:03 +0000

    3260.	[bug]		"rrset-order cyclic" could appear not to rotate 			for some query patterns.  [RT #27170/27185]

--- lib/dns/rbtdb.c
+++ lib/dns/rbtdb.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: rbtdb.c,v 1.270.12.39 2011/12/22 23:55:25 marka Exp $ */
+/* $Id: rbtdb.c,v 1.270.12.40 2012/01/04 03:12:03 each Exp $ */
 
 /*! \file */
 
@@ -2697,14 +2697,6 @@ zone_zonecut_callback(dns_rbtnode_t *node, dns_name_t *name, void *arg) {
 	return (result);
 }
 
-static inline unsigned int
-prand(isc_uint32_t val) {
-	val ^= val >> 16;
-	val ^= val >> 8;
-	val ^= val >> 4;
-	return (val & 0xf);
-}
-
 static inline void
 bind_rdataset(dns_rbtdb_t *rbtdb, dns_rbtnode_t *node,
 	      rdatasetheader_t *header, isc_stdtime_t now,
@@ -2744,8 +2736,6 @@ bind_rdataset(dns_rbtdb_t *rbtdb, dns_rbtnode_t *node,
 	raw = (unsigned char *)header + sizeof(*header);
 	rdataset->private3 = raw;
 	rdataset->count = header->count++;
-	/* Add a weak pseudo random value [0..15]. */
-	header->count += prand(header->count);
 	if (rdataset->count == ISC_UINT32_MAX)
 		rdataset->count = 0;
 
