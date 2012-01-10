25ec2a4109b6fc561c9e88247725299138177cd9
Automatic Updater <source@isc.org>
2012-01-10 23:46:00 +0000

    update copyright notice

--- bin/named/builtin.c
+++ bin/named/builtin.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005, 2007, 2009, 2010  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2007, 2009, 2010, 2012  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2001-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: builtin.c,v 1.15.154.3 2012/01/10 00:27:47 smann Exp $ */
+/* $Id: builtin.c,v 1.15.154.4 2012/01/10 23:46:00 tbox Exp $ */
 
 /*! \file
  * \brief
