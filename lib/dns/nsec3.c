553cfe55d58861e39b03f6801a71fdf2b768362d
Mark Andrews <marka@isc.org>
2012-01-27 00:59:07 +0000

    3266.   [bug]           The maximum number of NSEC3 iterations for a                         DNSKEY RRset was not being properly computed.                         [RT #26543]

--- lib/dns/nsec3.c
+++ lib/dns/nsec3.c
@@ -14,7 +14,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: nsec3.c,v 1.19.24.3 2011/06/08 23:02:42 each Exp $ */
+/* $Id: nsec3.c,v 1.19.24.4 2012/01/27 00:59:06 marka Exp $ */
 
 #include <config.h>
 
@@ -1784,7 +1784,7 @@ dns_nsec3_maxiterations(dns_db_t *db, dns_dbversion_t *version,
 	dst_key_t *key = NULL;
 	isc_buffer_t buffer;
 	isc_result_t result;
-	isc_uint16_t bits, minbits = 4096;
+	unsigned int bits, minbits = 4096;
 
 	result = dns_db_getoriginnode(db, &node);
 	if (result != ISC_R_SUCCESS)
@@ -1811,7 +1811,7 @@ dns_nsec3_maxiterations(dns_db_t *db, dns_dbversion_t *version,
 		isc_buffer_add(&buffer, rdata.length);
 		CHECK(dst_key_fromdns(dns_db_origin(db), rdataset.rdclass,
 				      &buffer, mctx, &key));
-		bits = dst_key_getbits(key);
+		bits = dst_key_size(key);
 		dst_key_free(&key);
 		if (minbits > bits)
 			minbits = bits;
