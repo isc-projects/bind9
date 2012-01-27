0d26ffb39800223580c3b29d26c87793e6c0b229
Mark Andrews <marka@isc.org>
2012-01-27 02:10:20 +0000

    back port dns_test_loaddb from 9.9.0

--- lib/dns/tests/dnstest.h
+++ lib/dns/tests/dnstest.h
@@ -14,7 +14,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: dnstest.h,v 1.2.4.4 2011/11/04 07:33:47 marka Exp $ */
+/* $Id: dnstest.h,v 1.2.4.5 2012/01/27 02:10:20 marka Exp $ */
 
 /*! \file */
 
@@ -75,3 +75,7 @@ dns_test_closezonemgr(void);
 
 void
 dns_test_nap(isc_uint32_t usec);
+
+isc_result_t
+dns_test_loaddb(dns_db_t **db, dns_dbtype_t dbtype, const char *origin,
+		const char *testfile);
