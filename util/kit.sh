3b3e45d9b1aef64fb3702bc5bed8faaefe4ee586
Automatic Updater <source@isc.org>
2012-01-30 23:45:37 +0000

    update copyright notice

--- util/kit.sh
+++ util/kit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2004, 2007-2010  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2004, 2007-2009, 2012  Internet Systems Consortium, Inc. ("ISC")
 # Copyright (C) 2000-2003  Internet Software Consortium.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: kit.sh,v 1.33.44.4 2012/01/30 00:56:34 marka Exp $
+# $Id: kit.sh,v 1.33.44.5 2012/01/30 23:45:37 tbox Exp $
 
 # Make a release kit
 #
