426a6a5dfb255b5885bbc435101c74ada4f6c089
Automatic Updater <source@isc.org>
2012-02-02 23:46:42 +0000

    update copyright notice

--- bin/tests/system/autosign/tests.sh
+++ bin/tests/system/autosign/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2009-2011  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2009-2012  Internet Systems Consortium, Inc. ("ISC")
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -14,7 +14,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: tests.sh,v 1.12.18.24 2012/02/02 03:50:14 marka Exp $
+# $Id: tests.sh,v 1.12.18.25 2012/02/02 23:46:42 tbox Exp $
 
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
