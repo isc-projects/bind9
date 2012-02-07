a0bde96546e5015c87c482bc78c092971da283b7
Mark Andrews <marka@isc.org>
2012-02-07 19:53:53 +0000

    3281.   [bug]           SOA refresh queries could be treated as cancelled                         despite succeeding over the loopback interface.                         [RT #27782]

--- lib/dns/request.c
+++ lib/dns/request.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: request.c,v 1.85.104.2 2011/03/12 04:58:28 tbox Exp $ */
+/* $Id: request.c,v 1.85.104.3 2012/02/07 19:53:53 marka Exp $ */
 
 /*! \file */
 
@@ -1130,9 +1130,7 @@ req_render(dns_message_t *message, isc_buffer_t **bufferp,
  */
 static void
 send_if_done(dns_request_t *request, isc_result_t result) {
-	if (!DNS_REQUEST_CONNECTING(request) &&
-	    !DNS_REQUEST_SENDING(request) &&
-	    !request->canceling)
+	if (request->event != NULL && !request->canceling)
 		req_sendevent(request, result);
 }
 
@@ -1316,8 +1314,8 @@ req_senddone(isc_task_t *task, isc_event_t *event) {
 		else
 			send_if_done(request, ISC_R_CANCELED);
 	} else if (sevent->result != ISC_R_SUCCESS) {
-			req_cancel(request);
-			send_if_done(request, ISC_R_CANCELED);
+		req_cancel(request);
+		send_if_done(request, ISC_R_CANCELED);
 	}
 	UNLOCK(&request->requestmgr->locks[request->hash]);
 
