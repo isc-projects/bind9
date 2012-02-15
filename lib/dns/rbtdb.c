1646f6b739aba6fc39d913d1006ffb6a3658ac60
Mark Andrews <marka@isc.org>
2012-02-15 12:34:26 +0000

    2x POST(locktype);

--- lib/dns/rbtdb.c
+++ lib/dns/rbtdb.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: rbtdb.c,v 1.270.12.44 2012/02/15 02:04:24 marka Exp $ */
+/* $Id: rbtdb.c,v 1.270.12.45 2012/02/15 12:34:26 marka Exp $ */
 
 /*! \file */
 
@@ -1576,6 +1576,8 @@ reactivate_node(dns_rbtdb_t *rbtdb, dns_rbtnode_t *node,
 	nodelock_t *nodelock = &rbtdb->node_locks[node->locknum].lock;
 	isc_boolean_t maybe_cleanup = ISC_FALSE;
 
+	POST(locktype);
+
 	NODE_STRONGLOCK(nodelock);
 	NODE_WEAKLOCK(nodelock, locktype);
 
@@ -1594,6 +1596,7 @@ reactivate_node(dns_rbtdb_t *rbtdb, dns_rbtnode_t *node,
 		 */
 		NODE_WEAKUNLOCK(nodelock, locktype);
 		locktype = isc_rwlocktype_write;
+		POST(locktype);
 		NODE_WEAKLOCK(nodelock, locktype);
 		if (ISC_LINK_LINKED(node, deadlink))
 			ISC_LIST_UNLINK(rbtdb->deadnodes[node->locknum],
