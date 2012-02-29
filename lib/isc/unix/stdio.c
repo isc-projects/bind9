9fc034d29ffd4ad95849a150b16d2e4ceeeac2e4
Automatic Updater <source@isc.org>
2012-02-29 23:46:55 +0000

    update copyright notice

--- lib/isc/unix/stdio.c
+++ lib/isc/unix/stdio.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2007, 2011  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2007, 2011, 2012  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000, 2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: stdio.c,v 1.11.20.1 2012/02/29 21:27:47 each Exp $ */
+/* $Id: stdio.c,v 1.11.20.2 2012/02/29 23:46:55 tbox Exp $ */
 
 #include <config.h>
 
