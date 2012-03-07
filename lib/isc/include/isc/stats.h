c95beeb07feb92f90fcae034ec1b1be53c98604f
Evan Hunt <each@isc.org>
2012-03-07 08:19:24 -0800

    set $Id$

--- lib/isc/include/isc/stats.h
+++ lib/isc/include/isc/stats.h
@@ -14,7 +14,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: stats.h,v 1.4 2009/01/29 01:03:56 jinmei Exp $ */
+/* $Id$ */
 
 #ifndef ISC_STATS_H
 #define ISC_STATS_H 1
--- lib/isc/unix/include/isc/net.h
+++ lib/isc/unix/include/isc/net.h
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: net.h,v 1.50 2008/12/01 04:14:54 marka Exp $ */
+/* $Id$ */
 
 #ifndef ISC_NET_H
 #define ISC_NET_H 1
