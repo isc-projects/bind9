d8e73b1c76f2c7dcc641bfa7759c15df58c82c7e
Mark Andrews <marka@isc.org>
2012-03-15 11:33:43 +1100

    3296.   [bug]           Named could die with a INSIST failure in                         client.c:exit_check. [RT #28346]

--- bin/named/client.c
+++ bin/named/client.c
@@ -240,7 +240,7 @@ ns_client_recursing(ns_client_t *client) {
 	REQUIRE(client->state == NS_CLIENTSTATE_WORKING);
 
 	LOCK(&client->manager->reclock);
-	client->state = NS_CLIENTSTATE_RECURSING;
+	client->newstate = client->state = NS_CLIENTSTATE_RECURSING;
 	ISC_LIST_APPEND(client->manager->recursing, client, rlink);
 	UNLOCK(&client->manager->reclock);
 }
