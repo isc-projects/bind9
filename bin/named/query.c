8b79e1aed570d951a23fbe8325d40317bf6f4d61
Tinderbox User <tbox@isc.org>
2012-07-25 23:45:36 +0000

    update copyright notice

--- bin/named/query.c
+++ bin/named/query.c
@@ -2783,7 +2783,7 @@ mark_secure(ns_client_t *client, dns_db_t *db, dns_name_t *name,
 	dns_dbnode_t *node = NULL;
 	dns_clientinfomethods_t cm;
 	dns_clientinfo_t ci;
-	isc_stdtime_t now;	
+	isc_stdtime_t now;
 	rdataset->trust = dns_trust_secure;
 	sigrdataset->trust = dns_trust_secure;
 
@@ -2793,7 +2793,7 @@ mark_secure(ns_client_t *client, dns_db_t *db, dns_name_t *name,
 	result = dns_db_findnode(db, name, ISC_TRUE, &node);
 	if (result != ISC_R_SUCCESS)
 		return;
-		
+
 	isc_stdtime_get(&now);
 	dns_rdataset_trimttl(rdataset, sigrdataset, rrsig, now,
 			     client->view->acceptexpired);
