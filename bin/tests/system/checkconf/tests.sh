5c13c203908c0f1f2bb6af3e5ba785738a73eb54
Evan Hunt <each@isc.org>
2012-08-10 20:16:59 -0700

    address memory leak with bad tsig secret
    3359.	[bug]		An improperly-formed TSIG secret could cause a
                            memory leak. [RT #30607]

--- bin/tests/system/checkconf/tests.sh
+++ bin/tests/system/checkconf/tests.sh
@@ -37,7 +37,13 @@ status=`expr $status + $ret`
 echo "I: checking that named-checkconf handles a known bad config"
 ret=0
 $CHECKCONF bad.conf > /dev/null 2>&1 && ret=1
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $? != 1 ]; then echo "I:failed"; ret=1; fi
+status=`expr $status + $ret`
+
+echo "I: checking that named-checkconf handles a known bad tsig secret"
+ret=0
+$CHECKCONF badtsig.conf > /dev/null 2>&1
+if [ $? != 1 ]; then echo "I:failed"; ret=1; fi
 status=`expr $status + $ret`
 
 echo "I: checking named-checkconf dnssec warnings"
