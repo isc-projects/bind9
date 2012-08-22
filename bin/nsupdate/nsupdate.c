5892f2804c6a7068a008a8b8c52a2b3adf1a089f
Mark Andrews <marka@isc.org>
2012-08-22 18:38:46 +1000

    back port parse_rdata fix to handle NULL commandline

--- bin/nsupdate/nsupdate.c
+++ bin/nsupdate/nsupdate.c
@@ -1153,6 +1153,11 @@ parse_rdata(char **cmdlinep, dns_rdataclass_t rdataclass,
 	dns_rdatacallbacks_t callbacks;
 	isc_result_t result;
 
+	if (cmdline == NULL) {
+		rdata->flags = DNS_RDATA_UPDATE;
+		return (STATUS_MORE);
+	}
+
 	while (*cmdline != 0 && isspace((unsigned char)*cmdline))
 		cmdline++;
 
