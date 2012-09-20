2d68e392f3bec0c6c468b8020517e1ee4677aec9
Mark Andrews <marka@isc.org>
2012-09-20 10:42:24 +1000

    copyright style

--- bin/tests/system/checkds/dig.sh
+++ bin/tests/system/checkds/dig.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-############################################################################
-# Copyright (C) 2012 Internet Systems Consortium, Inc. ("ISC")
+#
+# Copyright (C) 2012  Internet Systems Consortium, Inc. ("ISC")
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -13,7 +13,7 @@
 # LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
-############################################################################
+
 
 while [ "$#" != 0 ]; do
     case $1 in
