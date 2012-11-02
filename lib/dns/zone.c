631643833687fe94c4e9d3213594479be2bad332
Mark Andrews <marka@isc.org>
2012-11-02 11:19:02 +1100

    spelling

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -12798,7 +12798,7 @@ receive_secure_db(isc_task_t *task, isc_event_t *event) {
 
 	dns_db_closeversion(db, &version, ISC_TRUE);
 	/*
-	 * Lock hierachy zmgr, zone, raw.
+	 * Lock hierarchy: zmgr, zone, raw.
 	 */
 	LOCK_ZONE(zone);
 	if (inline_secure(zone))
@@ -13332,7 +13332,7 @@ zone_loaddone(void *arg, isc_result_t result) {
 		result = tresult;
 
 	/*
-	 * Lock hierachy zmgr, zone, raw.
+	 * Lock hierarchy: zmgr, zone, raw.
 	 */
 	LOCK_ZONE(zone);
 	if (inline_secure(zone))
@@ -15909,7 +15909,7 @@ dns_zone_dlzpostload(dns_zone_t *zone, dns_db_t *db)
 	TIME_NOW(&loadtime);
 
 	/*
-	 * Lock hierachy zmgr, zone, raw.
+	 * Lock hierarchy: zmgr, zone, raw.
 	 */
 	LOCK_ZONE(zone);
 	if (inline_secure(zone))
@@ -15959,7 +15959,7 @@ dns_zone_getserialupdatemethod(dns_zone_t *zone) {
 }
 
 /*
- * Lock hierachy zmgr, raw, zone.
+ * Lock hierarchy: zmgr, zone, raw.
  */
 isc_result_t
 dns_zone_link(dns_zone_t *zone, dns_zone_t *raw) {
@@ -15979,7 +15979,7 @@ dns_zone_link(dns_zone_t *zone, dns_zone_t *raw) {
 	REQUIRE(raw->secure == NULL);
 
 	/*
-	 * Lock heirachy: zmgr, zone, raw.
+	 * Lock hierarchy: zmgr, zone, raw.
 	 */
 	zmgr = zone->zmgr;
 	RWLOCK(&zmgr->rwlock, isc_rwlocktype_write);
