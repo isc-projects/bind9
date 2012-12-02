01d3af945b8d94e2d7866929f1c18efb84d61630
Mark Andrews <marka@isc.org>
2012-12-03 09:09:19 +1100

    treat clang 3.x and 4.x as independent release streams

--- lib/dns/tsig.c
+++ lib/dns/tsig.c
@@ -781,7 +781,9 @@ dns_tsig_sign(dns_message_t *msg) {
 				goto cleanup_context;
 		}
 #if defined(__clang__)  && \
-       ( __clang_major__ < 4 || (__clang_major__ == 4 && __clang_minor__ < 2))
+       ( __clang_major__ < 3 || \
+	(__clang_major__ == 3 && __clang_minor__ < 2) || \
+	(__clang_major__ == 4 && __clang_minor__ < 2))
 	/* false positive: http://llvm.org/bugs/show_bug.cgi?id=14461 */
 		else memset(&querytsig, 0, sizeof(querytsig));
 #endif
@@ -1041,7 +1043,9 @@ dns_tsig_verify(isc_buffer_t *source, dns_message_t *msg,
 			return (ret);
 	}
 #if defined(__clang__) && \
-       ( __clang_major__ < 4 || (__clang_major__ == 4 && __clang_minor__ < 2))
+       ( __clang_major__ < 3 || \
+	(__clang_major__ == 3 && __clang_minor__ < 2) || \
+	(__clang_major__ == 4 && __clang_minor__ < 2))
 	/* false positive: http://llvm.org/bugs/show_bug.cgi?id=14461 */
 		else memset(&querytsig, 0, sizeof(querytsig));
 #endif
