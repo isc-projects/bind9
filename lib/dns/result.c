530249bc9eb0fcdad4ea6ae6c14107382bad6887
Mark Andrews <marka@isc.org>
2013-04-05 09:07:28 +1100

    3546.   [func]          Add EUI48 and EUI64 types. [RT #33082] (cherry picked from commit 3a6d62c59f73d024d1b2ba2ac94d7600530166ff)

--- lib/dns/result.c
+++ lib/dns/result.c
@@ -161,7 +161,8 @@ static const char *text[DNS_R_NRESULTS] = {
 	"not master",			       /*%< 105 DNS_R_NOTMASTER */
 	"broken trust chain",		       /*%< 106 DNS_R_BROKENCHAIN */
 	"expired",			       /*%< 107 DNS_R_EXPIRED */
-	"not dynamic"			       /*%< 108 DNS_R_NOTDYNAMIC */
+	"not dynamic",			       /*%< 108 DNS_R_NOTDYNAMIC */
+	"bad EUI"			       /*%< 109 DNS_R_BADEUI */
 };
 
 static const char *rcode_text[DNS_R_NRCODERESULTS] = {
