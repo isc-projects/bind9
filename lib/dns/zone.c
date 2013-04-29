ec8a802114d3cb12805fecf1dd209fe46706b35b
Mark Andrews <marka@isc.org>
2013-04-29 15:20:09 +1000

    3557.   [bug]           Reloading redirect zones was broken. [RT #33292]

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -12508,6 +12508,7 @@ zone_debuglog(dns_zone_t *zone, const char *me, int debuglevel,
 	va_list ap;
 	char message[4096];
 	int level = ISC_LOG_DEBUG(debuglevel);
+	const char *zstr;
 
 	if (isc_log_wouldlog(dns_lctx, level) == ISC_FALSE)
 		return;
@@ -12515,9 +12516,21 @@ zone_debuglog(dns_zone_t *zone, const char *me, int debuglevel,
 	va_start(ap, fmt);
 	vsnprintf(message, sizeof(message), fmt, ap);
 	va_end(ap);
+
+	switch (zone->type) {
+	case dns_zone_key:
+		zstr = "managed-keys-zone";
+		break;
+	case dns_zone_redirect:
+		zstr = "redirect-zone";
+		break;
+	default:
+		zstr = "zone";
+	}
+
 	isc_log_write(dns_lctx, DNS_LOGCATEGORY_GENERAL, DNS_LOGMODULE_ZONE,
-		      level, "%s: %s %s: %s", me, zone->type != dns_zone_key ?
-		      "zone" : "managed-keys-zone", zone->strnamerd, message);
+		      level, "%s: %s %s: %s", me, zstr, zone->strnamerd,
+		      message);
 }
 
 static int
