d8ff87734cf4439801f55f536f07a278558a4abc
Evan Hunt <each@isc.org>
2013-05-03 15:25:33 -0700

    [v9_6] add product description
    3568.	[cleanup]	Add a product description line to the version file,
    			to be reported by named -v/-V. [RT #33366]
    (cherry picked from commit af9f195c9e128fc9cc5912498b8f7f26f07887c3)

--- bin/named/include/named/globals.h
+++ bin/named/include/named/globals.h
@@ -64,6 +64,7 @@ EXTERN isc_socketmgr_t *	ns_g_socketmgr		INIT(NULL);
 EXTERN cfg_parser_t *		ns_g_parser		INIT(NULL);
 EXTERN const char *		ns_g_version		INIT(VERSION);
 EXTERN const char *		ns_g_product		INIT(PRODUCT);
+EXTERN const char *		ns_g_description	INIT(DESCRIPTION);
 EXTERN const char *		ns_g_srcid		INIT(SRCID);
 EXTERN const char *		ns_g_configargs		INIT(CONFIGARGS);
 EXTERN in_port_t		ns_g_port		INIT(0);
