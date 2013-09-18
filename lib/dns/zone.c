94e6452a333b18291267b9197dbaab998f521e6c
Mark Andrews <marka@isc.org>
2013-09-18 12:56:12 +1000

    call zone_settimer; sub test failure was not being detected

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -12397,11 +12397,17 @@ dns_zone_getsigvalidityinterval(dns_zone_t *zone) {
 
 void
 dns_zone_setsigresigninginterval(dns_zone_t *zone, isc_uint32_t interval) {
+	isc_time_t now;
+
 	REQUIRE(DNS_ZONE_VALID(zone));
 
 	LOCK_ZONE(zone);
 	zone->sigresigninginterval = interval;
 	set_resigntime(zone);
+	if (zone->task != NULL) {
+		TIME_NOW(&now);
+		zone_settimer(zone, &now);
+	}
 	UNLOCK_ZONE(zone);
 }
 
