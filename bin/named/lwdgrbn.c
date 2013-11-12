a9e832442ef96f413e9461e74d43c331773e1e61
Tinderbox User <tbox@isc.org>
2013-11-12 23:45:34 +0000

    update copyright notice

--- bin/named/lwdgrbn.c
+++ bin/named/lwdgrbn.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2007, 2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007, 2009, 2013  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000, 2001, 2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
