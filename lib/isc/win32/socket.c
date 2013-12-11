aa7c993ded61cb4f58e5779a98fbcb6e012efda6
Evan Hunt <each@isc.org>
2013-12-10 18:28:53 -0800

    [v9_6] fix typo

--- lib/isc/win32/socket.c
+++ lib/isc/win32/socket.c
@@ -3045,7 +3045,7 @@ isc_socket_sendtov(isc_socket_t *sock, isc_bufferlist_t *buflist,
 isc_result_t
 isc__socket_sendtov2(isc_socket_t *sock, isc_bufferlist_t *buflist,
 		     isc_task_t *task, isc_taskaction_t action, const void *arg,
-		     isc_sockaddr_t *address, struct in6_pktinfo *pktinfo
+		     isc_sockaddr_t *address, struct in6_pktinfo *pktinfo,
 		     unsigned int flags)
 {
 	isc_socketevent_t *dev;
