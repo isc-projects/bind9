4bd05ab557ccacf60fe252e5851692ed9b076293
Tinderbox User <tbox@isc.org>
2014-01-09 23:45:31 +0000

    update copyright notice

--- bin/named/lwdgnba.c
+++ bin/named/lwdgnba.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005, 2007, 2008  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2007, 2008, 2014  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
