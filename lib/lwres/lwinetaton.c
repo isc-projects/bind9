6f2415aae0df523b6d0e52b6701fd40a2eb4f5c5
Tinderbox User <tbox@isc.org>
2014-01-24 23:45:12 +0000

    update copyright notice

--- lib/lwres/lwinetaton.c
+++ lib/lwres/lwinetaton.c
@@ -1,5 +1,5 @@
 /*
- * Portions Copyright (C) 2004, 2005, 2007, 2012  Internet Systems Consortium, Inc. ("ISC")
+ * Portions Copyright (C) 2004, 2005, 2007, 2012, 2014  Internet Systems Consortium, Inc. ("ISC")
  * Portions Copyright (C) 1996-2001, 2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
