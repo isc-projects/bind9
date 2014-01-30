f148939b08e00081a1fdf2663e1f7135ca1ea8a0
Tinderbox User <tbox@isc.org>
2014-01-30 23:45:43 +0000

    update copyright notice

--- lib/dns/include/dns/rbt.h
+++ lib/dns/include/dns/rbt.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2009  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2009, 2014  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
