1c67e9747b2978f17faeb642516074119b0eb61c
Mark Andrews <marka@isc.org>
2014-02-08 09:37:32 +1100

    fix typo in comment
    (cherry picked from commit 404d7c966cfbe86bf6d02a5160725105be328f5a)

--- bin/named/server.c
+++ bin/named/server.c
@@ -3072,7 +3072,7 @@ configure_view(dns_view_t *view, cfg_obj_t *config, cfg_obj_t *vconfig,
 
 	/*
 	 * Ignore case when compressing responses to the specified
-	 * clients. This causes case not always to be presrerved,
+	 * clients. This causes case not always to be preserved,
 	 * and is needed by some broken clients.
 	 */
 	CHECK(configure_view_acl(vconfig, config, "no-case-compress", NULL,
