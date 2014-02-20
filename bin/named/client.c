525237c395f1dd9577070d12d58ccc8431eaf7d8
Mark Andrews <marka@isc.org>
2014-02-20 14:00:54 +1100

    s/DNS_EDNSOPTIONS/DNS_EDNSOPTIONS/
    (cherry picked from commit 801b958a5c855816a36d92c7f533c485d9af8dcc)
    (cherry picked from commit 2fd5f9a3267f046fad0706f243b97b157396e3e6)

--- bin/named/client.c
+++ bin/named/client.c
@@ -1208,7 +1208,7 @@ client_addopt(ns_client_t *client) {
 	dns_view_t *view;
 	dns_resolver_t *resolver;
 	isc_uint16_t udpsize;
-	dns_ednsopt_t ednsopts[DNS_OPT_EDNSOPTIONS];
+	dns_ednsopt_t ednsopts[DNS_EDNSOPTIONS];
 	int count = 0;
 	unsigned int flags;
 
@@ -1237,7 +1237,7 @@ client_addopt(ns_client_t *client) {
 		} else
 			nsidp = ns_g_server->server_id;
 
-		INSIST(count < DNS_OPT_EDNSOPTIONS);
+		INSIST(count < DNS_EDNSOPTIONS);
 		ednsopts[count].code = DNS_OPT_NSID;
 		ednsopts[count].length = strlen(nsidp);
 		ednsopts[count].value = (unsigned char *)nsidp;
