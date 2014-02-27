938440694b33cd752e9e4b71a526368b4811c177
Tinderbox User <tbox@isc.org>
2014-02-27 23:46:22 +0000

    update copyright notice

--- lib/irs/win32/DLLMain.c
+++ lib/irs/win32/DLLMain.c
@@ -25,32 +25,32 @@
 __declspec(dllexport) BOOL WINAPI DllMain(HINSTANCE hinstDLL,
 					  DWORD fdwReason, LPVOID lpvReserved)
 {
-	switch (fdwReason) { 
+	switch (fdwReason) {
 	/*
-	 * The DLL is loading due to process 
-	 * initialization or a call to LoadLibrary. 
+	 * The DLL is loading due to process
+	 * initialization or a call to LoadLibrary.
 	 */
-	case DLL_PROCESS_ATTACH: 
-		break; 
- 
-	/* The attached process creates a new thread.  */
-	case DLL_THREAD_ATTACH: 
-		break; 
- 
-	/* The thread of the attached process terminates. */
-	case DLL_THREAD_DETACH: 
-		break; 
- 
-	/*
-	 * The DLL is unloading from a process due to 
-	 * process termination or a call to FreeLibrary. 
-	 */
-	case DLL_PROCESS_DETACH: 
-		break; 
+	case DLL_PROCESS_ATTACH:
+		break;
 
-	default: 
-		break; 
-	} 
+	/* The attached process creates a new thread.  */
+	case DLL_THREAD_ATTACH:
+		break;
+
+	/* The thread of the attached process terminates. */
+	case DLL_THREAD_DETACH:
+		break;
+
+	/*
+	 * The DLL is unloading from a process due to
+	 * process termination or a call to FreeLibrary.
+	 */
+	case DLL_PROCESS_DETACH:
+		break;
+
+	default:
+		break;
+	}
 	return (TRUE);
 }
 
