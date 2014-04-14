2dc978b8a1563f6dd413a8a9456659ade00f9e9e
Evan Hunt <each@isc.org>
2014-04-13 18:50:34 -0700

    [master] fix "prefetch" doc
    3808.	[doc]		Clean up "prefetch" documentation. [RT #35751]

--- lib/dns/view.c
+++ lib/dns/view.c
@@ -190,6 +190,8 @@ dns_view_create(isc_mem_t *mctx, dns_rdataclass_t rdclass,
 	view->provideixfr = ISC_TRUE;
 	view->maxcachettl = 7 * 24 * 3600;
 	view->maxncachettl = 3 * 3600;
+	view->prefetch_eligible = 0;
+	view->prefetch_trigger = 0;
 	view->dstport = 53;
 	view->preferred_glue = 0;
 	view->flush = ISC_FALSE;
