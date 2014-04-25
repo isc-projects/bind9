618053b8bec1dea3e9486c3b427b123c4093d56a
Mark Andrews <marka@isc.org>
2014-04-25 12:20:43 +1000

    additional places: 3818.   [bug]           Stop lying to the optimizer that 'void *arg' is a                         constant in isc_event_allocate.
    (cherry picked from commit 39b52695771532e3df0c83a9b6683f899afa403a)

--- lib/isc/include/isc/timer.h
+++ lib/isc/include/isc/timer.h
@@ -113,7 +113,7 @@ typedef struct {
 				       const isc_interval_t *interval,
 				       isc_task_t *task,
 				       isc_taskaction_t action,
-				       const void *arg,
+				       void *arg,
 				       isc_timer_t **timerp);
 } isc_timermgrmethods_t;
 
@@ -174,7 +174,7 @@ isc_timer_create(isc_timermgr_t *manager,
 		 const isc_interval_t *interval,
 		 isc_task_t *task,
 		 isc_taskaction_t action,
-		 const void *arg,
+		 void *arg,
 		 isc_timer_t **timerp);
 /*%<
  * Create a new 'type' timer managed by 'manager'.  The timers parameters
