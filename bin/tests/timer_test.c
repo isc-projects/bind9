37380ec31843ac9ec82862aeefc8e4fbaba71504
Mark Andrews <marka@isc.org>
2014-04-26 23:42:37 +1000

    silence warnings
    (cherry picked from commit 4aaaaadeaef86cd6ab144ea8f5acf7f17ae45952)

--- bin/tests/timer_test.c
+++ bin/tests/timer_test.c
@@ -94,6 +94,10 @@ timeout(isc_task_t *task, isc_event_t *event) {
 	isc_task_shutdown(task);
 }
 
+static char one[] = "1";
+static char two[] = "2";
+static char three[] = "3";
+
 int
 main(int argc, char *argv[]) {
 	isc_taskmgr_t *manager = NULL;
@@ -123,11 +127,11 @@ main(int argc, char *argv[]) {
 		      ISC_R_SUCCESS);
 	RUNTIME_CHECK(isc_task_create(manager, 0, &t3) ==
 		      ISC_R_SUCCESS);
-	RUNTIME_CHECK(isc_task_onshutdown(t1, shutdown_task, "1") ==
+	RUNTIME_CHECK(isc_task_onshutdown(t1, shutdown_task, one) ==
 		      ISC_R_SUCCESS);
-	RUNTIME_CHECK(isc_task_onshutdown(t2, shutdown_task, "2") ==
+	RUNTIME_CHECK(isc_task_onshutdown(t2, shutdown_task, two) ==
 		      ISC_R_SUCCESS);
-	RUNTIME_CHECK(isc_task_onshutdown(t3, shutdown_task, "3") ==
+	RUNTIME_CHECK(isc_task_onshutdown(t3, shutdown_task, three) ==
 		      ISC_R_SUCCESS);
 
 	printf("task 1: %p\n", t1);
@@ -138,12 +142,12 @@ main(int argc, char *argv[]) {
 
 	isc_interval_set(&interval, 2, 0);
 	RUNTIME_CHECK(isc_timer_create(timgr, isc_timertype_once, NULL,
-				       &interval, t2, timeout, "2", &ti2) ==
+				       &interval, t2, timeout, two, &ti2) ==
 		      ISC_R_SUCCESS);
 
 	isc_interval_set(&interval, 1, 0);
 	RUNTIME_CHECK(isc_timer_create(timgr, isc_timertype_ticker, NULL,
-				       &interval, t1, tick, "1", &ti1) ==
+				       &interval, t1, tick, one, &ti1) ==
 		      ISC_R_SUCCESS);
 
 	isc_interval_set(&interval, 10, 0);
@@ -151,7 +155,7 @@ main(int argc, char *argv[]) {
 		      ISC_R_SUCCESS);
 	isc_interval_set(&interval, 2, 0);
 	RUNTIME_CHECK(isc_timer_create(timgr, isc_timertype_once, &expires,
-				       &interval, t3, timeout, "3", &ti3) ==
+				       &interval, t3, timeout, three, &ti3) ==
 		      ISC_R_SUCCESS);
 
 	isc_task_detach(&t1);
