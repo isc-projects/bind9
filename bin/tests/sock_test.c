2d50cce72e08e29447c8b7adb56b55bca6c2e3c7
Tinderbox User <tbox@isc.org>
2014-04-26 23:45:20 +0000

    update copyright notice

--- bin/tests/sock_test.c
+++ bin/tests/sock_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2007, 2008, 2012, 2013  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2007, 2008, 2012-2014  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1998-2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
