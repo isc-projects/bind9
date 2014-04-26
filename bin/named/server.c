5d59817b4f6cf4f3d89f82e582e852da03c6a63c
Evan Hunt <each@isc.org>
2014-04-26 10:20:40 -0700

    [v9_8] log static-stub correctly when removing
    3822.	[bug]		Log the correct type of static-stub zones when
    			removing them. [RT #35842]
    
    (cherry picked from commit eb1a7730f013e9a16d709c9ee8f41d73cde3680e)
    (cherry picked from commit 00e9952c41282dbff115f12a1449ca08bc064081)

--- bin/named/server.c
+++ bin/named/server.c
@@ -4324,6 +4324,9 @@ removed(dns_zone_t *zone, void *uap) {
 	case dns_zone_stub:
 		type = "stub";
 		break;
+	case dns_zone_staticstub:
+		type = "static-stub";
+		break;
 	default:
 		type = "other";
 		break;
