cd07e4d038fda6f9bacecb3067cc6fa43a67fc0a
Mark Andrews <marka@isc.org>
2014-05-05 16:40:45 +1000

    3838.   [protocol]      EDNS EXPIRE as been assigned a code point of 9.

--- lib/dns/include/dns/message.h
+++ lib/dns/include/dns/message.h
@@ -106,9 +106,10 @@
 /*%< EDNS0 extended OPT codes */
 #define DNS_OPT_NSID		0x0003		/*%< NSID opt code */
 #define DNS_OPT_CLIENT_SUBNET	0x0008		/*%< client subnet opt code */
+#define DNS_OPT_EXPIRE		0x0009		/*%< EXPIRE opt code */
+
 /*%< Experimental options [65001...65534] as per RFC6891 */
 #define DNS_OPT_SIT		65001		/*%< SIT opt code */
-#define DNS_OPT_EXPIRE		65002		/*%< EXPIRE opt code */
 
 /*%< The number of EDNS options we know about. */
 #define DNS_EDNSOPTIONS	4
