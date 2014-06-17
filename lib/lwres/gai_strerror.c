67751a4d1b2da2283cf32e6c7f16a65c1d788793
Tinderbox User <tbox@isc.org>
2014-06-17 23:46:35 +0000

    update copyright notice

--- lib/lwres/gai_strerror.c
+++ lib/lwres/gai_strerror.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007, 2014  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000, 2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
