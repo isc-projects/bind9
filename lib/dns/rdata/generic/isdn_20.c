309930e49da935e074e757c37f52f200843af315
Tinderbox User <tbox@isc.org>
2014-07-29 23:47:34 +0000

    update copyright notice

--- lib/dns/rdata/generic/isdn_20.c
+++ lib/dns/rdata/generic/isdn_20.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2005, 2007, 2009, 2013  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2005, 2007, 2009, 2013, 2014  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
