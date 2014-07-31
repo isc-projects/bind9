570effe386b4c2523a49049ed663ee974528f867
Mark Andrews <marka@isc.org>
2014-07-31 10:51:48 +1000

    3904.   [func]          Add the RPZ SOA to the additional section. [RT36507]
    (cherry picked from commit 3a55d4352767acb4e0572aabfa917610001f5c9c)

--- lib/dns/resolver.c
+++ lib/dns/resolver.c
@@ -5676,7 +5676,7 @@ noanswer_response(fetchctx_t *fctx, dns_name_t *oqname,
 						    "unrelated %s %s in "
 						    "%s authority section",
 						    tbuf, qbuf, nbuf);
-					return (DNS_R_FORMERR);
+					goto nextname;
 				}
 				if (type == dns_rdatatype_ns) {
 					/*
@@ -5739,6 +5739,7 @@ noanswer_response(fetchctx_t *fctx, dns_name_t *oqname,
 				}
 			}
 		}
+ nextname:
 		result = dns_message_nextname(message, section);
 		if (result == ISC_R_NOMORE)
 			break;
