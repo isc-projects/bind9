039e58e905346d2dc2e4de16af16b58ec82e37d4
Evan Hunt <each@isc.org>
2014-09-05 10:12:28 -0700

    [v9_9] [rt37057] server-id tests
    3944.	[test]		Added a regression test for "server-id". [RT #37057]
    
    (cherry picked from commit c9e976dc43070e2ed87988eeabed71195fab88ba)
    (cherry picked from commit 9f6a6d24dc8a63995abb4f430efd14bd20699a7c)

--- bin/tests/system/builtin/clean.sh
+++ bin/tests/system/builtin/clean.sh
@@ -1,5 +1,6 @@
-# Copyright (C) 2004, 2007, 2008, 2010-2014  Internet Systems Consortium, Inc. ("ISC")
-# Copyright (C) 2000, 2001  Internet Software Consortium.
+#!/bin/sh
+#
+# Copyright (C) 2011-2014  Internet Systems Consortium, Inc. ("ISC")
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -13,34 +14,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-srcdir =	@srcdir@
-VPATH =		@srcdir@
-top_srcdir =	@top_srcdir@
-
-@BIND9_MAKE_INCLUDES@
-
-SUBDIRS =	builtin dlzexternal filter-aaaa lwresd rpz rrl \
-		rsabigexponent tkey tsiggss
-TARGETS =
-
-@BIND9_MAKE_RULES@
-
-# Running the scripts below is bypassed when a separate
-# build directory is used.
-
-check: test
-
-test: subdirs
-	if test -f ./runall.sh; then sh ./runall.sh; fi
-
-testclean clean distclean::
-	if test -f ./cleanall.sh; then sh ./cleanall.sh; fi
-	rm -f systests.output
-	rm -f random.data
-
-distclean::
-	rm -f conf.sh
-
-installdirs:
-
-install::
+rm -f ns?/named.run
+rm -f ns?/named.memstats
+rm -f rndc.status.ns*
+rm -f dig.out.ns*
