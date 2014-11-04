5781d00939dae8d9da5d502fec1562488abf7461
Tinderbox User <tbox@isc.org>
2014-11-04 23:45:20 +0000

    update copyright notice

--- lib/dns/rdata/generic/openpgpkey_61.c
+++ lib/dns/rdata/generic/openpgpkey_61.c
@@ -195,9 +195,9 @@ digest_openpgpkey(ARGS_DIGEST) {
 
 	REQUIRE(rdata->type == 61);
 
-        dns_rdata_toregion(rdata, &r);
+	dns_rdata_toregion(rdata, &r);
 
-        return ((digest)(arg, &r));
+	return ((digest)(arg, &r));
 }
 
 static inline isc_boolean_t
