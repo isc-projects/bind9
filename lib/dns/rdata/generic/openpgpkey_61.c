c75e6e975672dcc61cee66155421d317402fb3be
Mark Andrews <marka@isc.org>
2014-11-04 14:02:42 +1100

    DNS_STYLEFLAG_NOCRYPTO not supported in 9.9.x

--- lib/dns/rdata/generic/openpgpkey_61.c
+++ lib/dns/rdata/generic/openpgpkey_61.c
@@ -49,15 +49,11 @@ totext_openpgpkey(ARGS_TOTEXT) {
 	 * Keyring
 	 */
 	RETERR(str_totext(tctx->linebreak, target));
-	if ((tctx->flags & DNS_STYLEFLAG_NOCRYPTO) == 0) {
-		if (tctx->width == 0)   /* No splitting */
-			RETERR(isc_base64_totext(&sr, 60, "", target));
-		else
-			RETERR(isc_base64_totext(&sr, tctx->width - 2,
-						 tctx->linebreak, target));
-	} else
-		RETERR(str_totext("[omitted]", target));
-
+	if (tctx->width == 0)   /* No splitting */
+		RETERR(isc_base64_totext(&sr, 60, "", target));
+	else
+		RETERR(isc_base64_totext(&sr, tctx->width - 2,
+					 tctx->linebreak, target));
 	if ((tctx->flags & DNS_STYLEFLAG_MULTILINE) != 0)
 		RETERR(str_totext(" )", target));
 
