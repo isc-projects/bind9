aee6c351d3b517f3e6a4ddf770606617fd42015b
Tinderbox User <tbox@isc.org>
2014-11-15 23:45:22 +0000

    update copyright notice

--- lib/isccc/include/isccc/cc.h
+++ lib/isccc/include/isccc/cc.h
@@ -1,5 +1,5 @@
 /*
- * Portions Copyright (C) 2004-2007, 2013  Internet Systems Consortium, Inc. ("ISC")
+ * Portions Copyright (C) 2004-2007, 2013, 2014  Internet Systems Consortium, Inc. ("ISC")
  * Portions Copyright (C) 2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
