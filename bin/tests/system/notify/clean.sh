f8eb4e5bfd1129d7639af5c2c768f53f0895952a
Mark Andrews <marka@isc.org>
2015-01-20 16:42:56 +1100

    4037.   [bug]           also-notify was ignoring the tsig key when checking                         for duplicates resulting in some expected notify                         messages not being sent. [RT #38369]

--- bin/tests/system/notify/clean.sh
+++ bin/tests/system/notify/clean.sh
@@ -20,12 +20,18 @@
 #
 
 rm -f */named.memstats
+rm -f dig.out.?.ns5.test*
 rm -f dig.out.ns2.test*
 rm -f dig.out.ns3.test*
 rm -f dig.out.ns4.test*
 rm -f log.out
+rm -f ns*/named.lock
 rm -f ns2/example.db
 rm -f ns2/x21.db*
 rm -f ns3/example.bk
 rm -f ns4/x21.bk*
-rm -f ns*/named.lock
+rm -f ns5/x21.bk-b
+rm -f ns5/x21.bk-b.jnl
+rm -f ns5/x21.bk-c
+rm -f ns5/x21.bk-c.jnl
+rm -f ns5/x21.db.jnl
