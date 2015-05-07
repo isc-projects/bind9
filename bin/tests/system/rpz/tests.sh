5cbd509a3d3e9dc9ed6ef38430a205cabdf3c737
Mukund Sivaraman <muks@isc.org>
2015-05-07 08:26:27 +0530

    Fix a bug in RPZ that could cause unwanted recursion (#39229)
    Conflicts:
    	doc/arm/notes.xml
    
    (cherry picked from commit b947e1a521c6931f787d6d1b3604d5b138170c3d)
    
    Conflicts:
    	CHANGES
    	bin/tests/system/conf.sh.in

--- bin/tests/system/rpz/tests.sh
+++ bin/tests/system/rpz/tests.sh
@@ -635,7 +635,7 @@ for i in 1 2 3 4 5; do
     nsd $ns5 delete '*.example.com.policy1.' example.com.policy1.
 done
 
-echo "I:checking checking that going from a empty policy zone works"
+echo "I:checking that going from a empty policy zone works"
 nsd $ns5 add '*.x.servfail.policy2.' x.servfail.policy2.
 sleep 1
 $RNDCCMD $ns7 reload policy2
