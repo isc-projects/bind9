22f467116c907184eccf5a64bfdf8ea4b13efbdb
Mukund Sivaraman <muks@isc.org>
2015-06-23 14:54:34 +0530

    Fix parsing of NZFs saved by rndc addzone with view specified (#39845)
    (cherry picked from commit 0439bfedd97fa2e004cbf572773d703b33fda10f)
    
    Conflicts:
    	bin/named/server.c
    	bin/tests/system/addzone/tests.sh
    
    (cherry picked from commit 8134e7662e8da5d3f894bd33342dfd997de21c7c)
    
    Conflicts:
    	CHANGES

--- bin/tests/system/addzone/tests.sh
+++ bin/tests/system/addzone/tests.sh
@@ -215,6 +215,17 @@ n=`expr $n + 1`
 if [ $ret != 0 ]; then echo "I:failed"; fi
 status=`expr $status + $ret`
 
+echo "I:checking rndc reload causes named to reload the external view's NZF file ($n)"
+ret=0
+$RNDC -c ../common/rndc.conf -s 10.53.0.2 -p 9953 reload 2>&1 | sed 's/^/I:ns2 /'
+$DIG +norec $DIGOPTS @10.53.0.2 -b 10.53.0.2 a.added.example a > dig.out.ns2.int.$n || ret=1
+grep 'status: NOERROR' dig.out.ns2.int.$n > /dev/null || ret=1
+$DIG +norec $DIGOPTS @10.53.0.4 -b 10.53.0.4 a.added.example a > dig.out.ns2.ext.$n || ret=1
+grep 'status: NOERROR' dig.out.ns2.ext.$n > /dev/null || ret=1
+grep '^a.added.example' dig.out.ns2.ext.$n > /dev/null || ret=1
+n=`expr $n + 1`
+if [ $ret != 0 ]; then echo "I:failed"; fi
+status=`expr $status + $ret`
 
 echo "I:deleting newly added zone ($n)"
 ret=0
