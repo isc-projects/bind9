f524494512a51c0c16acbddfecff49f41079488c
Mark Andrews <marka@isc.org>
2015-09-09 17:02:11 +1000

    4196.   [doc]           Improve how "enum + other" types are documented.                         [RT #40608]
    4195.   [bug]           'max-zone-ttl unlimited;' was broken. [RT #40608]
    
    (cherry picked from commit 4ca7391e640bd4f0abb31508019d3bd62819fa8e)

--- bin/named/zoneconf.c
+++ bin/named/zoneconf.c
@@ -972,7 +972,10 @@ ns_zone_configure(const cfg_obj_t *config, const cfg_obj_t *vconfig,
 			      "with 'masterfile-format map'", zname);
 		return (ISC_R_FAILURE);
 	} else if (result == ISC_R_SUCCESS) {
-		dns_ttl_t maxttl = cfg_obj_asuint32(obj);
+		dns_ttl_t maxttl = 0;	/* unlimited */
+
+		if (cfg_obj_isuint32(obj))
+			maxttl = cfg_obj_asuint32(obj);
 		dns_zone_setmaxttl(zone, maxttl);
 		if (raw != NULL)
 			dns_zone_setmaxttl(raw, maxttl);
