fc9f0ac5778f78003a7acc957a23711811fec122
Mark Andrews <marka@isc.org>
2015-09-30 15:46:55 +1000

    4231.   [contrib]       Address unchecked memory allocation calls in                         query-loc and zone2ldap. [RT #40789]
    (cherry picked from commit ffdd3bc812a140fb0e3d5b6f0df0a4efbfe4b412)

--- contrib/query-loc-0.4.0/loc.h
+++ contrib/query-loc-0.4.0/loc.h
@@ -18,6 +18,9 @@
 #include <string.h>
 #include <signal.h>
 #include <arpa/nameser.h>
+#ifdef HAVE_ARPA_NAMESER_COMPAT_H
+#include <arpa/nameser_compat.h>
+#endif
 #include <resolv.h>
 
 #ifndef FALSE
