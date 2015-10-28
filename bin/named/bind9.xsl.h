1b6e38d7b83b26f123e6f113e4b30093a2774562
Tinderbox User <tbox@isc.org>
2015-10-28 23:30:01 +0000

    regen

--- bin/named/bind9.xsl.h
+++ bin/named/bind9.xsl.h
@@ -19,6 +19,7 @@ static char xslmsg[] =
 	" - OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR\n"
 	" - PERFORMANCE OF THIS SOFTWARE.\n"
 	"-->\n"
+	"\n"
 	"<xsl:stylesheet xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\" xmlns=\"http://www.w3.org/1999/xhtml\" version=\"1.0\">\n"
 	" <xsl:output method=\"html\" indent=\"yes\" version=\"4.0\"/>\n"
 	" <xsl:template match=\"statistics[@version=&quot;3.6&quot;]\">\n"
