95d37b0a0305cd503fc365b33c92ea61c78b885c
Mark Andrews <marka@isc.org>
2015-10-29 15:48:01 +1100

    return dns_dbtable_add result on error [RT #40653]
    (cherry picked from commit 1ff2118d6c63006a961de0b696f597197fb26f16)

--- bin/tests/db_test.c
+++ bin/tests/db_test.c
@@ -295,7 +295,8 @@ load(const char *filename, const char *origintext, isc_boolean_t cache) {
 		dns_dbtable_adddefault(dbtable, dbi->db);
 		cache_dbi = dbi;
 	} else {
-		if (dns_dbtable_add(dbtable, dbi->db) != ISC_R_SUCCESS) {
+		result = dns_dbtable_add(dbtable, dbi->db);
+		if (result != ISC_R_SUCCESS) {
 			dns_db_detach(&dbi->db);
 			isc_mem_put(mctx, dbi, sizeof(*dbi));
 			return (result);
