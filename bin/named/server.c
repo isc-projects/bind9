6c90a60a12ff758e9e5f332bd0f504e8972f0373
Mark Andrews <marka@isc.org>
2015-11-11 22:33:57 +1100

    check ptr is not NULL
    (cherry picked from commit 946e2cd35123cfd8cf224b32fc17fd164af02ab8)

--- bin/named/server.c
+++ bin/named/server.c
@@ -8096,6 +8096,9 @@ ns_server_rekey(ns_server_t *server, isc_lex_t *lex, isc_buffer_t *text) {
 	char *ptr;
 
 	ptr = next_token(lex, text);
+	if (ptr == NULL)
+		return (ISC_R_UNEXPECTEDEND);
+
 	if (strcasecmp(ptr, NS_COMMAND_SIGN) == 0)
 		fullsign = ISC_TRUE;
 
