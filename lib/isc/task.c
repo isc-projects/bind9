e7d8959e1b6fb4d483ebade48aa35686d91110c2
Evan Hunt <each@isc.org>
2015-12-03 20:48:46 -0800

    [v9_10] isc__taskmgr_pause() could deadlock
    4271.	[test]		Unit tests could deadlock in isc__taskmgr_pause().
    			[RT #41235]

--- lib/isc/task.c
+++ lib/isc/task.c
@@ -15,8 +15,6 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id$ */
-
 /*! \file
  * \author Principal Author: Bob Halley
  */
@@ -1645,11 +1643,11 @@ isc__taskmgr_dispatch(isc_taskmgr_t *manager0) {
 void
 isc__taskmgr_pause(isc_taskmgr_t *manager0) {
 	isc__taskmgr_t *manager = (isc__taskmgr_t *)manager0;
+	manager->pause_requested = ISC_TRUE;
 	LOCK(&manager->lock);
 	while (manager->tasks_running > 0) {
 		WAIT(&manager->paused, &manager->lock);
 	}
-	manager->pause_requested = ISC_TRUE;
 	UNLOCK(&manager->lock);
 }
 
