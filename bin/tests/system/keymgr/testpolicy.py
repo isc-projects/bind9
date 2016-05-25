ecb9c56ff631930420e39d82394797cb30decd9b
Mark Andrews <marka@isc.org>
2016-05-25 13:37:07 +1000

    use python3 compatible syntax

--- bin/tests/system/keymgr/testpolicy.py
+++ bin/tests/system/keymgr/testpolicy.py
@@ -20,25 +20,25 @@ from isc import *
 
 pp = policy.dnssec_policy()
 # print the unmodified default and a generated zone policy
-print pp.named_policy['default']
-print pp.named_policy['global']
-print pp.policy('example.com')
+print(pp.named_policy['default'])
+print(pp.named_policy['global'])
+print(pp.policy('example.com'))
 
 if len(sys.argv) > 0:
     for policy_file in sys.argv[1:]:
         pp.load(policy_file)
 
         # now print the modified default and generated zone policies
-        print pp.named_policy['default']
-        print pp.policy('example.com')
-        print pp.policy('example.org')
-        print pp.policy('example.net')
+        print(pp.named_policy['default'])
+        print(pp.policy('example.com'))
+        print(pp.policy('example.org'))
+        print(pp.policy('example.net'))
 
         # print algorithm policies
-        print pp.alg_policy['RSASHA1']
-        print pp.alg_policy['DSA']
+        print(pp.alg_policy['RSASHA1'])
+        print(pp.alg_policy['DSA'])
 
         # print another named policy
-        print pp.named_policy['extra']
+        print(pp.named_policy['extra'])
 else:
     print("ERROR: Please provide an input file")
