ed705b0f74f634f99d178e819a22cbcbab50c0b0
Witold Krecicki <wpk@isc.org>
2016-09-05 11:07:56 +0000

    [rt42931] Add D2U to upforwd test

--- bin/tests/system/upforwd/setup.sh
+++ bin/tests/system/upforwd/setup.sh
@@ -19,7 +19,7 @@ rm -f Ksig0.example2.*
 # SIG(0) required cryptographic support which may not be configured.
 #
 test -r $RANDFILE || $GENRANDOM 400 $RANDFILE 
-keyname=`$KEYGEN  -q -r $RANDFILE -n HOST -a RSASHA1 -b 1024 -T KEY sig0.example2 2>/dev/null`
+keyname=`$KEYGEN  -q -r $RANDFILE -n HOST -a RSASHA1 -b 1024 -T KEY sig0.example2 2>/dev/null | $D2U`
 if test -n "$keyname"
 then
 	cat ns1/example1.db $keyname.key > ns1/example2.db
