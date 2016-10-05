0e5efd536934fe43ddab9ee36a60e0d8c4591e4d
Tinderbox User <tbox@isc.org>
2016-10-05 23:47:17 +0000

    update copyright notice / whitespace

--- lib/isc/unix/stdio.c
+++ lib/isc/unix/stdio.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2007, 2011-2014  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2007, 2011-2014, 2016  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000, 2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -142,7 +142,7 @@ isc_stdio_sync(FILE *f) {
 
 	if (fstat(fileno(f), &buf) != 0)
 		return (isc__errno2result(errno));
- 
+
 	/*
 	 * Only call fsync() on regular files.
 	 */
