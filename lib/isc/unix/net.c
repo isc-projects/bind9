e19874e3297c5d3106e3a3fc97d21da5fe1fec82
Mark Andrews <marka@isc.org>
2016-10-27 09:02:06 +1100

    4491.   [bug]           Improve message emitted when testing whether sendmsg                         works with TOS/TCLASS fails. [RT #43483]
    (cherry picked from commit 8eaf918adf63cde59e52651cf66170c008f7ec4a)

--- lib/isc/unix/net.c
+++ lib/isc/unix/net.c
@@ -509,7 +509,7 @@ cmsgsend(int s, int level, int type, struct addrinfo *res) {
 		unsigned char b[256];
 	} control;
 	struct cmsghdr *cmsgp;
-	int dscp = 46;
+	int dscp = (46 << 2);	/* Expedited forwarding. */
 	struct iovec iovec;
 	char buf[1] = { 0 };
 	isc_result_t result;
@@ -571,6 +571,8 @@ cmsgsend(int s, int level, int type, struct addrinfo *res) {
 
 	if (sendmsg(s, &msg, 0) < 0) {
 		int debug = ISC_LOG_DEBUG(10);
+		const char *typestr;
+		const char *msgstr;
 		switch (errno) {
 #ifdef ENOPROTOOPT
 		case ENOPROTOOPT:
@@ -589,13 +591,12 @@ cmsgsend(int s, int level, int type, struct addrinfo *res) {
 				      ISC_LOGMODULE_SOCKET, ISC_LOG_DEBUG(10),
 				      "sendmsg: %s", strbuf);
 		} else {
-			UNEXPECTED_ERROR(__FILE__, __LINE__,
-					 "sendmsg() %s: %s",
-					 isc_msgcat_get(isc_msgcat,
-							ISC_MSGSET_GENERAL,
-							ISC_MSG_FAILED,
-							"failed"),
-					 strbuf);
+			typestr = (type == IP_TOS) ? "IP_TOS" : "IPV6_TCLASS";
+			msgstr = isc_msgcat_get(isc_msgcat, ISC_MSGSET_GENERAL,
+						ISC_MSG_FAILED, "failed");
+			UNEXPECTED_ERROR(__FILE__, __LINE__, "probing "
+					 "sendmsg() with %s=%02x %s: %s",
+					 typestr, dscp, msgstr, strbuf);
 		}
 		return (ISC_FALSE);
 	}
