d389069a397c99347b5b281f90577e19e7662b03
Mark Andrews <marka@isc.org>
2016-10-27 13:17:58 +1100

    4492.   [bug]           irs_resconf_load failed to initialise sortlistnxt                         causing bad writes if resolv.conf contained a                         sortlist directive. [RT #43459]
    (cherry picked from commit 55b78fff62967fa693d67ecf28d48229643f91e5)

---
+++ lib/irs/tests/resconf_test.c
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2014, 2016  Internet Systems Consortium, Inc. ("ISC")
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <config.h>
+
+#include <atf-c.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <isc/mem.h>
+#include <isc/util.h>
+
+#include <irs/types.h>
+#include <irs/resconf.h>
+
+static isc_mem_t *mctx = NULL;
+
+static void
+setup_test() {
+	isc_result_t result;
+
+	result = isc_mem_create(0, 0, &mctx);
+	ATF_REQUIRE_EQ(result, ISC_R_SUCCESS);
+
+	/*
+	 * atf-run changes us to a /tmp directory, so tests
+	 * that access test data files must first chdir to the proper
+	 * location.
+	 */
+	ATF_REQUIRE(chdir(TESTS) != -1);
+}
+
+ATF_TC(irs_resconf_load);
+ATF_TC_HEAD(irs_resconf_load, tc) {
+	atf_tc_set_md_var(tc, "descr", "irs_resconf_load");
+}
+ATF_TC_BODY(irs_resconf_load, tc) {
+	isc_result_t result;
+	irs_resconf_t *resconf = NULL;
+	unsigned int i;
+	struct {
+		const char *file;
+		isc_result_t loadres;
+		isc_result_t (*check)(irs_resconf_t *resconf);
+		isc_result_t checkres;
+	} tests[] = {
+		{
+			"testdata/sortlist-v4.conf", ISC_R_SUCCESS,
+			NULL, ISC_R_SUCCESS
+		}, {
+			"testdata/domain.conf", ISC_R_SUCCESS,
+			NULL, ISC_R_SUCCESS
+		}, {
+			"testdata/nameserver-v4.conf", ISC_R_SUCCESS,
+			NULL, ISC_R_SUCCESS
+		}, {
+			"testdata/nameserver-v6.conf", ISC_R_SUCCESS,
+			NULL, ISC_R_SUCCESS
+		}, {
+			"testdata/options-debug.conf", ISC_R_SUCCESS,
+			NULL, ISC_R_SUCCESS
+		}, {
+			"testdata/options-ndots.conf", ISC_R_SUCCESS,
+			NULL, ISC_R_SUCCESS
+		}, {
+			"testdata/options-timeout.conf", ISC_R_SUCCESS,
+			NULL, ISC_R_SUCCESS
+		}, {
+			"testdata/options-unknown.conf", ISC_R_SUCCESS,
+			NULL, ISC_R_SUCCESS
+		}, {
+			"testdata/options.conf", ISC_R_SUCCESS,
+			NULL, ISC_R_SUCCESS
+		}, {
+			"testdata/port.conf", ISC_R_SUCCESS,
+			NULL, ISC_R_SUCCESS
+		}, {
+			"testdata/resolv.conf", ISC_R_SUCCESS,
+			NULL, ISC_R_SUCCESS
+		}, {
+			"testdata/search.conf", ISC_R_SUCCESS,
+			NULL, ISC_R_SUCCESS
+		}, {
+			"testdata/sortlist-v4.conf", ISC_R_SUCCESS,
+			NULL, ISC_R_SUCCESS
+		}, {
+			"testdata/timeout.conf", ISC_R_SUCCESS,
+			NULL, ISC_R_SUCCESS
+		}, {
+			"testdata/unknown.conf", ISC_R_SUCCESS,
+			NULL, ISC_R_SUCCESS
+		}
+		
+	};
+
+	UNUSED(tc);
+
+	setup_test();
+
+	for (i = 0; i < sizeof(tests)/sizeof(tests[1]); i++) {
+		result = irs_resconf_load(mctx, tests[i].file, &resconf);
+		ATF_CHECK_EQ_MSG(result, tests[i].loadres, "%s", tests[i].file);
+		if (result == ISC_R_SUCCESS)
+			ATF_CHECK_MSG(resconf != NULL, "%s", tests[i].file);
+		else
+			ATF_CHECK_MSG(resconf == NULL, "%s", tests[i].file);
+		if (resconf != NULL && tests[i].check != NULL) {
+			result = (tests[i].check)(resconf);
+			ATF_CHECK_EQ_MSG(result, tests[i].checkres, "%s",
+					 tests[i].file);
+		}
+		if (resconf != NULL)
+			irs_resconf_destroy(&resconf);
+	}
+
+	isc_mem_detach(&mctx);
+}
+
+/*
+ * Main
+ */
+ATF_TP_ADD_TCS(tp) {
+	ATF_TP_ADD_TC(tp, irs_resconf_load);
+	return (atf_no_error());
+}
