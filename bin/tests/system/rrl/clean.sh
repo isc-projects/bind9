1ecdb4d59ef1ff03986342a475d91757b0acc778
Evan Hunt <each@isc.org>
2016-11-02 20:11:21 -0700

    [v9_10] make rrl system test more robust
    4508.	[test]		Make the rrl system test more reliable on slower
    			machines by using mdig instead of dig. [RT #43280]

--- bin/tests/system/rrl/clean.sh
+++ bin/tests/system/rrl/clean.sh
@@ -16,6 +16,6 @@
 
 # Clean up after rrl tests.
 
-rm -f dig.out*
+rm -f dig.out* *mdig.out*
 rm -f  */named.memstats */named.run */named.stats */log-* */session.key
 rm -f ns3/bl*.db */*.jnl */*.core */*.pid
