fb87feb6e7ce747269eb453c31c775364072a5b4
Francis Dupont <fdupont@isc.org>
2016-11-04 14:56:48 +0100

    Fixed filter-aaaa prereq.sh

--- bin/tests/system/filter-aaaa/prereq.sh
+++ bin/tests/system/filter-aaaa/prereq.sh
@@ -6,10 +6,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-if $FEATURETEST --enable-filter-aaaa
-then
-    :
-else
+SYSTEMTESTTOP=..
+. $SYSTEMTESTTOP/conf.sh
+
+$FEATURETEST --enable-filter-aaaa || {
     echo "I:This test requires --enable-filter-aaaa at compile time." >&2
     exit 255
-fi
+}
+exit 0
