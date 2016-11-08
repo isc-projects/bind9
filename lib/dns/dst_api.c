8176815e04c0049d388831b575f398f75d4e57b4
Mark Andrews <marka@isc.org>
2016-11-09 10:17:28 +1100

    back port alway use a private memory pool with openssl

--- lib/dns/dst_api.c
+++ lib/dns/dst_api.c
@@ -175,7 +175,7 @@ dst_lib_init2(isc_mem_t *mctx, isc_entropy_t *ectx,
 
 	dst__memory_pool = NULL;
 
-#if defined(OPENSSL) && defined(BIND9)
+#if defined(OPENSSL)
 	UNUSED(mctx);
 	/*
 	 * When using --with-openssl, there seems to be no good way of not
@@ -190,13 +190,11 @@ dst_lib_init2(isc_mem_t *mctx, isc_entropy_t *ectx,
 	if (result != ISC_R_SUCCESS)
 		return (result);
 	isc_mem_setname(dst__memory_pool, "dst", NULL);
-#else
-	isc_mem_attach(mctx, &dst__memory_pool);
-#endif
-#if defined(OPENSSL)
 #ifndef OPENSSL_LEAKS
 	isc_mem_setdestroycheck(dst__memory_pool, ISC_FALSE);
 #endif
+#else
+	isc_mem_attach(mctx, &dst__memory_pool);
 #endif
 #ifdef BIND9
 	isc_entropy_attach(ectx, &dst_entropy_pool);
