eb4ffd6685ca84e766a2407e3c22aeb774f14bbf
Curtis Blackburn <ckb@isc.org>
2016-11-18 11:12:42 -0800

    4515.   [port]          FreeBSD: Find readline headers when they are in                         edit/readline/ instead of readline/. [RT #43658]

--- bin/nsupdate/nsupdate.c
+++ bin/nsupdate/nsupdate.c
@@ -81,7 +81,12 @@
 #include <bind9/getaddresses.h>
 
 #if defined(HAVE_READLINE)
-#if defined(HAVE_EDITLINE_READLINE_H)
+#if defined(HAVE_EDIT_READLINE_READLINE_H)
+#include <edit/readline/readline.h>
+#if defined(HAVE_EDIT_READLINE_HISTORY_H)
+#include <edit/readline/history.h>
+#endif
+#elif defined(HAVE_EDITLINE_READLINE_H)
 #include <editline/readline.h>
 #else
 #include <readline/readline.h>
