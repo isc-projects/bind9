ff963a5d934ccdae2c2276c443acf3c0f778a7ff
Mark Andrews <marka@isc.org>
2017-01-12 14:12:05 +1100

    4552.   [bug]           Named could trigger a assertion when sending notify                         messages. [RT #44019]
    (cherry picked from commit 7b9e28f1a568ec217c570fdf15bb81e8068656e9)

--- lib/isc/ratelimiter.c
+++ lib/isc/ratelimiter.c
@@ -155,7 +155,7 @@ isc_ratelimiter_enqueue(isc_ratelimiter_t *rl, isc_task_t *task,
 	    rl->state == isc_ratelimiter_stalled) {
 		ev->ev_sender = task;
 		*eventp = NULL;
-		ISC_LIST_APPEND(rl->pending, ev, ev_link);
+		ISC_LIST_APPEND(rl->pending, ev, ev_ratelink);
 	} else if (rl->state == isc_ratelimiter_idle) {
 		result = isc_timer_reset(rl->timer, isc_timertype_ticker, NULL,
 					 &rl->interval, ISC_FALSE);
@@ -181,8 +181,8 @@ isc_ratelimiter_dequeue(isc_ratelimiter_t *rl, isc_event_t *event) {
 	REQUIRE(event != NULL);
 
 	LOCK(&rl->lock);
-	if (ISC_LINK_LINKED(event, ev_link)) {
-		ISC_LIST_UNLINK(rl->pending, event, ev_link);
+	if (ISC_LINK_LINKED(event, ev_ratelink)) {
+		ISC_LIST_UNLINK(rl->pending, event, ev_ratelink);
 		event->ev_sender = NULL;
 	} else
 		result = ISC_R_NOTFOUND;
@@ -210,7 +210,7 @@ ratelimiter_tick(isc_task_t *task, isc_event_t *event) {
 			/*
 			 * There is work to do.  Let's do it after unlocking.
 			 */
-			ISC_LIST_UNLINK(rl->pending, p, ev_link);
+			ISC_LIST_UNLINK(rl->pending, p, ev_ratelink);
 		} else {
 			/*
 			 * No work left to do.  Stop the timer so that we don't
@@ -244,7 +244,7 @@ isc_ratelimiter_shutdown(isc_ratelimiter_t *rl) {
 	(void)isc_timer_reset(rl->timer, isc_timertype_inactive,
 			      NULL, NULL, ISC_FALSE);
 	while ((ev = ISC_LIST_HEAD(rl->pending)) != NULL) {
-		ISC_LIST_UNLINK(rl->pending, ev, ev_link);
+		ISC_LIST_UNLINK(rl->pending, ev, ev_ratelink);
 		ev->ev_attributes |= ISC_EVENTATTR_CANCELED;
 		task = ev->ev_sender;
 		isc_task_send(task, &ev);
