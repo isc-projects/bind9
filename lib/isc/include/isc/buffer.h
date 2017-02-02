7769c929466d2cfda205fe2666ee5b2aaf4f3f44
Evan Hunt <each@isc.org>
2017-02-02 11:32:39 -0800

    [master] support autore in inline macro buffer functions
    4565.	[cleanup]	The inline macro versions of isc_buffer_put*()
    			did not implement automatic buffer reallocation.
    			[RT #44216]

--- lib/isc/include/isc/buffer.h
+++ lib/isc/include/isc/buffer.h
@@ -876,6 +876,11 @@ ISC_LANG_ENDDECLS
 
 #define ISC__BUFFER_PUTMEM(_b, _base, _length) \
 	do { \
+		if (ISC_UNLIKELY((_b)->autore)) { \
+			REQUIRE(isc_buffer_reserve(&(_b), _length) \
+				== ISC_R_SUCCESS); \
+		} \
+		REQUIRE(isc_buffer_availablelength(_b) >= _length); \
 		memmove(isc_buffer_used(_b), (_base), (_length)); \
 		(_b)->used += (_length); \
 	} while (0)
@@ -885,6 +890,11 @@ ISC_LANG_ENDDECLS
 		unsigned int _length; \
 		unsigned char *_cp; \
 		_length = strlen(_source); \
+		if (ISC_UNLIKELY((_b)->autore)) { \
+			REQUIRE(isc_buffer_reserve(&(_b), _length) \
+				== ISC_R_SUCCESS); \
+		} \
+		REQUIRE(isc_buffer_availablelength(_b) >= _length); \
 		_cp = isc_buffer_used(_b); \
 		memmove(_cp, (_source), _length); \
 		(_b)->used += (_length); \
@@ -894,6 +904,11 @@ ISC_LANG_ENDDECLS
 	do { \
 		unsigned char *_cp; \
 		isc_uint8_t _val2 = (_val); \
+		if (ISC_UNLIKELY((_b)->autore)) { \
+			REQUIRE(isc_buffer_reserve(&(_b), 1) \
+				== ISC_R_SUCCESS); \
+		} \
+		REQUIRE(isc_buffer_availablelength(_b) >= 1); \
 		_cp = isc_buffer_used(_b); \
 		(_b)->used++; \
 		_cp[0] = _val2 & 0x00ff; \
@@ -903,6 +918,10 @@ ISC_LANG_ENDDECLS
 	do { \
 		unsigned char *_cp; \
 		isc_uint16_t _val2 = (_val); \
+		if (ISC_UNLIKELY((_b)->autore)) { \
+			REQUIRE(isc_buffer_reserve(&(_b), 2) == ISC_R_SUCCESS); \
+		} \
+		REQUIRE(isc_buffer_availablelength(_b) >= 2); \
 		_cp = isc_buffer_used(_b); \
 		(_b)->used += 2; \
 		_cp[0] = (unsigned char)((_val2 & 0xff00U) >> 8); \
@@ -913,6 +932,11 @@ ISC_LANG_ENDDECLS
 	do { \
 		unsigned char *_cp; \
 		isc_uint32_t _val2 = (_val); \
+		if (ISC_UNLIKELY((_b)->autore)) { \
+			REQUIRE(isc_buffer_reserve(&(_b), 3) \
+				== ISC_R_SUCCESS); \
+		} \
+		REQUIRE(isc_buffer_availablelength(_b) >= 3); \
 		_cp = isc_buffer_used(_b); \
 		(_b)->used += 3; \
 		_cp[0] = (unsigned char)((_val2 & 0xff0000U) >> 16); \
@@ -924,6 +948,11 @@ ISC_LANG_ENDDECLS
 	do { \
 		unsigned char *_cp; \
 		isc_uint32_t _val2 = (_val); \
+		if (ISC_UNLIKELY((_b)->autore)) { \
+			REQUIRE(isc_buffer_reserve(&(_b), 4) \
+				== ISC_R_SUCCESS); \
+		} \
+		REQUIRE(isc_buffer_availablelength(_b) >= 4); \
 		_cp = isc_buffer_used(_b); \
 		(_b)->used += 4; \
 		_cp[0] = (unsigned char)((_val2 & 0xff000000) >> 24); \
