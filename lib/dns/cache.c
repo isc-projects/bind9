ddac00e3e0a84d0cdf50483b24390e96e8d3997b
Mark Andrews <marka@isc.org>
2017-04-20 10:21:00 +1000

    4584.   [bug]           A number of memory usage statistics were not properly                         reported when they exceeded 4G.  [RT #44750]

--- lib/dns/cache.c
+++ lib/dns/cache.c
@@ -1411,18 +1411,24 @@ dns_cache_dumpstats(dns_cache_t *cache, FILE *fp) {
 		(isc_uint64_t) dns_db_hashsize(cache->db),
 		"cache database hash buckets");
 
-	fprintf(fp, "%20u %s\n", (unsigned int) isc_mem_total(cache->mctx),
+	fprintf(fp, "%20" ISC_PLATFORM_QUADFORMAT "u %s\n",
+	        (isc_uint64_t) isc_mem_total(cache->mctx),
 		"cache tree memory total");
-	fprintf(fp, "%20u %s\n", (unsigned int) isc_mem_inuse(cache->mctx),
+	fprintf(fp, "%20" ISC_PLATFORM_QUADFORMAT "u %s\n",
+		(isc_uint64_t) isc_mem_inuse(cache->mctx),
 		"cache tree memory in use");
-	fprintf(fp, "%20u %s\n", (unsigned int) isc_mem_maxinuse(cache->mctx),
+	fprintf(fp, "%20" ISC_PLATFORM_QUADFORMAT "u %s\n",
+		(isc_uint64_t) isc_mem_maxinuse(cache->mctx),
 		"cache tree highest memory in use");
 
-	fprintf(fp, "%20u %s\n", (unsigned int) isc_mem_total(cache->hmctx),
+	fprintf(fp, "%20" ISC_PLATFORM_QUADFORMAT "u %s\n",
+		(isc_uint64_t) isc_mem_total(cache->hmctx),
 		"cache heap memory total");
-	fprintf(fp, "%20u %s\n", (unsigned int) isc_mem_inuse(cache->hmctx),
+	fprintf(fp, "%20" ISC_PLATFORM_QUADFORMAT "u %s\n",
+		(isc_uint64_t) isc_mem_inuse(cache->hmctx),
 		"cache heap memory in use");
-	fprintf(fp, "%20u %s\n", (unsigned int) isc_mem_maxinuse(cache->hmctx),
+	fprintf(fp, "%20" ISC_PLATFORM_QUADFORMAT "u %s\n",
+		(isc_uint64_t) isc_mem_maxinuse(cache->hmctx),
 		"cache heap highest memory in use");
 }
 
