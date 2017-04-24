8d59536d7c24e137f2bb1796b962bbafd8cac701
Evan Hunt <each@isc.org>
2017-04-23 17:06:46 -0700

    [v9_11] recent_changes script could terminate too early
    (cherry picked from commit 58502352f20caf2c9363ca8c4f7dbe141bd98e98)

--- util/recent_changes.sh
+++ util/recent_changes.sh
@@ -6,18 +6,15 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# $Id$
-
 # Find the list of files that have been touched in the Git repository
 # during the current calendar year.  This is done by walking backwards
 # through the output of "git whatchanged" until a year other than the
 # current one is seen.  Used by merge_copyrights.
 
 thisyear=`date +%Y`
-git whatchanged --pretty="date %ai" --date=iso8601 | awk -v re="${thisyear}-" '
+when="`expr $thisyear - 1`-12-31"
+git whatchanged --since="$when" --pretty="" | awk '
     BEGIN { change=0 }
-    $1 == "date" && $2 !~ re { exit(0); }
-    $1 == "date" { next; }
     NF == 0 { next; }
     $(NF-1) ~ /[AM]/ { print "./" $NF; change=1 }
     END { if (change) print "./COPYRIGHT" } ' | sort | uniq
