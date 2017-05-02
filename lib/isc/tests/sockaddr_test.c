7e48ed2240a9df1f7e1499acfd2d69955f6e5774
Evan Hunt <each@isc.org>
2017-05-02 13:41:31 -0700

    [v9_9] error in sockaddr unit test
    4614.	[test]		Fixed an error in the sockaddr unit test. [RT #45146]
    
    (cherry picked from commit d73c32c17fa6a59701bfdbfd78055f6d8c7c3d36)
    (cherry picked from commit 512765ba1e3f0b29e2c1ae4b0138838c6422f2bd)

--- lib/isc/tests/sockaddr_test.c
+++ lib/isc/tests/sockaddr_test.c
@@ -110,18 +110,18 @@ ATF_TC_BODY(sockaddr_isnetzero, tc) {
 	UNUSED(tc);
 
 	for (i = 0; i < sizeof(data4)/sizeof(data4[0]); i++) {
-		in.s_addr = inet_addr(data4[0].string);
+		in.s_addr = inet_addr(data4[i].string);
 		isc_sockaddr_fromin(&addr, &in, 1);
 		r = isc_sockaddr_isnetzero(&addr);
-		ATF_CHECK_EQ(r, data4[0].expect);
+		ATF_CHECK_EQ_MSG(r, data4[i].expect, "%s", data4[i].string);
 	}
 
 	for (i = 0; i < sizeof(data6)/sizeof(data6[0]); i++) {
-		ret = inet_pton(AF_INET6, "::ffff:127.0.0.1", &in6);
+		ret = inet_pton(AF_INET6, data6[i].string, &in6);
 		ATF_CHECK_EQ(ret, 1);
 		isc_sockaddr_fromin6(&addr, &in6, 1);
 		r = isc_sockaddr_isnetzero(&addr);
-		ATF_CHECK_EQ(r, data6[0].expect);
+		ATF_CHECK_EQ_MSG(r, data6[i].expect, "%s", data6[i].string);
 	}
 }
 
