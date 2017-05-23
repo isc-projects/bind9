d90f168dfb31b3af9a0bf8088f80a07683aadb1b
Evan Hunt <each@isc.org>
2017-05-22 17:15:39 -0700

    [v9_10] add chain ordering tests
    4626.	[test]		Added more tests for handling of different record
    			ordering in CNAME and DNAME responses. [QA #430]

---
+++ bin/tests/system/chain/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Copyright (C) 2017  Internet Systems Consortium, Inc. ("ISC")
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+SYSTEMTESTTOP=..
+. $SYSTEMTESTTOP/conf.sh
+
+$SHELL clean.sh
+
+test -r $RANDFILE || $GENRANDOM 400 $RANDFILE
+
+cd ns2
+$SHELL sign.sh
