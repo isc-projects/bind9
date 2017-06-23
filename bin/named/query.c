b2e71853060a384070d422afda6d1c692ff608e3
Mark Andrews <marka@isc.org>
2017-06-23 17:17:28 +1000

    4640.   [bug]           If query_findversion failed in query_getdb due to                         memory failure the error status was incorrectly                         discarded. [RT #45331]
    (cherry picked from commit b551ee14bd12fff52aa05f7f94cd4e17cea52be3)

--- bin/named/query.c
+++ bin/named/query.c
@@ -1267,7 +1267,7 @@ query_getdb(ns_client_t *client, dns_name_t *name, dns_rdatatype_t qtype,
 
 			dbversion = query_findversion(client, tdbp);
 			if (dbversion == NULL) {
-				result = ISC_R_NOMEMORY;
+				tresult = ISC_R_NOMEMORY;
 			} else {
 				/*
 				 * Be sure to return our database.
