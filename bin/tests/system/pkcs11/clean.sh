28061f80b698171892e81c0856bc722190947998
Evan Hunt <each@isc.org>
2017-06-29 15:54:35 -0700

    [v9_11] fix RSA parsing when md5 disabled
    4645.	[bug]		Fix PKCS#11 RSA parsing when MD5 is disabled.
    			[RT #45300]
    
    (cherry picked from commit b05b3fab3c37c9c07321f01b745719307c84aa2f)

--- bin/tests/system/pkcs11/clean.sh
+++ bin/tests/system/pkcs11/clean.sh
@@ -7,7 +7,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 rm -f K* ns1/K* keyset-* dsset-* ns1/*.db ns1/*.signed ns1/*.jnl
-rm -f dig.out pin
+rm -f dig.out* pin upd.log*
 rm -f ns1/*.key ns1/named.memstats
 rm -f supported
 rm -f ns*/named.lock
