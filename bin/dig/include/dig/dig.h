b5f2eaec62af2eec08a63e2113a95c89331e3fb4
Tinderbox User <tbox@isc.org>
2017-07-21 23:48:17 +0000

    update copyright notice / whitespace

--- bin/dig/include/dig/dig.h
+++ bin/dig/include/dig/dig.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2009, 2011-2016  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2009, 2011-2017  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 2000-2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
