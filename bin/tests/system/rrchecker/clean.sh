89cd73f8f7a1eeb6a703d77dbfa9a3dc674064ab
Michał Kępień <michal@isc.org>
2017-07-24 10:39:51 +0200

    [v9_10] Properly handle errors in rrchecker system test
    4657.	[bug]		rrchecker system test result could be improperly
    			determined. [RT #45602]
    
    (cherry picked from commit 984a28c771b762c5c2f64553cbf3d52f85a0cbce)

--- bin/tests/system/rrchecker/clean.sh
+++ bin/tests/system/rrchecker/clean.sh
@@ -12,4 +12,4 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-rm -f classlist.out privatelist.out typelist.out tempzone checker.out
+rm -f classlist.out privatelist.out typelist.out tempzone checkzone.out* checker.out
