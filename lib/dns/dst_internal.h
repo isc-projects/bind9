17a549b541a4cabe429d459beb09b6e9ac24248d
Tinderbox User <tbox@isc.org>
2017-07-31 23:47:22 +0000

    update copyright notice / whitespace

--- lib/dns/dst_internal.h
+++ lib/dns/dst_internal.h
@@ -1,5 +1,5 @@
 /*
- * Portions Copyright (C) 2004-2014, 2016  Internet Systems Consortium, Inc. ("ISC")
+ * Portions Copyright (C) 2004-2014, 2016, 2017  Internet Systems Consortium, Inc. ("ISC")
  * Portions Copyright (C) 2000-2002  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
