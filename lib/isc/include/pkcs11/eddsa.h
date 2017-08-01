b74e1c3b50c9c5258c8a23a3ac8bf2dea54b1cc8
Tinderbox User <tbox@isc.org>
2017-08-01 23:46:29 +0000

    update copyright notice / whitespace

--- lib/isc/include/pkcs11/eddsa.h
+++ lib/isc/include/pkcs11/eddsa.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2017  Internet Systems Consortium, Inc. ("ISC")
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #ifndef _EDDSA_H_
 #define _EDDSA_H_ 1
 
