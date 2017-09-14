acaa5bec31ef67ef84adcb6a0c1b501a629cb29b
Evan Hunt <each@isc.org>
2017-09-13 21:19:38 -0700

    [v9_9] clean up bufsize errors
    (cherry picked from commit 9b729a06b015165aacca2ed6f471eec41bef6ae1)

--- lib/dns/name.c
+++ lib/dns/name.c
@@ -1612,7 +1612,7 @@ dns_name_tofilenametext(dns_name_t *name, isc_boolean_t omit_final_dot,
 					trem--;
 					nlen--;
 				} else {
-					if (trem < 3)
+					if (trem < 4)
 						return (ISC_R_NOSPACE);
 					snprintf(tdata, trem, "%%%02X", c);
 					tdata += 3;
