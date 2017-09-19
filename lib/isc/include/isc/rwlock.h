fb548d30d72a2b48b6af5bcd5795868fa2f7df8d
Tinderbox User <tbox@isc.org>
2017-09-19 23:48:54 +0000

    update copyright notice / whitespace

--- lib/isc/include/isc/rwlock.h
+++ lib/isc/include/isc/rwlock.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004-2007, 2017  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1998-2001, 2003  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -104,7 +104,7 @@ struct isc_rwlock {
 	 * when the quota is reached and it is time to switch.
 	 */
 	unsigned int		granted;
-	
+
 	unsigned int		readers_waiting;
 	unsigned int		writers_waiting;
 	unsigned int		read_quota;
