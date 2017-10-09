c0f78692eeb711152437b26afacc5ed2b0185148
Michał Kępień <michal@isc.org>
2017-10-09 10:49:09 +0200

    [master] Sync draft-durand-doa-over-dns snippet in lib/dns/tests/rdata_test.c with draft version -03

--- lib/dns/tests/rdata_test.c
+++ lib/dns/tests/rdata_test.c
@@ -548,9 +548,10 @@ ATF_TC_BODY(csync, tc) {
  *
  *    The DOA-MEDIA-TYPE field is presented as a single <character-string>.
  *
- *    The DOA-DATA is presented as Base64 encoded data [RFC3548] unless the
+ *    The DOA-DATA is presented as Base64 encoded data [RFC4648] unless the
  *    DOA-DATA is empty in which case it is presented as a single dash
- *    character ("-", ASCII 45).  White space is permitted within Base64 data.
+ *    character ("-", ASCII 45).  White space is permitted within Base64
+ *    data.
  */
 ATF_TC(doa);
 ATF_TC_HEAD(doa, tc) {
