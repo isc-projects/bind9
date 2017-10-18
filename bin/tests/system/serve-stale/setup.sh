8a107c58f25a192ed37f50de6a55903a5985bd22
Ondřej Surý <ondrej@sury.org>
2017-10-18 13:01:14 +0200

    [rt46602] Update server-stale test to run on port passed from run.sh script
    (cherry picked from commit f83ebd34b9555a5a834c58146035173bcbd01dda)

--- bin/tests/system/serve-stale/setup.sh
+++ bin/tests/system/serve-stale/setup.sh
@@ -1,7 +1,17 @@
+#!/bin/sh
 # Copyright (C) 2017  Internet Systems Consortium, Inc. ("ISC")
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-cp -f ns1/named1.conf ns1/named.conf
+. ../getopts.sh
+
+sed -e "s/@PORT@/${port}/g;s/@CONTROLPORT@/${controlport}/g" < ns1/named1.conf.in > ns1/named.conf
+echo "${port}" > ns1/named.port
+
+sed -e "s/@PORT@/${port}/g;s/@CONTROLPORT@/${controlport}/g" < ans2/ans.pl.in > ans2/ans.pl
+echo "${port}" > ans2/named.port
+
+sed -e "s/@PORT@/${port}/g;s/@CONTROLPORT@/${controlport}/g" < ns3/named.conf.in > ns3/named.conf
+echo "${port}" > ns3/named.port
