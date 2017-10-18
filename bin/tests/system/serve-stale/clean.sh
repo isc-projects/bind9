acf4c2d80e2c4c83cd9df9c27c8693d35fbe5f9a
Ondřej Surý <ondrej@sury.org>
2017-10-18 13:01:14 +0200

    [rt46602] Update server-stale test to run on port passed from run.sh script

--- bin/tests/system/serve-stale/clean.sh
+++ bin/tests/system/serve-stale/clean.sh
@@ -5,6 +5,8 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 rm -f dig.out.test*
-rm -f ns1/named.conf
+rm -f ns1/named.conf ns1/named.port
+rm -f ans2/ans.pl ans2/named.port
+rm -f ns3/named.conf ns3/named.port
 rm -f ns3/root.bk
 rm -f rndc.out.test*
