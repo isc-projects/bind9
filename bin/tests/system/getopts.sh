9878b46447b6608e70781e5266dd2eab7be48f62
Ondřej Surý <ondrej@sury.org>
2017-10-18 13:00:15 +0200

    [rt46602] Change the system tests script to support running individual tests on different ports
    Resolved conflict in run.sh.

---
+++ bin/tests/system/getopts.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Copyright (C) 2017  Internet Systems Consortium, Inc. ("ISC")
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# shell script snippet, must be sourced
+
+port=5300
+controlport=9953
+
+while getopts "p:c:" flag; do
+    case "$flag" in
+	p) port=$OPTARG ;;
+	c) controlport=$OPTARG ;;
+	*) exit 1 ;;
+    esac
+done
+shift $(($OPTIND - 1))
