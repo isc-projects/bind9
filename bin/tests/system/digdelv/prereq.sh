f44a828f20e04171de823e89d200308b06b51221
Ondřej Surý <ondrej@sury.org>
2017-10-18 19:23:44 +0000

    Fix missing prereq Net::DNS in couple of tests

--- bin/tests/system/digdelv/prereq.sh
+++ bin/tests/system/digdelv/prereq.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2015-2017  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2017  Internet Systems Consortium, Inc. ("ISC")
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -13,4 +13,4 @@ if ! $PERL -e 'use Net::DNS;' 2>/dev/null; then
     exit 1
 fi
 
-exec $SHELL ../testcrypto.sh
+exit 0
--- bin/tests/system/zero/prereq.sh
+++ bin/tests/system/zero/prereq.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 #
-# Copyright (C) 2015-2017  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2017  Internet Systems Consortium, Inc. ("ISC")
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-. ../conf.sh
 
 if ! $PERL -e 'use Net::DNS;' 2>/dev/null; then
     echoinfo "I:This test requires the Net::DNS library." >&2
     exit 1
 fi
 
-exec $SHELL ../testcrypto.sh
+exit 0
