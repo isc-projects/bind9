0a5b0a447ede69494c81d5f6e98eab9f63babeeb
Evan Hunt <each@isc.org>
2017-10-23 19:38:29 -0700

    [v9_9] omit NS from authority section if it was in answer
    4780.	[bug]		When answering ANY queries, don't include the NS
    			RRset in the authority section if it was already
    			in the answer section. [RT #44543]
    
    (cherry picked from commit b7b76d6b855cd4c1152c26d34fb61af05f965c5e)
    (cherry picked from commit 13435af49af98853666155b4564db6580631431d)

--- bin/named/query.c
+++ bin/named/query.c
@@ -5699,6 +5699,7 @@ query_find(ns_client_t *client, dns_fetchevent_t *event, dns_rdatatype_t qtype)
 	dns_rdatasetiter_t *rdsiter;
 	isc_boolean_t want_restart, authoritative, is_zone, need_wildcardproof;
 	isc_boolean_t is_staticstub_zone;
+	isc_boolean_t answer_has_ns = ISC_FALSE;
 	unsigned int n, nlabels;
 	dns_namereln_t namereln;
 	int order;
@@ -7340,6 +7341,15 @@ query_find(ns_client_t *client, dns_fetchevent_t *event, dns_rdatatype_t qtype)
 					have_a = ISC_TRUE;
 			}
 #endif
+			/*
+			 * We found an NS RRset; no need to add one later.
+			 */
+			if (qtype == dns_rdatatype_any &&
+			    rdataset->type == dns_rdatatype_ns)
+			{
+				answer_has_ns = ISC_TRUE;
+			}
+
 			if (is_zone && qtype == dns_rdatatype_any &&
 			    !dns_db_issecure(db) &&
 			    dns_rdatatype_isdnssec(rdataset->type)) {
@@ -7666,14 +7676,13 @@ query_find(ns_client_t *client, dns_fetchevent_t *event, dns_rdatatype_t qtype)
 	 */
 	if (!want_restart && !NOAUTHORITY(client)) {
 		if (is_zone) {
-			if (!((qtype == dns_rdatatype_ns ||
-			       qtype == dns_rdatatype_any) &&
-			      dns_name_equal(client->query.qname,
-					     dns_db_origin(db))))
+			if (!answer_has_ns) {
 				(void)query_addns(client, db, version);
-		} else if (qtype != dns_rdatatype_ns) {
-			if (fname != NULL)
+			}
+		} else if (!answer_has_ns && qtype != dns_rdatatype_ns) {
+			if (fname != NULL) {
 				query_releasename(client, &fname);
+			}
 			query_addbestns(client);
 		}
 	}
