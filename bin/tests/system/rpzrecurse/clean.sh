b667faba1ca831b254d4e2a6438428ec3ee7a99b
Stephen Morris <stephen@isc.org>
2017-11-17 17:29:21 +0000

    [rt46602] Miscellaneous changes to existing parallelised tests
    Currently these tests are allow_query, rpzrecurse and serve-stale
    
    1. Function to copy files and set port numbers renamed from copy_config
    to copy_setports, as this is used to change the ports in Perl and Python
    test scripts as well.
    2. Changes to rpzrecurse/tests.sh to handle two calls to getopts (one to
    parse port numbers, the other to parse rpzrecurse-specific options). Also
    fixed various commands to use correct ports.
    3. Updates to "clean.sh" scripts to ensure that all files created in the
    test are removed.

--- bin/tests/system/rpzrecurse/clean.sh
+++ bin/tests/system/rpzrecurse/clean.sh
@@ -6,14 +6,21 @@
 
 # Clean up after rpz tests.
 
+rm -f test.output
 rm -f dig.out.*
+
 rm -f ns*/named.lock
 rm -f ns*/named.memstats
 rm -f ns*/*.run
+rm -f ns*/named.port
+rm -f ns*/*core *core
+rm -f ns*/named.conf
+
 rm -f ns2/*.local
 rm -f ns2/*.queries
 rm -f ns2/named.[0-9]*.conf
-rm -f ns2/named.conf
-rm -f ns3/named.conf
-rm -f ns*/*core *core
+rm -f ns2/named.conf.header
+
+rm -f ans4/ans.pl
+
 rm -f dnsrps*.conf dnsrpzd*
