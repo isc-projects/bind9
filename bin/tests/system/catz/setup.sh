55d2bb375cd88bdd102b7581b2dc3c18d3d023ee
Stephen Morris <stephen@isc.org>
2017-11-22 12:37:17 +0000

    [rt46602] Allow catz system test to run in parallel with other tests

--- bin/tests/system/catz/setup.sh
+++ bin/tests/system/catz/setup.sh
@@ -8,12 +8,19 @@
 
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
+. $SYSTEMTESTTOP/getopts.sh
 
-$SHELL clean.sh
+copy_setports ns1/named.conf.in ns1/named.conf
+copy_setports ns2/named.conf.in ns2/named.conf
+copy_setports ns3/named.conf.in ns3/named.conf
 
-cat ns1/catalog.example.db.in > ns1/catalog1.example.db
-cat ns1/catalog.example.db.in > ns3/catalog2.example.db
-cat ns1/catalog.example.db.in > ns1/catalog3.example.db
-cat ns1/catalog.example.db.in > ns1/catalog4.example.db
-cat ns2/named.conf.in > ns2/named.conf
-mkdir ns2/zonedir
+cp -f ns1/catalog.example.db.in ns1/catalog1.example.db
+cp -f ns1/catalog.example.db.in ns3/catalog2.example.db
+cp -f ns1/catalog.example.db.in ns1/catalog3.example.db
+cp -f ns1/catalog.example.db.in ns1/catalog4.example.db
+
+mkdir -p ns2/zonedir
+
+echo "${port}" > ns1/named.port
+echo "${port}" > ns2/named.port
+echo "${port}" > ns3/named.port
