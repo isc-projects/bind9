57aa7b60fdbde7902de2598e2d7cc086ff0ca0e1
Stephen Morris <stephen@isc.org>
2017-11-22 12:37:17 +0000

    [rt46602] Allow catz system test to run in parallel with other tests

--- bin/tests/system/catz/clean.sh
+++ bin/tests/system/catz/clean.sh
@@ -4,15 +4,18 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+rm -f test.output
 rm -f dig.out.*
 rm -f ns*/*.jnl
 rm -f ns*/*.nzf
 rm -f ns*/named.lock
 rm -f ns*/named.memstats
+rm -f ns*/named.conf
 rm -f ns*/named.run
+rm -f ns*/named.port
 rm -f ns1/*dom*example.db
 rm -f ns2/__catz__*db
-rm -f ns2/named.conf
+rm -f ns2/named.conf.tmp
 rm -f ns3/dom{13,14}.example.db
 rm -f nsupdate.out.*
 rm -f ns{1,2,3}/catalog{1,2,3,4}.example.db
