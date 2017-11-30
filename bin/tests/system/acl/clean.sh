e197a2bd150783f53044342cf0d02510dfb744df
Evan Hunt <each@isc.org>
2017-11-30 12:37:08 -0800

    [master] fix "allow-transfer" inheritance and clean up ACL configuration
    4836.	[bug]		Zones created using "rndc addzone" could
    			temporarily fail to inherit an "allow-transfer"
    			ACL that had been configured in the options
    			statement. [RT #46603]

--- bin/tests/system/acl/clean.sh
+++ bin/tests/system/acl/clean.sh
@@ -15,3 +15,5 @@ rm -f ns2/example.db ns2/tsigzone.db ns2/example.db.jnl ns2/named.conf
 rm -f */named.memstats
 rm -f */named.run
 rm -f ns*/named.lock
+rm -f ns*/_default.nzf
+rm -f ns*/_default.nzd*
