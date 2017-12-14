21e48340b3355d031037b51937bfd113641efdca
Stephen Morris <stephen@isc.org>
2017-12-14 15:02:01 +0000

    [rt46602] Pass port numbers to tests via environment variables
    (cherry picked from commit f5d8f079008b648d2e343543e66dd728054c6101)

--- bin/tests/system/serve-stale/setup.sh
+++ bin/tests/system/serve-stale/setup.sh
@@ -5,13 +5,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-. ../getopts.sh
+SYSTEMTESTTOP=..
+. $SYSTEMTESTTOP/conf.sh
 
 copy_setports ns1/named1.conf.in ns1/named.conf
-echo "${port}" > ns1/named.port
-
 copy_setports ans2/ans.pl.in ans2/ans.pl
-echo "${port}" > ans2/named.port
-
 copy_setports ns3/named.conf.in ns3/named.conf
-echo "${port}" > ns3/named.port
