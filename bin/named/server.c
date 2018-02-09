1d4d23dac8cf2e557941bc1df774abc38153c49d
Mark Andrews <marka@isc.org>
2018-02-09 12:20:34 +1100

    fix meger error
    (cherry picked from commit f6b7ff72808bb8b44daa06bb45fad18c2964a6df)

--- bin/named/server.c
+++ bin/named/server.c
@@ -7406,7 +7406,7 @@ load_configuration(const char *filename, ns_server_t *server,
 
 	if (access(server->bindkeysfile, R_OK) == 0) {
 		isc_log_write(ns_g_lctx, NS_LOGCATEGORY_GENERAL,
-			      NAMED_LOGMODULE_SERVER, ISC_LOG_INFO,
+			      NS_LOGMODULE_SERVER, ISC_LOG_INFO,
 			      "reading built-in trust anchors "
 			      "from file '%s'", server->bindkeysfile);
 
@@ -7418,7 +7418,7 @@ load_configuration(const char *filename, ns_server_t *server,
 		CHECK(result);
 	} else {
 		isc_log_write(ns_g_lctx, NS_LOGCATEGORY_GENERAL,
-			      NAMED_LOGMODULE_SERVER, ISC_LOG_INFO,
+			      NS_LOGMODULE_SERVER, ISC_LOG_INFO,
 			      "unable to open '%s'; using built-in keys "
 			      "instead", server->bindkeysfile);
 	}
