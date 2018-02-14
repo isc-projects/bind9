92eeb710c3e0ead70452020a66e6899ba2960260
Mark Andrews <marka@isc.org>
2018-02-14 17:53:23 +1100

    clear *logp

--- bin/dnssec/dnssectool.c
+++ bin/dnssec/dnssectool.c
@@ -212,12 +212,14 @@ cleanup_logging(isc_log_t **logp) {
 	REQUIRE(logp != NULL);
 
 	log = *logp;
+	*logp = NULL;
+
 	if (log == NULL)
 		return;
+
 	isc_log_destroy(&log);
 	isc_log_setcontext(NULL);
 	dns_log_setcontext(NULL);
-	logp = NULL;
 }
 
 void
