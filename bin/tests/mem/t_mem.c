4404fcb384d1ba18a64db9e9b735aa7df11292f9
Mark Andrews <marka@isc.org>
2018-02-15 16:14:08 +1100

    return failure count

--- bin/tests/mem/t_mem.c
+++ bin/tests/mem/t_mem.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2007, 2009, 2013  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2004, 2007, 2009, 2013, 2018  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -179,7 +179,7 @@ memtest(void) {
 
 	isc_mem_destroy(&mctx);
 
-	return(0);
+	return(nfails);
 }
 
 static const char *a1 =
