f7c7a87cb467130939115524b116d94adfc56c30
Mark Andrews <marka@isc.org>
2018-02-15 13:43:13 +1100

    use %u instead of %d

--- bin/named/client.c
+++ bin/named/client.c
@@ -2975,7 +2975,7 @@ ns_client_dumprecursing(FILE *f, ns_clientmgr_t *manager) {
 		}
 		UNLOCK(&client->query.fetchlock);
 		fprintf(f, "; client %s%s%s: id %u '%s/%s/%s'%s%s "
-			"requesttime %d\n", peerbuf, sep, name,
+			"requesttime %u\n", peerbuf, sep, name,
 			client->message->id, namebuf, typebuf, classbuf,
 			origfor, original, client->requesttime);
 		client = ISC_LIST_NEXT(client, rlink);
