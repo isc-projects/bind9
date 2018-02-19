60bd4b92b96e52e05362c0345ab1ea6f952a2d62
Ondřej Surý <ondrej@sury.org>
2018-02-19 12:03:52 +0100

    Update license headers to not include years in copyright in all applicable files

--- lib/dns/sdlz.c
+++ lib/dns/sdlz.c
@@ -1,9 +1,12 @@
 /*
- * Portions Copyright (C) 1999-2001, 2005-2018  Internet Systems Consortium, Inc. ("ISC")
+ * Portions Copyright (C) Internet Systems Consortium, Inc. ("ISC")
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * See the COPYRIGHT file distributed with this work for additional
+ * information regarding copyright ownership.
  */
 
 /*
