e4713ed1d49920942f43cfc1c0580bdc64e94371
Ondřej Surý <ondrej@sury.org>
2018-02-19 14:13:55 +0100

    Update copyrights for year 2018 in all applicable files

--- bin/tests/system/ifconfig.sh
+++ bin/tests/system/ifconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2000-2004, 2007-2010, 2012, 2013, 2016  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2000-2004, 2007-2010, 2012, 2013, 2016, 2018  Internet Systems Consortium, Inc. ("ISC")
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
