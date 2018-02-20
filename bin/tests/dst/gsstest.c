4fd64583266bc74e5284f1706ce8e03dd6fcb265
Mark Andrews <marka@isc.org>
2018-02-20 12:51:16 +1100

    remove unused struct dst_context declaration
    (cherry picked from commit f2c22662cc9de5c9be4ec03c7248e177eec26487)

--- bin/tests/dst/gsstest.c
+++ bin/tests/dst/gsstest.c
@@ -9,8 +9,6 @@
  * information regarding copyright ownership.
  */
 
-/* $Id: gsstest.c,v 1.19 2011/11/30 00:48:51 marka Exp $ */
-
 #include <config.h>
 
 #include <stdlib.h>
@@ -52,13 +50,6 @@
 
 #ifdef GSSAPI
 #include ISC_PLATFORM_GSSAPIHEADER
-
-struct dst_context {
-	unsigned int magic;
-	dst_key_t *key;
-	isc_mem_t *mctx;
-	void *opaque;
-};
 
 #define CHECK(str, x) { \
 	if ((x) != ISC_R_SUCCESS) { \
