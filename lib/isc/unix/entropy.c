4d8e501abec602d2c99b53a1cbc2ca0a5a05746f
Mark Andrews <marka@isc.org>
2018-02-20 13:08:08 +1100

    improve the conditional declaration and use of variable to silence cppcheck
    (cherry picked from commit a04bb76973e5d30d0ec7cc268eaa8db2a91934e6)

--- lib/isc/unix/entropy.c
+++ lib/isc/unix/entropy.c
@@ -6,8 +6,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-/* $Id: entropy.c,v 1.82 2008/12/01 23:47:45 tbox Exp $ */
-
 /* \file unix/entropy.c
  * \brief
  * This is the system dependent part of the ISC entropy API.
@@ -449,11 +447,14 @@ destroyusocketsource(isc_entropyusocketsource_t *source) {
 static isc_result_t
 make_nonblock(int fd) {
 	int ret;
-	int flags;
 	char strbuf[ISC_STRERRORSIZE];
 #ifdef USE_FIONBIO_IOCTL
 	int on = 1;
+#else
+	int flags;
+#endif
 
+#ifdef USE_FIONBIO_IOCTL
 	ret = ioctl(fd, FIONBIO, (char *)&on);
 #else
 	flags = fcntl(fd, F_GETFL, 0);
