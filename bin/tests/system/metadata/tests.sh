706fa3113dc14ffdecf67a33a2a4797b7736b473
Evan Hunt <each@isc.org>
2018-02-21 00:23:24 -0800

    [parallel-acl] parallelize metadata

--- bin/tests/system/metadata/tests.sh
+++ bin/tests/system/metadata/tests.sh
@@ -6,8 +6,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# $Id: tests.sh,v 1.9 2011/07/08 01:43:26 each Exp $
-
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
 
@@ -16,7 +14,7 @@ czone=child.parent.nil cfile=child.db
 status=0
 n=1
 
-echo "I:setting key timers"
+echo_i "setting key timers"
 $SETTIME -A now+15s `cat rolling.key` > /dev/null
 
 inact=`sed 's/^K'${czone}'.+005+0*\([0-9]\)/\1/' < inact.key`
@@ -30,7 +28,7 @@ zsk=`sed 's/^K'${czone}'.+005+0*\([0-9]\)/\1/' < zsk.key`
 
 $GENRANDOM 800 $RANDFILE
 
-echo "I:signing zones"
+echo_i "signing zones"
 $SIGNER -Sg -o $czone $cfile > /dev/null 2>&1
 $SIGNER -Sg -o $pzone $pfile > /dev/null 2>&1
 
@@ -51,81 +49,81 @@ awk '$2 ~ /DNSKEY/ {
 	print flags, id;
 }' < ${cfile}.signed > keys
 
-echo "I:checking that KSK signed DNSKEY only ($n)"
+echo_i "checking that KSK signed DNSKEY only ($n)"
 ret=0
 grep "DNSKEY $ksk"'$' sigs > /dev/null || ret=1
 grep "SOA $ksk"'$' sigs > /dev/null && ret=1
 n=`expr $n + 1`
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-echo "I:checking that ZSK signed ($n)"
+echo_i "checking that ZSK signed ($n)"
 ret=0
 grep "SOA $zsk"'$' sigs > /dev/null || ret=1
 n=`expr $n + 1`
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-echo "I:checking that standby ZSK did not sign ($n)"
+echo_i "checking that standby ZSK did not sign ($n)"
 ret=0
 grep " $standby"'$' sigs > /dev/null && ret=1
 n=`expr $n + 1`
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-echo "I:checking that inactive key did not sign ($n)"
+echo_i "checking that inactive key did not sign ($n)"
 ret=0
 grep " $inact"'$' sigs > /dev/null && ret=1
 n=`expr $n + 1`
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-echo "I:checking that pending key was not published ($n)"
+echo_i "checking that pending key was not published ($n)"
 ret=0
 grep " $pending"'$' keys > /dev/null && ret=1
 n=`expr $n + 1`
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-echo "I:checking that standby KSK did not sign but is delegated ($n)"
+echo_i "checking that standby KSK did not sign but is delegated ($n)"
 ret=0
 grep " $rolling"'$' sigs > /dev/null && ret=1
 grep " $rolling"'$' keys > /dev/null || ret=1
 egrep "DS[ 	]*$rolling[ 	]" ${pfile}.signed > /dev/null || ret=1
 n=`expr $n + 1`
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-echo "I:checking that key was revoked ($n)"
+echo_i "checking that key was revoked ($n)"
 ret=0
 grep " $prerev"'$' keys > /dev/null && ret=1
 grep " $postrev"'$' keys > /dev/null || ret=1
 n=`expr $n + 1`
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-echo "I:checking that revoked key self-signed ($n)"
+echo_i "checking that revoked key self-signed ($n)"
 ret=0
 grep "DNSKEY $postrev"'$' sigs > /dev/null || ret=1
 grep "SOA $postrev"'$' sigs > /dev/null && ret=1
 n=`expr $n + 1`
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-echo "I:waiting 20 seconds for key changes to occur"
+echo_i "waiting 20 seconds for key changes to occur"
 sleep 20
 
-echo "I:re-signing zone"
+echo_i "re-signing zone"
 $SIGNER  -Sg -o $czone -f ${cfile}.new ${cfile}.signed > /dev/null 2>&1
 
-echo "I:checking that standby KSK is now active ($n)"
+echo_i "checking that standby KSK is now active ($n)"
 ret=0
 grep "DNSKEY $rolling"'$' sigs > /dev/null && ret=1
 n=`expr $n + 1`
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-echo "I:checking update of an old-style key ($n)"
+echo_i "checking update of an old-style key ($n)"
 ret=0
 # printing metadata should not work with an old-style key
 $SETTIME -pall `cat oldstyle.key` > /dev/null 2>&1 && ret=1
@@ -133,13 +131,13 @@ $SETTIME -f `cat oldstyle.key` > /dev/null 2>&1 || ret=1
 # but now it should
 $SETTIME -pall `cat oldstyle.key` > /dev/null 2>&1 || ret=1
 n=`expr $n + 1`
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-echo "I:checking warning about permissions change on key with dnssec-settime ($n)"
+echo_i "checking warning about permissions change on key with dnssec-settime ($n)"
 uname=`uname -o 2> /dev/null`
 if [ Cygwin == "$uname"  ]; then
-	echo "I: Cygwin detected, skipping"
+	echo_i "Cygwin detected, skipping"
 else
 	ret=0
 	# settime should print a warning about changing the permissions
@@ -149,38 +147,38 @@ else
 	$SETTIME -P none `cat oldstyle.key` > settime2.test$n 2>&1 || ret=1
 	grep "warning: Permissions on the file.*have changed" settime2.test$n > /dev/null 2>&1 && ret=1
 	n=`expr $n + 1`
-	if [ $ret != 0 ]; then echo "I:failed"; fi
+	if [ $ret != 0 ]; then echo_i "failed"; fi
 	status=`expr $status + $ret`
 fi
 
-echo "I:checking warning about delete date < inactive date with dnssec-settime ($n)"
+echo_i "checking warning about delete date < inactive date with dnssec-settime ($n)"
 ret=0
 # settime should print a warning about delete < inactive
 $SETTIME -I now+15s -D now `cat oldstyle.key` > tmp.out 2>&1 || ret=1
 grep "warning" tmp.out > /dev/null 2>&1 || ret=1
 n=`expr $n + 1`
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-echo "I:checking no warning about delete date < inactive date with dnssec-settime when delete date is unset ($n)"
+echo_i "checking no warning about delete date < inactive date with dnssec-settime when delete date is unset ($n)"
 ret=0
 $SETTIME -D none `cat oldstyle.key` > tmp.out 2>&1 || ret=1
 $SETTIME -p all `cat oldstyle.key` > tmp.out 2>&1 || ret=1
 grep "warning" tmp.out > /dev/null 2>&1 && ret=1
 n=`expr $n + 1`
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-echo "I:checking warning about delete date < inactive date with dnssec-keygen ($n)"
+echo_i "checking warning about delete date < inactive date with dnssec-keygen ($n)"
 ret=0
 # keygen should print a warning about delete < inactive
 $KEYGEN -q -a rsasha1 -r $RANDFILE -I now+15s -D now $czone > tmp.out 2>&1 || ret=1
 grep "warning" tmp.out > /dev/null 2>&1 || ret=1
 n=`expr $n + 1`
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-echo "I:checking correct behavior setting activation without publication date ($n)"
+echo_i "checking correct behavior setting activation without publication date ($n)"
 ret=0
 key=`$KEYGEN -q -a rsasha1 -r $RANDFILE -A +1w $czone`
 pub=`$SETTIME -upP $key | awk '{print $2}'`
@@ -194,10 +192,10 @@ key=`$KEYGEN -q -a rsasha1 -r $RANDFILE -A +1w -P never $czone`
 pub=`$SETTIME -upP $key | awk '{print $2}'`
 [ $pub = "UNSET" ] || ret=1
 n=`expr $n + 1`
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-echo "I:checking calculation of dates for a successor key ($n)"
+echo_i "checking calculation of dates for a successor key ($n)"
 ret=0
 oldkey=`$KEYGEN -a RSASHA1 -q -r $RANDFILE $czone`
 newkey=`$KEYGEN -a RSASHA1 -q -r $RANDFILE $czone`
@@ -205,8 +203,8 @@ $SETTIME -A -2d -I +2d $oldkey > settime1.test$n 2>&1 || ret=1
 $SETTIME -i 1d -S $oldkey $newkey > settime2.test$n 2>&1 || ret=1
 $SETTIME -pA $newkey | grep "1970" > /dev/null && ret=1
 n=`expr $n + 1`
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-echo "I:exit status: $status"
+echo_i "exit status: $status"
 [ $status -eq 0 ] || exit 1
