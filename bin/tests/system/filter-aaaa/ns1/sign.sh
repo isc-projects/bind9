d21aec5d8ea37eb91cf30adf4363c022ad82e4db
Evan Hunt <each@isc.org>
2018-02-20 23:17:01 -0800

    [parallel-acl] parallelize filter-aaaa

--- bin/tests/system/filter-aaaa/ns1/sign.sh
+++ bin/tests/system/filter-aaaa/ns1/sign.sh
@@ -6,8 +6,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# $Id: sign.sh,v 1.2 2010/06/22 03:58:37 marka Exp $
-
 SYSTEMTESTTOP=../..
 . $SYSTEMTESTTOP/conf.sh
 
@@ -24,4 +22,4 @@ keyname2=`$KEYGEN -f KSK -r $RANDFILE -a DSA -b 768 -n zone $zone 2> /dev/null`
 cat $infile $keyname1.key $keyname2.key >$zonefile
 
 $SIGNER -r $RANDFILE -o $zone -f $outfile $zonefile > /dev/null 2> signer.err || cat signer.err
-echo "I: signed $zone"
+echo_i "signed $zone"
