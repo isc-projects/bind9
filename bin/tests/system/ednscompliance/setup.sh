d8e8d42fed611c723f666857a1433824acbcab16
Evan Hunt <each@isc.org>
2018-02-20 22:31:56 -0800

    [parallel-acl] parallelize ednscompliance

--- bin/tests/system/ednscompliance/setup.sh
+++ bin/tests/system/ednscompliance/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
-# Copyright (C) 2014-2016  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C)  2018  Internet Systems Consortium, Inc. ("ISC")
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-rm -f dig.out*
-rm -f ns*/named.lock
-rm -f ns*/named.conf
-rm -f ns*/named.run
-rm -f ns*/named.memstats
+SYSTEMTESTTOP=..
+. $SYSTEMTESTTOP/conf.sh
+
+$SHELL clean.sh
+copy_setports ns1/named.conf.in ns1/named.conf
