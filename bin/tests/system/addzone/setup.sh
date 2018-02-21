9817820e0e93014bf517a8d79e54123e723dc592
Evan Hunt <each@isc.org>
2018-02-20 16:49:33 -0800

    [parallel-acl] parallelize addzone

--- bin/tests/system/addzone/setup.sh
+++ bin/tests/system/addzone/setup.sh
@@ -14,9 +14,12 @@ $SHELL clean.sh
 cp -f ns1/redirect.db.1 ns1/redirect.db
 cp -f ns2/redirect.db.1 ns2/redirect.db
 cp -f ns3/redirect.db.1 ns3/redirect.db
-cp -f ns2/named1.conf ns2/named.conf
+
+copy_setports ns1/named.conf.in ns1/named.conf
+copy_setports ns2/named1.conf.in ns2/named.conf
+copy_setports ns3/named1.conf.in ns3/named.conf
+
 cp -f ns2/default.nzf.in ns2/3bf305731dd26307.nzf
-cp -f ns3/named1.conf ns3/named.conf
 rm -f ns3/*.nzf ns3/*.nzf~
 rm -f ns3/*.nzd ns3/*.nzd-lock
 rm -f ns3/inlineslave.db
