6550c0832b1c4dc1c3dc087859e173fd4e23d6f0
Evan Hunt <each@isc.org>
2018-02-20 22:54:36 -0800

    [parallel-acl] parallelize emptyzones and fetchlimit

--- bin/tests/system/fetchlimit/clean.sh
+++ bin/tests/system/fetchlimit/clean.sh
@@ -6,8 +6,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-rm -f */named.memstats */ans.run */named.recursing */named.run
+rm -f */named.conf */named.memstats */ans.run */named.recursing */named.run
 rm -f dig.out*
 rm -f ans4/norespond
-rm -f ns3/named.conf ns3/named.stats ns3/named_dump.db
+rm -f ns3/named.stats ns3/named_dump.db
 rm -f burst.input.*
