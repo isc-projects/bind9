c89ae6a73dd03e4bdade34a4738e4d9a5df68eff
Evan Hunt <each@isc.org>
2018-02-21 16:25:27 -0800

    [parallel-acl] parallelize resolver

--- bin/tests/system/resolver/ans3/ans.pl
+++ bin/tests/system/resolver/ans3/ans.pl
@@ -6,8 +6,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# $Id: ans.pl,v 1.12 2009/11/04 02:15:30 marka Exp $
-
 #
 # Ad hoc name server
 #
@@ -17,8 +15,11 @@ use IO::Socket;
 use Net::DNS;
 use Net::DNS::Packet;
 
+my $localport = int($ENV{'PORT'});
+if (!$localport) { $localport = 5300; }
+
 my $sock = IO::Socket::INET->new(LocalAddr => "10.53.0.3",
-   LocalPort => 5300, Proto => "udp") or die "$!";
+   LocalPort => $localport, Proto => "udp") or die "$!";
 
 my $pidf = new IO::File "ans.pid", "w" or die "cannot open pid file: $!";
 print $pidf "$$\n" or die "cannot write pid file: $!";
--- bin/tests/system/resolver/setup.sh
+++ bin/tests/system/resolver/setup.sh
@@ -14,5 +14,11 @@ test -r $RANDFILE || $GENRANDOM 800 $RANDFILE
 cp ns4/tld1.db ns4/tld.db
 cp ns6/to-be-removed.tld.db.in ns6/to-be-removed.tld.db
 cp ns7/server.db.in ns7/server.db
-cp ns7/named1.conf ns7/named.conf
+
+copy_setports ns1/named.conf.in ns1/named.conf
+copy_setports ns4/named.conf.in ns4/named.conf
+copy_setports ns5/named.conf.in ns5/named.conf
+copy_setports ns6/named.conf.in ns6/named.conf
+copy_setports ns7/named1.conf.in ns7/named.conf
+
 (cd ns6 && $SHELL keygen.sh)
