b67408aeb51e6e93816b02292ea4fc8056bd34dd
Evan Hunt <each@isc.org>
2018-02-21 22:35:49 -0800

    [parallel-acl] parallelize sortlist

--- bin/tests/system/sortlist/setup.sh
+++ bin/tests/system/sortlist/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
-# Copyright (C) 2000, 2001, 2004, 2007, 2009, 2012, 2014-2016  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C)  2018  Internet Systems Consortium, Inc. ("ISC")
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-rm -f *.dig *.good *.out
-rm -f */named.memstats
-rm -f */named.run
-rm -f */named.conf
-rm -f ns*/named.lock
+SYSTEMTESTTOP=..
+. $SYSTEMTESTTOP/conf.sh
+
+$SHELL clean.sh
+copy_setports ns1/named.conf.in ns1/named.conf
