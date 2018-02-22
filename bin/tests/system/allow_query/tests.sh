d1f20cffb4a9bb044dfa4b0ea131081d6d671ba2
Evan Hunt <each@isc.org>
2018-02-22 15:10:37 -0800

    final cleanup
    - add CHANGES note
    - update copyrights and license headers
    - add -j 12 to the make commands in .gitlab-ci.yml so that
      parallelization will be used in the gitlab CI process

--- bin/tests/system/allow_query/tests.sh
+++ bin/tests/system/allow_query/tests.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 #
-# Copyright (C) 2010, 2012, 2013, 2016  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2010, 2012, 2013, 2016, 2018  Internet Systems Consortium, Inc. ("ISC")
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-# $Id: tests.sh,v 1.3 2010/12/02 23:22:41 marka Exp $
 
 # Test of allow-query statement.
 # allow-query takes an address match list and can be included in either the
