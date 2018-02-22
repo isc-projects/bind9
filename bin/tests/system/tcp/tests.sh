0a8d080c7930ddeab7f8662fd17ef7f5796ce745
Evan Hunt <each@isc.org>
2018-02-21 23:11:40 -0800

    [parallel-acl] parallelize tcp

--- bin/tests/system/tcp/tests.sh
+++ bin/tests/system/tcp/tests.sh
@@ -9,11 +9,12 @@
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
 
-RNDCCMD="$RNDC -p 9953 -c ../common/rndc.conf"
+DIGOPTS="-p ${PORT}"
+RNDCCMD="$RNDC -p ${CONTROLPORT} -c ../common/rndc.conf"
 
 status=0
 
-echo "I:initialize counters"
+echo_i "initialize counters"
 $RNDCCMD -s 10.53.0.1 stats > /dev/null 2>&1
 $RNDCCMD -s 10.53.0.2 stats > /dev/null 2>&1
 ntcp10=`grep "TCP requests received" ns1/named.stats | tail -1 | awk '{print $1}'`
@@ -21,9 +22,9 @@ ntcp20=`grep "TCP requests received" ns2/named.stats | tail -1 | awk '{print $1}
 #echo ntcp10 ':' "$ntcp10"
 #echo ntcp20 ':' "$ntcp20"
 
-echo "I:check TCP transport"
+echo_i "check TCP transport"
 ret=0
-$DIG -p 5300 @10.53.0.3 txt.example. > dig.out.3
+$DIG $DIGOPTS @10.53.0.3 txt.example. > dig.out.3
 $RNDCCMD -s 10.53.0.1 stats > /dev/null 2>&1
 $RNDCCMD -s 10.53.0.2 stats > /dev/null 2>&1
 ntcp11=`grep "TCP requests received" ns1/named.stats | tail -1 | awk '{print $1}'`
@@ -32,12 +33,12 @@ ntcp21=`grep "TCP requests received" ns2/named.stats | tail -1 | awk '{print $1}
 #echo ntcp21 ':' "$ntcp21"
 if [ "$ntcp10" -ge "$ntcp11" ]; then ret=1; fi
 if [ "$ntcp20" -ne "$ntcp21" ]; then ret=1; fi
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-echo "I:check TCP forwarder"
+echo_i "check TCP forwarder"
 ret=0
-$DIG -p 5300 @10.53.0.4 txt.example. > dig.out.4
+$DIG $DIGOPTS @10.53.0.4 txt.example. > dig.out.4
 $RNDCCMD -s 10.53.0.1 stats > /dev/null 2>&1
 $RNDCCMD -s 10.53.0.2 stats > /dev/null 2>&1
 ntcp12=`grep "TCP requests received" ns1/named.stats | tail -1 | awk '{print $1}'`
@@ -46,8 +47,8 @@ ntcp22=`grep "TCP requests received" ns2/named.stats | tail -1 | awk '{print $1}
 #echo ntcp22 ':' "$ntcp22"
 if [ "$ntcp11" -ne "$ntcp12" ]; then ret=1; fi
 if [ "$ntcp21" -ge "$ntcp22" ];then ret=1; fi
-if [ $ret != 0 ]; then echo "I:failed"; fi
+if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-echo "I:exit status: $status"
+echo_i "exit status: $status"
 [ $status -eq 0 ] || exit 1
