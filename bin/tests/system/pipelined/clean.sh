c95c64ffb39384ccfd1c1752053226a1685a181e
Evan Hunt <each@isc.org>
2018-02-21 22:14:59 -0800

    [parallel-acl] parallelize pipelined

--- bin/tests/system/pipelined/clean.sh
+++ bin/tests/system/pipelined/clean.sh
@@ -6,6 +6,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+rm -f */named.conf
 rm -f */named.memstats
 rm -f */named.run
 rm -f raw* output*
