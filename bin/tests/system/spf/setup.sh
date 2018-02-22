ba10217d62ad11bc25f74c5d5a0c5b2fed09f8c9
Evan Hunt <each@isc.org>
2018-02-21 22:37:37 -0800

    [parallel-acl] parallelize spf

--- bin/tests/system/spf/setup.sh
+++ bin/tests/system/spf/setup.sh
@@ -1,10 +1,13 @@
-# Copyright (C) 2013, 2014, 2016  Internet Systems Consortium, Inc. ("ISC")
+#!/bin/sh
+#
+# Copyright (C)  2018  Internet Systems Consortium, Inc. ("ISC")
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-rm -f ns1/named.run
-rm -f ns1/named.memstats
-rm -f ns*/named.lock
-rm -f ns*/named.conf
+SYSTEMTESTTOP=..
+. $SYSTEMTESTTOP/conf.sh
+
+$SHELL clean.sh
+copy_setports ns1/named.conf.in ns1/named.conf
