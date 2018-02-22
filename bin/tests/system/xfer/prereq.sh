b3bacf19307642ba5a824f05856196ec4bf01f14
Evan Hunt <each@isc.org>
2018-02-21 20:17:49 -0800

    [parallel-acl] parallelize xfer

--- bin/tests/system/xfer/prereq.sh
+++ bin/tests/system/xfer/prereq.sh
@@ -12,10 +12,10 @@ then
     then
         :
     else
-        echo "I:Net::DNS versions 0.69 to 0.74 have bugs that cause this test to fail: please update." >&2
+        echo_i "Net::DNS versions 0.69 to 0.74 have bugs that cause this test to fail: please update." >&2
         exit 1
     fi
 else
-    echo "I:This test requires the Net::DNS library." >&2
+    echo_i "This test requires the Net::DNS library." >&2
     exit 1
 fi
