557a5c3f781e824302b9d4613cb509af223bc6f4
Ondřej Surý <ondrej@sury.org>
2018-02-23 09:53:12 +0100

    Update license headers to not include years in copyright in all applicable files
    (cherry picked from commit 843d389661c98d4cbf1af51aec5c31feae6da184)

--- bin/tests/system/tkey/clean.sh
+++ bin/tests/system/tkey/clean.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 #
-# Copyright (C) 2001, 2004, 2007, 2011-2016  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) Internet Systems Consortium, Inc. ("ISC")
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# See the COPYRIGHT file distributed with this work for additional
+# information regarding copyright ownership.
 
 rm -f dig.out.* rndc.out.* ns1/named.conf
 rm -f K* ns1/K*
