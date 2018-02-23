8dec04ab68c2c7b2f2b951ea31cc2dceaddb4607
Ondřej Surý <ondrej@sury.org>
2018-02-23 09:53:12 +0100

    Update license headers to not include years in copyright in all applicable files
    (cherry picked from commit 843d389661c98d4cbf1af51aec5c31feae6da184)
    (cherry picked from commit 557a5c3f781e824302b9d4613cb509af223bc6f4)

--- lib/dns/include/dns/ssu.h
+++ lib/dns/include/dns/ssu.h
@@ -1,9 +1,12 @@
 /*
- * Copyright (C) 2000, 2001, 2003-2008, 2010, 2011, 2016-2018  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) Internet Systems Consortium, Inc. ("ISC")
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * See the COPYRIGHT file distributed with this work for additional
+ * information regarding copyright ownership.
  */
 
 #ifndef DNS_SSU_H
--- lib/dns/rdata/generic/hip_55.h
+++ lib/dns/rdata/generic/hip_55.h
@@ -1,9 +1,12 @@
 /*
- * Copyright (C) 2009, 2016  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) Internet Systems Consortium, Inc. ("ISC")
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * See the COPYRIGHT file distributed with this work for additional
+ * information regarding copyright ownership.
  */
 
 /* $Id: hip_55.h,v 1.2 2009/02/26 06:09:19 marka Exp $ */
