8f4977122e8ab05a06b4db34ea4d6eedc8a86410
Ondřej Surý <ondrej@sury.org>
2018-02-23 09:53:12 +0100

    Update license headers to not include years in copyright in all applicable files

--- bin/tests/system/ecdsa/clean.sh
+++ bin/tests/system/ecdsa/clean.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 #
-# Copyright (C) 2012, 2014, 2016  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) Internet Systems Consortium, Inc. ("ISC")
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# See the COPYRIGHT file distributed with this work for additional
+# information regarding copyright ownership.
 
 rm -f */K* */dsset-* */*.signed */trusted.conf
 rm -f ns1/root.db
