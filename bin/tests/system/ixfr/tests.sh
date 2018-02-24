17a9d158fe9d97a216c4deadfcd024b2a0d6778f
Evan Hunt <each@isc.org>
2018-02-24 01:23:00 -0800

    add a re-notification step if the transfer hasn't happened

--- bin/tests/system/ixfr/tests.sh
+++ bin/tests/system/ixfr/tests.sh
@@ -172,6 +172,9 @@ for i in 0 1 2 3 4 5 6 7 8 9
 do
 	$DIG $DIGOPTS +tcp @10.53.0.4 SOA test > dig.out
 	grep -i "hostmaster\.test\..2" dig.out > /dev/null && break
+
+        # re-notify if necessary
+        $RNDCCMD 10.53.0.3 notify sub.test | set 's/^/ns3 /' | cat_i
 	sleep 1
 done
 
@@ -212,6 +215,9 @@ for i in 0 1 2 3 4 5 6 7 8 9
 do
 	$DIG $DIGOPTS +tcp @10.53.0.4 SOA sub.test > dig.out
 	grep -i "hostmaster\.test\..3" dig.out > /dev/null && break
+
+        # re-notify if necessary
+        $RNDCCMD 10.53.0.3 notify sub.test | set 's/^/ns3 /' | cat_i
 	sleep 1
 done
 
@@ -247,6 +253,9 @@ for i in 0 1 2 3 4 5 6 7 8 9
 do
 	$DIG $DIGOPTS +tcp @10.53.0.4 SOA test > dig.out
 	grep -i "hostmaster\.test\..4" dig.out > /dev/null && break
+
+        # re-notify if necessary
+        $RNDCCMD 10.53.0.3 notify sub.test | set 's/^/ns3 /' | cat_i
 	sleep 1
 done
 
