7014d0994b1d585c23287f17448d25c503450da0
Evan Hunt <each@isc.org>
2018-02-27 10:20:28 -0800

    migrate t_mem to lib/isc/tests/mem_test
    (cherry picked from commit 979f054702878ed74b9a7b22c636fc2fc5cf1659)
    (cherry picked from commit af48544b753696dc50e76e22bd77b921906c20c3)
    (cherry picked from commit 814cd21e22cd13e2fb9ed5d9c7fd2f5e7a3471fb)

--- lib/isc/tests/mem_test.c
+++ lib/isc/tests/mem_test.c
@@ -41,6 +41,108 @@ default_memfree(void *arg, void *ptr) {
 	free(ptr);
 }
 
+ATF_TC(isc_mem);
+ATF_TC_HEAD(isc_mem, tc) {
+	atf_tc_set_md_var(tc, "descr", "general memory system tests");
+}
+
+#define	MP1_FREEMAX	10
+#define	MP1_FILLCNT	10
+#define	MP1_MAXALLOC	30
+
+#define	MP2_FREEMAX	25
+#define	MP2_FILLCNT	25
+
+ATF_TC_BODY(isc_mem, tc) {
+	isc_result_t result;
+	void *items1[50];
+	void *items2[50];
+	void *tmp;
+	isc_mem_t *localmctx = NULL;
+	isc_mempool_t *mp1 = NULL, *mp2 = NULL;
+	unsigned int i, j;
+	int rval;
+
+	result = isc_test_begin(NULL, ISC_TRUE);
+	ATF_REQUIRE_EQ(result, ISC_R_SUCCESS);
+
+	result = isc_mem_create(0, 0, &localmctx);
+	ATF_REQUIRE_EQ(result, ISC_R_SUCCESS);
+
+	result = isc_mempool_create(localmctx, 24, &mp1);
+	ATF_REQUIRE_EQ(result, ISC_R_SUCCESS);
+
+	result = isc_mempool_create(localmctx, 31, &mp2);
+	ATF_REQUIRE_EQ(result, ISC_R_SUCCESS);
+
+	isc_mempool_setfreemax(mp1, MP1_FREEMAX);
+	isc_mempool_setfillcount(mp1, MP1_FILLCNT);
+	isc_mempool_setmaxalloc(mp1, MP1_MAXALLOC);
+
+	/*
+	 * Allocate MP1_MAXALLOC items from the pool.  This is our max.
+	 */
+	for (i = 0; i < MP1_MAXALLOC; i++) {
+		items1[i] = isc_mempool_get(mp1);
+		ATF_CHECK(items1[i] != NULL);
+	}
+
+	/*
+	 * Try to allocate one more.  This should fail.
+	 */
+	tmp = isc_mempool_get(mp1);
+	ATF_CHECK_EQ(tmp, NULL);
+
+	/*
+	 * Free the first 11 items.  Verify that there are 10 free items on
+	 * the free list (which is our max).
+	 */
+	for (i = 0; i < 11; i++) {
+		isc_mempool_put(mp1, items1[i]);
+		items1[i] = NULL;
+	}
+
+	rval = isc_mempool_getfreecount(mp1);
+	ATF_CHECK_EQ(rval, 10);
+
+	rval = isc_mempool_getallocated(mp1);
+	ATF_CHECK_EQ(rval, 19);
+
+	/*
+	 * Now, beat up on mp2 for a while.  Allocate 50 items, then free
+	 * them, then allocate 50 more, etc.
+	 */
+
+	isc_mempool_setfreemax(mp2, 25);
+	isc_mempool_setfillcount(mp2, 25);
+
+	for (j = 0; j < 500000; j++) {
+		for (i = 0; i < 50; i++) {
+			items2[i] = isc_mempool_get(mp2);
+			ATF_CHECK(items2[i] != NULL);
+		}
+		for (i = 0; i < 50; i++) {
+			isc_mempool_put(mp2, items2[i]);
+			items2[i] = NULL;
+		}
+	}
+
+	/*
+	 * Free all the other items and blow away this pool.
+	 */
+	for (i = 11; i < MP1_MAXALLOC; i++) {
+		isc_mempool_put(mp1, items1[i]);
+		items1[i] = NULL;
+	}
+
+	isc_mempool_destroy(&mp1);
+	isc_mempool_destroy(&mp2);
+
+	isc_mem_destroy(&localmctx);
+
+	isc_test_end();
+}
+
 ATF_TC(isc_mem_total);
 ATF_TC_HEAD(isc_mem_total, tc) {
 	atf_tc_set_md_var(tc, "descr", "test TotalUse calculation");
@@ -153,6 +255,7 @@ ATF_TC_BODY(isc_mem_inuse, tc) {
  * Main
  */
 ATF_TP_ADD_TCS(tp) {
+	ATF_TP_ADD_TC(tp, isc_mem);
 	ATF_TP_ADD_TC(tp, isc_mem_total);
 	ATF_TP_ADD_TC(tp, isc_mem_inuse);
 
