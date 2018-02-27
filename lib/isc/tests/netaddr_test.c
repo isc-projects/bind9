00bc2c2355d26666cef7b4f3ff57376b0153aa82
Evan Hunt <each@isc.org>
2018-02-26 23:18:59 -0800

    migrate t_net to lib/isc/tests
    (cherry picked from commit 05b7251d51b4b31ad822d701beb0313ad1bcb4d9)
    (cherry picked from commit af0c9b2ceecbca3f71f6895bbfac6cdf11c46094)
    (cherry picked from commit faedafd2b48eeb04bb504645e6c6909bf06cdf79)
    (cherry picked from commit 67b63a3e1095e48ccf19ac1520a927f8cf5f8c57)

--- lib/isc/tests/netaddr_test.c
+++ lib/isc/tests/netaddr_test.c
@@ -27,11 +27,11 @@
 #include <isc/sockaddr.h>
 #include <isc/util.h>
 
-ATF_TC(isc_netaddr_isnetzero);
-ATF_TC_HEAD(isc_netaddr_isnetzero, tc) {
-	atf_tc_set_md_var(tc, "descr", "test isc_netaddr_isnetzero");
+ATF_TC(netaddr_isnetzero);
+ATF_TC_HEAD(netaddr_isnetzero, tc) {
+	atf_tc_set_md_var(tc, "descr", "test netaddr_isnetzero");
 }
-ATF_TC_BODY(isc_netaddr_isnetzero, tc) {
+ATF_TC_BODY(netaddr_isnetzero, tc) {
 	unsigned int i;
 	struct in_addr ina;
 	struct {
@@ -107,12 +107,55 @@ ATF_TC_BODY(netaddr_masktoprefixlen, tc) {
 	ATF_CHECK_EQ(plen, 24);
 }
 
+ATF_TC(netaddr_multicast);
+ATF_TC_HEAD(netaddr_multicast, tc) {
+	atf_tc_set_md_var(tc, "descr",
+			  "check multicast addresses are detected properly");
+}
+ATF_TC_BODY(netaddr_multicast, tc) {
+	unsigned int i;
+	struct {
+		int family;
+		const char *addr;
+		isc_boolean_t is_multicast;
+	} tests[] = {
+		{ AF_INET, "1.2.3.4", ISC_FALSE },
+		{ AF_INET, "4.3.2.1", ISC_FALSE },
+		{ AF_INET, "224.1.1.1", ISC_TRUE },
+		{ AF_INET, "1.1.1.244", ISC_FALSE },
+		{ AF_INET6, "::1", ISC_FALSE },
+		{ AF_INET6, "ff02::1", ISC_TRUE }
+	};
+
+	for (i = 0; i < sizeof(tests) / sizeof(tests[0]); i++) {
+		isc_netaddr_t na;
+		struct in_addr in;
+		struct in6_addr in6;
+		int r;
+
+		if (tests[i].family == AF_INET) {
+			r = inet_pton(AF_INET, tests[i].addr,
+				      (unsigned char *)&in);
+			ATF_REQUIRE_EQ(r, 1);
+			isc_netaddr_fromin(&na, &in);
+		} else {
+			r = inet_pton(AF_INET6, tests[i].addr,
+				      (unsigned char *)&in6);
+			ATF_REQUIRE_EQ(r, 1);
+			isc_netaddr_fromin6(&na, &in6);
+		}
+
+		ATF_CHECK_EQ(isc_netaddr_ismulticast(&na),
+			     tests[i].is_multicast);
+	}
+}
 /*
  * Main
  */
 ATF_TP_ADD_TCS(tp) {
-	ATF_TP_ADD_TC(tp, isc_netaddr_isnetzero);
+	ATF_TP_ADD_TC(tp, netaddr_isnetzero);
 	ATF_TP_ADD_TC(tp, netaddr_masktoprefixlen);
+	ATF_TP_ADD_TC(tp, netaddr_multicast);
 
 	return (atf_no_error());
 }
