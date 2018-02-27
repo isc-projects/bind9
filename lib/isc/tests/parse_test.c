c945e28639af0f288349e5df02050b585961dc77
Evan Hunt <each@isc.org>
2018-02-27 14:29:49 -0800

    migrate t_timers to lib/isc/tests/timer_test

--- lib/isc/tests/parse_test.c
+++ lib/isc/tests/parse_test.c
@@ -9,8 +9,6 @@
  * information regarding copyright ownership.
  */
 
-/* $Id$ */
-
 /*! \file */
 
 #include <config.h>
@@ -38,7 +36,7 @@ ATF_TC_BODY(parse_overflow, tc) {
 	isc_uint32_t output;
 	UNUSED(tc);
 
-	result = isc_test_begin(NULL, ISC_TRUE);
+	result = isc_test_begin(NULL, ISC_TRUE, 0);
 	ATF_REQUIRE_EQ(result, ISC_R_SUCCESS);
 
 	result = isc_parse_uint32(&output, "1234567890", 10);
