700188d63e415a32ece36e8bccad27bd0b990d94
Evan Hunt <each@isc.org>
2018-02-27 10:57:08 -0800

    move tests from bin/tests/dnssec-signzone to bin/tests/system/dnssec/signer

--- bin/tests/system/dnssec/clean.sh
+++ bin/tests/system/dnssec/clean.sh
@@ -92,4 +92,7 @@ rm -f signer/example.db.after signer/example.db.before
 rm -f signer/example.db.changed
 rm -f signer/nsec3param.out
 rm -f signer/signer.out.*
+rm -f signer/general/signed.zone
+rm -f signer/general/signer.out.*
+rm -f signer/general/dsset*
 rm -f signing.out*
