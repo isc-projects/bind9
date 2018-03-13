819a154fc1b5fdaf1941dc09951ae36659d4f044
Michał Kępień <michal@isc.org>
2018-03-13 14:06:07 +0100

    Add lib/dns/zone_p.h
    Add a new private header file, lib/dns/zone_p.h, which will hold type
    definitions and function prototypes not meant to be exported by libdns,
    but required by zone-related unit tests.

---
+++ lib/dns/zone_p.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * See the COPYRIGHT file distributed with this work for additional
+ * information regarding copyright ownership.
+ */
+
+#ifndef DNS_ZONE_P_H
+#define DNS_ZONE_P_H
+
+/*! \file */
+
+/*%
+ *     Types and functions below not be used outside this module and its
+ *     associated unit tests.
+ */
+
+ISC_LANG_BEGINDECLS
+
+ISC_LANG_ENDDECLS
+
+#endif /* DNS_ZONE_P_H */
