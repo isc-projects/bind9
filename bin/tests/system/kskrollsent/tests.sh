5dc3087e06ee7c40a9f3896d5b597f07780a0342
Mark Andrews <marka@isc.org>
2018-03-14 13:22:56 +1100

    add system test for root-key-sentinel

---
+++ bin/tests/system/kskrollsent/tests.sh
@@ -0,0 +1,127 @@
+#!/bin/sh
+#
+# Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# See the COPYRIGHT file distributed with this work for additional
+# information regarding copyright ownership.
+
+SYSTEMTESTTOP=..
+. $SYSTEMTESTTOP/conf.sh
+
+status=0
+n=0
+
+rm -f dig.out.*
+
+DIGOPTS="+tcp +noadd +nosea +nostat +nocmd +dnssec -p ${PORT}"
+
+newtest() {
+	n=`expr $n + 1`
+	echo_i "$@ ($n)"
+	ret=0
+}
+
+newtest "get test ids"
+$DIG $DIGOPTS . dnskey +short +rrcomm @10.53.0.1 > dig.out.ns1.test$n || ret=1
+oldid=`sed -n 's/.*key id = //p' < dig.out.ns1.test$n`
+oldid=`expr "0000${oldid}" : '.*\(.....\)$'`
+newid=`expr \( ${oldid} + 1000 \) % 65536`
+newid=`expr "0000${newid}" : '.*\(.....\)$'`
+badid=`expr \( ${oldid} + 7777 \) % 65536`
+badid=`expr "0000${badid}" : '.*\(.....\)$'`
+echo_i "test ids: oldid=${oldid} (configured) newid=${newid} (not configured) badid=${badid}"
+if [ $ret != 0 ]; then echo_i "failed"; fi
+status=`expr $status + $ret`
+
+newtest "check authoritative server (expect NOERROR)"
+$DIG $DIGOPTS @10.53.0.2 example SOA > dig.out.ns2.test$n
+grep "status: NOERROR" dig.out.ns2.test$n > /dev/null || ret=1
+if [ $ret != 0 ]; then echo_i "failed"; fi
+status=`expr $status + $ret`
+
+newtest "check test zone resolves with 'kskroll-sentinel-enable yes;' (expect NOERROR)"
+$DIG $DIGOPTS @10.53.0.3 example SOA > dig.out.ns3.test$n
+grep "status: NOERROR" dig.out.ns3.test$n > /dev/null || ret=1
+if [ $ret != 0 ]; then echo_i "failed"; fi
+status=`expr $status + $ret`
+
+newtest "check kskroll-sentinel-is-ta with old ta and 'kskroll-sentinel-enable yes;' (expect SERVFAIL)"
+$DIG $DIGOPTS @10.53.0.3 kskroll-sentinel-is-ta-${oldid}.example A > dig.out.ns3.test$n || ret=1
+grep "status: SERVFAIL" dig.out.ns3.test$n > /dev/null || ret=1
+if [ $ret != 0 ]; then echo_i "failed"; fi
+status=`expr $status + $ret`
+
+newtest "check kskroll-sentinel-not-ta with old ta and 'kskroll-sentinel-enable yes;' (expect NOERROR)"
+$DIG $DIGOPTS @10.53.0.3 kskroll-sentinel-not-ta-${oldid}.example A > dig.out.ns3.test$n || ret=1
+grep "status: NOERROR" dig.out.ns3.test$n > /dev/null || ret=1
+if [ $ret != 0 ]; then echo_i "failed"; fi
+status=`expr $status + $ret`
+
+newtest "check kskroll-sentinel-is-ta with new ta and 'kskroll-sentinel-enable yes;' (expect NOERROR)"
+$DIG $DIGOPTS @10.53.0.3 kskroll-sentinel-is-ta-${newid}.example A > dig.out.ns3.test$n || ret=1
+grep "status: NOERROR" dig.out.ns3.test$n > /dev/null || ret=1
+if [ $ret != 0 ]; then echo_i "failed"; fi
+status=`expr $status + $ret`
+
+newtest "check kskroll-sentinel-not-ta with new ta and 'kskroll-sentinel-enable yes;' (expect SERVFAIL)"
+$DIG $DIGOPTS @10.53.0.3 kskroll-sentinel-not-ta-${newid}.example A > dig.out.ns3.test$n || ret=1
+grep "status: SERVFAIL" dig.out.ns3.test$n > /dev/null || ret=1
+if [ $ret != 0 ]; then echo_i "failed"; fi
+
+newtest "check kskroll-sentinel-is-ta with bad ta and 'kskroll-sentinel-enable yes;' (expect NXDOMAIN)"
+$DIG $DIGOPTS @10.53.0.3 kskroll-sentinel-is-ta-${badid}.example A > dig.out.ns3.test$n || ret=1
+grep "status: NXDOMAIN" dig.out.ns3.test$n > /dev/null || ret=1
+if [ $ret != 0 ]; then echo_i "failed"; fi
+status=`expr $status + $ret`
+
+newtest "check kskroll-sentinel-not-ta with bad ta and 'kskroll-sentinel-enable yes;' (expect NXDOMAIN)"
+$DIG $DIGOPTS @10.53.0.3 kskroll-sentinel-not-ta-${bad}.example A > dig.out.ns3.test$n || ret=1
+grep "status: NXDOMAIN" dig.out.ns3.test$n > /dev/null || ret=1
+if [ $ret != 0 ]; then echo_i "failed"; fi
+
+newtest "check test zone resolves with 'kskroll-sentinel-enable no;' (expect NOERROR)"
+$DIG $DIGOPTS @10.53.0.4 example SOA > dig.out.ns4.test$n
+grep "status: NOERROR" dig.out.ns4.test$n > /dev/null || ret=1
+if [ $ret != 0 ]; then echo_i "failed"; fi
+status=`expr $status + $ret`
+
+newtest "check kskroll-sentinel-is-ta with old ta and 'kskroll-sentinel-enable no;' (expect NOERROR)"
+$DIG $DIGOPTS @10.53.0.4 kskroll-sentinel-is-ta-${oldid}.example A > dig.out.ns4.test$n || ret=1
+grep "status: NOERROR" dig.out.ns4.test$n > /dev/null || ret=1
+if [ $ret != 0 ]; then echo_i "failed"; fi
+status=`expr $status + $ret`
+
+newtest "check kskroll-sentinel-not-ta with old ta and 'kskroll-sentinel-enable no;' (expect NOERROR)"
+$DIG $DIGOPTS @10.53.0.4 kskroll-sentinel-not-ta-${oldid}.example A > dig.out.ns4.test$n || ret=1
+grep "status: NOERROR" dig.out.ns4.test$n > /dev/null || ret=1
+if [ $ret != 0 ]; then echo_i "failed"; fi
+status=`expr $status + $ret`
+
+newtest "check kskroll-sentinel-is-ta with new ta and 'kskroll-sentinel-enable no;' (expect NOERROR)"
+$DIG $DIGOPTS @10.53.0.4 kskroll-sentinel-is-ta-${newid}.example A > dig.out.ns4.test$n || ret=1
+grep "status: NOERROR" dig.out.ns4.test$n > /dev/null || ret=1
+if [ $ret != 0 ]; then echo_i "failed"; fi
+status=`expr $status + $ret`
+
+newtest "check kskroll-sentinel-not-ta with new ta and 'kskroll-sentinel-enable no;' (expect NOERROR)"
+$DIG $DIGOPTS @10.53.0.4 kskroll-sentinel-not-ta-${newid}.example A > dig.out.ns4.test$n || ret=1
+grep "status: NOERROR" dig.out.ns4.test$n > /dev/null || ret=1
+if [ $ret != 0 ]; then echo_i "failed"; fi
+
+newtest "check kskroll-sentinel-is-ta with bad ta and 'kskroll-sentinel-enable no;' (expect NXDOMAIN)"
+$DIG $DIGOPTS @10.53.0.4 kskroll-sentinel-is-ta-${badid}.example A > dig.out.ns4.test$n || ret=1
+grep "status: NXDOMAIN" dig.out.ns4.test$n > /dev/null || ret=1
+if [ $ret != 0 ]; then echo_i "failed"; fi
+status=`expr $status + $ret`
+
+newtest "check kskroll-sentinel-not-ta with bad ta and 'kskroll-sentinel-enable no;' (expect NXDOMAIN)"
+$DIG $DIGOPTS @10.53.0.4 kskroll-sentinel-not-ta-${bad}.example A > dig.out.ns4.test$n || ret=1
+grep "status: NXDOMAIN" dig.out.ns4.test$n > /dev/null || ret=1
+if [ $ret != 0 ]; then echo_i "failed"; fi
+
+echo_i "exit status: $status"
+[ $status -eq 0 ] || exit 1
