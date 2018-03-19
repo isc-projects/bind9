e0b6c1184066ba193dbc423bb2c90f97ab0f38d7
Stephen Morris <stephen@isc.org>
2018-03-19 16:12:47 +0000

    Initial set of IDNA tests.

---
+++ bin/tests/system/idna/prereq.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# See the COPYRIGHT file distributed with this work for additional
+# information regarding copyright ownership.
+
+SYSTEMTESTTOP=..
+. $SYSTEMTESTTOP/conf.sh
+
+$FEATURETEST --with-idn
+if [ $? -ne 1 ]; then
+    echo_i "This test requires that BIND be built with IDN support"
+    exit 
+fi
+exit 0
