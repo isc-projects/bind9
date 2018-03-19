be1bf514d0395e05e186c51dabc59b110c87f98d
Mukund Sivaraman <muks@isc.org>
2018-03-19 18:09:05 +0530

    Add a answer-cookie named config option
    (cherry picked from commit 29305073575459a66f0a93b9becc4863fd1c0c6b)

--- lib/ns/client.c
+++ lib/ns/client.c
@@ -1909,7 +1909,9 @@ process_cookie(ns_client_t *client, isc_buffer_t *buf, size_t optlen) {
 	/*
 	 * If we have already seen a cookie option skip this cookie option.
 	 */
-	if ((client->attributes & NS_CLIENTATTR_WANTCOOKIE) != 0) {
+	if ((!client->sctx->answercookie) ||
+	    (client->attributes & NS_CLIENTATTR_WANTCOOKIE) != 0)
+	{
 		isc_buffer_forward(buf, (unsigned int)optlen);
 		return;
 	}
