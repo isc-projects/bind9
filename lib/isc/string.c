11ddc258bf1e05389d8cbe7caa63b38415051a71
Ondřej Surý <ondrej@sury.org>
2018-03-21 21:08:29 +0000

    Replace usage of strsep with POSIX strtok_r()

--- lib/isc/string.c
+++ lib/isc/string.c
@@ -50,27 +50,6 @@
 #include <isc/string.h>
 #include <isc/util.h>
 
-char *
-isc_string_separate(char **stringp, const char *delim) {
-	char *string = *stringp;
-	char *s;
-	const char *d;
-	char sc, dc;
-
-	if (string == NULL)
-		return (NULL);
-
-	for (s = string; (sc = *s) != '\0'; s++)
-		for (d = delim; (dc = *d) != '\0'; d++)
-			if (sc == dc) {
-				*s++ = '\0';
-				*stringp = s;
-				return (string);
-			}
-	*stringp = NULL;
-	return (string);
-}
-
 size_t
 isc_string_strlcpy(char *dst, const char *src, size_t size)
 {
