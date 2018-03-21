2e72f8f2f8e26142d502c4e9eb1ef8c3878a4cdc
Mark Andrews <marka@isc.org>
2018-03-21 12:03:27 +1100

    check authoritative server was testing the wrong server

--- bin/tests/system/kskrollsent/tests.sh
+++ bin/tests/system/kskrollsent/tests.sh
@@ -38,7 +38,7 @@ if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
 newtest "check authoritative server"
-$DIG $DIGOPTS @10.53.0.3 example SOA > dig.out.ns3.test$n
+$DIG $DIGOPTS @10.53.0.2 example SOA > dig.out.ns3.test$n
 grep "status: NOERROR" dig.out.ns3.test$n > /dev/null || ret=1
 if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
