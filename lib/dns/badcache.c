2923e5005e227129a06eb7fc541ede987fc03f40
Ondřej Surý <ondrej@sury.org>
2018-03-28 14:56:40 +0200

    Replace ISC_PRINT_QUADFORMAT with inttypes.h format constants
    (cherry picked from commit 64fe6bbaf2019f444475dfbf744eb6ea4e619c19)

--- lib/dns/badcache.c
+++ lib/dns/badcache.c
@@ -13,6 +13,8 @@
 
 #include <config.h>
 
+#include <inttypes.h>
+
 #include <isc/buffer.h>
 #include <isc/log.h>
 #include <isc/hash.h>
@@ -429,7 +431,7 @@ dns_badcache_print(dns_badcache_t *bc, const char *cachename, FILE *fp) {
 			t = isc_time_microdiff(&bad->expire, &now);
 			t /= 1000;
 			fprintf(fp, "; %s/%s [ttl "
-				"%" ISC_PLATFORM_QUADFORMAT "u]\n",
+				"%" PRIu64 "]\n",
 				namebuf, typebuf, t);
 		}
 	}
