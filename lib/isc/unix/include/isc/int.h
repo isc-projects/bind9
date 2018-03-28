01adae73c1a011e385e0ad0ad61b203419b3a5c8
Ondřej Surý <ondrej@sury.org>
2018-03-28 14:19:37 +0200

    Redefine isc integer types to just use C99 equivalents

--- lib/isc/unix/include/isc/int.h
+++ lib/isc/unix/include/isc/int.h
@@ -14,36 +14,33 @@
 #ifndef ISC_INT_H
 #define ISC_INT_H 1
 
+#include <stdint.h>
+
 /*! \file */
 
-typedef signed char			isc_int8_t;
-typedef unsigned char			isc_uint8_t;
-typedef short				isc_int16_t;
-typedef unsigned short			isc_uint16_t;
-typedef int				isc_int32_t;
-typedef unsigned int			isc_uint32_t;
-typedef long long			isc_int64_t;
-typedef unsigned long long		isc_uint64_t;
+#define	isc_int8_t	int8_t
+#define isc_uint8_t	uint8_t
+#define isc_int16_t	int16_t
+#define isc_uint16_t	uint16_t
+#define isc_int32_t	int32_t
+#define isc_uint32_t	uint32_t
+#define isc_int64_t	int64_t
+#define isc_uint64_t	uint64_t
 
-#define ISC_INT8_MIN	-128
-#define ISC_INT8_MAX	127
-#define ISC_UINT8_MAX	255
+#define ISC_INT8_MIN	INT8_MIN
+#define ISC_INT8_MAX	INT8_MAX
+#define ISC_UINT8_MAX	UINT8_MAX
 
-#define ISC_INT16_MIN	-32768
-#define ISC_INT16_MAX	32767
-#define ISC_UINT16_MAX	65535
+#define ISC_INT16_MIN	INT16_MIN
+#define ISC_INT16_MAX	INT16_MAX
+#define ISC_UINT16_MAX	UINT16_MAX
 
-/*%
- * Note that "int" is 32 bits on all currently supported Unix-like operating
- * systems, but "long" can be either 32 bits or 64 bits, thus the 32 bit
- * constants are not qualified with "L".
- */
-#define ISC_INT32_MIN	-2147483648
-#define ISC_INT32_MAX	2147483647
-#define ISC_UINT32_MAX	4294967295U
+#define ISC_INT32_MIN	INT32_MIN
+#define ISC_INT32_MAX	INT32_MAX
+#define ISC_UINT32_MAX	UINT32_MAX
 
-#define ISC_INT64_MIN	-9223372036854775808LL
-#define ISC_INT64_MAX	9223372036854775807LL
-#define ISC_UINT64_MAX	18446744073709551615ULL
+#define ISC_INT64_MIN	INT64_MIN
+#define ISC_INT64_MAX	INT64_MAX
+#define ISC_UINT64_MAX	UINT64_MAX
 
 #endif /* ISC_INT_H */
