57973cedad66fdcdbcb2cbde55e354cec98b0808
Ondřej Surý <ondrej@sury.org>
2018-04-18 12:13:28 -0700

    Always use random data from the crypto provider

--- bin/confgen/keygen.c
+++ bin/confgen/keygen.c
@@ -154,11 +154,9 @@ generate_key(isc_mem_t *mctx, const char *randomfile, dns_secalg_t alg,
 
 	DO("create entropy context", isc_entropy_create(mctx, &ectx));
 
-#ifdef ISC_PLATFORM_CRYPTORANDOM
 	if (randomfile == NULL) {
 		isc_entropy_usehook(ectx, ISC_TRUE);
 	}
-#endif
 	if (randomfile != NULL && strcmp(randomfile, "keyboard") == 0) {
 		randomfile = NULL;
 		open_keyboard = ISC_ENTROPY_KEYBOARDYES;
