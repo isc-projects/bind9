96dbf2830ca9e90e497a068e9ad53684b2b9a6f9
Ondřej Surý <ondrej@sury.org>
2018-04-24 20:58:58 +0200

    Update tests to not use '>&-' that closes file descriptor, but instead use correct redirection to '>/dev/null'

--- bin/tests/system/smartsign/tests.sh
+++ bin/tests/system/smartsign/tests.sh
@@ -38,7 +38,7 @@ czsk5=`$KEYGEN -q -a rsasha1 -r $RANDFILE -P now+12h -A now+12h -I now+24h $czon
 
 # explicit successor to czk5
 # (suppressing warning about lack of removal date)
-czsk6=`$KEYGEN -q -r $RANDFILE -S $czsk5 -i 6h 2>&-` 
+czsk6=`$KEYGEN -q -r $RANDFILE -S $czsk5 -i 6h 2>/dev/null` 
 
 # active ksk
 cksk1=`$KEYGEN -q -a rsasha1 -r $RANDFILE -fk -L 30 $czone`
