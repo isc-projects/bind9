e5ddd09e2ccf9b250f56a4d3e6aa6421f16dfad1
Mukund Sivaraman <muks@isc.org>
2018-05-02 12:18:44 +0530

    Don't validate non-pending glue when adding to the additional section
    (cherry picked from commit 31bd3147d118106faa62fe90ec39f36c64239e5d)

--- bin/named/query.c
+++ bin/named/query.c
@@ -1583,8 +1583,7 @@ query_addadditional(void *arg, dns_name_t *name, dns_rdatatype_t qtype) {
 			have_a = ISC_TRUE;
 #endif
 			if (additionaltype == dns_rdatasetadditional_fromcache &&
-			    (DNS_TRUST_PENDING(rdataset->trust) ||
-			     DNS_TRUST_GLUE(rdataset->trust)) &&
+			    DNS_TRUST_PENDING(rdataset->trust) &&
 			    !validate(client, db, fname, rdataset, sigrdataset))
 			{
 				dns_rdataset_disassociate(rdataset);
@@ -1654,8 +1653,7 @@ query_addadditional(void *arg, dns_name_t *name, dns_rdatatype_t qtype) {
 				goto addname;
 #endif
 			if (additionaltype == dns_rdatasetadditional_fromcache &&
-			    (DNS_TRUST_PENDING(rdataset->trust) ||
-			     DNS_TRUST_GLUE(rdataset->trust)) &&
+			    DNS_TRUST_PENDING(rdataset->trust) &&
 			    !validate(client, db, fname, rdataset, sigrdataset))
 			{
 				dns_rdataset_disassociate(rdataset);
