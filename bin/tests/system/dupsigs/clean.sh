aca6b27a0044d1d7b231552316ff923dc96300bd
Mark Andrews <marka@isc.org>
2018-05-03 16:50:32 +1000

    add duplicate signature test

---
+++ bin/tests/system/dupsigs/clean.sh
@@ -0,0 +1,8 @@
+rm -f ns1/named.conf
+rm -f ns1/named.lock
+rm -f ns1/named.memstats
+rm -f ns1/named.run
+rm -f ns1/signing.test.db
+rm -f ns1/signing.test.db.jbk
+rm -f ns1/signing.test.db.signed
+rm -f ns1/signing.test.db.signed.jnl
