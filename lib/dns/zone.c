2642d64637b85a8e88770e78a38f5b49ba03888e
Michał Kępień <michal@isc.org>
2018-05-17 08:26:27 +0200

    Add dns_zone_logv()
    Add a new libdns function, dns_zone_logv(), which takes a single va_list
    argument rather than a variable number of arguments and can be used as a
    base for implementing more specific zone logging functions.

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -13655,6 +13655,37 @@ dns_zone_nameonly(dns_zone_t *zone, char *buf, size_t length) {
 	zone_name_tostr(zone, buf, length);
 }
 
+void
+dns_zone_logv(dns_zone_t *zone, isc_logcategory_t *category, int level,
+	      const char *prefix, const char *fmt, va_list ap)
+{
+	char message[4096];
+	const char *zstr;
+
+	if (!isc_log_wouldlog(dns_lctx, level)) {
+		return;
+	}
+
+	vsnprintf(message, sizeof(message), fmt, ap);
+
+	switch (zone->type) {
+	case dns_zone_key:
+		zstr = "managed-keys-zone";
+		break;
+	case dns_zone_redirect:
+		zstr = "redirect-zone";
+		break;
+	default:
+		zstr = "zone ";
+	}
+
+	isc_log_write(dns_lctx, category, DNS_LOGMODULE_ZONE, level,
+		      "%s%s%s%s: %s",
+		      (prefix != NULL ? prefix : ""),
+		      (prefix != NULL ? ": " : ""),
+		      zstr, zone->strnamerd, message);
+}
+
 static void
 notify_log(dns_zone_t *zone, int level, const char *fmt, ...) {
 	va_list ap;
