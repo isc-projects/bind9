4898db33c81f22b9b5864e96a03d0fdd7175cf75
Ondřej Surý <ondrej@sury.org>
2018-05-29 14:10:32 +0200

    Make the xoshiro128plusplus thread-safe

--- lib/isc/xoshiro128starstar.c
+++ lib/isc/xoshiro128starstar.c
@@ -17,14 +17,51 @@ See <http://creativecommons.org/publicdomain/zero/1.0/>. */
 
    The state must be seeded so that it is not everywhere zero. */
 
+#ifndef HAVE_TLS
+
+#if defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
+static volatile HANDLE _mtx = NULL;
+
+/*
+ * Initialize the mutex on the first lock attempt. On collision, each thread
+ * will attempt to allocate a mutex and compare-and-swap it into place as the
+ * global mutex. On failure to swap in the global mutex, the mutex is closed.
+ */
+#define _LOCK() { \
+	if (!_mtx) { \
+		HANDLE p = CreateMutex(NULL, FALSE, NULL); \
+		if (InterlockedCompareExchangePointer((void **)&_mtx, (void *)p, NULL)) \
+			CloseHandle(p); \
+	} \
+	WaitForSingleObject(_mtx, INFINITE); \
+}
+
+#define _ARC4_UNLOCK() ReleaseMutex(arc4random_mtx)
+
+#else /* defined(_WIN32) || defined(_WIN64) */
+
+#include <pthread.h>
+static pthread_mutex_t _mtx = PTHREAD_MUTEX_INITIALIZER;
+#define _ARC4_LOCK()   pthread_mutex_lock(&_mtx)
+#define _ARC4_UNLOCK() pthread_mutex_unlock(&_mtx)
+#endif /* defined(_WIN32) || defined(_WIN64) */
+
+#else /* HAVE_TLS */
+#define _LOCK()
+#define _UNLOCK()
+#endif /* HAVE_TLS */
+
 static inline uint32_t rotl(const uint32_t x, int k) {
 	return (x << k) | (x >> (32 - k));
 }
 
-static uint32_t seed[4];
+static thread_local uint32_t seed[4];
 
 static inline uint32_t
 next(void) {
+	_LOCK();
+
 	const uint32_t result_starstar = rotl(seed[0] * 5, 7) * 9;
 
 	const uint32_t t = seed[1] << 9;
@@ -37,6 +74,8 @@ next(void) {
 	seed[2] ^= t;
 
 	seed[3] = rotl(seed[3], 11);
+
+	_UNLOCK();
 
 	return result_starstar;
 }
