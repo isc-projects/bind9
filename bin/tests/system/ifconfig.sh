1e56d78c6eb04a1f487ba2d090c74b6ff6c3921d
Evan Hunt <each@isc.org>
2018-05-30 20:07:53 -0700

    use "ip" on linux, falling back to "ifconfig" when it isn't available
    (cherry picked from commit d7c54007985609133a8fd22b451a0c43b00defcf)
    (cherry picked from commit 3e1a0c2b62b80d30a01eb4054fc1e1594f91355e)

--- bin/tests/system/ifconfig.sh
+++ bin/tests/system/ifconfig.sh
@@ -53,6 +53,20 @@ case `uname -a` in
 	*) sys=`sh $config_guess` ;;
 esac
 
+use_ip=
+case "$sys" in
+        *-*-linux*)
+                if type ip > /dev/null; then
+                        use_ip=yes
+                elif type ifconfig > /dev/null; then
+                        :
+                else
+                        echo "$0: can't find ip or ifconfig" >&2
+                        exit 1
+                fi
+                ;;
+esac
+
 case "$1" in
 
     start|up)
@@ -85,10 +99,18 @@ case "$1" in
 					inet6 fd92:7065:b8e:${ipv6}ff::$ns up
 				;;
 			    *-*-linux*)
-				ifconfig lo:$int 10.53.$i.$ns up \
-					netmask 255.255.255.0
-				[ "$ipv6" ] && ifconfig lo inet6 add \
-					fd92:7065:b8e:${ipv6}ff::$ns/64
+                                if [ $use_ip ]; then
+                                        ip address add 10.53.$i.$ns/24 \
+                                            dev lo:$int
+                                        [ "$ipv6" ] && ip address add \
+                                            fd92:7065:b8e:${ipv6}ff::$ns/64 \
+                                            dev lo
+                                else
+                                        ifconfig lo:$int 10.53.$i.$ns up \
+                                                netmask 255.255.255.0
+                                        [ "$ipv6" ] && ifconfig lo inet6 add \
+                                                fd92:7065:b8e:${ipv6}ff::$ns/64
+                                fi
 				;;
 			    *-unknown-freebsd*)
 				ifconfig lo0 10.53.$i.$ns alias \
@@ -180,9 +202,17 @@ case "$1" in
 				ifconfig lo0:$int inet6 unplumb
 				;;
 			    *-*-linux*)
-				ifconfig lo:$int 10.53.$i.$ns down
-				[ "$ipv6" ] && ifconfig lo inet6 \
-					del fd92:7065:b8e:${ipv6}ff::$ns/64
+                                if [ $use_ip ]; then
+                                        ip address del 10.53.$i.$ns/24 \
+                                            dev lo:$int
+                                        [ "$ipv6" ] && ip address del \
+                                            fd92:7065:b8e:${ipv6}ff::$ns/64 \
+                                            dev lo
+                                else
+                                        ifconfig lo:$int 10.53.$i.$ns down
+                                        [ "$ipv6" ] && ifconfig lo inet6 \
+                                            del fd92:7065:b8e:${ipv6}ff::$ns/64
+                                fi
 				;;
 			    *-unknown-freebsd*)
 				ifconfig lo0 10.53.$i.$ns delete
