f3893b6d0b7152fe80c12c7a530206a0ce6a394a
Ondřej Surý <ondrej@sury.org>
2018-06-01 21:13:11 +0200

    Remove obsoleted isc_md5 API

--- lib/dns/dst_internal.h
+++ lib/dns/dst_internal.h
@@ -33,7 +33,6 @@
 #include <isc/magic.h>
 #include <isc/region.h>
 #include <isc/types.h>
-#include <isc/md5.h>
 #include <isc/refcount.h>
 #include <isc/sha1.h>
 #include <isc/sha2.h>
@@ -158,9 +157,6 @@ struct dst_context {
 	union {
 		void *generic;
 		dst_gssapi_signverifyctx_t *gssctx;
-#ifndef PK11_MD5_DISABLE
-		isc_md5_t *md5ctx;
-#endif
 		isc_sha1_t *sha1ctx;
 		isc_sha256_t *sha256ctx;
 		isc_sha512_t *sha512ctx;
