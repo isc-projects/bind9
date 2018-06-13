9a4145168ce80abd218d4ea46a380555d16d6de9
Michał Kępień <michal@isc.org>
2018-06-13 12:19:54 +0200

    Treat records below a DNAME as out-of-zone data
    DNAME records indicate bottom of zone and thus no records below a DNAME
    should be DNSSEC-signed or included in NSEC(3) chains.  Add a helper
    function, has_dname(), for detecting DNAME records at a given node.
    Prevent signing DNAME-obscured records.  Check that DNAME-obscured
    records are not signed.
    
    (cherry picked from commit 6d8a514ecbd72d1f8b2b12fbbbca5c5f87085abd)

--- bin/dnssec/dnssec-signzone.c
+++ bin/dnssec/dnssec-signzone.c
@@ -1501,8 +1501,14 @@ assignwork(isc_task_t *task, isc_task_t *worker) {
 					if (!OPTOUT(nsec3flags) ||
 					    secure(name, node))
 						found = ISC_TRUE;
-				} else
+				} else if (has_dname(gdb, gversion, node)) {
+					dns_fixedname_init(&fzonecut);
+					zonecut = dns_fixedname_name(&fzonecut);
+					dns_name_copy(name, zonecut, NULL);
 					found = ISC_TRUE;
+				} else {
+					found = ISC_TRUE;
+				}
 			}
 		}
 
@@ -1810,6 +1816,9 @@ nsecify(void) {
 			remove_sigs(node, ISC_TRUE, 0);
 			if (generateds)
 				add_ds(name, node, nsttl);
+		} else if (has_dname(gdb, gversion, node)) {
+			zonecut = dns_fixedname_name(&fzonecut);
+			dns_name_copy(name, zonecut, NULL);
 		}
 
 		result = dns_dbiterator_next(dbiter);
@@ -2253,6 +2262,11 @@ nsec3ify(unsigned int hashalg, dns_iterations_t iterations,
 			(void)active_node(node);
 		}
 
+		if (has_dname(gdb, gversion, node)) {
+			zonecut = dns_fixedname_name(&fzonecut);
+			dns_name_copy(name, zonecut, NULL);
+		}
+
 		result = dns_dbiterator_next(dbiter);
 		nextnode = NULL;
 		while (result == ISC_R_SUCCESS) {
@@ -2287,6 +2301,9 @@ nsec3ify(unsigned int hashalg, dns_iterations_t iterations,
 					result = dns_dbiterator_next(dbiter);
 					continue;
 				}
+			} else if (has_dname(gdb, gversion, nextnode)) {
+				zonecut = dns_fixedname_name(&fzonecut);
+				dns_name_copy(nextname, zonecut, NULL);
 			}
 			dns_db_detachnode(gdb, &nextnode);
 			break;
@@ -2385,6 +2402,12 @@ nsec3ify(unsigned int hashalg, dns_iterations_t iterations,
 			dns_db_detachnode(gdb, &node);
 			continue;
 		}
+
+		if (has_dname(gdb, gversion, node)) {
+			zonecut = dns_fixedname_name(&fzonecut);
+			dns_name_copy(name, zonecut, NULL);
+		}
+
 		result = dns_dbiterator_next(dbiter);
 		nextnode = NULL;
 		while (result == ISC_R_SUCCESS) {
@@ -2415,6 +2438,9 @@ nsec3ify(unsigned int hashalg, dns_iterations_t iterations,
 					result = dns_dbiterator_next(dbiter);
 					continue;
 				}
+			} else if (has_dname(gdb, gversion, nextnode)) {
+				zonecut = dns_fixedname_name(&fzonecut);
+				dns_name_copy(nextname, zonecut, NULL);
 			}
 			dns_db_detachnode(gdb, &nextnode);
 			break;
