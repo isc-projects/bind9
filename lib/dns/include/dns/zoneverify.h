d5b2491baaeebd8f387c75f5ef505d64f3de5c40
Michał Kępień <michal@isc.org>
2018-06-13 21:32:26 +0200

    Move verifyzone() and its dependencies into lib/dns/zoneverify.c
    This commit only moves code around, with the following exceptions:
    
      - the check_dns_dbiterator_current() macro and functions
        type_format(), is_delegation(), and has_dname() were removed from
        bin/dnssec/dnssectool.{c,h} and duplicated in two locations:
        bin/dnssec/dnssec-signzone.c and lib/dns/zoneverify.c; these
        functions are used both by the code in bin/dnssec/dnssec-signzone.c
        and verifyzone(), but are not a good fit for being exported by a
        code module responsible for zone verification,
    
      - fatal() and check_result() were duplicated in lib/dns/zoneverify.c
        as static functions which do not use the "program" variable any more
        (as it is only set by the tools in bin/dnssec/); this is a temporary
        step which only aims to prevent compilation from breaking - this
        duplicate, static fatal() function will be removed once
        lib/dns/zoneverify.c is refactored not to use it,
    
      - the list of header files included from lib/dns/zoneverify.c was
        expanded to encompass all header files that are actually used by the
        code in that file,
    
      - a description of the purpose of the commented out "fields" inside
        struct nsec3_chain_fixed was added.

---
+++ lib/dns/include/dns/zoneverify.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * See the COPYRIGHT file distributed with this work for additional
+ * information regarding copyright ownership.
+ */
+
+#pragma once
+
+/*! \file dns/zoneverify.h */
+
+#include <dns/types.h>
+
+#include <isc/types.h>
+
+ISC_LANG_BEGINDECLS
+
+/*%
+ * Verify that certain things are sane:
+ *
+ *   The apex has a DNSKEY record with at least one KSK, and at least
+ *   one ZSK if the -x flag was not used.
+ *
+ *   The DNSKEY record was signed with at least one of the KSKs in this
+ *   set.
+ *
+ *   The rest of the zone was signed with at least one of the ZSKs
+ *   present in the DNSKEY RRSET.
+ */
+void
+verifyzone(dns_db_t *db, dns_dbversion_t *ver, dns_name_t *origin,
+	   isc_mem_t *mctx, isc_boolean_t ignore_kskflag,
+	   isc_boolean_t keyset_kskonly);
+
+ISC_LANG_ENDDECLS
