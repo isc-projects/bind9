fa0761ac9fc2dc415dd64607dc64e4ee2872a19a
Ondřej Surý <ondrej@sury.org>
2018-06-18 11:43:45 +0200

    Disable DH test with PKCS#11

--- lib/dns/tests/dh_test.c
+++ lib/dns/tests/dh_test.c
@@ -30,6 +30,7 @@
 
 #include "dnstest.h"
 
+#if USE_OPENSSL
 ATF_TC(isc_dh_computesecret);
 ATF_TC_HEAD(isc_dh_computesecret, tc) {
 	atf_tc_set_md_var(tc, "descr", "OpenSSL DH_compute_key() failure");
@@ -67,11 +68,24 @@ ATF_TC_BODY(isc_dh_computesecret, tc) {
 	dst_key_free(&key);
 	dns_test_end();
 }
-
+#else /* USE_OPENSSL */
+ATF_TC(untested);
+ATF_TC_HEAD(untested, tc) {
+	atf_tc_set_md_var(tc, "descr", "skipping dh test");
+}
+ATF_TC_BODY(untested, tc) {
+	UNUSED(tc);
+	atf_tc_skip("dh test is broken with PKCS#11");
+}
+#endif /* USE_OPENSSL */
 /*
  * Main
  */
 ATF_TP_ADD_TCS(tp) {
+#if USE_OPENSSL
 	ATF_TP_ADD_TC(tp, isc_dh_computesecret);
+#else
+	ATF_TP_ADD_TC(tp, untested);
+#endif
 	return (atf_no_error());
 }
