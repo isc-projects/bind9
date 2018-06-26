c212a7f6a51ea5db3bac8fb7d223d5858956facb
Michał Kępień <michal@isc.org>
2018-06-26 15:22:12 +0200

    Enable dns_zoneverify_dnssec() to check whether the zone was signed by a trust anchor
    Extend check_dnskey_sigs() so that, if requested, it checks whether the
    DNSKEY RRset at zone apex is signed by at least one trust anchor.  The
    trust anchor table is passed as an argument to dns_zoneverify_dnssec()
    and passed around in the verification context structure.  Neither
    dnssec-signzone nor dnssec-verify are yet modified to make use of that
    feature, though.

--- bin/dnssec/dnssec-verify.c
+++ bin/dnssec/dnssec-verify.c
@@ -323,8 +323,8 @@ main(int argc, char *argv[]) {
 	result = dns_db_newversion(gdb, &gversion);
 	check_result(result, "dns_db_newversion()");
 
-	result = dns_zoneverify_dnssec(NULL, gdb, gversion, gorigin, mctx,
-				       ignore_kskflag, keyset_kskonly);
+	result = dns_zoneverify_dnssec(NULL, gdb, gversion, gorigin, NULL,
+				       mctx, ignore_kskflag, keyset_kskonly);
 
 	dns_db_closeversion(gdb, &gversion, ISC_FALSE);
 	dns_db_detach(&gdb);
