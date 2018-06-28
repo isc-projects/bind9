7eacd5ac46cac0d11339a877739881babfa602ab
Mark Andrews <marka@isc.org>
2018-06-28 13:43:22 +1000

    add srv-full-additional flag

--- lib/dns/include/dns/view.h
+++ lib/dns/include/dns/view.h
@@ -130,6 +130,7 @@ struct dns_view {
 	bool				synthfromdnssec;
 	bool				trust_anchor_telemetry;
 	bool				root_key_sentinel;
+	bool				srv_full_additional; /* Experimental */
 	dns_transfer_format_t		transfer_format;
 	dns_acl_t *			cacheacl;
 	dns_acl_t *			cacheonacl;
