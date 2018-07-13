39d988c0fae32506fafb19bbdd5662bcc82ebe3c
Michał Kępień <michal@isc.org>
2018-07-13 12:46:40 +0200

    Do not silently skip the "dnstap" system test

---
+++ bin/tests/system/dnstap/prereq.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# See the COPYRIGHT file distributed with this work for additional
+# information regarding copyright ownership.
+
+SYSTEMTESTTOP=..
+. $SYSTEMTESTTOP/conf.sh
+
+if ! $FEATURETEST --have-dnstap; then
+	echo_i "This test requires DNSTAP support." >&2
+	exit 1
+fi
