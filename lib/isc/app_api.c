338233571638602dfade50d4175e3cf5c669b9ef
Witold Kręcicki <wpk@isc.org>
2018-08-06 12:15:50 +0200

    Get rid of isc_bind9 conditional

--- lib/isc/app_api.c
+++ lib/isc/app_api.c
@@ -51,30 +51,14 @@ isc_app_register(isc_appctxcreatefunc_t createfunc) {
 
 isc_result_t
 isc_appctx_create(isc_mem_t *mctx, isc_appctx_t **ctxp) {
-	isc_result_t result;
-
-	if (isc_bind9)
-		return (isc__appctx_create(mctx, ctxp));
-
-	LOCK(&createlock);
-
-	REQUIRE(appctx_createfunc != NULL);
-	result = (*appctx_createfunc)(mctx, ctxp);
-
-	UNLOCK(&createlock);
-
-	return (result);
+	return (isc__appctx_create(mctx, ctxp));
 }
 
 void
 isc_appctx_destroy(isc_appctx_t **ctxp) {
 	REQUIRE(ctxp != NULL && ISCAPI_APPCTX_VALID(*ctxp));
 
-	if (isc_bind9)
-		isc__appctx_destroy(ctxp);
-	else
-		(*ctxp)->methods->ctxdestroy(ctxp);
-
+	isc__appctx_destroy(ctxp);
 	ENSURE(*ctxp == NULL);
 }
 
@@ -82,20 +66,14 @@ isc_result_t
 isc_app_ctxstart(isc_appctx_t *ctx) {
 	REQUIRE(ISCAPI_APPCTX_VALID(ctx));
 
-	if (isc_bind9)
-		return (isc__app_ctxstart(ctx));
-
-	return (ctx->methods->ctxstart(ctx));
+	return (isc__app_ctxstart(ctx));
 }
 
 isc_result_t
 isc_app_ctxrun(isc_appctx_t *ctx) {
 	REQUIRE(ISCAPI_APPCTX_VALID(ctx));
 
-	if (isc_bind9)
-		return (isc__app_ctxrun(ctx));
-
-	return (ctx->methods->ctxrun(ctx));
+	return (isc__app_ctxrun(ctx));
 }
 
 isc_result_t
@@ -105,40 +83,28 @@ isc_app_ctxonrun(isc_appctx_t *ctx, isc_mem_t *mctx,
 {
 	REQUIRE(ISCAPI_APPCTX_VALID(ctx));
 
-	if (isc_bind9)
-		return (isc__app_ctxonrun(ctx, mctx, task, action, arg));
-
-	return (ctx->methods->ctxonrun(ctx, mctx, task, action, arg));
+	return (isc__app_ctxonrun(ctx, mctx, task, action, arg));
 }
 
 isc_result_t
 isc_app_ctxsuspend(isc_appctx_t *ctx) {
 	REQUIRE(ISCAPI_APPCTX_VALID(ctx));
 
-	if (isc_bind9)
-		return (isc__app_ctxsuspend(ctx));
-
-	return (ctx->methods->ctxsuspend(ctx));
+	return (isc__app_ctxsuspend(ctx));
 }
 
 isc_result_t
 isc_app_ctxshutdown(isc_appctx_t *ctx) {
 	REQUIRE(ISCAPI_APPCTX_VALID(ctx));
 
-	if (isc_bind9)
-		return (isc__app_ctxshutdown(ctx));
-
-	return (ctx->methods->ctxshutdown(ctx));
+	return (isc__app_ctxshutdown(ctx));
 }
 
 void
 isc_app_ctxfinish(isc_appctx_t *ctx) {
 	REQUIRE(ISCAPI_APPCTX_VALID(ctx));
 
-	if (isc_bind9)
-		isc__app_ctxfinish(ctx);
-
-	ctx->methods->ctxfinish(ctx);
+	isc__app_ctxfinish(ctx);
 }
 
 void
@@ -146,10 +112,7 @@ isc_appctx_settaskmgr(isc_appctx_t *ctx, isc_taskmgr_t *taskmgr) {
 	REQUIRE(ISCAPI_APPCTX_VALID(ctx));
 	REQUIRE(taskmgr != NULL);
 
-	if (isc_bind9)
-		isc__appctx_settaskmgr(ctx, taskmgr);
-
-	ctx->methods->settaskmgr(ctx, taskmgr);
+	isc__appctx_settaskmgr(ctx, taskmgr);
 }
 
 void
@@ -157,10 +120,7 @@ isc_appctx_setsocketmgr(isc_appctx_t *ctx, isc_socketmgr_t *socketmgr) {
 	REQUIRE(ISCAPI_APPCTX_VALID(ctx));
 	REQUIRE(socketmgr != NULL);
 
-	if (isc_bind9)
-		isc__appctx_setsocketmgr(ctx, socketmgr);
-
-	ctx->methods->setsocketmgr(ctx, socketmgr);
+	isc__appctx_setsocketmgr(ctx, socketmgr);
 }
 
 void
@@ -168,43 +128,30 @@ isc_appctx_settimermgr(isc_appctx_t *ctx, isc_timermgr_t *timermgr) {
 	REQUIRE(ISCAPI_APPCTX_VALID(ctx));
 	REQUIRE(timermgr != NULL);
 
-	if (isc_bind9)
-		isc__appctx_settimermgr(ctx, timermgr);
-
-	ctx->methods->settimermgr(ctx, timermgr);
+	isc__appctx_settimermgr(ctx, timermgr);
 }
 
 isc_result_t
 isc_app_start(void) {
-	if (isc_bind9)
-		return (isc__app_start());
-
-	return (ISC_R_NOTIMPLEMENTED);
+	return (isc__app_start());
 }
 
 isc_result_t
 isc_app_onrun(isc_mem_t *mctx, isc_task_t *task,
 	       isc_taskaction_t action, void *arg)
 {
-	if (isc_bind9)
-		return (isc__app_onrun(mctx, task, action, arg));
-
-	return (ISC_R_NOTIMPLEMENTED);
+	return (isc__app_onrun(mctx, task, action, arg));
 }
 
 isc_result_t
 isc_app_run() {
-	if (isc_bind9) {
-		isc_result_t result;
+	isc_result_t result;
 
-		is_running = true;
-		result = isc__app_run();
-		is_running = false;
+	is_running = ISC_TRUE;
+	result = isc__app_run();
+	is_running = ISC_FALSE;
 
-		return (result);
-	}
-
-	return (ISC_R_NOTIMPLEMENTED);
+	return (result);
 }
 
 bool
@@ -214,40 +161,25 @@ isc_app_isrunning() {
 
 isc_result_t
 isc_app_shutdown(void) {
-	if (isc_bind9)
-		return (isc__app_shutdown());
-
-	return (ISC_R_NOTIMPLEMENTED);
+	return (isc__app_shutdown());
 }
 
 isc_result_t
 isc_app_reload(void) {
-	if (isc_bind9)
-		return (isc__app_reload());
-
-	return (ISC_R_NOTIMPLEMENTED);
+	return (isc__app_reload());
 }
 
 void
 isc_app_finish(void) {
-	if (!isc_bind9)
-		return;
-
 	isc__app_finish();
 }
 
 void
 isc_app_block(void) {
-	if (!isc_bind9)
-		return;
-
 	isc__app_block();
 }
 
 void
 isc_app_unblock(void) {
-	if (!isc_bind9)
-		return;
-
 	isc__app_unblock();
 }
