730942f09f982ee6e10c9877b859bc2ed1be94c3
Evan Hunt <each@isc.org>
2018-08-12 11:19:36 -0700

    add hook statement to configuration parser
    - allow multiple "hook" statements at global or view level
    - add "optional bracketed text" type for optional parameter list
    - load hook module from specified path rather than hardcoded path
    - add a hooktable pointer (and a callback for freeing it) to the
      view structure
    - change the hooktable functions so they no longer update ns__hook_table
      by default, and modify PROCESS_HOOK so it uses the view hooktable, if
      set, rather than ns__hook_table. (ns__hook_table is retained for
      use by unit tests.)
    - update the filter-aaaa system test to load filter-aaaa.so
    - add a prereq script to check for dlopen support before running
      the filter-aaaa system test
    
    not yet done:
    - configuration parameters are not being passed to the filter-aaaa
      module; the filter-aaaa ACL and filter-aaaa-on-{v4,v6} settings are
      still stored in dns_view

--- lib/dns/view.c
+++ lib/dns/view.c
@@ -262,6 +262,9 @@ dns_view_create(isc_mem_t *mctx, dns_rdataclass_t rdclass,
 	view->dtenv = NULL;
 	view->dttypes = 0;
 
+	view->hooktable = NULL;
+	view->hooktable_free = NULL;
+
 	result = isc_mutex_init(&view->new_zone_lock);
 	if (result != ISC_R_SUCCESS)
 		goto cleanup_dynkeys;
@@ -548,6 +551,9 @@ destroy(dns_view_t *view) {
 	isc_refcount_destroy(&view->references);
 	isc_mem_free(view->mctx, view->nta_file);
 	isc_mem_free(view->mctx, view->name);
+	if (view->hooktable != NULL && view->hooktable_free != NULL) {
+		view->hooktable_free(view->mctx, &view->hooktable);
+	}
 	isc_mem_putanddetach(&view->mctx, view, sizeof(*view));
 }
 
