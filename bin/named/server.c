4e721d5c6f093bde896ba2e5cb52c0c8ff3694e6
Evan Hunt <each@isc.org>
2018-08-13 01:18:09 -0700

    enable modules to store data in qctx
    - added a 'hookdata' array to qctx to store pointers to up to
      16 blobs of data which are allocated by modules as needed.
      each module is assigned an ID number as it's loaded, and this
      is the index into the hook data array. this is to be used for
      holding persistent state between calls to a hook module for a
      specific query.
    - instead of using qctx->filter_aaaa, we now use qctx->hookdata.
      (this was the last piece of filter-aaaa specific code outside the
      module.)
    - added hook points for qctx initialization and destruction. we get
      a filter-aaaa data pointer from the mempool when initializing and
      store it in the qctx->hookdata table; return to to the mempool
      when destroying the qctx.
    - link the view to the qctx so that detaching the client doesn't cause
      hooks to fail
    - added a qctx_destroy() function which must be called after qctx_init;
      this calls the QCTX_DESTROY hook and detaches the view
    - general cleanup and comments

--- bin/named/server.c
+++ bin/named/server.c
@@ -1525,7 +1525,8 @@ configure_dyndb(const cfg_obj_t *dyndb, isc_mem_t *mctx,
 }
 
 static isc_result_t
-configure_hook(ns_hooktable_t *hooktable, const cfg_obj_t *hook,
+configure_hook(ns_hooktable_t *hooktable, const unsigned int modid,
+	       const cfg_obj_t *hook,
 	       const cfg_obj_t *config, ns_hookctx_t *hctx)
 {
 	isc_result_t result = ISC_R_SUCCESS;
@@ -1547,7 +1548,7 @@ configure_hook(ns_hooktable_t *hooktable, const cfg_obj_t *hook,
 
 	obj = cfg_tuple_get(hook, "parameters");
 	if (obj != NULL && cfg_obj_isstring(obj)) {
-		result = ns_hookmodule_load(library,
+		result = ns_hookmodule_load(library, modid,
 					    cfg_obj_asstring(obj),
 					    cfg_obj_file(obj),
 					    cfg_obj_line(obj),
@@ -1555,7 +1556,7 @@ configure_hook(ns_hooktable_t *hooktable, const cfg_obj_t *hook,
 					    named_g_aclconfctx,
 					    hctx, hooktable);
 	} else {
-		result = ns_hookmodule_load(library, NULL,
+		result = ns_hookmodule_load(library, modid, NULL,
 					    cfg_obj_file(hook),
 					    cfg_obj_line(hook),
 					    config,
@@ -3745,6 +3746,7 @@ configure_view(dns_view_t *view, dns_viewlist_t *viewlist,
 	unsigned int resolver_param;
 	dns_ntatable_t *ntatable = NULL;
 	const char *qminmode = NULL;
+	unsigned int module_counter = 0;
 
 	REQUIRE(DNS_VIEW_VALID(view));
 
@@ -5231,7 +5233,10 @@ configure_view(dns_view_t *view, dns_viewlist_t *viewlist,
 						&hctx));
 		}
 
-		CHECK(configure_hook(view->hooktable, hook, config, hctx));
+		CHECK(configure_hook(view->hooktable, module_counter,
+				     hook, config, hctx));
+
+		module_counter++;
 	}
 #endif
 
