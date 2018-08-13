441a6de5b634a26e48dfc69832638b9bb05538d9
Michał Kępień <michal@isc.org>
2018-08-13 15:42:53 +0200

    Do not treat a referral with a non-empty ANSWER section as an error
    As part of resquery_response() refactoring [1], a goto statement was
    replaced [2] with a call to a new function - originally called
    rctx_delegation(), now folded into rctx_answer_none() - extracted from
    existing code.  However, one call site of that refactored function does
    not reset the "result" variable, causing a referral with a non-empty
    ANSWER section to be inadvertently treated as an error, which prevents
    resolution of names reliant on servers sending such responses.  Fix by
    resetting the "result" variable to ISC_R_SUCCESS when a response
    containing a non-empty ANSWER section can be treated as a delegation.
    
    [1] see RT #45362
    
    [2] see commit e1380a16741a3b4a57e54d7a9ce09dd12691522f

--- lib/dns/resolver.c
+++ lib/dns/resolver.c
@@ -7881,7 +7881,9 @@ rctx_answer(respctx_t *rctx) {
 			}
 		}
 
-		if (result != DNS_R_DELEGATION) {
+		if (result == DNS_R_DELEGATION) {
+			result = ISC_R_SUCCESS;
+		} else {
 			/*
 			 * At this point, AA is not set, the response
 			 * is not a referral, and the server is not a
