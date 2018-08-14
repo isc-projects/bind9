783f9fc58de2100b4182ff00e99ab4c4ecaf3982
Ondřej Surý <ondrej@sury.org>
2018-08-14 10:23:50 +0200

    Replace arch specific atomic.h with global atomic.h header using either stdatomic, __atomic or __sync primitives

---
+++ lib/isc/win32/include/isc/stdatomic.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * See the COPYRIGHT file distributed with this work for additional
+ * information regarding copyright ownership.
+ */
+
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <windows.h>
+
+typedef intptr_t atomic_int_fast32_t;
+typedef intptr_t atomic_uint_fast32_t;
+typedef intptr_t atomic_int_fast64_t;
+typedef intptr_t atomic_uint_fast64_t;
+
+#define atomic_init(obj, desired)		\
+	(*(obj) = (desired))
+
+#ifdef _WIN64
+#define atomic_store(obj, desired)	 	\
+	(void)InterlockedExchange64(obj, desired)
+#define atomic_load(obj)			\
+	InterlockedExchangeAdd64(obj, 0)
+#define atomic_fetch_add(obj, arg) \
+	InterlockedExchangeAdd64(obj, arg)
+#define atomic_fetch_sub(obj, arg) \
+	InterlockedExchangeAdd64(obj, -(arg))
+static inline bool
+#define atomic_compare_exchange_strong(obj, expected, desired)		\
+	({								\
+		intptr_t __v;						\
+		_Bool __r;						\
+		__v = InterlockedCompareExchange64(obj, desired, old);	\
+		__r = *(expected) == __v;				\
+		*(expected) = __v;					\
+		__r;							\
+	})
+#else /* _WIN64 */
+#define atomic_store(obj, desired)	 	\
+	(void)InterlockedExchange(obj, desired)
+#define atomic_load(obj)			\
+	InterlockedExchangeAdd(obj, arg)
+#define atomic_fetch_add(obj, arg)		\
+	InterlockedExchangeAdd(obj, arg)
+#define atomic_fetch_sub(obj, arg) \
+	InterlockedExchangeAdd(obj, -(arg))
+#define atomic_compare_exchange_strong(obj, expected, desired)		\
+	({								\
+		intptr_t __v;						\
+		_Bool __r;						\
+		__v = InterlockedCompareExchange(obj, desired, old);	\
+		__r = *(expected) == __v;				\
+		*(expected) = __v;					\
+		__r;							\
+	})
+#endif /* _WIN64 */
+
+#define atomic_compare_exchange_weak(obj, expected, desired) \
+	atomic_compare_exchange_strong(obj, expected, desired)
+
+#define atomic_store_explicit(obj, desired, order)	\
+	atomic_store(obj, desired)
+#define atomic_load_explicit(obj, order)		\
+	atomic_load(obj)
+#define atomic_fetch_add_explicit(obj, arg, order)	\
+	atomic_fetch_add(obj, arg)
+#define atomic_fetch_sub_explicit(obj, arg, order)	\
+	atomic_fetch_sub(obj, arg)
+#define atomic_compare_exchange_strong_explicit(obj, expected, desired, succ, fail) \
+	atomic_compare_exchange_strong(obj, expected, desired)
+#define atomic_compare_exchange_weak_explicit(obj, expected, desired, succ, fail) \
+	atomic_compare_exchange_weak(obj, expected, desired)
