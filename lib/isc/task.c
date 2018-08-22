fdf2b9211d0ed3b3a554027aaa34834f51c7eb71
Witold Kręcicki <wpk@isc.org>
2018-08-22 14:45:39 +0200

    Set quantum to 50

--- lib/isc/task.c
+++ lib/isc/task.c
@@ -159,7 +159,7 @@ isc__taskmgr_resume(isc_taskmgr_t *manager0);
 
 
 #define DEFAULT_TASKMGR_QUANTUM		10
-#define DEFAULT_DEFAULT_QUANTUM		5
+#define DEFAULT_DEFAULT_QUANTUM		50
 #define FINISHED(m)			((m)->exiting && EMPTY((m)->tasks))
 
 /*%
