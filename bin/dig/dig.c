999163cd77bf397391dda6cfe0e23f2720f81840
Mark Andrews <marka@isc.org>
2018-11-06 11:59:04 +1100

    dig and mdig failed to properly preparse dash value pairs when value was a seperate argument and started with a dash.
    (cherry picked from commit 75fa84b67b13bd47ce509f9379747b2cd4e457c9)

--- bin/dig/dig.c
+++ bin/dig/dig.c
@@ -1849,6 +1849,20 @@ preparse_args(int argc, char **argv) {
 			}
 			option = &option[1];
 		}
+		if (strlen(option) == 0U) {
+			continue;
+		}
+		/* Look for dash value option. */
+		if (strpbrk(option, dash_opts) != &option[0] ||
+		    strlen(option) > 1U) {
+			/* Error or value in option. */
+			continue;
+		}
+		/* Dash value is next argument so we need to skip it. */
+		rc--, rv++;
+		/* Handle missing argument */
+		if (rc == 0)
+			break;
 	}
 }
 
