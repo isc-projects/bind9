b12d442625a9988b463289e730ab2fc0ab472e78
Mark Andrews <marka@isc.org>
2018-12-10 08:41:26 +1100

    silently ignore additional keytag options
    (cherry picked from commit 078f668e0d869f26c8c409eececc33c7dd2c5cf7)

--- lib/ns/client.c
+++ lib/ns/client.c
@@ -2115,6 +2115,12 @@ process_keytag(ns_client_t *client, isc_buffer_t *buf, size_t optlen) {
 		return (DNS_R_OPTERR);
 	}
 
+	/* Silently drop additional keytag options. */
+	if (client->keytag != NULL) {
+		isc_buffer_forward(buf, (unsigned int)optlen);
+		return (ISC_R_SUCCESS);
+	}
+
 	client->keytag = isc_mem_get(client->mctx, optlen);
 	if (client->keytag != NULL) {
 		client->keytag_len = (uint16_t)optlen;
