8bb42d781205c198910c522de86ea0dd82a27244
Mark Andrews <marka@isc.org>
2018-12-27 13:44:16 +1100

    update refreshkeytime
    (cherry picked from commit ca977e39767902612498776db4609456b600d108)

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -9852,6 +9852,7 @@ zone_refreshkeys(dns_zone_t *zone) {
 	isc_stdtime_t now;
 	bool commit = false;
 	bool fetching = false, fetch_err = false;
+	bool timerset = false;
 
 	ENTER;
 	REQUIRE(zone->db != NULL);
@@ -9917,6 +9918,9 @@ zone_refreshkeys(dns_zone_t *zone) {
 			/* Or do we just need to refresh the keyset? */
 			if (timer > kd.refresh)
 				timer = kd.refresh;
+
+			set_refreshkeytimer(zone, &kd, now, false);
+			timerset = true;
 		}
 
 		if (timer > now)
@@ -10021,6 +10025,8 @@ zone_refreshkeys(dns_zone_t *zone) {
 		isc_time_formattimestamp(&zone->refreshkeytime, timebuf, 80);
 		dns_zone_log(zone, ISC_LOG_DEBUG(1), "retry key refresh: %s",
 			     timebuf);
+	} else if (!timerset) {
+		isc_time_settoepoch(&zone->refreshkeytime);
 	}
 
 	if (!fetching)
