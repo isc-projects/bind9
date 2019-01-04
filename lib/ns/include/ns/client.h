7278b66cdf2e9e3ec79f0c7586091c2fc96317d0
Witold Kręcicki <wpk@isc.org>
2019-01-04 12:50:51 +0100

    tcp-clients could still be exceeded (v2)
    the TCP client quota could still be ineffective under some
    circumstances.  this change:
    
    - improves quota accounting to ensure that TCP clients are
      properly limited, while still guaranteeing that at least one client
      is always available to serve TCP connections on each interface.
    - uses more descriptive names and removes one (ntcptarget) that
      was no longer needed
    - adds comments
    
    (cherry picked from commit a43fe7cd3f051f12bb544b6fa364135b1719c587)

--- lib/ns/include/ns/client.h
+++ lib/ns/include/ns/client.h
@@ -95,7 +95,8 @@ struct ns_client {
 	int			nupdates;
 	int			nctls;
 	int			references;
-	bool		needshutdown; 	/*
+	bool			tcpactive;
+	bool			needshutdown; 	/*
 						 * Used by clienttest to get
 						 * the client to go from
 						 * inactive to free state
@@ -131,9 +132,9 @@ struct ns_client {
 	isc_stdtime_t		now;
 	isc_time_t		tnow;
 	dns_name_t		signername;   /*%< [T]SIG key name */
-	dns_name_t *		signer;	      /*%< NULL if not valid sig */
-	bool		mortal;	      /*%< Die after handling request */
-	bool		pipelined;   /*%< TCP queries not in sequence */
+	dns_name_t		*signer;      /*%< NULL if not valid sig */
+	bool			mortal;	      /*%< Die after handling request */
+	bool			pipelined;   /*%< TCP queries not in sequence */
 	isc_quota_t		*tcpquota;
 	isc_quota_t		*recursionquota;
 	ns_interface_t		*interface;
@@ -166,6 +167,7 @@ struct ns_client {
 
 	ISC_LINK(ns_client_t)	link;
 	ISC_LINK(ns_client_t)	rlink;
+	ISC_LINK(ns_client_t)	glink;
 	ISC_QLINK(ns_client_t)	ilink;
 	unsigned char		cookie[8];
 	uint32_t		expire;
