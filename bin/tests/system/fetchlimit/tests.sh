f1906e8ea315de7e8fb855c94c42ae55e2e88eb1
Evan Hunt <each@isc.org>
2019-01-23 22:00:00 -0800

    fix fetchlimit test
    use TCP for the test queries in between UDP bursts; this avoids
    congestion issues that interfered with the test on windows

--- bin/tests/system/fetchlimit/tests.sh
+++ bin/tests/system/fetchlimit/tests.sh
@@ -12,7 +12,7 @@
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
 
-DIGCMD="$DIG @10.53.0.3 -p ${PORT} +tries=1 +time=1"
+DIGCMD="$DIG @10.53.0.3 -p ${PORT} +tcp +tries=1 +time=1"
 RNDCCMD="$RNDC -p ${CONTROLPORT} -s 10.53.0.3 -c ../common/rndc.conf"
 
 burst() {
@@ -112,9 +112,6 @@ quota=$5
 if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
-if [ $ret != 0 ]; then echo_i "failed"; fi
-status=`expr $status + $ret`
-
 copy_setports ns3/named2.conf.in ns3/named.conf
 $RNDCCMD reconfig 2>&1 | sed 's/^/ns3 /' | cat_i
 
@@ -165,8 +162,8 @@ success=0
 touch ans4/norespond
 for try in 1 2 3 4 5; do
     burst b $try 400
-    $DIG @10.53.0.3 -p ${PORT}  a ${try}.example > dig.out.ns3.$try
-    stat 360 || exceeded=`expr $exceeded + 1`
+    $DIGCMD a ${try}.example > dig.out.ns3.$try
+    stat 380 || exceeded=`expr $exceeded + 1`
     grep "status: NOERROR" dig.out.ns3.$try > /dev/null 2>&1 && \
             success=`expr $success + 1`
     grep "status: SERVFAIL" dig.out.ns3.$try > /dev/null 2>&1 && \
