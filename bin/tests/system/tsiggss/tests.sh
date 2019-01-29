4754fa2ded2d828e367d802ba4e94680199c6cc3
Evan Hunt <each@isc.org>
2019-01-29 11:57:57 -0800

    "grep -q" isn't portable

--- bin/tests/system/tsiggss/tests.sh
+++ bin/tests/system/tsiggss/tests.sh
@@ -40,7 +40,7 @@ EOF
     }
 
     # Weak verification that TKEY response is signed.
-    grep -q "flags: qr; QUESTION: 1, ANSWER: 1, AUTHORITY: 0, ADDITIONAL: 1" nsupdate.out${num} || {
+    grep "flags: qr; QUESTION: 1, ANSWER: 1, AUTHORITY: 0, ADDITIONAL: 1" nsupdate.out${num} > /dev/null || {
 	echo "I:bad tkey response (not tsig signed)"
 	return 1
     }
