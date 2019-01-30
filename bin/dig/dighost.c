bde93c8472279066ace4897415a705de76a35ec5
Petr Menšík <pemensik@redhat.com>
2019-01-30 15:38:54 +0100

    Emit warning on IDN output failure
    Warning is emitted before any dig headers.
    
    (cherry picked from commit 21371abd729e1dac0565425f1d5330c181c401ee)

--- bin/dig/dighost.c
+++ bin/dig/dighost.c
@@ -4865,16 +4865,19 @@ idn_ace_to_locale(const char *from, char *to, size_t tolen) {
 	 */
 	res = idn2_to_unicode_8zlz(utf8_src, &tmp_str, 0);
 	if (res != IDN2_OK) {
-		/*
-		 * Cannot represent in current locale.
-		 * Output ACE form undecoded.
-		 */
+		static bool warned = false;
+
 		res = idn2_to_ascii_8z(utf8_src, &tmp_str, 0);
 		if (res != IDN2_OK) {
 			fatal("Cannot represent '%s' "
 			      "in the current locale nor ascii (%s), "
 			      "use +noidnout or a different locale",
 			      from, idn2_strerror(res));
+		} else if (!warned) {
+			fprintf(stderr, ";; Warning: cannot represent '%s' "
+			      "in the current locale",
+			      tmp_str);
+			warned = true;
 		}
 	}
 
