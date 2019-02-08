d3a962db1ebe1c9442eb2e8a8d4b4a3a638878c7
Evan Hunt <each@isc.org>
2019-02-07 16:42:12 -0800

    adjust style, match test to other tests
    (cherry picked from commit 9949163936e22adabf94ea05a9dfed76527cf1a5)

--- bin/tests/system/coverage/setup.sh
+++ bin/tests/system/coverage/setup.sh
@@ -134,3 +134,8 @@ ksk1=`$KEYGEN -q -K $dir -3fk example.com`
 dir=12-ksk-deletion
 ksk1=`$KEYGEN -q -K $dir -f KSK -a 8 -b 2048 -I +40d -D +40d example.com`
 ksk2=`$KEYGEN -q -K $dir -S $ksk1.key example.com`
+
+# Test 13: check names with/without dots at the end
+dir=13-dotted-dotless
+zsk1=`$KEYGEN -q -K $dir -3 one.example`
+zsk2=`$KEYGEN -q -K $dir -3 two.example`
