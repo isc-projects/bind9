63c03cdb2d5ea425dd34c77c5d3291228b4dfffc
Mark Andrews <marka@isc.org>
2019-02-13 15:57:03 +1100

    remove seen_dname

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -6946,8 +6946,7 @@ sign_a_node(dns_db_t *db, dns_name_t *name, dns_dbnode_t *node,
 	dns_rdata_t rdata = DNS_RDATA_INIT;
 	isc_buffer_t buffer;
 	unsigned char data[1024];
-	bool seen_soa, seen_ns, seen_rr, seen_dname, seen_nsec,
-		      seen_nsec3, seen_ds;
+	bool seen_soa, seen_ns, seen_rr, seen_nsec, seen_nsec3, seen_ds;
 
 	result = dns_db_allrdatasets(db, node, version, 0, &iterator);
 	if (result != ISC_R_SUCCESS) {
@@ -6958,8 +6957,7 @@ sign_a_node(dns_db_t *db, dns_name_t *name, dns_dbnode_t *node,
 
 	dns_rdataset_init(&rdataset);
 	isc_buffer_init(&buffer, data, sizeof(data));
-	seen_rr = seen_soa = seen_ns = seen_dname = seen_nsec =
-	seen_nsec3 = seen_ds = false;
+	seen_rr = seen_soa = seen_ns = seen_nsec = seen_nsec3 = seen_ds = false;
 	for (result = dns_rdatasetiter_first(iterator);
 	     result == ISC_R_SUCCESS;
 	     result = dns_rdatasetiter_next(iterator)) {
@@ -6970,8 +6968,6 @@ sign_a_node(dns_db_t *db, dns_name_t *name, dns_dbnode_t *node,
 			seen_ns = true;
 		else if (rdataset.type == dns_rdatatype_ds)
 			seen_ds = true;
-		else if (rdataset.type == dns_rdatatype_dname)
-			seen_dname = true;
 		else if (rdataset.type == dns_rdatatype_nsec)
 			seen_nsec = true;
 		else if (rdataset.type == dns_rdatatype_nsec3)
