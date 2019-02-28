7941a9554fe00697c81b52051b41912966a1e36a
Mark Andrews <marka@isc.org>
2019-02-28 17:00:15 +1100

    Set 'specials' to match 'specials' in 'lib/dns/master.c'

--- lib/dns/tests/dnstest.c
+++ lib/dns/tests/dnstest.c
@@ -481,6 +481,7 @@ dns_test_rdatafromstring(dns_rdata_t *rdata, dns_rdataclass_t rdclass,
 	dns_rdatacallbacks_t callbacks;
 	isc_buffer_t source, target;
 	isc_lex_t *lex = NULL;
+	isc_lexspecials_t specials = { 0 };
 	isc_result_t result;
 	size_t length;
 
@@ -503,6 +504,17 @@ dns_test_rdatafromstring(dns_rdata_t *rdata, dns_rdataclass_t rdclass,
 	if (result != ISC_R_SUCCESS) {
 		return (result);
 	}
+
+	/*
+	 * Set characters which will be treated as valid multi-line RDATA
+	 * delimiters while reading the source string.  These should match
+	 * specials from lib/dns/master.c.
+	 */
+	specials[0] = 1;
+	specials['('] = 1;
+	specials[')'] = 1;
+	specials['"'] = 1;
+	isc_lex_setspecials(lex, specials);
 
 	/*
 	 * Point lexer at source.
