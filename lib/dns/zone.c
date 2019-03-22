3cb8c49c73906b28921012619a3bb87805613b81
Matthijs Mekking <matthijs@isc.org>
2019-03-22 15:42:10 +0100

    With update-check-ksk also consider offline keys
    The option `update-check-ksk` will look if both KSK and ZSK are
    available before signing records.  It will make sure the keys are
    active and available.  However, for operational practices keys may
    be offline.  This commit relaxes the update-check-ksk check and will
    mark a key that is offline to be available when adding signature
    tasks.

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -8867,9 +8867,6 @@ zone_sign(dns_zone_t *zone) {
 			if (!dst_key_isprivate(zone_keys[i])) {
 				continue;
 			}
-			/*
-			 * Should be redundant.
-			 */
 			if (dst_key_inactive(zone_keys[i])) {
 				continue;
 			}
@@ -8913,11 +8910,10 @@ zone_sign(dns_zone_t *zone) {
 					{
 						continue;
 					}
-					if (!dst_key_isprivate(zone_keys[j])) {
-						continue;
-					}
-					/*
-					 * Should be redundant.
+					/* Don't consider inactive keys, however
+					 * the key may be temporary offline, so do
+					 * consider keys which private key files are
+					 * unavailable.
 					 */
 					if (dst_key_inactive(zone_keys[j])) {
 						continue;
