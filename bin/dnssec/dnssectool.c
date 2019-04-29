c38a3230823a8a16eee4e3502419ccf5a309e354
Tony Finch <dot@dotat.at>
2019-04-29 13:56:05 +0100

    Teach dnssec-settime to read times that it writes
    The dnssec-settime -p and -up options print times in asctime() and
    UNIX time_t formats, respectively. The asctime() format can also be
    found inside K*.key public key files. Key files also contain times in
    the YYYYMMDDHHMMSS format that can be used in timing parameter
    options.
    
    The dnssec-settime -p and -up time formats are now acceptable in
    timing parameter options to dnssec-settime and dnssec-keygen, so it is
    no longer necessary to parse key files to retrieve times that are
    acceptable in timing parameter options.

--- bin/dnssec/dnssectool.c
+++ bin/dnssec/dnssectool.c
@@ -33,6 +33,7 @@
 #include <isc/result.h>
 #include <isc/string.h>
 #include <isc/time.h>
+#include <isc/tm.h>
 #include <isc/util.h>
 
 #include <dns/db.h>
@@ -285,6 +286,7 @@ strtotime(const char *str, int64_t now, int64_t base, bool *setp) {
 	const char *orig = str;
 	char *endp;
 	size_t n;
+	struct tm tm;
 
 	if (isnone(str)) {
 		if (setp != NULL) {
@@ -306,6 +308,8 @@ strtotime(const char *str, int64_t now, int64_t base, bool *setp) {
 	 *   now([+-]offset)
 	 *   YYYYMMDD([+-]offset)
 	 *   YYYYMMDDhhmmss([+-]offset)
+	 *   Day Mon DD HH:MM:SS YYYY([+-]offset)
+	 *   1234567890([+-]offset)
 	 *   [+-]offset
 	 */
 	n = strspn(str, "0123456789");
@@ -325,9 +329,21 @@ strtotime(const char *str, int64_t now, int64_t base, bool *setp) {
 		}
 		base = val;
 		str += n;
+	} else if (n == 10u &&
+		   (str[n] == '\0' || str[n] == '-' || str[n] == '+')) {
+		base = strtoll(str, &endp, 0);
+		str += 10;
 	} else if (strncmp(str, "now", 3) == 0) {
 		base = now;
 		str += 3;
+	} else if (str[0] >= 'A' && str[0] <= 'Z') {
+		/* parse ctime() format as written by `dnssec-settime -p` */
+		endp = isc_tm_strptime(str, "%a %b %d %H:%M:%S %Y", &tm);
+		if (endp != str + 24) {
+			fatal("time value %s is invalid", orig);
+		}
+		base = mktime(&tm);
+		str += 24;
 	}
 
 	if (str[0] == '\0') {
