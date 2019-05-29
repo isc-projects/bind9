96a4c329f1f58210f1baf6c7b5667d12054e157d
Michał Kępień <michal@isc.org>
2019-05-29 11:05:01 +0200

    Fix the name of the file to inspect
    One of the checks in the "legacy" system test inspects dig.out.1.test$n
    instead of dig.out.2.test$n.  Fix the file name used in that check.
    
    (cherry picked from commit 3e7fa15ca378c1bf55938cd13d9839bcd98c21f7)

--- bin/tests/system/legacy/tests.sh
+++ bin/tests/system/legacy/tests.sh
@@ -158,7 +158,7 @@ ret=0
 $DIG $DIGOPTS +edns @10.53.0.6 edns512 soa > dig.out.1.test$n || ret=1
 grep "status: NOERROR" dig.out.1.test$n > /dev/null || ret=1
 $DIG $DIGOPTS +edns +tcp @10.53.0.6 edns512 soa > dig.out.2.test$n || ret=1
-grep "status: NOERROR" dig.out.1.test$n > /dev/null || ret=1
+grep "status: NOERROR" dig.out.2.test$n > /dev/null || ret=1
 $DIG $DIGOPTS +edns @10.53.0.6 txt500.edns512 txt > dig.out.3.test$n
 grep "connection timed out; no servers could be reached" dig.out.3.test$n > /dev/null || ret=1
 $DIG $DIGOPTS +edns +bufsize=512 +ignor @10.53.0.6 txt500.edns512 txt > dig.out.4.test$n
