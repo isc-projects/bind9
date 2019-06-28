d1503cbfa28f1abf6052320753097b956d4a286d
Matthijs Mekking <matthijs@isc.org>
2019-06-28 11:52:08 +0200

    Deprecate dnssec-lookaside option
    Add the CFG_CLAUSEFLAG_DEPRECATED flag to the option so that people are
    discouraged from using DLV.

--- lib/isccfg/namedconf.c
+++ lib/isccfg/namedconf.c
@@ -1879,7 +1879,8 @@ view_clauses[] = {
 #endif
 	{ "dnssec-accept-expired", &cfg_type_boolean, 0 },
 	{ "dnssec-enable", &cfg_type_boolean, CFG_CLAUSEFLAG_OBSOLETE },
-	{ "dnssec-lookaside", &cfg_type_lookaside, CFG_CLAUSEFLAG_MULTI },
+	{ "dnssec-lookaside", &cfg_type_lookaside,
+	  CFG_CLAUSEFLAG_MULTI|CFG_CLAUSEFLAG_DEPRECATED },
 	{ "dnssec-must-be-secure",  &cfg_type_mustbesecure,
 	  CFG_CLAUSEFLAG_MULTI },
 	{ "dnssec-validation", &cfg_type_boolorauto, 0 },
