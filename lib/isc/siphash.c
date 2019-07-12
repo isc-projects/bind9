5b5930dca11f6058cfa699835caeefd9a88f510a
Ondřej Surý <ondrej@sury.org>
2019-07-12 15:28:58 +0200

    Remove #include <config.h> usage from siphash.c and siphash_test.c

--- lib/isc/siphash.c
+++ lib/isc/siphash.c
@@ -9,24 +9,6 @@
  * information regarding copyright ownership.
  */
 
-/*
-   siphash() function is SipHash reference C implementation
-
-   Copyright (c) 2012-2016 Jean-Philippe Aumasson <jeanphilippe.aumasson@gmail.com>
-   Copyright (c) 2012-2014 Daniel J. Bernstein <djb@cr.yp.to>
-
-   To the extent possible under law, the author(s) have dedicated all copyright
-   and related and neighboring rights to this software to the public domain
-   worldwide. This software is distributed without any warranty.
-
-   You should have received a copy of the CC0 Public Domain Dedication along
-   with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
- */
-
-/*! \file isc/siphash.c */
-
-#include <config.h>
-
 #include <inttypes.h>
 #include <unistd.h>
 #include <string.h>
