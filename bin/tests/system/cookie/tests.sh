c356748700a62c9a1728ded82d48326230118e28
Michał Kępień <michal@isc.org>
2019-08-08 14:27:55 +0200

    Use rndc_dumpdb() in the "cookie" system test
    (cherry picked from commit cbf32b901b91b191c47feaf4c5f40f2d3c8ea885)

--- bin/tests/system/cookie/tests.sh
+++ bin/tests/system/cookie/tests.sh
@@ -141,13 +141,8 @@ status=`expr $status + $ret`
 n=`expr $n + 1`
 echo_i "checking for COOKIE value in adb ($n)"
 ret=0
-$RNDCCMD 10.53.0.1 dumpdb
-for i in 1 2 3 4 5 6 7 8 9 10
-do
-  sleep 1
-  grep "10.53.0.2.*\[cookie=" ns1/named_dump.db > /dev/null && break
-done
-grep "10.53.0.2.*\[cookie=" ns1/named_dump.db > /dev/null || ret=1
+rndc_dumpdb ns1
+grep "10.53.0.2.*\[cookie=" ns1/named_dump.db.test$n > /dev/null || ret=1
 if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
