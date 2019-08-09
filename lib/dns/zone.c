545e7cca9da20d9d38af5956127b80700afa6a79
Mark Andrews <marka@isc.org>
2019-08-09 16:26:32 +1000

    add dns_zone_cdscheck to integrity checks
    (cherry picked from commit cd40c9fe611ac39977ff837e6ba4b6df4b055833)

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -4767,6 +4767,16 @@ zone_postload(dns_zone_t *zone, dns_db_t *db, isc_time_t loadtime,
 			goto cleanup;
 		}
 
+		if (zone->type == dns_zone_master) {
+			result = dns_zone_cdscheck(zone, db, NULL);
+			if (result != ISC_R_SUCCESS) {
+				dns_zone_log(zone, ISC_LOG_ERROR,
+					     "CDS/CDNSKEY consistency checks "
+					     "failed");
+				goto cleanup;
+			}
+		}
+
 		result = dns_zone_verifydb(zone, db, NULL);
 		if (result != ISC_R_SUCCESS) {
 			goto cleanup;
