fa3fbf62e8bf2177a7b5caad5a592701835632e2
Diego dos Santos Fronza <diego@isc.org>
2019-09-13 12:08:56 -0300

    Allowing include directive to use glob expressions, e.g. include "/opt/named.conf/*.conf".

--- lib/isccfg/parser.c
+++ lib/isccfg/parser.c
@@ -15,9 +15,13 @@
 #include <inttypes.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <glob.h>
 
 #include <isc/buffer.h>
 #include <isc/dir.h>
+#include <isc/errno.h>
 #include <isc/formatcheck.h>
 #include <isc/lex.h>
 #include <isc/log.h>
@@ -105,6 +109,9 @@ cfg_getstringtoken(cfg_parser_t *pctx);
 static void
 parser_complain(cfg_parser_t *pctx, bool is_warning, unsigned int flags,
 		const char *format, va_list args);
+
+static isc_result_t
+glob_include(const char * restrict pattern, glob_t * restrict pglob);
 
 #if defined(HAVE_GEOIP2)
 static isc_result_t
@@ -2387,9 +2394,17 @@ cfg_parse_mapbody(cfg_parser_t *pctx, const cfg_type_t *type, cfg_obj_t **ret) {
 			CHECK(cfg_parse_obj(pctx, &cfg_type_qstring,
 					    &includename));
 			CHECK(parse_semicolon(pctx));
-			CHECK(parser_openfile(pctx,
-					      includename->value.string.base));
+
+			/* Allow include to specify a pattern that follows
+			 * the same rules as the shell e.g "/path/zone*.conf" */
+			glob_t glob_obj;
+			CHECK(glob_include(includename->value.string.base, &glob_obj));
 			cfg_obj_destroy(pctx, &includename);
+
+			for (size_t i = 0; i < glob_obj.gl_pathc; ++i) {
+				CHECK(parser_openfile(pctx, glob_obj.gl_pathv[i]));
+			}
+
 			goto redo;
 		}
 
@@ -4079,4 +4094,28 @@ cfg_pluginlist_foreach(const cfg_obj_t *config, const cfg_obj_t *list,
 	}
 
 	return (result);
+}
+
+
+static isc_result_t
+glob_include(const char * restrict pattern, glob_t * restrict pglob)
+{
+	int rc = glob(pattern, GLOB_ERR, NULL, pglob);
+
+	switch (rc) {
+		case 0:
+			return (ISC_R_SUCCESS);
+		case GLOB_NOMATCH:
+			/* if a magic char (*?[]) was in pattern
+			 * and no path matched we report error early,
+			 * otherwise proceed as normal */
+			return (strpbrk(pattern, "[]*?")) ?
+					(ISC_R_FILENOTFOUND) : (ISC_R_SUCCESS);
+
+		case GLOB_NOSPACE:
+			return (ISC_R_NOMEMORY);
+		default:
+			return (errno != 0 ? isc_errno_toresult(errno) : ISC_R_IOERROR);
+	}
+
 }
