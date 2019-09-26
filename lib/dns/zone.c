679ef8bb95c7ec15f6f54a285262061709d12bfb
Ondřej Surý <ondrej@sury.org>
2019-09-26 09:46:01 +0200

    Fix miscelaneous zone object validity checking errors (missing REQUIRE(), out of order REQUIRE())

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -212,7 +212,7 @@ struct dns_zone {
 	dns_masterformat_t	masterformat;
 	const dns_master_style_t *masterstyle;
 	char			*journal;
-	int32_t		journalsize;
+	int32_t			journalsize;
 	dns_rdataclass_t	rdclass;
 	dns_zonetype_t		type;
 	atomic_uint_fast64_t	flags;
@@ -5465,8 +5465,8 @@ zone_iattach(dns_zone_t *source, dns_zone_t **target) {
 	/*
 	 * 'source' locked by caller.
 	 */
-	REQUIRE(LOCKED_ZONE(source));
 	REQUIRE(DNS_ZONE_VALID(source));
+	REQUIRE(LOCKED_ZONE(source));
 	REQUIRE(target != NULL && *target == NULL);
 	INSIST(source->irefs + isc_refcount_current(&source->erefs) > 0);
 	source->irefs++;
@@ -14208,6 +14208,8 @@ dns_zone_logv(dns_zone_t *zone, isc_logcategory_t *category, int level,
 	char message[4096];
 	const char *zstr;
 
+	REQUIRE(DNS_ZONE_VALID(zone));
+
 	if (!isc_log_wouldlog(dns_lctx, level)) {
 		return;
 	}
@@ -19888,10 +19890,11 @@ dns_zone_verifydb(dns_zone_t *zone, dns_db_t *db, dns_dbversion_t *ver) {
 	dns_name_t *origin;
 
 	const char me[] = "dns_zone_verifydb";
-	ENTER;
 
 	REQUIRE(DNS_ZONE_VALID(zone));
 	REQUIRE(db != NULL);
+
+	ENTER;
 
 	if (dns_zone_gettype(zone) != dns_zone_mirror) {
 		return (ISC_R_SUCCESS);
