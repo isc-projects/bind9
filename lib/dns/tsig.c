adb4e80f584bc393d7bb90a88624edb8e5b6167c
Ondřej Surý <ondrej@sury.org>
2019-09-27 09:39:02 +0200

    Change dns_tsigkey_identity from macro to a function and const argument and result
    (cherry picked from commit 2e304b0b7f5f08d01572cc855deadb1026e15ca8)

--- lib/dns/tsig.c
+++ lib/dns/tsig.c
@@ -660,6 +660,20 @@ dns_tsigkeyring_dumpanddetach(dns_tsig_keyring_t **ringp, FILE *fp) {
 	return (result);
 }
 
+const dns_name_t *
+dns_tsigkey_identity(const dns_tsigkey_t *tsigkey) {
+	REQUIRE(tsigkey == NULL || VALID_TSIG_KEY(tsigkey));
+
+	if (tsigkey == NULL) {
+		return (NULL);
+	}
+	if (tsigkey->generated) {
+		return (tsigkey->creator);
+	} else {
+		return (&tsigkey->name);
+	}
+}
+
 isc_result_t
 dns_tsigkey_create(const dns_name_t *name, const dns_name_t *algorithm,
 		   unsigned char *secret, int length, bool generated,
