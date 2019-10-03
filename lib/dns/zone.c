fedfd48a089450a8358742084c38fcfa443aeb9d
Ondřej Surý <ondrej@sury.org>
2019-10-03 10:16:03 +0200

    lib/dns/zone.c: Fix invalid order of DbC checks that could cause dereference before NULL check

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -5507,8 +5507,8 @@ zone_iattach(dns_zone_t *source, dns_zone_t **target) {
 	/*
 	 * 'source' locked by caller.
 	 */
-	REQUIRE(LOCKED_ZONE(source));
 	REQUIRE(DNS_ZONE_VALID(source));
+	REQUIRE(LOCKED_ZONE(source));
 	REQUIRE(target != NULL && *target == NULL);
 	INSIST(source->irefs + isc_refcount_current(&source->erefs) > 0);
 	source->irefs++;
