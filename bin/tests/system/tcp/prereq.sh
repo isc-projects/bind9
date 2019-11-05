558d7ff5e56f80de5e39bae94763e367f4fb6736
Diego Fronza <diego@isc.org>
2019-11-05 18:08:01 -0300

    Added TCP high-water system tests
    Note: ans6/ans6.py is a helper script that allows tests.sh to open/close
    TCP connections to some BIND instance.
    
    (cherry picked from commit 29be224a049d5f3c05390019fb92e8fe3e81e59c)

--- bin/tests/system/tcp/prereq.sh
+++ bin/tests/system/tcp/prereq.sh
@@ -12,10 +12,8 @@
 SYSTEMTESTTOP=..
 . $SYSTEMTESTTOP/conf.sh
 
-$SHELL clean.sh
+if ! test -n "$PYTHON"; then
+	echo_i "This test requires Python."
+	exit 1
+fi
 
-copy_setports ns1/named.conf.in ns1/named.conf
-copy_setports ns2/named.conf.in ns2/named.conf
-copy_setports ns3/named.conf.in ns3/named.conf
-copy_setports ns4/named.conf.in ns4/named.conf
-copy_setports ns5/named.conf.in ns5/named.conf
