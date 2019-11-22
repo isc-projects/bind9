25306ea1ad3b8c403386fd6d8a76e1fa1e601c30
Mark Andrews <marka@isc.org>
2019-11-22 12:55:03 +1100

    Lock dispatch manager buffer_lock before accessing buffers; Only test buffers for UDP dispatches.
    (cherry picked from commit 011af4de71f54743e650746c9ab7db80d1636800)

--- lib/dns/dispatch.c
+++ lib/dns/dispatch.c
@@ -1504,8 +1504,14 @@ startrecv(dns_dispatch_t *disp, dispsocket_t *dispsock) {
 	if (disp->recv_pending != 0 && dispsock == NULL)
 		return (ISC_R_SUCCESS);
 
-	if (disp->mgr->buffers >= disp->mgr->maxbuffers)
-		return (ISC_R_NOMEMORY);
+	if (disp->socktype == isc_sockettype_udp) {
+		LOCK(&disp->mgr->buffer_lock);
+		if (disp->mgr->buffers >= disp->mgr->maxbuffers) {
+			UNLOCK(&disp->mgr->buffer_lock);
+			return (ISC_R_NOMEMORY);
+		}
+		UNLOCK(&disp->mgr->buffer_lock);
+	}
 
 	if ((disp->attributes & DNS_DISPATCHATTR_EXCLUSIVE) != 0 &&
 	    dispsock == NULL)
