557b051fc40eca02efa0f29b61ca7f721f19d108
Ondřej Surý <ondrej@isc.org>
2019-11-26 11:56:49 +0100

    Use retry() and nextpart*() to simplify catz test
    (cherry picked from commit 51b05189f74aaf8395d6bdfa8e953170a40da2aa)

--- bin/tests/system/catz/clean.sh
+++ bin/tests/system/catz/clean.sh
@@ -14,6 +14,7 @@ rm -f ns*/named.lock
 rm -f ns*/named.memstats
 rm -f ns*/named.conf
 rm -f ns*/named.run
+rm -f ns*/named.run.prev
 rm -f ns1/*dom*example.db
 rm -f ns2/__catz__*db
 rm -f ns2/named.conf.tmp
