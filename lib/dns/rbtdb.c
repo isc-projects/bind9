1e1c3f2e7f804fa46c55ac2f13da4145028fc5a2
Witold Kręcicki <wpk@isc.org>
2020-01-28 11:53:24 +0100

    Increase nodelock count for both cache and regular db.
    (cherry picked from commit 03446843856a3efbaaec41409b5eea5542501118)

--- lib/dns/rbtdb.c
+++ lib/dns/rbtdb.c
@@ -306,7 +306,7 @@ typedef ISC_LIST(dns_rbtnode_t) rbtnodelist_t;
 	(((header)->rdh_ttl > (now)) || \
 	 ((header)->rdh_ttl == (now) && ZEROTTL(header)))
 
-#define DEFAULT_NODE_LOCK_COUNT	   7 /*%< Should be prime. */
+#define DEFAULT_NODE_LOCK_COUNT	   53 /*%< Should be prime. */
 #define RBTDB_GLUE_TABLE_INIT_SIZE 2U
 
 /*%
@@ -326,7 +326,7 @@ typedef ISC_LIST(dns_rbtnode_t) rbtnodelist_t;
 #define DEFAULT_CACHE_NODE_LOCK_COUNT DNS_RBTDB_CACHE_NODE_LOCK_COUNT
 #endif /* if DNS_RBTDB_CACHE_NODE_LOCK_COUNT <= 1 */
 #else  /* ifdef DNS_RBTDB_CACHE_NODE_LOCK_COUNT */
-#define DEFAULT_CACHE_NODE_LOCK_COUNT 16
+#define DEFAULT_CACHE_NODE_LOCK_COUNT 97
 #endif /* DNS_RBTDB_CACHE_NODE_LOCK_COUNT */
 
 typedef struct {
