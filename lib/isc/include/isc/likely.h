6c2e138d7aac0e7487b2ba2df6796ba64ccfc26b
Mark Andrews <marka@isc.org>
2020-01-30 17:27:08 +1100

    simplify ISC_LIKELY/ISC_UNLIKELY for CPPCHECK

--- lib/isc/include/isc/likely.h
+++ lib/isc/include/isc/likely.h
@@ -15,12 +15,17 @@
 /*%
  * Performance
  */
+#ifdef CPPCHECK
+#define ISC_LIKELY(x)            (x)
+#define ISC_UNLIKELY(x)          (x)
+#else
 #ifdef HAVE_BUILTIN_EXPECT
 #define ISC_LIKELY(x)            __builtin_expect(!!(x), 1)
 #define ISC_UNLIKELY(x)          __builtin_expect(!!(x), 0)
 #else
 #define ISC_LIKELY(x)            (x)
 #define ISC_UNLIKELY(x)          (x)
+#endif
 #endif
 
 #endif /* ISC_LIKELY_H */
