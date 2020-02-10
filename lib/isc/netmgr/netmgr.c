a133239698e27e02e8ee0a60f56a3e5d348dcc52
Witold Kręcicki <wpk@isc.org>
2020-02-10 14:00:36 +0100

    Don't limit the size of uvreq/nmhandle pool artificially.
    There was a hard limit set on number of uvreq and nmhandles
    that can be allocated by a pool, but we don't handle a situation
    where we can't get an uvreq. Don't limit the number at all,
    let the OS deal with it.

--- lib/isc/netmgr/netmgr.c
+++ lib/isc/netmgr/netmgr.c
@@ -174,8 +174,7 @@ isc_nm_start(isc_mem_t *mctx, uint32_t workers) {
 	isc_mutex_init(&mgr->reqlock);
 	isc_mempool_create(mgr->mctx, sizeof(isc__nm_uvreq_t), &mgr->reqpool);
 	isc_mempool_setname(mgr->reqpool, "nm_reqpool");
-	isc_mempool_setmaxalloc(mgr->reqpool, 32768);
-	isc_mempool_setfreemax(mgr->reqpool, 32768);
+	isc_mempool_setfreemax(mgr->reqpool, 4096);
 	isc_mempool_associatelock(mgr->reqpool, &mgr->reqlock);
 	isc_mempool_setfillcount(mgr->reqpool, 32);
 
@@ -183,8 +182,7 @@ isc_nm_start(isc_mem_t *mctx, uint32_t workers) {
 	isc_mempool_create(mgr->mctx, sizeof(isc__netievent_storage_t),
 			   &mgr->evpool);
 	isc_mempool_setname(mgr->evpool, "nm_evpool");
-	isc_mempool_setmaxalloc(mgr->evpool, 32768);
-	isc_mempool_setfreemax(mgr->evpool, 32768);
+	isc_mempool_setfreemax(mgr->evpool, 4096);
 	isc_mempool_associatelock(mgr->evpool, &mgr->evlock);
 	isc_mempool_setfillcount(mgr->evpool, 32);
 
