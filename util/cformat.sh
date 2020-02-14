d2b5853b8e28966af01ae6b9c3aec3f7eb228608
Ondřej Surý <ondrej@isc.org>
2020-02-14 07:10:06 +0100

    Start enforcing the clang-format rules on changed files

--- util/cformat.sh
+++ util/cformat.sh
@@ -9,19 +9,27 @@
 # See the COPYRIGHT file distributed with this work for additional
 # information regarding copyright ownership.
 
+CLANG_FORMAT=clang-format
+if [ -n "$1" ]; then
+    CLANG_FORMAT="$1"
+fi
+
+CLANG_FORMAT_VERSION=$("$CLANG_FORMAT" --version | sed -e 's/clang-format version \([0-9]*\)\..*/\1/')
+
+if [ "$CLANG_FORMAT_VERSION" -lt 11 ]; then
+    echo "clang-format version 11 required"
+    exit 1
+fi
+
 # use the main .clang-format for C files
-find bin lib -name "*.c" |
-    xargs clang-format --style=file --sort-includes -i
+"$CLANG_FORMAT" --style=file --sort-includes -i $(git ls-files '*.c')
 
 # set up a temporary .clang-format file for headers ONLY
 cp -f .clang-format .clang-format.bak
 sed -e 's/\(AlignConsecutiveDeclarations\).*/\1: true/' \
-    -e 's/\(AlwaysBreakAfterReturnType\).*/\1: All/' \
     .clang-format.bak > .clang-format
 
-# modify header files
-find bin lib -name "*.h" |
-    xargs clang-format --style=file --sort-includes -i
+"$CLANG_FORMAT" --style=file --sort-includes -i $(git ls-files '*.h')
 
 # restore the original .clang-format file
 cp -f .clang-format.bak .clang-format
