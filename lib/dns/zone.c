633f07e298b1bc1733d9fd8043f2e0b4a779a465
Ondřej Surý <ondrej@isc.org>
2020-02-27 08:45:10 +0100

    Adjust lock/unlock in zone_rekey()
    (cherry picked from commit aaaa7e67fa2b0ec618ec4be1af175fd6d73b76a0)

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -19478,10 +19478,11 @@ zone_rekey(dns_zone_t *zone) {
 
 	dns_db_closeversion(db, &ver, true);
 
+	LOCK_ZONE(zone);
+
 	if (commit) {
 		dns_difftuple_t *tuple;
 
-		LOCK_ZONE(zone);
 		DNS_ZONE_SETFLAG(zone, DNS_ZONEFLG_NEEDNOTIFY);
 
 		zone_needdump(zone, DNS_DUMP_DELAY);
@@ -19603,10 +19604,8 @@ zone_rekey(dns_zone_t *zone) {
 		 * Schedule the next resigning event
 		 */
 		set_resigntime(zone);
-		UNLOCK_ZONE(zone);
 	}
 
-	LOCK_ZONE(zone);
 	isc_time_settoepoch(&zone->refreshkeytime);
 
 	/*
