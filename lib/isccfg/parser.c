7e2d9531a79d289ee99dd436da14efb6d9a505fc
Ondřej Surý <ondrej@isc.org>
2020-06-03 14:42:11 +0200

    Change the invalid CIDR from parser error to warning
    In [RT #43367], the BIND 9 changed the strictness of address / prefix
    length checks:
    
        Check prefixes in acls to make sure the address and
        prefix lengths are consistent.  Warn only in
        BIND 9.11 and earlier.
    
    Unfortunately, a regression slipped in and the check was made an error
    also in the BIND 9.11.  This commit fixes the regression, but turning
    the error into a warning.

--- lib/isccfg/parser.c
+++ lib/isccfg/parser.c
@@ -2634,15 +2634,6 @@ cfg_parse_netprefix(cfg_parser_t *pctx, const cfg_type_t *type,
 					 "invalid prefix length");
 			return (ISC_R_RANGE);
 		}
-		result = isc_netaddr_prefixok(&netaddr, prefixlen);
-		if (result != ISC_R_SUCCESS) {
-			char buf[ISC_NETADDR_FORMATSIZE + 1];
-			isc_netaddr_format(&netaddr, buf, sizeof(buf));
-			cfg_parser_error(pctx, CFG_LOG_NOPREP,
-					 "'%s/%u': address/prefix length "
-					 "mismatch", buf, prefixlen);
-			return (ISC_R_FAILURE);
-		}
 	} else {
 		if (expectprefix) {
 			cfg_parser_error(pctx, CFG_LOG_NEAR,
