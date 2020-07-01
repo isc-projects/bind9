dd32fb9284987888ab8fe267b2f9a8671e302559
Mark Andrews <marka@isc.org>
2020-07-01 17:06:28 +1000

    Make the stdatomic shim and mutexatomic type complete
    The stdatomic shims for non-C11 compilers (Windows, old gcc, ...) and
    mutexatomic implemented only and minimal subset of the atomic types.
    This commit adds 16-bit operations for Windows and all atomic types as
    defined in standard.
    
    (cherry picked from commit bccea5862d009f82cfc49ee56082b090ec850593)

--- lib/isc/unix/include/isc/stdatomic.h
+++ lib/isc/unix/include/isc/stdatomic.h
@@ -13,6 +13,10 @@
 
 #include <inttypes.h>
 #include <stdbool.h>
+#include <stddef.h>
+#if HAVE_UCHAR_H
+#include <uchar.h>
+#endif /* HAVE_UCHAR_H */
 
 #if !defined(__has_feature)
 #define __has_feature(x) 0
@@ -73,13 +77,48 @@ enum memory_order {
 
 typedef enum memory_order memory_order;
 
-typedef int_fast32_t  atomic_int_fast32_t;
-typedef uint_fast32_t atomic_uint_fast32_t;
-typedef int_fast64_t  atomic_int_fast64_t;
-typedef uint_fast64_t atomic_uint_fast64_t;
-typedef bool	      atomic_bool;
+#ifndef HAVE_UCHAR_H
+typedef uint_least16_t char16_t;
+typedef uint_least32_t char32_t;
+#endif /* HAVE_UCHAR_H */
 
-typedef uint_fast64_t atomic_uintptr_t;
+typedef bool		   atomic_bool;
+typedef char		   atomic_char;
+typedef signed char	   atomic_schar;
+typedef unsigned char	   atomic_uchar;
+typedef short		   atomic_short;
+typedef unsigned short	   atomic_ushort;
+typedef int		   atomic_int;
+typedef unsigned int	   atomic_uint;
+typedef long		   atomic_long;
+typedef unsigned long	   atomic_ulong;
+typedef long long	   atomic_llong;
+typedef unsigned long long atomic_ullong;
+typedef char16_t	   atomic_char16_t;
+typedef char32_t	   atomic_char32_t;
+typedef wchar_t		   atomic_wchar_t;
+typedef int_least8_t	   atomic_int_least8_t;
+typedef uint_least8_t	   atomic_uint_least8_t;
+typedef int_least16_t	   atomic_int_least16_t;
+typedef uint_least16_t	   atomic_uint_least16_t;
+typedef int_least32_t	   atomic_int_least32_t;
+typedef uint_least32_t	   atomic_uint_least32_t;
+typedef int_least64_t	   atomic_int_least64_t;
+typedef uint_least64_t	   atomic_uint_least64_t;
+typedef int_fast8_t	   atomic_int_fast8_t;
+typedef uint_fast8_t	   atomic_uint_fast8_t;
+typedef int_fast16_t	   atomic_int_fast16_t;
+typedef uint_fast16_t	   atomic_uint_fast16_t;
+typedef int_fast32_t	   atomic_int_fast32_t;
+typedef uint_fast32_t	   atomic_uint_fast32_t;
+typedef int_fast64_t	   atomic_int_fast64_t;
+typedef uint_fast64_t	   atomic_uint_fast64_t;
+typedef intptr_t	   atomic_intptr_t;
+typedef uintptr_t	   atomic_uintptr_t;
+typedef size_t		   atomic_size_t;
+typedef ptrdiff_t	   atomic_ptrdiff_t;
+typedef intmax_t	   atomic_intmax_t;
+typedef uintmax_t	   atomic_uintmax_t;
 
 #if defined(__CLANG_ATOMICS) /* __c11_atomic builtins */
 #define atomic_init(obj, desired)	 __c11_atomic_init(obj, desired)
