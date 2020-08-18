dd425254a71992e63ebd9d9f576632a15a0bf423
Michal Nowak <mnowak@isc.org>
2020-08-18 18:27:29 +0200

    Fix warnings in when build with --enable-buffer-useinline
        sockaddr.c:147:49: error: pointer targets in passing argument 2 of ‘isc__buffer_putmem’ differ in signedness
        rdata.c:1780:30: error: pointer targets in passing argument 2 of ‘isc__buffer_putmem’ differ in signedness

--- lib/isc/sockaddr.c
+++ lib/isc/sockaddr.c
@@ -144,7 +144,10 @@ isc_sockaddr_totext(const isc_sockaddr_t *sockaddr, isc_buffer_t *target) {
 			return (ISC_R_NOSPACE);
 		}
 
-		isc_buffer_putmem(target, sockaddr->type.sunix.sun_path, plen);
+		isc_buffer_putmem(
+			target,
+			(const unsigned char *)sockaddr->type.sunix.sun_path,
+			plen);
 
 		/*
 		 * Null terminate after used region.
