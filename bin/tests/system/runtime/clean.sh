a7be4e219ee55abdcf63b1050ad470b00b698dc1
Mark Andrews <marka@isc.org>
2020-08-31 13:54:12 +1000

    Wait for test instance of named to exit before completing subtest.
    (cherry picked from commit dda5b7b0b86ca3250bcc3e6a4e3f931fdd253561)

--- bin/tests/system/runtime/clean.sh
+++ bin/tests/system/runtime/clean.sh
@@ -9,13 +9,15 @@
 # See the COPYRIGHT file distributed with this work for additional
 # information regarding copyright ownership.
 
-rm -f ns2/named.conf ns2/named-alt*.conf
-rm -f */named.memstats
-rm -f */named*.run
-rm -f ns*/named.lock ns*/named*.pid ns*/other.lock
-rm -f *.pid
-rm -f rndc.out*
 [ -d ns2/nope ] && chmod 755 ns2/nope
+
+rm -f *.pid
+rm -f */named*.run
+rm -f */named.memstats
+rm -f kill*.out
+rm -f ns*/managed-keys.bind*
+rm -f ns*/named.lock ns*/named*.pid ns*/other.lock
+rm -f ns2/named.conf ns2/named-alt*.conf
+rm -f rndc.out*
 rm -rf ns2/nope
 rm -rf ns2/tmp.*
-rm -f ns*/managed-keys.bind*
