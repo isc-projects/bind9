acef95ce44690783fdd48652d9765dfbc95798c9
Diego Fronza <diego@isc.org>
2020-09-10 15:33:15 -0300

    Added test for the proposed fix
    This test is very simple, two nameserver instances are created:
        - ns4: master, with 'minimal-responses yes', authoritative
            for example. zone
        - ns5: slave, stub zone
    
    The first thing verified is the transfer of zone data from master
    to slave, which should be saved in ns5/example.db.
    
    After that, a query is issued to ns5 asking for target.example.
    TXT, a record present in the master database with the "test" string
    as content.
    
    If that query works, it means stub zone successfully request
    nameserver addresses from master, ns4.example. A/AAAA
    
    The presence of both A/AAAA records for ns4 is also verified in the
    stub zone local file, ns5/example.db.

--- bin/tests/system/stub/clean.sh
+++ bin/tests/system/stub/clean.sh
@@ -12,8 +12,9 @@
 #
 # Clean up after stub tests.
 #
-rm -f dig.out.ns3 ns3/child.example.st
+rm -f dig.out.ns[35] ns3/child.example.st
 rm -f */named.memstats
 rm -f */named.conf
 rm -f */named.run
 rm -f ns*/named.lock
+rm -f ns5/example.db
