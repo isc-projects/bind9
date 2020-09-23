3ab08cdaa40451e45e85f52077d8e1c3b78baaef
Ondřej Surý <ondrej@sury.org>
2020-09-23 16:36:44 +0200

    Adjust digdelv tests for default 1232 EDNS Buffer Size
    * digdelv test was just expecting default server EDNS buffer size to be
      4096, the test needed only slight adjustment
    
    (cherry picked from commit f1556f8c4126eced7cf71de3dfc007dbd9940c4a)
    (cherry picked from commit 6b303721f1ee472dc8ab2d1df03191a98d4e4e5e)

--- bin/tests/system/digdelv/tests.sh
+++ bin/tests/system/digdelv/tests.sh
@@ -672,8 +672,10 @@ ret=0
   echo_i "check that dig +bufsize restores default bufsize ($n)"
   ret=0
   $DIG $DIGOPTS @10.53.0.3 a.example +bufsize=0 +bufsize +qr > dig.out.test$n 2>&1 || ret=1
-  lines=`grep "EDNS:.* udp: 4096" dig.out.test$n | wc -l`
-  test $lines -eq 2 || ret=1
+  lines1232=`grep "EDNS:.* udp: 1232" dig.out.test$n | wc -l`
+  lines4096=`grep "EDNS:.* udp: 4096" dig.out.test$n | wc -l`
+  test $lines1232 -eq 1 || ret=1
+  test $lines4096 -eq 1 || ret=1
   if [ $ret -ne 0 ]; then echo_i "failed"; fi
   status=`expr $status + $ret`
 
