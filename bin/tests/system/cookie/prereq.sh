2898f530cdc58f7b2d961e5345d72c90e7ef3ad6
Mark Andrews <marka@isc.org>
2020-11-13 15:38:49 +1100

    Check that missing cookies are handled
    (cherry picked from commit bd9155590ec34c27f1af1d00d09791292309064c)

--- bin/tests/system/cookie/prereq.sh
+++ bin/tests/system/cookie/prereq.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+#
 # Copyright (C) Internet Systems Consortium, Inc. ("ISC")
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
@@ -7,14 +9,23 @@
 # See the COPYRIGHT file distributed with this work for additional
 # information regarding copyright ownership.
 
-rm -f ns*/named.conf
-rm -f dig.out.*
-rm -f named.run.*
-rm -f rndc.out.*
-rm -f ns1/named_dump.db*
-rm -f ns*/named.memstats
-rm -f ns*/named.run
-rm -f ns*/named.lock
-rm -f ns*/managed-keys.bind*
-rm -f ns*/named.run.prev
-rm -f ans*/ans.run ans*/ans.log ans*/query.log
+# shellcheck source=conf.sh
+. "$SYSTEMTESTTOP/conf.sh"
+
+set -e
+
+if test -n "$PYTHON"
+then
+    if $PYTHON -c "import dns" 2> /dev/null
+    then
+        :
+    else
+        echo_i "This test requires the dnspython module." >&2
+        exit 1
+    fi
+else
+    echo_i "This test requires Python and the dnspython module." >&2
+    exit 1
+fi
+
+exit 0
