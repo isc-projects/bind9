001413ed509d7761e3bbe0ad4a6a9ebe39ffc4e5
Michal Nowak <mnowak@isc.org>
2021-01-27 18:06:33 +0100

    Drop AddressSanitizer constraint from libns unit tests
    The AddressSanitizer constraint in some libns unit tests does not seem
    to be necessary anymore, these tests run fine under AddressSanitizer.
    
    (cherry picked from commit 613be8706eca81fee97434e25fda4e9118cfe511)

--- lib/ns/tests/query_test.c
+++ lib/ns/tests/query_test.c
@@ -11,7 +11,7 @@
 
 #include <isc/util.h>
 
-#if HAVE_CMOCKA && !__SANITIZE_ADDRESS__
+#if HAVE_CMOCKA
 
 #include <inttypes.h>
 #include <sched.h> /* IWYU pragma: keep */
@@ -617,22 +617,15 @@ main(void) {
 #endif /* if defined(USE_LIBTOOL) || LD_WRAP */
 }
 
-#else /* HAVE_CMOCKA && !__SANITIZE_ADDRESS__ */
+#else /* HAVE_CMOCKA */
 
 #include <stdio.h>
 
 int
 main(void) {
-#if __SANITIZE_ADDRESS__
-	/*
-	 * We disable this test when the address sanitizer is in
-	 * the use, as libuv will trigger errors.
-	 */
-	printf("1..0 # Skip ASAN is in use\n");
-#else  /* ADDRESS_SANIZITER */
 	printf("1..0 # Skip cmocka not available\n");
-#endif /* __SANITIZE_ADDRESS__ */
+
 	return (0);
 }
 
-#endif /* HAVE_CMOCKA && !__SANITIZE_ADDRESS__ */
+#endif /* HAVE_CMOCKA */
