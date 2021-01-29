a55bdb28f9004a2db49b3389efe09d99d93c256d
Ondřej Surý <ondrej@isc.org>
2021-01-29 13:16:38 +0100

    Assigning uint64_t from buffer might be misaligned in netmgr tests
    Resolve possible 8-byte unaligned access when assigning the magic
    value from the received buffer.

--- lib/isc/tests/tcpdns_test.c
+++ lib/isc/tests/tcpdns_test.c
@@ -328,7 +328,7 @@ tcpdns_connect_read_cb(isc_nmhandle_t *handle, isc_result_t eresult,
 
 	atomic_fetch_add(&creads, 1);
 
-	magic = *(uint64_t *)region->base;
+	memmove(&magic, region->base, sizeof(magic));
 
 	assert_true(magic == stop_magic || magic == send_magic);
 
@@ -478,7 +478,7 @@ tcpdns_listen_read_cb(isc_nmhandle_t *handle, isc_result_t eresult,
 
 	assert_int_equal(region->length, sizeof(magic));
 
-	magic = *(uint64_t *)region->base;
+	memmove(&magic, region->base, sizeof(magic));
 	assert_true(magic == stop_magic || magic == send_magic);
 
 	if (magic == send_magic) {
