4775e9f256025026e9a9d7f246b66148375c9d5e
Ondřej Surý <ondrej@sury.org>
2021-02-09 13:25:46 +0100

    Move most of the OpenSSL initialization to isc_tls
    Since we now require both libcrypto and libssl to be initialized for
    netmgr, we move all the OpenSSL initialization code except the engine
    initialization to isc_tls API.
    
    The isc_tls_initialize() and isc_tls_destroy() has been made idempotent,
    so they could be called multiple time.  However when isc_tls_destroy()
    has been called, the isc_tls_initialize() could not be called again.

--- lib/dns/dst_api.c
+++ lib/dns/dst_api.c
@@ -201,7 +201,7 @@ dst_lib_init(isc_mem_t *mctx, const char *engine) {
 	RETERR(dst__hmacsha256_init(&dst_t_func[DST_ALG_HMACSHA256]));
 	RETERR(dst__hmacsha384_init(&dst_t_func[DST_ALG_HMACSHA384]));
 	RETERR(dst__hmacsha512_init(&dst_t_func[DST_ALG_HMACSHA512]));
-	RETERR(dst__openssl_init(mctx, engine));
+	RETERR(dst__openssl_init(engine));
 	RETERR(dst__openssldh_init(&dst_t_func[DST_ALG_DH]));
 #if USE_OPENSSL
 	RETERR(dst__opensslrsa_init(&dst_t_func[DST_ALG_RSASHA1],
