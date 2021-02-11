30729c7013d0ea2f7eac85f44129df33fb28aaa3
Diego Fronza <diego@isc.org>
2021-02-11 11:32:20 -0300

    Fix dangling references to outdated views after reconfig
    This commit fix a leak which was happening every time an inline-signed
    zone was added to the configuration, followed by a rndc reconfig.
    
    During the reconfig process, the secure version of every inline-signed
    zone was "moved" to a new view upon a reconfig and it "took the raw
    version along", but only once the secure version was freed (at shutdown)
    was prev_view for the raw version detached from, causing the old view to
    be released as well.
    
    This caused dangling references to be kept for the previous view, thus
    keeping all resources used by that view in memory.

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -1616,6 +1616,9 @@ dns_zone_setviewcommit(dns_zone_t *zone) {
 	if (zone->prev_view != NULL) {
 		dns_view_weakdetach(&zone->prev_view);
 	}
+	if (inline_secure(zone)) {
+		dns_zone_setviewcommit(zone->raw);
+	}
 	UNLOCK_ZONE(zone);
 }
 
@@ -1627,6 +1630,9 @@ dns_zone_setviewrevert(dns_zone_t *zone) {
 	if (zone->prev_view != NULL) {
 		dns_zone_setview_helper(zone, zone->prev_view);
 		dns_view_weakdetach(&zone->prev_view);
+	}
+	if (inline_secure(zone)) {
+		dns_zone_setviewrevert(zone->raw);
 	}
 	UNLOCK_ZONE(zone);
 }
