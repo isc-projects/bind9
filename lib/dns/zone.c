314b5362a8161a42c08906149f1e3187d0f1ad83
Mark Andrews <marka@isc.org>
2021-03-04 12:35:11 +1100

    Remove dns_zone_setflag()
    This function has never been used since it was added to the source tree
    by commit 686b27bfd3ef906721fad485e3dae759679890b7 back in 1999.  As
    the dns_zoneflg_t type is only defined in lib/dns/zone.c, no function
    external to that file would be able to use dns_zone_setflag() properly
    anyway - the DNS_ZONE_SETFLAG() and DNS_ZONE_CLRFLAG() macros should be
    used instead. Zone options that can be set from outside zone.c are set
    using dns_zone_setoption().

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -5705,17 +5705,6 @@ dns_zone_getmgr(dns_zone_t *zone) {
 }
 
 void
-dns_zone_setflag(dns_zone_t *zone, unsigned int flags, bool value) {
-	REQUIRE(DNS_ZONE_VALID(zone));
-
-	if (value) {
-		DNS_ZONE_SETFLAG(zone, flags);
-	} else {
-		DNS_ZONE_CLRFLAG(zone, flags);
-	}
-}
-
-void
 dns_zone_setkasp(dns_zone_t *zone, dns_kasp_t *kasp) {
 	REQUIRE(DNS_ZONE_VALID(zone));
 
