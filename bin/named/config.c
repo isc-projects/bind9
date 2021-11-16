1a94a31484a31c7c928bd77c920c2f3ca47a95b1
Mark Andrews <marka@isc.org>
2021-11-16 14:39:05 +1100

    Embed NAMED_SYSCONFDIR contents in the bind.keys comment
    (cherry picked from commit 1d7b1f74c9ec841967741406a22034282b608f56)

--- bin/named/config.c
+++ bin/named/config.c
@@ -281,7 +281,7 @@ view \"_bind\" chaos {\n\
 			    "#\n\
 #  Default trusted key(s), used if \n\
 # \"dnssec-validation auto;\" is set and\n\
-#  sysconfdir/bind.keys doesn't exist).\n\
+#  " NAMED_SYSCONFDIR "/bind.keys doesn't exist).\n\
 #\n\
 # BEGIN TRUST ANCHORS\n"
 
