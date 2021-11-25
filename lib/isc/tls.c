89f4f8f0c89a5243ba9fa343d492b15fd97e4df0
Matthijs Mekking <matthijs@isc.org>
2021-11-25 15:10:00 +0100

    Add OPENSSL_cleanup to tls_shutdown function
    This prevents a direct leak in OPENSSL_init_crypto (called from
    OPENSSL_init_ssl).
    
    Add shim version of OPENSSL_cleanup because it is missing in LibreSSL on
    OpenBSD.

--- lib/isc/tls.c
+++ lib/isc/tls.c
@@ -17,6 +17,7 @@
 
 #include <openssl/bn.h>
 #include <openssl/conf.h>
+#include <openssl/crypto.h>
 #include <openssl/dh.h>
 #include <openssl/err.h>
 #include <openssl/opensslv.h>
@@ -128,8 +129,9 @@ tls_shutdown(void) {
 	REQUIRE(atomic_load(&init_done));
 	REQUIRE(!atomic_load(&shut_done));
 
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
-
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+	OPENSSL_cleanup();
+#else
 	CONF_modules_unload(1);
 	OBJ_cleanup();
 	EVP_cleanup();
