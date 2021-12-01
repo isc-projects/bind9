5c17919019ef0af8226e5bb61214b805bb3e2451
Ondřej Surý <ondrej@sury.org>
2021-12-01 12:22:59 +0100

    Add TCP connection reset test
    The TCP connection reset test starts mock UDP and TCP server which
    always returns empty DNS answer with TC bit set over UDP and resets the
    TCP connection after five seconds.
    
    When tested without the fix, the DNS query to 10.53.0.2 times out and
    the ns2 server hangs at shutdown.

---
+++ bin/tests/system/dispatch/tests-connreset.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+############################################################################
+# Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# See the COPYRIGHT file distributed with this work for additional
+# information regarding copyright ownership.
+############################################################################
+
+import pytest
+
+pytest.importorskip("dns")
+import dns.message
+import dns.query
+import dns.rcode
+
+
+def test_connreset(port):
+    msg = dns.message.make_query("sub.example.", "A", want_dnssec=True,
+                                 use_edns=0, payload=1232)
+    ans = dns.query.udp(msg, "10.53.0.2", timeout=10, port=port)
+    assert ans.rcode() == dns.rcode.SERVFAIL
