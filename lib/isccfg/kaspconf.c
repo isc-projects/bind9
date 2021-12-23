b8845454c8c67c5c5650e597936f6303708bdd7d
Mark Andrews <marka@isc.org>
2021-12-23 17:54:53 +1100

    Report duplicate dnssec-policy names
    Duplicate dnssec-policy names were detected as an error condition
    but were not logged.

--- lib/isccfg/kaspconf.c
+++ lib/isccfg/kaspconf.c
@@ -272,6 +272,10 @@ cfg_kasp_fromconfig(const cfg_obj_t *config, const char *name, isc_mem_t *mctx,
 	result = dns_kasplist_find(kasplist, kaspname, &kasp);
 
 	if (result == ISC_R_SUCCESS) {
+		cfg_obj_log(
+			config, logctx, ISC_LOG_ERROR,
+			"dnssec-policy: duplicately named policy found '%s'",
+			kaspname);
 		dns_kasp_detach(&kasp);
 		return (ISC_R_EXISTS);
 	}
