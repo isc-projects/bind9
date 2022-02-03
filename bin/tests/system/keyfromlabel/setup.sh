a4497094414598537eb3cbbc9d92f38e072f5d7b
Aram Sargsyan <aram@isc.org>
2022-02-03 10:59:45 +0000

    Use unique SoftHSMv2 token label for the "keyfromlabel" test
    When there are more than one tokens initialized in SoftHSMv2,
    care must be taken to correctly identify them.
    
    Use a SoftHSMv2 token label which will uniquely identify the
    token used for this test.
    
    Use the "--token-label" parameter for the `pkcs11-tool` program
    to make sure that it finds and uses the correct token.

--- bin/tests/system/keyfromlabel/setup.sh
+++ bin/tests/system/keyfromlabel/setup.sh
@@ -16,7 +16,7 @@
 
 set -e
 
-softhsm2-util --init-token --free --pin 1234 --so-pin 1234 --label "softhsm2" | awk '/^The token has been initialized and is reassigned to slot/ { print $NF }'
+softhsm2-util --init-token --free --pin 1234 --so-pin 1234 --label "softhsm2-keyfromlabel" | awk '/^The token has been initialized and is reassigned to slot/ { print $NF }'
 
 printf '%s' "${HSMPIN:-1234}" > pin
 PWD=$(pwd)
