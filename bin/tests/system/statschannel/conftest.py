098bd03f7dc7a1403d837c6911465058ddf5ea61
Michał Kępień <michal@isc.org>
2022-03-14 08:59:32 +0100

    Simplify skipping tests depending on libxml2
    All tests in bin/tests/system/statschannel/tests-xml.py require libxml2
    support to be enabled in BIND 9 at build-time.  Instead of applying the
    same pytest.mark.skipif() decorator to every test in that file, set the
    'pytestmark' global accordingly in order to immediately skip all tests
    in tests-xml.py if libxml2 support is not compiled in.
    
    Remove all occurrences of the @pytest.mark.xml decorator (and all
    associated code) from the "statschannel" system test as the
    xml.etree.ElementTree module is a part of the Python standard library
    since Python 2.5 (so checking whether it is available is redundant) and
    checking for libxml2 support in the tested BIND 9 build is already
    handled by setting the 'pytestmark' global accordingly.
    
    (cherry picked from commit 286b57c7f165c441170bc3aa6f7faa097161b7ee)

--- bin/tests/system/statschannel/conftest.py
+++ bin/tests/system/statschannel/conftest.py
@@ -17,9 +17,6 @@ def pytest_configure(config):
     config.addinivalue_line(
         "markers", "requests: mark tests that need requests to function"
     )
-    config.addinivalue_line(
-        "markers", "xml: mark tests that need xml.etree to function"
-    )
 
 
 def pytest_collection_modifyitems(config, items):
@@ -34,22 +31,6 @@ def pytest_collection_modifyitems(config, items):
         for item in items:
             if "requests" in item.keywords:
                 item.add_marker(skip_requests)
-    # Test for xml module
-    skip_xml = pytest.mark.skip(
-        reason="need xml module to run")
-    try:
-        import xml.etree.ElementTree  # noqa: F401
-    except ModuleNotFoundError:
-        for item in items:
-            if "xml" in item.keywords:
-                item.add_marker(skip_xml)
-    # Test if XML statistics channel was enabled
-    no_xmlstats = pytest.mark.skip(
-        reason="need XML statistics to be enabled")
-    if os.getenv("HAVEXMLSTATS") is None:
-        for item in items:
-            if "xml" in item.keywords:
-                item.add_marker(no_xmlstats)
 
 
 @pytest.fixture
