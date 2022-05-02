isc-projects/bind9

commit 3f78c6053947900b5bf5a06483c5dac42f4882c7

Use compile-time paths in the manual pages
Replace the hard-coded paths for various BIND 9 files (configuration,
pid, etc.) in the man pages and ARM with compile-time values using the
sphinx-build replace system.

This is more complicated, because the restructured text specification
doesn't allow |substitions| inside ``code-blocks``, so for each specific
file we had to create own substition which is sub-optimal, but it is
only way how to do this without adding Sphinx extension.

(cherry picked from commit b42681c4e973fc819a42c36b18cfb59415d0fcba)

doc/arm/conf.py
@@ -178,3 +178,17 @@ latex_documents = [
     ]
 
 latex_logo = "isc-logo.pdf"
+
+#
+# The rst_epilog will be completely overwritten from the Makefile,
+# the definition here is provided purely for situations when
+# sphinx-build is run by hand.
+#
+rst_epilog = """
+.. |rndc_conf| replace: ``/etc/rndc.conf``
+.. |rndc_key| replace: ``/etc/rndc.key``
+.. |named_conf| replace: ``/etc/named.conf``
+.. |bind_keys| replace: ``/etc/bind.keys``
+.. |named_pid| replace: ``/run/named.pid``
+.. |session_key| replace: ``/run/session.key``
+"""

doc/man/conf.py
@@ -102,3 +102,17 @@ man_pages = [
     ('rndc', 'rndc', 'name server control utility', author, 8),
     ('tsig-keygen', 'tsig-keygen', 'TSIG key generation tool', author, 8),
     ]
+
+#
+# The rst_epilog will be completely overwritten from the Makefile,
+# the definition here is provided purely for situations when
+# sphinx-build is run by hand.
+#
+rst_epilog = """
+.. |rndc_conf| replace: ``@sysconfdir@/rndc.conf``
+.. |rndc_key| replace: ``@sysconfdir@/rndc.key``
+.. |named_conf| replace: ``@sysconfdir@/named.conf``
+.. |bind_keys| replace: ``@sysconfdir@/bind.keys``
+.. |named_pid| replace: ``@runstatedir@/named.pid``
+.. |session_key| replace: ``@runstatedir@/session.key``
+"""

Makefile.docs
@@ -9,6 +9,16 @@ AM_V_SPHINX_0 = @echo "  SPHINX   $@";
 
 SPHINXBUILDDIR = $(builddir)/_build
 
+LF = \n
+RNDC_CONF = .. |rndc_conf| replace:: ``$(sysconfdir)/rndc.conf``
+RNDC_KEY = .. |rndc_key| replace:: ``$(sysconfdir)/rndc.key``
+NAMED_CONF = .. |named_conf| replace:: ``$(sysconfdir)/named.conf``
+BIND_KEYS = .. |bind_keys| replace:: ``$(sysconfdir)/bind.keys``
+NAMED_PID = .. |named_pid| replace:: ``$(runstatedir)/named.pid``
+SESSION_KEY = .. |session_key| replace:: ``$(runstatedir)/session.key``
+
+export RST_EPILOG = $(RNDC_CONF)$(LF)$(RNDC_KEY)$(LF)$(NAMED_CONF)$(LF)$(BIND_KEYS)$(LF)$(NAMED_PID)$(LF)$(SESSION_KEY)
+
 common_SPHINXOPTS =			\
 	-W				\
 	-c $(srcdir)			\
@@ -17,18 +27,30 @@ common_SPHINXOPTS =			\
 
 # The "today" variable set below is not directly used in the ARM, but its value
 # is implicitly inserted on the title page of the PDF file produced by Sphinx.
-ALLSPHINXOPTS =				\
-	$(common_SPHINXOPTS)		\
-	-D today="$(RELEASE_DATE)"	\
-	$(SPHINXOPTS)			\
+ALLSPHINXOPTS =					\
+	$(common_SPHINXOPTS)			\
+	-D today="$(RELEASE_DATE)"		\
+	-D rst_epilog="$$(printf "$${RST_EPILOG}")"	\
+	$(SPHINXOPTS)				\
 	$(srcdir)
 
-man_SPHINXOPTS =			\
-	$(common_SPHINXOPTS)		\
-	-D version="@""PACKAGE_VERSION@"\
-	-D today="@""RELEASE_DATE@"	\
-	-D release="@""PACKAGE_VERSION@"\
-	$(SPHINXOPTS)			\
+_ = @
+man_RNDC_CONF = .. |rndc_conf| replace:: ``$(_)sysconfdir$(_)/rndc.conf``
+man_RNDC_KEY = .. |rndc_key| replace:: ``$(_)sysconfdir$(_)/rndc.key``
+man_NAMED_CONF = .. |named_conf| replace:: ``$(_)sysconfdir$(_)/named.conf``
+man_BIND_KEYS = .. |bind_keys| replace:: ``$(_)sysconfdir$(_)/bind.keys``
+man_NAMED_PID = .. |named_pid| replace:: ``$(_)runstatedir$(_)/named.pid``
+man_SESSION_KEY = .. |session_key| replace:: ``$(_)runstatedir$(_)/session.key``
+
+export man_RST_EPILOG = $(man_RNDC_CONF)$(LF)$(man_RNDC_KEY)$(LF)$(man_NAMED_CONF)$(LF)$(man_BIND_KEYS)$(LF)$(man_NAMED_PID)$(LF)$(man_SESSION_KEY)
+
+man_SPHINXOPTS =				\
+	$(common_SPHINXOPTS)			\
+	-D version="@""PACKAGE_VERSION@"	\
+	-D today="@""RELEASE_DATE@"		\
+	-D release="@""PACKAGE_VERSION@"	\
+	-D rst_epilog="$$(printf "$${man_RST_EPILOG}")"	\
+	$(SPHINXOPTS)				\
 	$(srcdir)
 
 AM_V_SED = $(AM_V_SED_@AM_V@)

doc/man/Makefile.am
@@ -132,6 +132,9 @@ man_SUBST =							\
 	$(AM_V_SED)$(SED)					\
 	  -e 's,[@]PACKAGE_VERSION@,$(PACKAGE_VERSION),'	\
 	  -e 's,[@]RELEASE_DATE@,$(RELEASE_DATE),'		\
+	  -e 's,[@]libdir[@],$(libdir),g'			\
+	  -e 's,[@]runstatedir[@],$(runstatedir),g'		\
+	  -e 's,[@]sysconfdir[@],$(sysconfdir),g'		\
 	  $(srcdir)/$@in >$@
 
 .1in.1:
@@ -170,6 +173,7 @@ if HAVE_SPHINX_BUILD
 manpages.stamp: $(MANPAGES_RST)
 	@rm -f manpages.tmp
 	@touch manpages.tmp
+	echo "${man_RST_EPILOG}"
 	$(AM_V_SPHINX)$(SPHINX_BUILD) -b man -d $(SPHINXBUILDDIR)/.doctrees/man $(man_SPHINXOPTS) $(SPHINXBUILDDIR)/man
 	for f in $(SPHINXBUILDDIR)/man/*; do \
 	  cp -a "$$f" "$(srcdir)/$$(basename $$f)in"; \

doc/man/delv.1in
@@ -113,7 +113,7 @@ supplied, \fBdelv\fP performs a lookup for an A record.
 .TP
 .B \fB\-a anchor\-file\fP
 This option specifies a file from which to read DNSSEC trust anchors. The default
-is \fB/etc/bind.keys\fP, which is included with BIND 9 and contains one
+is \fB@sysconfdir@/bind.keys\fP, which is included with BIND 9 and contains one
 or more trust anchors for the root zone (".").
 .sp
 Keys that do not match the root zone name are ignored. An alternate
@@ -123,8 +123,8 @@ Note: When reading the trust anchor file, \fBdelv\fP treats \fBtrust\-anchors\fP
 \fBinitial\-key\fP, and \fBstatic\-key\fP identically. That is, for a managed key,
 it is the \fIinitial\fP key that is trusted; \fI\%RFC 5011\fP key management is not
 supported. \fBdelv\fP does not consult the managed\-keys database maintained by
-\fBnamed\fP, which means that if either of the keys in \fB/etc/bind.keys\fP is
-revoked and rolled over, \fB/etc/bind.keys\fP must be updated to
+\fBnamed\fP, which means that if either of the keys in \fB@sysconfdir@/bind.keys\fP is
+revoked and rolled over, \fB@sysconfdir@/bind.keys\fP must be updated to
 use DNSSEC validation in \fBdelv\fP\&.
 .TP
 .B \fB\-b address\fP
@@ -331,7 +331,7 @@ This option prints response data in YAML format.
 .UNINDENT
 .SH FILES
 .sp
-\fB/etc/bind.keys\fP
+\fB@sysconfdir@/bind.keys\fP
 .sp
 \fB/etc/resolv.conf\fP
 .SH SEE ALSO

doc/man/filter-a.8in
@@ -44,7 +44,7 @@ For example:
 .sp
 .nf
 .ft C
-plugin query "/usr/local/lib/filter\-a.so" {
+plugin query "filter\-a.so" {
         filter\-a\-on\-v6 yes;
         filter\-a\-on\-v4 yes;
         filter\-a { 192.0.2.1; 2001:db8:2::1; };

doc/man/filter-aaaa.8in
@@ -48,7 +48,7 @@ enabled with the \fBfilter\-aaaa\fP ACL and the \fBfilter\-aaaa\-on\-v4\fP and
 .sp
 .nf
 .ft C
-plugin query "/usr/local/lib/filter\-aaaa.so" {
+plugin query "filter\-aaaa.so" {
         filter\-aaaa\-on\-v4 yes;
         filter\-aaaa\-on\-v6 yes;
         filter\-aaaa { 192.0.2.1; 2001:db8:2::1; };

doc/man/named-checkconf.1in
@@ -38,7 +38,7 @@ named-checkconf \- named configuration file syntax checking tool
 \fBnamed\-checkconf\fP checks the syntax, but not the semantics, of a
 \fBnamed\fP configuration file. The file, along with all files included by it, is parsed and checked for syntax
 errors. If no file is specified,
-\fB/etc/named.conf\fP is read by default.
+\fB@sysconfdir@/named.conf\fP is read by default.
 .sp
 Note: files that \fBnamed\fP reads in separate parser contexts, such as
 \fBrndc.key\fP and \fBbind.keys\fP, are not automatically read by
@@ -91,7 +91,7 @@ This option performs a test load of all zones of type \fBprimary\fP found in \fB
 .TP
 .B \fBfilename\fP
 This indicates the name of the configuration file to be checked. If not specified,
-it defaults to \fB/etc/named.conf\fP\&.
+it defaults to \fB@sysconfdir@/named.conf\fP\&.
 .UNINDENT
 .SH RETURN VALUES
 .sp

commit cd31391294f38b311ddc9accc2ef384c6f7bf67c

Fix default file path substitution in the ARM and man pages
Default paths were not substituted correctly when Python-only build was
used, i.e. it affected only ReadTheDocs. The incorrect rst_epilog was
overriden by Makefile for all "ordinary" builds.

This error was introduced by 3f78c6053947900b5bf5a06483c5dac42f4882c7.

Related: !5815

doc/arm/conf.py
@@ -186,10 +186,10 @@ latex_logo = "isc-logo.pdf"
 # sphinx-build is run by hand.
 #
 rst_epilog = """
-.. |rndc_conf| replace: ``/etc/rndc.conf``
-.. |rndc_key| replace: ``/etc/rndc.key``
-.. |named_conf| replace: ``/etc/named.conf``
-.. |bind_keys| replace: ``/etc/bind.keys``
-.. |named_pid| replace: ``/run/named.pid``
-.. |session_key| replace: ``/run/session.key``
+.. |rndc_conf| replace:: ``/etc/rndc.conf``
+.. |rndc_key| replace:: ``/etc/rndc.key``
+.. |named_conf| replace:: ``/etc/named.conf``
+.. |bind_keys| replace:: ``/etc/bind.keys``
+.. |named_pid| replace:: ``/run/named.pid``
+.. |session_key| replace:: ``/run/session.key``
 """

doc/man/conf.py
@@ -109,12 +109,12 @@ man_pages = [
 # sphinx-build is run by hand.
 #
 rst_epilog = """
-.. |rndc_conf| replace: ``@sysconfdir@/rndc.conf``
-.. |rndc_key| replace: ``@sysconfdir@/rndc.key``
-.. |named_conf| replace: ``@sysconfdir@/named.conf``
-.. |bind_keys| replace: ``@sysconfdir@/bind.keys``
-.. |named_pid| replace: ``@runstatedir@/named.pid``
-.. |session_key| replace: ``@runstatedir@/session.key``
+.. |rndc_conf| replace:: ``@sysconfdir@/rndc.conf``
+.. |rndc_key| replace:: ``@sysconfdir@/rndc.key``
+.. |named_conf| replace:: ``@sysconfdir@/named.conf``
+.. |bind_keys| replace:: ``@sysconfdir@/bind.keys``
+.. |named_pid| replace:: ``@runstatedir@/named.pid``
+.. |session_key| replace:: ``@runstatedir@/session.key``
 """
 
 def setup(app):