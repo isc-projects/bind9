513cb24b5539f5173b31f154fa401b356ed563b4
Mark Andrews <marka@isc.org>
2022-06-23 14:22:10 +1000

    Make "checking revoked key with duplicate key ID" work
    There should be 2 keys with the same key id after the numerically
    lower one is revoked (serial space arithmetic).  The DS points
    at the non-revoked key so validation should still succeed.

--- bin/tests/system/autosign/ns1/keygen.sh
+++ bin/tests/system/autosign/ns1/keygen.sh
@@ -19,7 +19,7 @@ infile=root.db.in
 
 (cd ../ns2 && $SHELL keygen.sh )
 
-cat $infile ../ns2/dsset-example. > $zonefile
+cat $infile ../ns2/dsset-example. ../ns2/dsset-bar. > $zonefile
 
 zskact=$($KEYGEN -3 -a ${DEFAULT_ALGORITHM} -q $zone)
 zskvanish=$($KEYGEN -3 -a ${DEFAULT_ALGORITHM} -q $zone)
