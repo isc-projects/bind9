7009f9d270964824893bdbd2503b6ad2f6fb6154
Michał Kępień <michal@isc.org>
2022-07-13 13:19:32 +0200

    Improve reporting for read-write lock errors
    Replace direct uses of implementation-specific rwlock functions in
    lib/isc/include/isc/rwlock.h with preprocessor macros that use
    ERRNO_CHECK(), in order to augment rwlock-related error messages with
    file/line/caller information and the error string corresponding to
    errno.  Adjust the implementation-specific functions for pthreads-based
    rwlocks so that they return any errors encountered to the caller instead
    of aborting execution immediately using RUNTIME_CHECK().
    
    To keep code modifications simple, make the non-pthreads-based
    implementation-specific rwlock functions always return 0; these
    functions continue to handle errors using less verbose run-time
    assertions as they do not set errno anyway.

--- lib/isc/include/isc/rwlock.h
+++ lib/isc/include/isc/rwlock.h
@@ -50,11 +50,11 @@ typedef struct isc_rwlock  isc__rwlock_t;
 		*rwl = malloc(sizeof(**rwl));   \
 		isc__rwlock_init(*rwl, rq, wq); \
 	}
-#define isc_rwlock_lock(rwl, type)    isc___rwlock_lock(*rwl, type)
+#define isc_rwlock_lock(rwl, type)    isc__rwlock_lock(*rwl, type)
 #define isc_rwlock_trylock(rwl, type) isc___rwlock_trylock(*rwl, type)
-#define isc_rwlock_unlock(rwl, type)  isc___rwlock_unlock(*rwl, type)
+#define isc_rwlock_unlock(rwl, type)  isc__rwlock_unlock(*rwl, type)
 #define isc_rwlock_tryupgrade(rwl)    isc___rwlock_tryupgrade(*rwl)
-#define isc_rwlock_downgrade(rwl)     isc___rwlock_downgrade(*rwl)
+#define isc_rwlock_downgrade(rwl)     isc__rwlock_downgrade(*rwl)
 #define isc_rwlock_destroy(rwl)             \
 	{                                   \
 		isc___rwlock_destroy(*rwl); \
@@ -67,12 +67,12 @@ typedef struct isc_rwlock isc_rwlock_t;
 typedef struct isc_rwlock isc__rwlock_t;
 
 #define isc_rwlock_init(rwl, rq, wq)  isc__rwlock_init(rwl, rq, wq)
-#define isc_rwlock_lock(rwl, type)    isc___rwlock_lock(rwl, type)
+#define isc_rwlock_lock(rwl, type)    isc__rwlock_lock(rwl, type)
 #define isc_rwlock_trylock(rwl, type) isc___rwlock_trylock(rwl, type)
-#define isc_rwlock_unlock(rwl, type)  isc___rwlock_unlock(rwl, type)
+#define isc_rwlock_unlock(rwl, type)  isc__rwlock_unlock(rwl, type)
 #define isc_rwlock_tryupgrade(rwl)    isc___rwlock_tryupgrade(rwl)
-#define isc_rwlock_downgrade(rwl)     isc___rwlock_downgrade(rwl)
-#define isc_rwlock_destroy(rwl)	      isc___rwlock_destroy(rwl)
+#define isc_rwlock_downgrade(rwl)     isc__rwlock_downgrade(rwl)
+#define isc_rwlock_destroy(rwl)	      isc__rwlock_destroy(rwl)
 
 #endif /* ISC_TRACK_PTHREADS_OBJECTS */
 
@@ -118,12 +118,12 @@ typedef struct isc_rwlock isc_rwlock_t;
 typedef struct isc_rwlock isc__rwlock_t;
 
 #define isc_rwlock_init(rwl, rq, wq)  isc__rwlock_init(rwl, rq, wq)
-#define isc_rwlock_lock(rwl, type)    isc___rwlock_lock(rwl, type)
+#define isc_rwlock_lock(rwl, type)    isc__rwlock_lock(rwl, type)
 #define isc_rwlock_trylock(rwl, type) isc___rwlock_trylock(rwl, type)
-#define isc_rwlock_unlock(rwl, type)  isc___rwlock_unlock(rwl, type)
+#define isc_rwlock_unlock(rwl, type)  isc__rwlock_unlock(rwl, type)
 #define isc_rwlock_tryupgrade(rwl)    isc___rwlock_tryupgrade(rwl)
-#define isc_rwlock_downgrade(rwl)     isc___rwlock_downgrade(rwl)
-#define isc_rwlock_destroy(rwl)	      isc___rwlock_destroy(rwl)
+#define isc_rwlock_downgrade(rwl)     isc__rwlock_downgrade(rwl)
+#define isc_rwlock_destroy(rwl)	      isc__rwlock_destroy(rwl)
 
 #endif /* USE_PTHREAD_RWLOCK */
 
@@ -133,26 +133,50 @@ typedef struct isc_rwlock isc__rwlock_t;
 		ERRNO_CHECK(isc___rwlock_init, _ret);      \
 	}
 
+#define isc__rwlock_lock(rwl, type)                      \
+	{                                                \
+		int _ret = isc___rwlock_lock(rwl, type); \
+		ERRNO_CHECK(isc___rwlock_lock, _ret);    \
+	}
+
+#define isc__rwlock_unlock(rwl, type)                      \
+	{                                                  \
+		int _ret = isc___rwlock_unlock(rwl, type); \
+		ERRNO_CHECK(isc___rwlock_unlock, _ret);    \
+	}
+
+#define isc__rwlock_downgrade(rwl)                         \
+	{                                                  \
+		int _ret = isc___rwlock_downgrade(rwl);    \
+		ERRNO_CHECK(isc___rwlock_downgrade, _ret); \
+	}
+
+#define isc__rwlock_destroy(rwl)                         \
+	{                                                \
+		int _ret = isc___rwlock_destroy(rwl);    \
+		ERRNO_CHECK(isc___rwlock_destroy, _ret); \
+	}
+
 int
 isc___rwlock_init(isc__rwlock_t *rwl, unsigned int read_quota,
 		  unsigned int write_quota);
 
-void
+int
 isc___rwlock_lock(isc__rwlock_t *rwl, isc_rwlocktype_t type);
 
 isc_result_t
 isc___rwlock_trylock(isc__rwlock_t *rwl, isc_rwlocktype_t type);
 
-void
+int
 isc___rwlock_unlock(isc__rwlock_t *rwl, isc_rwlocktype_t type);
 
 isc_result_t
 isc___rwlock_tryupgrade(isc__rwlock_t *rwl);
 
-void
+int
 isc___rwlock_downgrade(isc__rwlock_t *rwl);
 
-void
+int
 isc___rwlock_destroy(isc__rwlock_t *rwl);
 
 ISC_LANG_ENDDECLS
