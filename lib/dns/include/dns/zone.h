39c0c5022d144c160226d192fd37f14a21d727ed
Matthijs Mekking <matthijs@isc.org>
2022-08-10 15:29:59 +0200

    Wait with NSEC3 during a DNSSEC policy change
    When doing a dnssec-policy reconfiguration from a zone with NSEC only
    keys to a zone that uses NSEC3, figure out to wait with building the
    NSEC3 chain.
    
    Previously, BIND 9 would attempt to sign such a zone, but failed to
    do so because the NSEC3 chain conflicted with existing DNSKEY records
    in the zone that were not compatible with NSEC3.
    
    There exists logic for detecting such a case in the functions
    dnskey_sane() (in lib/dns/zone.c) and check_dnssec() (in
    lib/ns/update.c). Both functions look very similar so refactor them
    to use the same code and call the new function (called
    dns_zone_check_dnskey_nsec3()).
    
    Also update the dns_nsec_nseconly() function to take an additional
    parameter 'diff' that, if provided, will be checked whether an
    offending NSEC only DNSKEY will be deleted from the zone. If so,
    this key will not be considered when checking the zone for NSEC only
    DNSKEYs. This is needed to allow a transition from an NSEC zone with
    NSEC only DNSKEYs to an NSEC3 zone.
    
    (cherry picked from commit 09a81dc84ce0fee37442f03cdbd63c2398215376)

--- lib/dns/include/dns/zone.h
+++ lib/dns/include/dns/zone.h
@@ -29,6 +29,7 @@
 #include <isc/tls.h>
 
 #include <dns/catz.h>
+#include <dns/diff.h>
 #include <dns/master.h>
 #include <dns/masterdump.h>
 #include <dns/rdatastruct.h>
@@ -2763,4 +2764,21 @@ const char *
 dns_zonetype_name(dns_zonetype_t type);
 /*%<
  * Return the name of the zone type 'type'.
+ */
+
+bool
+dns_zone_check_dnskey_nsec3(dns_zone_t *zone, dns_db_t *db,
+			    dns_dbversion_t *ver, dns_diff_t *diff,
+			    dst_key_t **keys, unsigned int numkeys);
+/**<
+ * Return whether the zone would enter an inconsistent state where NSEC only
+ * DNSKEYs are present along NSEC3 chains.
+ *
+ * Requires:
+ * \li	'zone' to be a valid zone.
+ * \li	'db'is not NULL.
+ *
+ * Returns:
+ * \li	'true' if the check passes, that is the zone remains consistent,
+ *	'false' if the zone would have NSEC only DNSKEYs and an NSEC3 chain.
  */
