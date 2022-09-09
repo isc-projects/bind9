isc-projects/bind9

commit 3608abc8fa6a33046e1d34a0789cf7c9547f09ad

Add an ERRNO_CHECK() preprocessor macro
In a number of situations in pthreads-related code, a common sequence of
steps is taken: if the value returned by a library function is not 0,
pass errno to strerror_r(), log the string returned by the latter, and
immediately abort execution.  Add an ERRNO_CHECK() preprocessor macro
which takes those exact steps and use it wherever (conveniently)
possible.

Notes:

 1. The "log the return value of strerror_r() and abort" pattern is used
    in a number of other places that this commit does not touch; only
    "!= 0" checks followed by isc_error_fatal() calls with
    non-customized error messages are replaced here.

 2. This change temporarily breaks file name & line number reporting for
    isc__mutex_init() errors, to prevent breaking the build.  This issue
    will be rectified in a subsequent change.

lib/isc/include/isc/condition.h
@@ -21,20 +21,16 @@
 #include <isc/lang.h>
 #include <isc/mutex.h>
 #include <isc/result.h>
-#include <isc/strerr.h>
 #include <isc/string.h>
 #include <isc/types.h>
+#include <isc/util.h>
 
 typedef pthread_cond_t isc_condition_t;
 
-#define isc_condition_init(cond)                                \
-	if (pthread_cond_init(cond, NULL) != 0) {               \
-		char isc_condition_strbuf[ISC_STRERRORSIZE];    \
-		strerror_r(errno, isc_condition_strbuf,         \
-			   sizeof(isc_condition_strbuf));       \
-		isc_error_fatal(__FILE__, __LINE__,             \
-				"pthread_cond_init failed: %s", \
-				isc_condition_strbuf);          \
+#define isc_condition_init(cond)                          \
+	{                                                 \
+		int _ret = pthread_cond_init(cond, NULL); \
+		ERRNO_CHECK(pthread_cond_init, _ret);     \
 	}
 
 #define isc_condition_wait(cp, mp)                            \

lib/isc/include/isc/mutex.h
@@ -26,9 +26,9 @@ ISC_LANG_BEGINDECLS
 typedef pthread_mutex_t isc_mutex_t;
 
 void
-isc__mutex_init(isc_mutex_t *mp, const char *file, unsigned int line);
+isc__mutex_init(isc_mutex_t *mp);
 
-#define isc_mutex_init(mp) isc__mutex_init((mp), __FILE__, __LINE__)
+#define isc_mutex_init(mp) isc__mutex_init((mp))
 
 #define isc_mutex_lock(mp) \
 	((pthread_mutex_lock((mp)) == 0) ? ISC_R_SUCCESS : ISC_R_UNEXPECTED)

lib/isc/include/isc/util.h
@@ -309,7 +309,10 @@ mock_assert(const int result, const char *const expression,
 /*
  * Errors
  */
-#include <isc/error.h> /* Contractual promise. */
+#include <errno.h> /* for errno */
+
+#include <isc/error.h>	/* Contractual promise. */
+#include <isc/strerr.h> /* for ISC_STRERRORSIZE */
 
 /*% Unexpected Error */
 #define UNEXPECTED_ERROR isc_error_unexpected
@@ -329,6 +332,15 @@ mock_assert(const int result, const char *const expression,
 #define RUNTIME_CHECK(cond) ISC_ERROR_RUNTIMECHECK(cond)
 
 #endif /* UNIT_TESTING */
+
+/*% Runtime check which logs the error string corresponding to errno */
+#define ERRNO_CHECK(func, ret)                                                 \
+	if ((ret) != 0) {                                                      \
+		char _strerrorbuf[ISC_STRERRORSIZE];                           \
+		strerror_r(errno, _strerrorbuf, sizeof(_strerrorbuf));         \
+		isc_error_fatal(__FILE__, __LINE__, "%s() failed in %s(): %s", \
+				#func, __func__, _strerrorbuf);                \
+	}
 
 /*%
  * Time

lib/isc/thread.c
@@ -38,13 +38,6 @@
 #define THREAD_MINSTACKSIZE (1024U * 1024)
 #endif /* ifndef THREAD_MINSTACKSIZE */
 
-#define _FATAL(r, f)                                                          \
-	{                                                                     \
-		char strbuf[ISC_STRERRORSIZE];                                \
-		strerror_r(r, strbuf, sizeof(strbuf));                        \
-		isc_error_fatal(__FILE__, __LINE__, f " failed: %s", strbuf); \
-	}
-
 void
 isc_thread_create(isc_threadfunc_t func, isc_threadarg_t arg,
 		  isc_thread_t *thread) {
@@ -65,24 +58,18 @@ isc_thread_create(isc_threadfunc_t func, isc_threadarg_t arg,
 #if defined(HAVE_PTHREAD_ATTR_GETSTACKSIZE) && \
 	defined(HAVE_PTHREAD_ATTR_SETSTACKSIZE)
 	ret = pthread_attr_getstacksize(&attr, &stacksize);
-	if (ret != 0) {
-		_FATAL(ret, "pthread_attr_getstacksize()");
-	}
+	ERRNO_CHECK(pthread_attr_getstacksize, ret);
 
 	if (stacksize < THREAD_MINSTACKSIZE) {
 		ret = pthread_attr_setstacksize(&attr, THREAD_MINSTACKSIZE);
-		if (ret != 0) {
-			_FATAL(ret, "pthread_attr_setstacksize()");
-		}
+		ERRNO_CHECK(pthread_attr_setstacksize, ret);
 	}
 #endif /* if defined(HAVE_PTHREAD_ATTR_GETSTACKSIZE) && \
 	* defined(HAVE_PTHREAD_ATTR_SETSTACKSIZE) */
 
 	ret = pthread_create(thread, &attr, isc__trampoline_run,
 			     trampoline_arg);
-	if (ret != 0) {
-		_FATAL(ret, "pthread_create()");
-	}
+	ERRNO_CHECK(pthread_create, ret);
 
 	pthread_attr_destroy(&attr);
 
@@ -92,9 +79,8 @@ isc_thread_create(isc_threadfunc_t func, isc_threadarg_t arg,
 void
 isc_thread_join(isc_thread_t thread, isc_threadresult_t *result) {
 	int ret = pthread_join(thread, result);
-	if (ret != 0) {
-		_FATAL(ret, "pthread_join()");
-	}
+
+	ERRNO_CHECK(pthread_join, ret);
 }
 
 void

lib/isc/mutex.c
@@ -41,7 +41,7 @@ initialize_attr(void) {
 #endif /* HAVE_PTHREAD_MUTEX_ADAPTIVE_NP */
 
 void
-isc__mutex_init(isc_mutex_t *mp, const char *file, unsigned int line) {
+isc__mutex_init(isc_mutex_t *mp) {
 	int err;
 
 #ifdef HAVE_PTHREAD_MUTEX_ADAPTIVE_NP
@@ -53,10 +53,5 @@ isc__mutex_init(isc_mutex_t *mp, const char *file, unsigned int line) {
 #else  /* HAVE_PTHREAD_MUTEX_ADAPTIVE_NP */
 	err = pthread_mutex_init(mp, NULL);
 #endif /* HAVE_PTHREAD_MUTEX_ADAPTIVE_NP */
-	if (err != 0) {
-		char strbuf[ISC_STRERRORSIZE];
-		strerror_r(err, strbuf, sizeof(strbuf));
-		isc_error_fatal(file, line, "pthread_mutex_init failed: %s",
-				strbuf);
-	}
+	ERRNO_CHECK(pthread_mutex_init, err);
 }

commit 3b1c80fd0fe1d45d230c1f9a9d5602c2e21f0e0b

Fix error reporting for POSIX Threads functions
Commit 3608abc8fa6a33046e1d34a0789cf7c9547f09ad inadvertently carried
over a mistake in logging pthread_cond_init() errors to the
ERRNO_CHECK() preprocessor macro: instead of passing the value returned
by a given pthread_*() function to strerror_r(), ERRNO_CHECK() passes
the errno variable to strerror_r().  This causes bogus error reports
because POSIX Threads API functions do not set the errno variable.

Fix by passing the value returned by a given pthread_*() function
instead of the errno variable to strerror_r().  Since this change makes
the name of the affected macro (ERRNO_CHECK()) confusing, rename the
latter to PTHREADS_RUNTIME_CHECK().  Also log the integer error value
returned by a given pthread_*() function verbatim to rule out any
further confusion in runtime error reporting.

lib/isc/include/isc/condition.h
@@ -60,34 +60,34 @@ typedef pthread_cond_t isc_condition_t;
 
 #endif /* ISC_TRACK_PTHREADS_OBJECTS */
 
-#define isc__condition_init(cond)                         \
-	{                                                 \
-		int _ret = pthread_cond_init(cond, NULL); \
-		ERRNO_CHECK(pthread_cond_init, _ret);     \
+#define isc__condition_init(cond)                                \
+	{                                                        \
+		int _ret = pthread_cond_init(cond, NULL);        \
+		PTHREADS_RUNTIME_CHECK(pthread_cond_init, _ret); \
 	}
 
-#define isc__condition_wait(cp, mp)                   \
-	{                                             \
-		int _ret = pthread_cond_wait(cp, mp); \
-		ERRNO_CHECK(pthread_cond_wait, _ret); \
+#define isc__condition_wait(cp, mp)                              \
+	{                                                        \
+		int _ret = pthread_cond_wait(cp, mp);            \
+		PTHREADS_RUNTIME_CHECK(pthread_cond_wait, _ret); \
 	}
 
-#define isc__condition_signal(cp)                       \
-	{                                               \
-		int _ret = pthread_cond_signal(cp);     \
-		ERRNO_CHECK(pthread_cond_signal, _ret); \
+#define isc__condition_signal(cp)                                  \
+	{                                                          \
+		int _ret = pthread_cond_signal(cp);                \
+		PTHREADS_RUNTIME_CHECK(pthread_cond_signal, _ret); \
 	}
 
-#define isc__condition_broadcast(cp)                       \
-	{                                                  \
-		int _ret = pthread_cond_broadcast(cp);     \
-		ERRNO_CHECK(pthread_cond_broadcast, _ret); \
+#define isc__condition_broadcast(cp)                                  \
+	{                                                             \
+		int _ret = pthread_cond_broadcast(cp);                \
+		PTHREADS_RUNTIME_CHECK(pthread_cond_broadcast, _ret); \
 	}
 
-#define isc__condition_destroy(cp)                       \
-	{                                                \
-		int _ret = pthread_cond_destroy(cp);     \
-		ERRNO_CHECK(pthread_cond_destroy, _ret); \
+#define isc__condition_destroy(cp)                                  \
+	{                                                           \
+		int _ret = pthread_cond_destroy(cp);                \
+		PTHREADS_RUNTIME_CHECK(pthread_cond_destroy, _ret); \
 	}
 
 isc_result_t

lib/isc/include/isc/mutex.h
@@ -60,28 +60,28 @@ extern pthread_mutexattr_t isc__mutex_init_attr;
 #define isc__mutex_init(mp)                                               \
 	{                                                                 \
 		int _ret = pthread_mutex_init(mp, &isc__mutex_init_attr); \
-		ERRNO_CHECK(pthread_mutex_init, _ret);                    \
+		PTHREADS_RUNTIME_CHECK(pthread_mutex_init, _ret);         \
 	}
 
-#define isc__mutex_lock(mp)                            \
-	{                                              \
-		int _ret = pthread_mutex_lock(mp);     \
-		ERRNO_CHECK(pthread_mutex_lock, _ret); \
+#define isc__mutex_lock(mp)                                       \
+	{                                                         \
+		int _ret = pthread_mutex_lock(mp);                \
+		PTHREADS_RUNTIME_CHECK(pthread_mutex_lock, _ret); \
 	}
 
-#define isc__mutex_unlock(mp)                            \
-	{                                                \
-		int _ret = pthread_mutex_unlock(mp);     \
-		ERRNO_CHECK(pthread_mutex_unlock, _ret); \
+#define isc__mutex_unlock(mp)                                       \
+	{                                                           \
+		int _ret = pthread_mutex_unlock(mp);                \
+		PTHREADS_RUNTIME_CHECK(pthread_mutex_unlock, _ret); \
 	}
 
 #define isc__mutex_trylock(mp) \
 	((pthread_mutex_trylock(mp) == 0) ? ISC_R_SUCCESS : ISC_R_LOCKBUSY)
 
-#define isc__mutex_destroy(mp)                            \
-	{                                                 \
-		int _ret = pthread_mutex_destroy(mp);     \
-		ERRNO_CHECK(pthread_mutex_destroy, _ret); \
+#define isc__mutex_destroy(mp)                                       \
+	{                                                            \
+		int _ret = pthread_mutex_destroy(mp);                \
+		PTHREADS_RUNTIME_CHECK(pthread_mutex_destroy, _ret); \
 	}
 
 ISC_LANG_ENDDECLS

lib/isc/include/isc/rwlock.h
@@ -127,34 +127,34 @@ typedef struct isc_rwlock isc__rwlock_t;
 
 #endif /* USE_PTHREAD_RWLOCK */
 
-#define isc__rwlock_init(rwl, rq, wq)                      \
-	{                                                  \
-		int _ret = isc___rwlock_init(rwl, rq, wq); \
-		ERRNO_CHECK(isc___rwlock_init, _ret);      \
+#define isc__rwlock_init(rwl, rq, wq)                            \
+	{                                                        \
+		int _ret = isc___rwlock_init(rwl, rq, wq);       \
+		PTHREADS_RUNTIME_CHECK(isc___rwlock_init, _ret); \
 	}
 
-#define isc__rwlock_lock(rwl, type)                      \
-	{                                                \
-		int _ret = isc___rwlock_lock(rwl, type); \
-		ERRNO_CHECK(isc___rwlock_lock, _ret);    \
+#define isc__rwlock_lock(rwl, type)                              \
+	{                                                        \
+		int _ret = isc___rwlock_lock(rwl, type);         \
+		PTHREADS_RUNTIME_CHECK(isc___rwlock_lock, _ret); \
 	}
 
-#define isc__rwlock_unlock(rwl, type)                      \
-	{                                                  \
-		int _ret = isc___rwlock_unlock(rwl, type); \
-		ERRNO_CHECK(isc___rwlock_unlock, _ret);    \
+#define isc__rwlock_unlock(rwl, type)                              \
+	{                                                          \
+		int _ret = isc___rwlock_unlock(rwl, type);         \
+		PTHREADS_RUNTIME_CHECK(isc___rwlock_unlock, _ret); \
 	}
 
-#define isc__rwlock_downgrade(rwl)                         \
-	{                                                  \
-		int _ret = isc___rwlock_downgrade(rwl);    \
-		ERRNO_CHECK(isc___rwlock_downgrade, _ret); \
+#define isc__rwlock_downgrade(rwl)                                    \
+	{                                                             \
+		int _ret = isc___rwlock_downgrade(rwl);               \
+		PTHREADS_RUNTIME_CHECK(isc___rwlock_downgrade, _ret); \
 	}
 
-#define isc__rwlock_destroy(rwl)                         \
-	{                                                \
-		int _ret = isc___rwlock_destroy(rwl);    \
-		ERRNO_CHECK(isc___rwlock_destroy, _ret); \
+#define isc__rwlock_destroy(rwl)                                    \
+	{                                                           \
+		int _ret = isc___rwlock_destroy(rwl);               \
+		PTHREADS_RUNTIME_CHECK(isc___rwlock_destroy, _ret); \
 	}
 
 int

lib/isc/include/isc/util.h
@@ -334,13 +334,17 @@ mock_assert(const int result, const char *const expression,
 
 #endif /* UNIT_TESTING */
 
-/*% Runtime check which logs the error string corresponding to errno */
-#define ERRNO_CHECK(func, ret)                                                 \
-	if ((ret) != 0) {                                                      \
-		char _strerrorbuf[ISC_STRERRORSIZE];                           \
-		strerror_r(errno, _strerrorbuf, sizeof(_strerrorbuf));         \
-		isc_error_fatal(__FILE__, __LINE__, "%s() failed in %s(): %s", \
-				#func, __func__, _strerrorbuf);                \
+/*%
+ * Runtime check which logs the error value returned by a POSIX Threads
+ * function and the error string that corresponds to it
+ */
+#define PTHREADS_RUNTIME_CHECK(func, ret)                               \
+	if ((ret) != 0) {                                               \
+		char _strerrorbuf[ISC_STRERRORSIZE];                    \
+		strerror_r(ret, _strerrorbuf, sizeof(_strerrorbuf));    \
+		isc_error_fatal(__FILE__, __LINE__,                     \
+				"%s(): %s() failed with error %d (%s)", \
+				__func__, #func, ret, _strerrorbuf);    \
 	}
 
 /*%

lib/isc/thread.c
@@ -58,18 +58,18 @@ isc_thread_create(isc_threadfunc_t func, isc_threadarg_t arg,
 #if defined(HAVE_PTHREAD_ATTR_GETSTACKSIZE) && \
 	defined(HAVE_PTHREAD_ATTR_SETSTACKSIZE)
 	ret = pthread_attr_getstacksize(&attr, &stacksize);
-	ERRNO_CHECK(pthread_attr_getstacksize, ret);
+	PTHREADS_RUNTIME_CHECK(pthread_attr_getstacksize, ret);
 
 	if (stacksize < THREAD_MINSTACKSIZE) {
 		ret = pthread_attr_setstacksize(&attr, THREAD_MINSTACKSIZE);
-		ERRNO_CHECK(pthread_attr_setstacksize, ret);
+		PTHREADS_RUNTIME_CHECK(pthread_attr_setstacksize, ret);
 	}
 #endif /* if defined(HAVE_PTHREAD_ATTR_GETSTACKSIZE) && \
 	* defined(HAVE_PTHREAD_ATTR_SETSTACKSIZE) */
 
 	ret = pthread_create(thread, &attr, isc__trampoline_run,
 			     trampoline_arg);
-	ERRNO_CHECK(pthread_create, ret);
+	PTHREADS_RUNTIME_CHECK(pthread_create, ret);
 
 	pthread_attr_destroy(&attr);
 
@@ -80,7 +80,7 @@ void
 isc_thread_join(isc_thread_t thread, isc_threadresult_t *result) {
 	int ret = pthread_join(thread, result);
 
-	ERRNO_CHECK(pthread_join, ret);
+	PTHREADS_RUNTIME_CHECK(pthread_join, ret);
 }
 
 void