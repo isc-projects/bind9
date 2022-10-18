cb0a2ae1dd9f36c7dfb909d06453cd2beba595ea
Tom Krizek <tkrizek@isc.org>
2022-10-18 17:16:27 +0200

    Revive dupsigs system test
    Correctly source conf.sh in dupsigs test scripts (fix issue introduced
    by 093af1c00ac25e4f132fe2442a24e1264aadb28d).
    
    Update dupsigs test for dnssec-dnskey-kskonly default. Since v9.17.20,
    the dnssec-dnskey-kskonly is set to yes. Update the test to not expect
    the additional RRSIG with ZSK for DNSKEY.
    
    Speed up the test from 20 minutes to 2.5 minutes and make it part of the
    default test suite executed in CI.
    - decrease number of records to sign from 2000 to 500
    - decrease the signing interval by a factor of 6
    - shorten the final part of the test after last signing (since nothing
      new happens there)
    
    Finally, clarify misleading comments about (in)sufficient time for zone
    re-signing. The time used in the test is in fact sufficient for the
    re-signing to happen. If it wasn't, the previous ZSK would end up being
    deleted while its signatures would still be present, which is a
    situation where duplicate signatures can still happen.

--- bin/tests/system/dupsigs/tests.sh
+++ bin/tests/system/dupsigs/tests.sh
@@ -13,8 +13,9 @@
 
 status=0
 start=`date +%s`
-end=`expr $start + 1200`
-now=$start
+end=`expr $start + 150`
+sleep 10  # wait for a bit for the initial signing
+now=`expr $start + 10`
 while test $now -lt $end
 do
 	et=`expr $now - $start`
@@ -23,12 +24,12 @@ do
 	$DIG axfr signing.test -p ${PORT} @10.53.0.1 > dig.out.at$et
 	awk '$4 == "RRSIG" { print $11 }' dig.out.at$et | sort | uniq -c
 	lines=`awk '$4 == "RRSIG" { print}' dig.out.at$et | wc -l`
-	if [ ${et} -ne 0 -a ${lines} -ne 4009 ]
+	if [ ${et} -ne 0 -a ${lines} -ne 1008 ]
 	then
 		echo_i "failed"
 		status=`expr $status + 1`
 	fi
-	sleep 20
+	sleep 5
 	now=`date +%s`
 done
 
