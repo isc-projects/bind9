82991451b41793af201d070aba654c4ea89819cb
Aram Sargsyan <aram@isc.org>
2022-10-18 08:54:04 +0000

    Fix ns_statscounter_recursclients counting bug
    The incrementing and decrementing of 'ns_statscounter_recursclients'
    were not properly balanced: for example, it would be incremented for
    a prefetch query but not decremented if the query failed.
    
    This commit ensures that the recursion quota and the recursive clients
    counter are always in sync with each other.

--- lib/ns/query.c
+++ lib/ns/query.c
@@ -2543,10 +2543,9 @@ prefetch_done(isc_task_t *task, isc_event_t *event) {
 	 */
 	if (client->recursionquota != NULL) {
 		isc_quota_detach(&client->recursionquota);
+		ns_stats_decrement(client->sctx->nsstats,
+				   ns_statscounter_recursclients);
 	}
-
-	ns_stats_decrement(client->sctx->nsstats,
-			   ns_statscounter_recursclients);
 
 	free_devent(client, &event, &devent);
 	isc_nmhandle_detach(&client->prefetchhandle);
@@ -2575,6 +2574,8 @@ query_prefetch(ns_client_t *client, dns_name_t *qname,
 					  &client->recursionquota);
 		switch (result) {
 		case ISC_R_SUCCESS:
+			ns_stats_increment(client->sctx->nsstats,
+					   ns_statscounter_recursclients);
 			break;
 		case ISC_R_SOFTQUOTA:
 			isc_quota_detach(&client->recursionquota);
@@ -2583,9 +2584,6 @@ query_prefetch(ns_client_t *client, dns_name_t *qname,
 			return;
 		}
 	}
-
-	ns_stats_increment(client->sctx->nsstats,
-			   ns_statscounter_recursclients);
 
 	tmprdataset = ns_client_newrdataset(client);
 	if (tmprdataset == NULL) {
@@ -2793,6 +2791,8 @@ query_rpzfetch(ns_client_t *client, dns_name_t *qname, dns_rdatatype_t type) {
 					  &client->recursionquota);
 		switch (result) {
 		case ISC_R_SUCCESS:
+			ns_stats_increment(client->sctx->nsstats,
+					   ns_statscounter_recursclients);
 			break;
 		case ISC_R_SOFTQUOTA:
 			isc_quota_detach(&client->recursionquota);
@@ -2801,9 +2801,6 @@ query_rpzfetch(ns_client_t *client, dns_name_t *qname, dns_rdatatype_t type) {
 			return;
 		}
 	}
-
-	ns_stats_increment(client->sctx->nsstats,
-			   ns_statscounter_recursclients);
 
 	tmprdataset = ns_client_newrdataset(client);
 	if (tmprdataset == NULL) {
@@ -6252,10 +6249,9 @@ fetch_callback(isc_task_t *task, isc_event_t *event) {
 
 	if (client->recursionquota != NULL) {
 		isc_quota_detach(&client->recursionquota);
+		ns_stats_decrement(client->sctx->nsstats,
+				   ns_statscounter_recursclients);
 	}
-
-	ns_stats_decrement(client->sctx->nsstats,
-			   ns_statscounter_recursclients);
 
 	LOCK(&client->manager->reclock);
 	if (ISC_LINK_LINKED(client, rlink)) {
@@ -6390,6 +6386,11 @@ check_recursionquota(ns_client_t *client) {
 	if (client->recursionquota == NULL) {
 		result = isc_quota_attach(&client->sctx->recursionquota,
 					  &client->recursionquota);
+		if (result == ISC_R_SUCCESS || result == ISC_R_SOFTQUOTA) {
+			ns_stats_increment(client->sctx->nsstats,
+					   ns_statscounter_recursclients);
+		}
+
 		if (result == ISC_R_SOFTQUOTA) {
 			isc_stdtime_t now;
 			isc_stdtime_get(&now);
@@ -6471,9 +6472,6 @@ ns_query_recurse(ns_client_t *client, dns_rdatatype_t qtype, dns_name_t *qname,
 	if (result != ISC_R_SUCCESS) {
 		return (result);
 	}
-
-	ns_stats_increment(client->sctx->nsstats,
-			   ns_statscounter_recursclients);
 
 	/*
 	 * Invoke the resolver.
@@ -6765,10 +6763,9 @@ query_hookresume(isc_task_t *task, isc_event_t *event) {
 
 	if (client->recursionquota != NULL) {
 		isc_quota_detach(&client->recursionquota);
+		ns_stats_decrement(client->sctx->nsstats,
+				   ns_statscounter_recursclients);
 	}
-
-	ns_stats_decrement(client->sctx->nsstats,
-			   ns_statscounter_recursclients);
 
 	LOCK(&client->manager->reclock);
 	if (ISC_LINK_LINKED(client, rlink)) {
@@ -6904,9 +6901,6 @@ ns_query_hookasync(query_ctx_t *qctx, ns_query_starthookasync_t runasync,
 	if (result != ISC_R_SUCCESS) {
 		goto cleanup;
 	}
-
-	ns_stats_increment(client->sctx->nsstats,
-			   ns_statscounter_recursclients);
 
 	saved_qctx = isc_mem_get(client->mctx, sizeof(*saved_qctx));
 	qctx_save(qctx, saved_qctx);
