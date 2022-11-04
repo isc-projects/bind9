303cdf8e2762781da71f24e7bfdeb4280e6bd1b4
Tony Finch <fanf@isc.org>
2022-11-04 12:02:33 +0000

    Deduplicate time unit conversion factors
    The various factors like NS_PER_MS are now defined in a single place
    and the names are no longer inconsistent. I chose the _PER_SEC names
    rather than _PER_S because it is slightly more clear in isolation;
    but the smaller units are always NS, US, and MS.
    
    (cherry picked from commit 00307fe318acaf04f4891cc1b99993af4c7a708b)

--- lib/isc/include/isc/time.h
+++ lib/isc/include/isc/time.h
@@ -21,6 +21,15 @@
 #include <isc/lang.h>
 #include <isc/types.h>
 
+enum {
+	MS_PER_SEC = 1000,		 /*%< Milliseonds per second. */
+	US_PER_MS = 1000,		 /*%< Microseconds per millisecond. */
+	US_PER_SEC = 1000 * 1000,	 /*%< Microseconds per second. */
+	NS_PER_US = 1000,		 /*%< Nanoseconds per millisecond. */
+	NS_PER_MS = 1000 * 1000,	 /*%< Nanoseconds per microsecond. */
+	NS_PER_SEC = 1000 * 1000 * 1000, /*%< Nanoseconds per second. */
+};
+
 /***
  *** Intervals
  ***/
