a92b9e40ce2b48352303ba2ced796c9a04b36dd1
Matthijs Mekking <matthijs@isc.org>
2022-11-22 16:30:19 +0100

    Small comment change in remote.h header file
    The documentation for 'dns_remote_addr()' was incorrect (copy paste
    error).

--- lib/dns/include/dns/remote.h
+++ lib/dns/include/dns/remote.h
@@ -170,8 +170,7 @@ dns_remote_sourceaddr(dns_remote_t *remote);
 isc_sockaddr_t
 dns_remote_addr(dns_remote_t *remote, unsigned int i);
 /*%<
- *	Return the address at index 'i'. Returns NULL if we i is equal
- *	or larger than the address count.
+ *	Return the address at index 'i'.
  *
  *	Requires:
  *		'remote' is a valid remote structure.
