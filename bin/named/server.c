2d5160a9b9146dc94bf51911b62a9cfdf0ce704d
Tony Finch <fanf@isc.org>
2022-12-14 15:47:03 +0000

    A couple of RPZ options were not reconfigured as expected
    [bug]	Changes to the RPZ response-policy min-update-interval
    	and add-soa options now take effect as expected when
    	named is reconfigured. [GL #3740]
    
    (cherry picked from commit d8a3d328db1fb530b6f29c03291e80252251a2e1)

--- bin/named/server.c
+++ bin/named/server.c
@@ -2330,6 +2330,9 @@ configure_rpz_zone(dns_view_t *view, const cfg_listelt_t *element,
 	} else {
 		zone->max_policy_ttl = ttl_default;
 	}
+	if (*old_rpz_okp && zone->max_policy_ttl != old->max_policy_ttl) {
+		*old_rpz_okp = false;
+	}
 
 	obj = cfg_tuple_get(rpz_obj, "min-update-interval");
 	if (cfg_obj_isduration(obj)) {
@@ -2337,8 +2340,9 @@ configure_rpz_zone(dns_view_t *view, const cfg_listelt_t *element,
 	} else {
 		zone->min_update_interval = minupdateinterval_default;
 	}
-
-	if (*old_rpz_okp && zone->max_policy_ttl != old->max_policy_ttl) {
+	if (*old_rpz_okp &&
+	    zone->min_update_interval != old->min_update_interval)
+	{
 		*old_rpz_okp = false;
 	}
 
@@ -2440,6 +2444,9 @@ configure_rpz_zone(dns_view_t *view, const cfg_listelt_t *element,
 		zone->addsoa = add_soa_default;
 	} else {
 		zone->addsoa = cfg_obj_asboolean(obj);
+	}
+	if (*old_rpz_okp && zone->addsoa != old->addsoa) {
+		*old_rpz_okp = false;
 	}
 
 	return (ISC_R_SUCCESS);
