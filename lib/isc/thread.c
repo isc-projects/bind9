555690a3c97da03f75778d90156c4a5a5d38a518
Tony Finch <fanf@isc.org>
2023-01-02 19:56:27 +0000

    Simplify thread spawning
    The `isc_trampoline` module had a lot of machinery to support stable
    thread IDs for use by hazard pointers. But the hazard pointer code
    is gone, and the `isc_loop` module now has its own per-loop thread
    IDs.
    
    The trampoline machinery seems over-complicated for its remaining
    tasks, so move the per-thread initialization into `isc/thread.c`,
    and delete the rest.

--- lib/isc/thread.c
+++ lib/isc/thread.c
@@ -28,23 +28,72 @@
 #include <sys/types.h>
 #endif /* if defined(HAVE_SYS_PROCSET_H) */
 
+#include <stdlib.h>
+
+#include <isc/iterated_hash.h>
 #include <isc/strerr.h>
 #include <isc/thread.h>
 #include <isc/util.h>
 
-#include "trampoline_p.h"
+#include "random_p.h"
 
 #ifndef THREAD_MINSTACKSIZE
 #define THREAD_MINSTACKSIZE (1024U * 1024)
 #endif /* ifndef THREAD_MINSTACKSIZE */
 
+/*
+ * We can't use isc_mem API here, because it's called too early and the
+ * isc_mem_debugging flags can be changed later causing mismatch between flags
+ * used for isc_mem_get() and isc_mem_put().
+ */
+
+struct thread_wrap {
+	isc_threadfunc_t func;
+	isc_threadarg_t arg;
+	isc_threadresult_t result;
+	void *jemalloc_enforce_init;
+};
+
+static isc_threadresult_t
+thread_run(isc_threadarg_t arg) {
+	struct thread_wrap *wrap = arg;
+
+	/*
+	 * Ensure every thread starts with a malloc() call to prevent memory
+	 * bloat caused by a jemalloc quirk.  While this dummy allocation is
+	 * not used for anything, free() must not be immediately called for it
+	 * so that an optimizing compiler does not strip away such a pair of
+	 * malloc() + free() calls altogether, as it would foil the fix.
+	 */
+	wrap->jemalloc_enforce_init = malloc(8);
+
+	/* Re-seed the random number generator in each thread. */
+	isc__random_initialize();
+
+	/* Get a thread-local digest context. */
+	isc__iterated_hash_initialize();
+
+	/* Run the main function */
+	wrap->result = wrap->func(wrap->arg);
+
+	/* Cleanup */
+	isc__iterated_hash_shutdown();
+
+	/* Return the wrapper struct for jemalloc cleanup */
+	return (wrap);
+}
+
 void
 isc_thread_create(isc_threadfunc_t func, isc_threadarg_t arg,
 		  isc_thread_t *thread) {
 	pthread_attr_t attr;
-	isc__trampoline_t *trampoline_arg;
+	struct thread_wrap *wrap = malloc(sizeof(*wrap));
+	RUNTIME_CHECK(wrap != NULL);
 
-	trampoline_arg = isc__trampoline_get(func, arg);
+	*wrap = (struct thread_wrap){
+		.func = func,
+		.arg = arg,
+	};
 
 #if defined(HAVE_PTHREAD_ATTR_GETSTACKSIZE) && \
 	defined(HAVE_PTHREAD_ATTR_SETSTACKSIZE)
@@ -67,8 +116,7 @@ isc_thread_create(isc_threadfunc_t func, isc_threadarg_t arg,
 #endif /* if defined(HAVE_PTHREAD_ATTR_GETSTACKSIZE) && \
 	* defined(HAVE_PTHREAD_ATTR_SETSTACKSIZE) */
 
-	ret = pthread_create(thread, &attr, isc__trampoline_run,
-			     trampoline_arg);
+	ret = pthread_create(thread, &attr, thread_run, wrap);
 	PTHREADS_RUNTIME_CHECK(pthread_create, ret);
 
 	pthread_attr_destroy(&attr);
@@ -78,9 +126,17 @@ isc_thread_create(isc_threadfunc_t func, isc_threadarg_t arg,
 
 void
 isc_thread_join(isc_thread_t thread, isc_threadresult_t *result) {
-	int ret = pthread_join(thread, result);
+	void *wrap_v;
+	int ret = pthread_join(thread, &wrap_v);
 
 	PTHREADS_RUNTIME_CHECK(pthread_join, ret);
+
+	struct thread_wrap *wrap = wrap_v;
+	if (result != NULL) {
+		*result = wrap->result;
+	}
+	free(wrap->jemalloc_enforce_init);
+	free(wrap);
 }
 
 void
