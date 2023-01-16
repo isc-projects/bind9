e6bfb8e45626e58a1cca687c798ee00973bf26d3
Ondřej Surý <ondrej@isc.org>
2023-01-16 09:16:35 +0100

    Avoid implicit algorithm fetch for OpenSSL EVP_MD family
    The implicit algorithm fetch causes a lock contention and significant
    slowdown for small input buffers.  For more details, see:
    
    https://github.com/openssl/openssl/issues/19612
    
    Instead of using EVP_DigestInit_ex() initialize empty MD_CTX objects for
    each algorithm and use EVP_MD_CTX_copy_ex() to initialize MD_CTX from a
    static copy.  Additionally avoid implicit algorithm fetching by using
    EVP_MD_fetch() for OpenSSL 3.0.

--- lib/isc/lib.c
+++ lib/isc/lib.c
@@ -14,6 +14,7 @@
 /*! \file */
 
 #include <isc/bind9.h>
+#include <isc/md.h>
 #include <isc/mem.h>
 #include <isc/os.h>
 #include <isc/tls.h>
@@ -49,11 +50,13 @@ isc__initialize(void) {
 	isc__trampoline_initialize();
 	isc__uv_initialize();
 	isc__xml_initialize();
+	isc__md_initialize();
 	(void)isc_os_ncpus();
 }
 
 void
 isc__shutdown(void) {
+	isc__md_shutdown();
 	isc__xml_shutdown();
 	isc__uv_shutdown();
 	isc__trampoline_shutdown();
