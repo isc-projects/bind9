639e5b671c0422ec52df91236db8a110c034aefa
Mark Andrews <marka@isc.org>
2023-06-20 15:21:36 +1000

    Limit isccc_cc_fromwire recursion depth
    Named and rndc do not need a lot of recursion so the depth is
    set to 10.
    
    (cherry picked from commit 820b0cceef0b67b041973da4041ea53d5e276363)

--- lib/isccc/result.c
+++ lib/isccc/result.c
@@ -40,7 +40,8 @@ static const char *text[ISCCC_R_NRESULTS] = {
 	"bad auth",				/* 3 */
 	"expired",				/* 4 */
 	"clock skew",				/* 5 */
-	"duplicate"				/* 6 */
+	"duplicate",				/* 6 */
+	"max depth"				/* 7 */
 };
 
 static const char *ids[ISCCC_R_NRESULTS] = {
@@ -50,6 +51,7 @@ static const char *ids[ISCCC_R_NRESULTS] = {
 	"ISCCC_R_EXPIRED",
 	"ISCCC_R_CLOCKSKEW",
 	"ISCCC_R_DUPLICATE",
+	"ISCCC_R_MAXDEPTH",
 };
 
 #define ISCCC_RESULT_RESULTSET			2
