e196ba61682694c107d0a6cadf6255da550a69a2
Matthijs Mekking <matthijs@isc.org>
2023-10-23 13:52:12 +0200

    Test case for issue #4355
    Add a test case where serve-stale is enabled on a server that also
    servers a local authoritative zone.
    
    The particular case tests a lame delegation and checks if falling
    back to serving stale data does not attempt to retrieve the query
    by recursing from the root down.

--- bin/tests/system/serve-stale/setup.sh
+++ bin/tests/system/serve-stale/setup.sh
@@ -16,6 +16,7 @@
 $SHELL clean.sh
 
 copy_setports ns1/named1.conf.in ns1/named.conf
-copy_setports ns3/named1.conf.in ns3/named.conf
+copy_setports ns3/named.conf.in ns3/named.conf
 copy_setports ns4/named.conf.in ns4/named.conf
 copy_setports ns5/named.conf.in ns5/named.conf
+copy_setports ns6/named.conf.in ns6/named.conf
