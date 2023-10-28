831efa40d616d56fc86c3eb55a8e8fe953ab9409
Mark Andrews <marka@isc.org>
2023-10-28 15:40:22 +1100

    Emit "no servers could be reached" for TCP as well as UDP
    When all the servers are exhausted for TCP emit "no servers could
    be reached" in tcp_connected.  This message is already emitted for
    UDP.

--- bin/dig/dighost.c
+++ bin/dig/dighost.c
@@ -3602,6 +3602,7 @@ tcp_connected(isc_nmhandle_t *handle, isc_result_t eresult, void *arg) {
 			start_tcp(next);
 			check_if_done();
 		} else {
+			dighost_error("no servers could be reached\n");
 			clear_current_lookup();
 		}
 
