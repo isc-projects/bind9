8302db407c836109c0d313ae82060c42aeef0230
Tom Krizek <tkrizek@isc.org>
2024-01-04 17:27:32 +0100

    Move test algorithm configuration to isctest
    Instead of invoking get_algorithms.py script repeatedly (which may yield
    different results), move the algorithm configuration to an isctest
    module. This ensures the variables are consistent across the entire test
    run.

--- bin/tests/system/isctest/vars/all.py
+++ bin/tests/system/isctest/vars/all.py
@@ -15,6 +15,7 @@ from collections import ChainMap
 from .autoconf import AC_VARS  # type: ignore
 
 # pylint: enable=import-error
+from .algorithms import ALG_VARS
 from .basic import BASIC_VARS
 from .dirs import DIR_VARS
 from .openssl import OPENSSL_VARS
@@ -52,4 +53,4 @@ class VarLookup(ChainMap):
         return iter(self.keys())
 
 
-ALL = VarLookup(AC_VARS, BASIC_VARS, OPENSSL_VARS, PORT_VARS, DIR_VARS)
+ALL = VarLookup(AC_VARS, BASIC_VARS, OPENSSL_VARS, PORT_VARS, DIR_VARS, ALG_VARS)
