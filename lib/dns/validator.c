dc94f422095d55727248d2137a3681e1015adde4
Mark Andrews <marka@isc.org>
2024-01-15 13:39:44 +1100

    Dissassociate rdatasets returned from dns_ncache_current
    lib/dns/validator.c:findnsec3proofs failed to disassociate the
    temporary rdataset returned by dns_ncache_current on all paths.

--- lib/dns/validator.c
+++ lib/dns/validator.c
@@ -2116,6 +2116,9 @@ findnsec3proofs(dns_validator_t *val) {
 	POST(result);
 
 	if (dns_name_countlabels(zonename) == 0) {
+		if (dns_rdataset_isassociated(&trdataset)) {
+			dns_rdataset_disassociate(&trdataset);
+		}
 		return (ISC_R_SUCCESS);
 	}
 
@@ -2184,6 +2187,9 @@ findnsec3proofs(dns_validator_t *val) {
 			{
 				proofs[DNS_VALIDATOR_NOWILDCARDPROOF] = name;
 			}
+			if (dns_rdataset_isassociated(&trdataset)) {
+				dns_rdataset_disassociate(&trdataset);
+			}
 			return (result);
 		}
 		if (result != ISC_R_SUCCESS) {
@@ -2237,8 +2243,14 @@ findnsec3proofs(dns_validator_t *val) {
 	{
 		result = checkwildcard(val, dns_rdatatype_nsec3, zonename);
 		if (result != ISC_R_SUCCESS) {
+			if (dns_rdataset_isassociated(&trdataset)) {
+				dns_rdataset_disassociate(&trdataset);
+			}
 			return (result);
 		}
+	}
+	if (dns_rdataset_isassociated(&trdataset)) {
+		dns_rdataset_disassociate(&trdataset);
 	}
 	return (result);
 }
