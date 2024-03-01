eba7fb5f9f4925bbfd0d85847117847586b1ee9e
Ondřej Surý <ondrej@isc.org>
2024-03-01 12:43:15 +0100

    Create a second pruning task for rbtdb with unlimited quantum
    Previously, rbtdb->task had quantum of 1 because it was originally used
    just for freeing RBTDB contents, which can happen on a "best effort"
    basis (does not need to be prioritized).  However, when tree pruning was
    implemented, it also started sending events to that task, enabling the
    latter to become clogged up with a significant event backlog because it
    only pruned a single RBTDB node per event.
    
    To prioritize tree pruning (as it is necessary for enforcing the
    configured memory use limit for the cache memory context), create a
    second task with a virtually unlimited quantum (UINT_MAX) and send the
    tree-pruning events to this new task, to ensure that all nodes scheduled
    for pruning will be processed before further nodes are queued in a
    similar fashion.
    
    This change enables dropping the prunenodes list and restoring the
    originally-used logic that allocates and sends a separate event for each
    node to prune.
    
    (cherry picked from commit 540a5b5a2c82170acc5c08d2c2ef74a700c7236f)

--- lib/dns/rbtdb.c
+++ lib/dns/rbtdb.c
@@ -500,6 +500,7 @@ struct dns_rbtdb {
 	rbtdb_version_t *future_version;
 	rbtdb_versionlist_t open_versions;
 	isc_task_t *task;
+	isc_task_t *prunetask;
 	dns_dbnode_t *soanode;
 	dns_dbnode_t *nsnode;
 
@@ -528,10 +529,6 @@ struct dns_rbtdb {
 	 * nodes that await being cleaned up.
 	 */
 	rbtnodelist_t *deadnodes;
-
-	/* List of nodes from which recursive tree pruning can be started from.
-	 * Locked by tree_lock. */
-	rbtnodelist_t *prunenodes;
 
 	/*
 	 * Heaps.  These are used for TTL based expiry in a cache,
@@ -1116,14 +1113,6 @@ free_rbtdb(dns_rbtdb_t *rbtdb, bool log, isc_event_t *event) {
 		}
 	}
 
-	for (i = 0; i < rbtdb->node_lock_count; i++) {
-		node = ISC_LIST_HEAD(rbtdb->prunenodes[i]);
-		while (node != NULL) {
-			ISC_LIST_UNLINK(rbtdb->prunenodes[i], node, prunelink);
-			node = ISC_LIST_HEAD(rbtdb->prunenodes[i]);
-		}
-	}
-
 	if (event == NULL) {
 		rbtdb->quantum = (rbtdb->task != NULL) ? 100 : 0;
 	}
@@ -1211,17 +1200,6 @@ free_rbtdb(dns_rbtdb_t *rbtdb, bool log, isc_event_t *event) {
 			    rbtdb->node_lock_count * sizeof(rbtnodelist_t));
 	}
 	/*
-	 * Clean up prune node buckets.
-	 */
-	if (rbtdb->prunenodes != NULL) {
-		for (i = 0; i < rbtdb->node_lock_count; i++) {
-			INSIST(ISC_LIST_EMPTY(rbtdb->prunenodes[i]));
-		}
-		isc_mem_put(rbtdb->common.mctx, rbtdb->prunenodes,
-			    rbtdb->node_lock_count *
-				    sizeof(rbtdb->prunenodes[i]));
-	}
-	/*
 	 * Clean up heap objects.
 	 */
 	if (rbtdb->heaps != NULL) {
@@ -1248,6 +1226,9 @@ free_rbtdb(dns_rbtdb_t *rbtdb, bool log, isc_event_t *event) {
 	isc_refcount_destroy(&rbtdb->references);
 	if (rbtdb->task != NULL) {
 		isc_task_detach(&rbtdb->task);
+	}
+	if (rbtdb->prunetask != NULL) {
+		isc_task_detach(&rbtdb->prunetask);
 	}
 
 	RBTDB_DESTROYLOCK(&rbtdb->lock);
@@ -1873,7 +1854,6 @@ delete_node(dns_rbtdb_t *rbtdb, dns_rbtnode_t *node) {
 	isc_result_t result = ISC_R_UNEXPECTED;
 
 	INSIST(!ISC_LINK_LINKED(node, deadlink));
-	INSIST(!ISC_LINK_LINKED(node, prunelink));
 
 	if (isc_log_wouldlog(dns_lctx, ISC_LOG_DEBUG(1))) {
 		char printname[DNS_NAME_FORMATSIZE];
@@ -1971,10 +1951,6 @@ is_last_node_on_its_level(dns_rbtnode_t *node) {
 		node->left == NULL && node->right == NULL);
 }
 
-/*%
- * The tree lock must be held when this function is called as it reads and
- * updates rbtdb->prunenodes.
- */
 static void
 send_to_prune_tree(dns_rbtdb_t *rbtdb, dns_rbtnode_t *node,
 		   isc_rwlocktype_t nlocktype) {
@@ -8193,7 +8169,7 @@ adjusthashsize(dns_db_t *db, size_t size) {
 }
 
 static void
-settask(dns_db_t *db, isc_task_t *task) {
+settask(dns_db_t *db, isc_task_t *task, isc_task_t *prunetask) {
 	dns_rbtdb_t *rbtdb;
 
 	rbtdb = (dns_rbtdb_t *)db;
@@ -8206,6 +8182,12 @@ settask(dns_db_t *db, isc_task_t *task) {
 	}
 	if (task != NULL) {
 		isc_task_attach(task, &rbtdb->task);
+	}
+	if (rbtdb->prunetask != NULL) {
+		isc_task_detach(&rbtdb->prunetask);
+	}
+	if (prunetask != NULL) {
+		isc_task_attach(prunetask, &rbtdb->prunetask);
 	}
 	RBTDB_UNLOCK(&rbtdb->lock, isc_rwlocktype_write);
 }
@@ -8781,12 +8763,6 @@ dns_rbtdb_create(isc_mem_t *mctx, const dns_name_t *origin, dns_dbtype_t type,
 		ISC_LIST_INIT(rbtdb->deadnodes[i]);
 	}
 
-	rbtdb->prunenodes = isc_mem_get(
-		mctx, rbtdb->node_lock_count * sizeof(rbtdb->prunenodes[0]));
-	for (i = 0; i < (int)rbtdb->node_lock_count; i++) {
-		ISC_LIST_INIT(rbtdb->prunenodes[i]);
-	}
-
 	rbtdb->active = rbtdb->node_lock_count;
 
 	for (i = 0; i < (int)(rbtdb->node_lock_count); i++) {
@@ -8894,6 +8870,7 @@ dns_rbtdb_create(isc_mem_t *mctx, const dns_name_t *origin, dns_dbtype_t type,
 	isc_refcount_init(&rbtdb->references, 1);
 	rbtdb->attributes = 0;
 	rbtdb->task = NULL;
+	rbtdb->prunetask = NULL;
 	rbtdb->serve_stale_ttl = 0;
 
 	/*
