998522e68eaad29284a6be1fe8e051031db312a8
Artem Boldariev <artem@boldariev.com>
2024-03-13 18:04:46 +0200

    Keep the endpoints set reference within an HTTP/2 socket
    This commit ensures that an HTTP endpoints set reference is stored in
    a socket object associated with an HTTP/2 stream instead of
    referencing the global set stored inside a listener.
    
    This helps to prevent an issue like follows:
    
    1. BIND is configured to serve DoH clients;
    2. A client is connected and one or more HTTP/2 stream is
    created. Internal pointers are now pointing to the data on the
    associated HTTP endpoints set;
    3. BIND is reconfigured - the new endpoints set object is created and
    promoted to all listeners;
    4. The old pointers to the HTTP endpoints set data are now invalid.
    
    Instead referencing a global object that is updated on
    re-configurations we now store a local reference which prevents the
    endpoints set objects to go out of scope prematurely.
    
    (cherry picked from commit b9b5d0c01a3a546c4a6a8b3bff8ae9dd31fee224)

--- lib/isc/netmgr/netmgr-int.h
+++ lib/isc/netmgr/netmgr-int.h
@@ -883,13 +883,8 @@ typedef enum isc_http_scheme_type {
 	ISC_HTTP_SCHEME_UNSUPPORTED
 } isc_http_scheme_type_t;
 
-typedef struct isc_nm_httpcbarg {
-	isc_nm_recv_cb_t cb;
-	void *cbarg;
-	LINK(struct isc_nm_httpcbarg) link;
-} isc_nm_httpcbarg_t;
-
 typedef struct isc_nm_httphandler {
+	int magic;
 	char *path;
 	isc_nm_recv_cb_t cb;
 	void *cbarg;
@@ -902,7 +897,6 @@ struct isc_nm_http_endpoints {
 	isc_mem_t *mctx;
 
 	ISC_LIST(isc_nm_httphandler_t) handlers;
-	ISC_LIST(isc_nm_httpcbarg_t) handler_cbargs;
 
 	isc_refcount_t references;
 	atomic_bool in_use;
@@ -914,7 +908,6 @@ typedef struct isc_nmsocket_h2 {
 	char *query_data;
 	size_t query_data_len;
 	bool query_too_large;
-	isc_nm_httphandler_t *handler;
 
 	isc_buffer_t rbuf;
 	isc_buffer_t wbuf;
@@ -946,6 +939,8 @@ typedef struct isc_nmsocket_h2 {
 
 	isc_nm_http_endpoints_t **listener_endpoints;
 	size_t n_listener_endpoints;
+
+	isc_nm_http_endpoints_t *peer_endpoints;
 
 	bool response_submitted;
 	struct {
