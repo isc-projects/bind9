d1f1b6a934e1ac0b26cf0f276108ba34b88f162b
Tom Krizek <tkrizek@isc.org>
2024-03-20 11:43:35 +0100

    Mark the enginepkcs11 test as flaky
    There are frequent intermittent failures due to "crypto failure".

--- bin/tests/system/enginepkcs11/tests_sh_enginepkcs11.py
+++ bin/tests/system/enginepkcs11/tests_sh_enginepkcs11.py
@@ -9,6 +9,9 @@
 # See the COPYRIGHT file distributed with this work for additional
 # information regarding copyright ownership.
 
+import isctest.mark
 
+
+@isctest.mark.flaky(max_runs=3)  # GL#4605
 def test_enginepkcs11(run_tests_sh):
     run_tests_sh()
