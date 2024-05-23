bf0df5322c46949c56ea6b3d5e8b56123e6f71ae
Ondřej Surý <ondrej@isc.org>
2024-05-23 19:12:40 +0200

    Add test for not-loading and not-transfering huge RRSets
    Add two new masterformat tests - the 'huge' zone fits within the ns1
    limit and loads on the primary ns1 server, but must not transfer to the
    ns2 secondary, and the 'uber' zone should not even load on the primary
    ns1 server.
    
    (cherry picked from commit cee9ad81db6e5a1167b311e5c2f42cf65ba457cd)

--- bin/tests/system/masterformat/clean.sh
+++ bin/tests/system/masterformat/clean.sh
@@ -16,6 +16,8 @@ rm -f ./ns1/example.db.compat
 rm -f ./ns1/example.db.serial.raw
 rm -f ./ns1/large.db ./ns1/large.db.raw
 rm -f ./ns1/example.db.map ./ns1/signed.db.map
+rm -f ./ns1/huge.db ./ns1/huge.db.raw
+rm -f ./ns1/uber.db ./ns1/uber.db.raw
 rm -f ./ns1/session.key
 rm -f ./dig.out.*
 rm -f ./dig.out
