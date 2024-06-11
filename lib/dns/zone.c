b6721db490e186367876d10e7c05119e4cc3b30a
Aram Sargsyan <aram@isc.org>
2024-06-11 10:53:06 +0000

    Do not automatically restart a canceled zone transfer
    If a zone transfer is canceled there is no need to try the
    next primary or retry with AXFR.
    
    (cherry picked from commit b156531b299819104195c8d964a72d76d542c0e7)

--- lib/dns/zone.c
+++ lib/dns/zone.c
@@ -17795,6 +17795,11 @@ again:
 		inc_stats(zone, dns_zonestatscounter_xfrfail);
 		break;
 
+	case ISC_R_CANCELED:
+	case ISC_R_SHUTTINGDOWN:
+		dns_remote_reset(&zone->primaries, false);
+		break;
+
 	default:
 	next_primary:
 		/*
