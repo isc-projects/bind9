e7a469133caf37bb5056677ac47f495c52abfbf2
Nicki Křížek <nicki@isc.org>
2024-08-28 15:03:27 +0200

    Test cipher-suites after zone transfers complete
    Ensure the zone transfers have completed (successfully or not) before
    running the test cases, because they assume zone transfers have been
    done.
    
    (cherry picked from commit 23fb6159636241c1e7e66ba16d5728c28bcd6b97)

--- bin/tests/system/cipher-suites/tests_cipher_suites.py
+++ bin/tests/system/cipher-suites/tests_cipher_suites.py
@@ -9,6 +9,8 @@
 # See the COPYRIGHT file distributed with this work for additional
 # information regarding copyright ownership.
 
+import re
+
 import pytest
 
 pytest.importorskip("dns", minversion="2.5.0")
@@ -24,6 +26,17 @@ pytestmark = pytest.mark.extra_artifacts(
         "ns*/example*.db",
     ]
 )
+
+
+@pytest.fixture(scope="module")
+def transfers_complete(servers):
+    for zone in ["example", "example-aes-128", "example-aes-256", "example-chacha-20"]:
+        pattern = re.compile(
+            f"transfer of '{zone}/IN' from 10.53.0.1#[0-9]+: Transfer completed"
+        )
+        for ns in ["ns2", "ns3", "ns4", "ns5"]:
+            with servers[ns].watch_log_from_start() as watcher:
+                watcher.wait_for_line(pattern)
 
 
 @pytest.mark.requires_zones_loaded("ns1", "ns2", "ns3", "ns4", "ns5")
@@ -69,7 +82,8 @@ pytestmark = pytest.mark.extra_artifacts(
         ),
     ],
 )
-def test_cipher_suites_tls_xfer(qname, ns, rcode):
+# pylint: disable=redefined-outer-name,unused-argument
+def test_cipher_suites_tls_xfer(qname, ns, rcode, transfers_complete):
     msg = dns.message.make_query(qname, "AXFR")
     ans = isctest.query.tls(msg, f"10.53.0.{ns}")
     assert ans.rcode() == rcode
