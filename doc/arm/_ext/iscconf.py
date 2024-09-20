833ea7926cf62358f1498692112411975d23ca6c
Nicki Křížek <nicki@isc.org>
2024-09-20 16:09:33 +0200

    Fix arguments-renamed pylint issue in iscconf.py
    The argument name was different in the base class.

--- doc/arm/_ext/iscconf.py
+++ doc/arm/_ext/iscconf.py
@@ -226,16 +226,16 @@ def domain_factory(domainname, domainlabel, todolist, grammar):
                 self.state.nested_parse(raw, self.content_offset, parsed)
                 return parsed
 
-            def transform_content(self, contentnode: addnodes.desc_content) -> None:
+            def transform_content(self, content_node: addnodes.desc_content) -> None:
                 """autogenerate content from structured data"""
                 self.workaround_transform_content = True
                 if self.isc_short:
-                    contentnode.insert(0, self.isc_short_node)
+                    content_node.insert(0, self.isc_short_node)
                 if self.isc_tags:
                     tags = nodes.paragraph()
                     tags += nodes.strong(text="Tags: ")
                     tags += nodes.Text(", ".join(self.isc_tags))
-                    contentnode.insert(0, tags)
+                    content_node.insert(0, tags)
 
                 iscconf = self.env.get_domain(domainname)
 
@@ -244,7 +244,7 @@ def domain_factory(domainname, domainlabel, todolist, grammar):
                     return  # not defined in grammar, nothing to render
 
                 blocks = self.format_blocks(iscconf.statement_blocks[name])
-                contentnode.insert(0, blocks)
+                content_node.insert(0, blocks)
 
                 grammars = iscconf.statement_grammar_groups[name]
                 multi_grammar = len(grammars) > 1
@@ -257,11 +257,11 @@ def domain_factory(domainname, domainlabel, todolist, grammar):
                     if "suppress_grammar" in self.options:
                         continue
                     grammarnode = self.format_grammar(multi_grammar, grammar_grp)
-                    contentnode.insert(0, grammarnode)
+                    content_node.insert(0, grammarnode)
 
                 warn = self.format_warnings(union_flags)
                 if len(warn):
-                    contentnode.insert(0, warn)
+                    content_node.insert(0, warn)
 
             def __init__(self, *args, **kwargs):
                 """Compability with Sphinx < 3.0.0"""
