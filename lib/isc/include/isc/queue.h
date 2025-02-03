a9f4e3369a4452d5d13228509bed85cd19001715
Ondřej Surý <ondrej@isc.org>
2025-02-03 13:36:27 +0100

    Reduce false sharing in dns_qpcache
    Instead of having many node_lock_count * sizeof(<member>) arrays, pack
    all the members into a qpcache_bucket_t struct that is cacheline aligned
    and have a single array of those.
    
    Additionaly, make both the head and the tail of isc_queue_t padded, not
    just the head, to prevent false sharing of the lock-free structure with
    the lock that follows it.
    
    (cherry picked from commit c602d76c1fb6cfe54da9094ae1d12912d852d359)

--- lib/isc/include/isc/queue.h
+++ lib/isc/include/isc/queue.h
@@ -20,9 +20,11 @@ STATIC_ASSERT(sizeof(struct __cds_wfcq_head) <= ISC_OS_CACHELINE_SIZE,
 
 typedef struct isc_queue {
 	struct __cds_wfcq_head head;
-	uint8_t		       __padding[ISC_OS_CACHELINE_SIZE -
-				 sizeof(struct __cds_wfcq_head)];
+	uint8_t		       __padding_head[ISC_OS_CACHELINE_SIZE -
+				      sizeof(struct __cds_wfcq_head)];
 	struct cds_wfcq_tail   tail;
+	uint8_t		       __padding_tail[ISC_OS_CACHELINE_SIZE -
+				      sizeof(struct __cds_wfcq_head)];
 } isc_queue_t;
 
 typedef struct cds_wfcq_node isc_queue_node_t;
