7001056eabdee147ab51d5a8ca99aa6904e17c7f
Nicki Křížek <nicki@isc.org>
2025-06-05 17:51:40 +0200

    Isolate rollover-going-insecure test case

---
+++ bin/tests/system/rollover-going-insecure/tests_rollover_going_insecure_initial.py
@@ -0,0 +1,46 @@
+# Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0.  If a copy of the MPL was not distributed with this
+# file, you can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# See the COPYRIGHT file distributed with this work for additional
+# information regarding copyright ownership.
+
+# pylint: disable=redefined-outer-name,unused-import
+
+import pytest
+
+import isctest
+from common import (
+    pytestmark,
+    alg,
+    size,
+    CDSS,
+    DURATION,
+    UNSIGNING_CONFIG,
+)
+
+
+@pytest.mark.parametrize(
+    "zone",
+    [
+        "going-insecure.kasp",
+        "going-insecure-dynamic.kasp",
+    ],
+)
+def test_going_insecure_initial(zone, servers, alg, size):
+    config = UNSIGNING_CONFIG
+    policy = "unsigning"
+    step = {
+        "zone": f"step1.{zone}",
+        "cdss": CDSS,
+        "keyprops": [
+            f"ksk 0 {alg} {size} goal:omnipresent dnskey:omnipresent krrsig:omnipresent ds:omnipresent offset:{-DURATION['P10D']}",
+            f"zsk {DURATION['P60D']} {alg} {size} goal:omnipresent dnskey:omnipresent zrrsig:omnipresent offset:{-DURATION['P10D']}",
+        ],
+        "nextev": None,
+    }
+    isctest.kasp.check_rollover_step(servers["ns6"], config, policy, step)
