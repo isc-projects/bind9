55cce1c8720d455d19cca12ecd105e6c9992baf2
Nicki Křížek <nicki@isc.org>
2025-07-02 17:53:18 +0200

    Increase test reruns for fetchlimit
    The fetchlimit test has failed 8 times in the nightly CI over the past
    three weeks. That makes the overall failure rate somewhere around 1 %,
    which isn't a lot, but is still annoying when lots of testing is going
    on.
    
    (cherry picked from commit ae932eefc5c8f049f29ecb14fe7a53cad5500056)

--- bin/tests/system/fetchlimit/tests_sh_fetchlimit.py
+++ bin/tests/system/fetchlimit/tests_sh_fetchlimit.py
@@ -27,6 +27,6 @@ pytestmark = pytest.mark.extra_artifacts(
 import isctest.mark
 
 
-@isctest.mark.flaky(max_runs=2)
+@isctest.mark.flaky(max_runs=3)
 def test_fetchlimit(run_tests_sh):
     run_tests_sh()
