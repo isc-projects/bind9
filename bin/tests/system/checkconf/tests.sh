dd4af20dc8235b861b745abd651daa0d5e55e09d
Evan Hunt <each@isc.org>
2026-03-17 13:45:11 -0700

    Test CHAOS view recursion behavior
    Check that recursive and forward queries to views of type CHAOS
    are REFUSED, but that authoritative queries are answered correctly.

--- bin/tests/system/checkconf/tests.sh
+++ bin/tests/system/checkconf/tests.sh
@@ -739,5 +739,16 @@ if [ $ret != 0 ]; then
 fi
 status=$((status + ret))
 
+n=$((n + 1))
+echo_i "check 'recursion yes;' is warned and disabled in a non-IN view ($n)"
+ret=0
+$CHECKCONF warn-chaos-recursion.conf >checkconf.out$n 2>&1 || ret=1
+grep -F "recursion will be disabled" checkconf.out$n >/dev/null || ret=1
+if [ $ret != 0 ]; then
+  echo_i "failed"
+  ret=1
+fi
+status=$((status + ret))
+
 echo_i "exit status: $status"
 [ $status -eq 0 ] || exit 1
