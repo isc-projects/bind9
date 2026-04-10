7c45caf6fe491731abed3a78e5ec6d4f6bc8e0e3
Mark Andrews <marka@isc.org>
2026-04-10 13:07:26 +1000

    Remove unnecessary dns_name_free call
    When processing a catalog zone member's primaries definition and
    there is a TXT record containing an invalid name TSIG key name,
    dns_name_free was incorrectly called triggering an assertion.
    This has been fixed.
    
    (cherry picked from commit 9f411c93c4d87b8097d39024da5a9da447cddaf7)

--- lib/dns/catz.c
+++ lib/dns/catz.c
@@ -1469,7 +1469,6 @@ catz_process_primaries(dns_catz_zone_t *catz, dns_ipkeylist_t *ipkl,
 			dns_rdata_freestruct(&rdata_txt);
 			result = dns_name_fromstring(keyname, keycbuf, 0, mctx);
 			if (result != ISC_R_SUCCESS) {
-				dns_name_free(keyname, mctx);
 				isc_mem_put(mctx, keyname, sizeof(*keyname));
 				return result;
 			}
