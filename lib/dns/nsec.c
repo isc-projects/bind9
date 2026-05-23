d3c85737f7591b8e283911a1dcff1fac67abe5fa
Evan Hunt <each@isc.org>
2026-05-22 21:04:03 -0700

    Check that an NSEC signer is at or above the name to be validated
    Add a check that an NSEC record being used as a proof of nonexistence
    for a given name is not signed by a name lower in the DNS hierarchy than
    the one in question.
    
    Fixes: isc-projects/bind9#5876
    
    (cherry picked from commit c5f99f3508b74e9c4532441d6a63f0d5e490dea7)

--- lib/dns/nsec.c
+++ lib/dns/nsec.c
@@ -441,10 +441,23 @@ dns_nsec_noexistnodata(dns_rdatatype_t type, const dns_name_t *name,
 		return DNS_R_DNAME;
 	}
 
+	if (relation != dns_namereln_subdomain &&
+	    dns_nsec_typepresent(&rdata, dns_rdatatype_soa))
+	{
+		/*
+		 * An NSEC with an SOA in the bitmap can only cover
+		 * names that are subdomains of the owner.
+		 */
+		(*logit)(arg, ISC_LOG_DEBUG(3),
+			 "ignoring nsec with SOA covering non-subdomain");
+		return ISC_R_IGNORE;
+	}
+
 	result = dns_rdata_tostruct(&rdata, &nsec, NULL);
 	if (result != ISC_R_SUCCESS) {
 		return result;
 	}
+
 	relation = dns_name_fullcompare(&nsec.next, name, &order, &nlabels);
 	if (order == 0) {
 		dns_rdata_freestruct(&nsec);
