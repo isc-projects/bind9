a5bc4955c71125e801ca0c85f598cc6155bd4f52
Alessio Podda <alessio@isc.org>
2026-06-17 13:38:30 +0200

    Only print per zone glue cache stats when zone-statistics is full
    The code printing glue cache statistics was ignoring the
    zone-statistics option. This has been fixed.
    
    (cherry picked from commit f655b6af84c3901af6acfad5d7cb51591334f632)

--- bin/named/statschannel.c
+++ bin/named/statschannel.c
@@ -4114,7 +4114,12 @@ named_stats_dump(named_server_t *server, FILE *fp) {
 	     result == ISC_R_SUCCESS;
 	     next = NULL, result = dns_zone_next(zone, &next), zone = next)
 	{
+		if (dns_zone_getstatlevel(zone) != dns_zonestat_full) {
+			continue;
+		}
+
 		isc_stats_t *zonestats = dns_zone_getrequeststats(zone);
+
 		if (zonestats != NULL) {
 			char zonename[DNS_NAME_FORMATSIZE];
 
@@ -4144,7 +4149,12 @@ named_stats_dump(named_server_t *server, FILE *fp) {
 	     result == ISC_R_SUCCESS;
 	     next = NULL, result = dns_zone_next(zone, &next), zone = next)
 	{
+		if (dns_zone_getstatlevel(zone) != dns_zonestat_full) {
+			continue;
+		}
+
 		isc_stats_t *gluecachestats = dns_zone_getgluecachestats(zone);
+
 		if (gluecachestats != NULL) {
 			char zonename[DNS_NAME_FORMATSIZE];
 
