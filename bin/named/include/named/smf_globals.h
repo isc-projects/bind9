41bdf279e9288bf15826c251cfeddccb51ff74f7
Mark Andrews <marka@isc.org>
2026-07-08 23:53:09 +1000

    Fix named_smf_add_message prototype
    There was a prototype mismatch for named_smf_add_message.  This
    has been fixed.

--- bin/named/include/named/smf_globals.h
+++ bin/named/include/named/smf_globals.h
@@ -30,7 +30,7 @@ EXTERN unsigned int named_smf_chroot	   INIT(0);
 EXTERN unsigned int named_smf_want_disable INIT(0);
 
 isc_result_t
-named_smf_add_message(isc_buffer_t **text);
+named_smf_add_message(isc_buffer_t *text);
 isc_result_t
 named_smf_get_instance(char **name, int debug, isc_mem_t *mctx);
 
