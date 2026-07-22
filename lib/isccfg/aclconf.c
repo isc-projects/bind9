d1e1125227c076e468af22512bf881cca179471a
Evan Hunt <each@isc.org>
2026-07-22 12:59:17 -0700

    check asnum validity in geoip ACLs
    We now check the validity of autonomous system (AS) numbers when
    parsing geoip ACLs that use "asnum" elements at configuration time.
    
    "asnum" values start with an optional case-insensitive "AS" prefix,
    followed only by decimal digits, with no spaces or other extraneous
    characters. The value represented cannot exceed MAXUINT32.
    
    (cherry picked from commit c37d432c5ea4aa46b4fde6a3ddb7c9080d1d5eab)

--- lib/isccfg/aclconf.c
+++ lib/isccfg/aclconf.c
@@ -11,11 +11,13 @@
  * information regarding copyright ownership.
  */
 
+#include <ctype.h>
 #include <inttypes.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
 #include <isc/mem.h>
+#include <isc/parseint.h>
 #include <isc/string.h>
 #include <isc/util.h>
 
@@ -582,7 +584,20 @@ parse_geoip_element(const cfg_obj_t *obj, isc_log_t *lctx,
 		strlcpy(de.geoip_elem.as_string, search,
 			sizeof(de.geoip_elem.as_string));
 	} else if (strcasecmp(stype, "asnum") == 0) {
+		const char *s = search;
+		uint32_t val;
+
+		/* check asnum validity */
 		subtype = dns_geoip_as_asnum;
+		if (strncasecmp(s, "AS", 2) == 0) {
+			s += 2;
+		}
+		if (isc_parse_uint32(&val, s, 10) != ISC_R_SUCCESS) {
+			cfg_obj_log(obj, lctx, ISC_LOG_ERROR,
+				    "invalid asnum '%s'", search);
+			return ISC_R_UNEXPECTEDTOKEN;
+		}
+
 		strlcpy(de.geoip_elem.as_string, search,
 			sizeof(de.geoip_elem.as_string));
 	} else if (strcasecmp(stype, "org") == 0) {
