ac43c3163839ef171a8ca0376291f97bc8ea9487
Ondřej Surý <ondrej@isc.org>
2026-08-15 15:48:26 +0200

    Require NSEC wildcard proofs from the same zone
    A valid NSEC proof from an unrelated signed zone could be combined
    with another zone's NOQNAME proof. Bind wildcard denial to the
    validated NOQNAME signer so proofs cannot cross zone boundaries.

--- lib/dns/include/dns/validator.h
+++ lib/dns/include/dns/validator.h
@@ -146,6 +146,7 @@ struct dns_validator {
 	dns_fixedname_t	   fname;
 	dns_fixedname_t	   wild;
 	dns_fixedname_t	   wildsigner;
+	dns_fixedname_t	   nseczone;
 	dns_fixedname_t	   closest;
 	ISC_LINK(dns_validator_t) link;
 	unsigned int   depth;
