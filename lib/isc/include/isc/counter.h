1b8a1b048713adcf37abe05ad239c75b30b2f996
Aydın Mercan <aydin@isc.org>
2026-09-02 16:03:18 +0300

    simplify and modernize header
    Remove the unnecessary header includes and change the documentation
    to be friendlier to clangd once it fixes trailing function comments.

--- lib/isc/include/isc/counter.h
+++ lib/isc/include/isc/counter.h
@@ -13,10 +13,6 @@
 
 #pragma once
 
-/*****
-***** Module Info
-*****/
-
 /*! \file isc/counter.h
  *
  * \brief The isc_counter_t object is a simplified version of the
@@ -26,49 +22,49 @@
  * from a counter object does not increment or decrement the counter.
  */
 
-/***
- *** Imports.
- ***/
+#include <stddef.h>
 
-#include <isc/mutex.h>
 #include <isc/refcount.h>
 #include <isc/types.h>
 
-/*****
-***** Types.
-*****/
-
 void
 isc_counter_create(isc_mem_t *mctx, size_t limit, isc_counter_t **counterp);
-/*%<
+/**<
+ * \brief
  * Allocate and initialize a counter object.
  */
 
 isc_result_t
 isc_counter_increment(isc_counter_t *counter);
-/*%<
+/**<
+ * \brief
  * Increment the counter.
  *
- * If the counter limit is nonzero and has been reached, then
- * return ISC_R_QUOTA, otherwise ISC_R_SUCCESS. (The counter is
- * incremented regardless of return value.)
+ * \details
+ * The counter is incremented regardless of return value.
+ *
+ * \retval ISC_R_QUOTA if the counter limit is non-zero and has been reached.
+ * \retval ISC_R_SUCCESS otherwise
  */
 
 size_t
 isc_counter_used(isc_counter_t *counter);
-/*%<
+/**<
+ * \brief
  * Return the current counter value.
  */
 
 void
 isc_counter_setlimit(isc_counter_t *counter, size_t limit);
-/*%<
+/**<
+ * \brief
  * Set the counter limit.
  */
 
 size_t
 isc_counter_getlimit(isc_counter_t *counter);
-/*%<
+/**<
+ * \brief
  * Get the counter limit.
  */
 
