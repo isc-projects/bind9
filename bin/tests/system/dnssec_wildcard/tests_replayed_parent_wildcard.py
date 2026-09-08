934f971d5f8f6c0cfbcf97fd39815f20353b277e
Michał Kępień <michal@isc.org>
2026-09-08 17:47:12 +0200

    Simplify replayed_parent_wildcard test
    Replace boilerplate code generating the signing keys for the zones used
    by the ans1 custom server with canonical bootstrap() code employing
    isctest.zone.Zone.
    
    Replace boilerplate code crafting DNSSEC responses in ans1/*.py with
    zone file data in ans1/zones/ signed in bootstrap(), reducing
    ans1/ans.py down to just the logic implementing the attack attempted by
    the test.

--- bin/tests/system/dnssec_wildcard/tests_replayed_parent_wildcard.py
+++ bin/tests/system/dnssec_wildcard/tests_replayed_parent_wildcard.py
@@ -11,21 +11,14 @@
 # See the COPYRIGHT file distributed with this work for additional
 # information regarding copyright ownership.
 
-from pathlib import Path
-
-import json
-
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import ec
-
-import dns.dnssec
 import dns.name
 import dns.rdataclass
 import dns.rdatatype
 import pytest
 
 import isctest
-import isctest.mark
+import isctest.template
+import isctest.zone
 
 PARENT = "f045.test."
 CHILD = f"child.{PARENT}"
@@ -33,47 +26,31 @@ QUERY = f"q.{PARENT}"
 SERVICE = f"svc.{CHILD}"
 FORGED_A = "198.51.100.45"
 LEGIT_A = "192.0.2.113"
-AUTH = "10.53.0.1"
-RESOLVER = "10.53.0.2"
+AUTH = isctest.template.ANS1.ip
+RESOLVER = isctest.template.NS2.ip
 
 pytestmark = [
-    isctest.mark.with_ecdsa_deterministic,
     pytest.mark.extra_artifacts(
         [
             "ans*/ans.run",
-            "ans*/keys.json",
+            "ans*/dsset-*",
+            "ans*/keys/",
+            "ans*/zones/*.db",
+            "ans*/zones/*.db.signed",
         ]
     ),
 ]
 
 
-def _make_key(zone):
-    private_key = ec.generate_private_key(ec.SECP256R1())
-    dnskey = dns.dnssec.make_dnskey(
-        private_key.public_key(),
-        algorithm="ECDSAP256SHA256",
-        flags=257,
-    )
-    ds = dns.dnssec.make_ds(dns.name.from_text(zone), dnskey, "SHA256")
-    private_pem = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption(),
-    ).decode("ascii")
-    return {
-        "private_pem": private_pem,
-        "dnskey": dnskey.to_text(),
-        "ds": ds.to_text(),
-    }
-
-
 def bootstrap():
-    keys = {zone: _make_key(zone) for zone in [PARENT, CHILD]}
+    child = isctest.zone.Zone("child.f045.test.", isctest.template.ANS1, signed=True)
+    child.configure(csk=True)
 
-    Path("ans1/keys.json").write_text(json.dumps(keys, indent=2), encoding="ascii")
+    parent = isctest.zone.Zone("f045.test.", isctest.template.ANS1, signed=True)
+    parent.delegations = [child]
+    parent.configure(csk=True)
 
-    parent_dnskey = "".join(keys[PARENT]["dnskey"].split()[3:])
-    return {"PARENT_DNSKEY": parent_dnskey}
+    return {"trust_anchors": parent.trust_anchors()}
 
 
 def _query(server, qname, qtype, cd=False):
