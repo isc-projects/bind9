a8f1b391ae5af1101b3c141dd371db130e83605e
Mark Andrews <marka@isc.org>
2026-09-18 09:55:24 +0200

    Make first argument of isccc_sexpr_print (sexpr) a const

--- lib/isccc/include/isccc/sexpr.h
+++ lib/isccc/include/isccc/sexpr.h
@@ -79,7 +79,7 @@ void
 isccc_sexpr_free(isccc_sexpr_t **sexprp);
 
 void
-isccc_sexpr_print(isccc_sexpr_t *sexpr, FILE *stream);
+isccc_sexpr_print(const isccc_sexpr_t *sexpr, FILE *stream);
 
 isccc_sexpr_t *
 isccc_sexpr_car(isccc_sexpr_t *list);
